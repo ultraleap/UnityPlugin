0b9469777cfa7682aa7500b4b94fde2aebfd50ea
protodeep <fox@brainstormer-project.net>
2016-02-26 15:43:25 -0800

    Passing chirality vs isLeft to TestHandFactory.MakeTestHand

--- Assets/LeapMotion/Scripts/Hands/IHandModel.cs
+++ Assets/LeapMotion/Scripts/Hands/IHandModel.cs
@@ -19,25 +19,18 @@ public abstract class IHandModel : MonoBehaviour {
   public abstract void UpdateHand();
   public abstract Hand GetLeapHand(); 
   public abstract void SetLeapHand(Hand hand);
-  private bool isLeft;
 #if UNITY_EDITOR
   void Awake() {
     if (!EditorApplication.isPlaying) {
       //Debug.Log("IHandModel.Awake()");
-      if (Handedness == Chirality.Left) {
-        isLeft = true;
-      }
-      SetLeapHand(TestHandFactory.MakeTestHand(0, 0, isLeft).TransformedCopy(UnityMatrixExtension.GetLeapMatrix(transform)));
+      SetLeapHand(TestHandFactory.MakeTestHand(0, 0, Handedness == Chirality.Left).TransformedCopy(UnityMatrixExtension.GetLeapMatrix(transform)));
       InitHand();
     }
   }
   void Update() {
     if (!EditorApplication.isPlaying) {
-      if (Handedness == Chirality.Left) {
-        isLeft = true;
-      }
       //Debug.Log("IHandModel.Update()");
-      SetLeapHand(TestHandFactory.MakeTestHand(0, 0, isLeft).TransformedCopy(UnityMatrixExtension.GetLeapMatrix(transform)));
+      SetLeapHand(TestHandFactory.MakeTestHand(0, 0, Handedness == Chirality.Left).TransformedCopy(UnityMatrixExtension.GetLeapMatrix(transform)));
       UpdateHand();
     }
   }
