78c560b85e81dd36b17d37c99f53b1bb37e88062
Amarcolina <Amarcoli@ucsc.edu>
2016-03-01 19:42:40 -0800

    Added comment

--- Assets/LeapC/PtrToStructUtil.cs
+++ Assets/LeapC/PtrToStructUtil.cs
@@ -4,6 +4,9 @@ using System.Runtime.InteropServices;
 
 namespace LeapInternal {
 
+  /**
+   * A helper class to marshal from unmanaged memory into structs without creating garbage.
+   */
   public class StructUtil<T> where T : struct {
     [StructLayout(LayoutKind.Sequential)]
     private class StructContainer {
@@ -13,6 +16,10 @@ namespace LeapInternal {
     private static StructContainer _container = new StructContainer();
     private static int _sizeofT = Marshal.SizeOf(typeof(T));
 
+    /**
+     * Converts an IntPtr to a struct of type T.  Does not allocate any
+     * garbage unlike Marshal.PtrToStruct(IntPtr, Type).
+     */
     public static T PtrToStruct(IntPtr ptr) {
       try {
         Marshal.PtrToStructure(ptr, _container);
@@ -23,9 +30,13 @@ namespace LeapInternal {
       }
     }
 
+    /**
+     * Converts a single element in an array pointed to by ptr to a struct
+     * of type T.  This method does not and cannot do any bounds checking!
+     * This method does not create any garbage.
+     */
     public static T ArrayElementToStruct(IntPtr ptr, int arrayIndex) {
       return PtrToStruct(new IntPtr(ptr.ToInt64() + _sizeofT * arrayIndex));
     }
-
   }
 }
