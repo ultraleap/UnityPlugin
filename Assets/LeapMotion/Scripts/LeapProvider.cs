c7333aaa87a9789e25960684bd7b0e29af49d82b
Amarcolina <Amarcoli@ucsc.edu>
2016-03-07 15:14:47 -0800

    Added abstract LeapProvider class

---
+++ Assets/LeapMotion/Scripts/LeapProvider.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Leap {
+  public abstract class LeapProvider : MonoBehaviour {
+    public abstract Frame CurrentFrame { get; }
+    public abstract Frame CurrentFixedFrame { get; }
+    public abstract Image CurrentImage { get; }
+  }
+}
