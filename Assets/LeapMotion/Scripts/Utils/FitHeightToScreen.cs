dec9194deef921f97d778e670a9122291ad4fb63
Joe Ward <jward@leapmotion.com>
2016-03-07 20:29:48 -0800

    Updated copyright dates

--- Assets/LeapMotion/Scripts/Utils/FitHeightToScreen.cs
+++ Assets/LeapMotion/Scripts/Utils/FitHeightToScreen.cs
@@ -1,5 +1,5 @@
 ﻿/******************************************************************************\
-* Copyright (C) Leap Motion, Inc. 2011-2014.                                   *
+* Copyright (C) Leap Motion, Inc. 2011-2016.                                   *
 * Leap Motion proprietary. Licensed under Apache 2.0                           *
 * Available at http://www.apache.org/licenses/LICENSE-2.0.html                 *
 \******************************************************************************/
