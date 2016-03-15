8a577216d0969bc05a0b5d314f99a66fe6c9d84e
Veronica Zheng <vzheng@leapmotion.com>
2016-03-15 13:44:35 -0700

    fix unity hang when service is not running

--- Assets/LeapC/Connection.cs
+++ Assets/LeapC/Connection.cs
@@ -427,7 +427,7 @@ namespace LeapInternal
     {
       //TODO update connection on CONNECTION_LOST_EVENT
       this.LeapConnectionLost.Dispatch<ConnectionLostEventArgs>(this, new ConnectionLostEventArgs()); //TODO Meaningful ConnectionLost event args
-      this.Stop();
+      this.Cleanup();
     }
 
     private void handleDevice(ref LEAP_DEVICE_EVENT deviceMsg)
