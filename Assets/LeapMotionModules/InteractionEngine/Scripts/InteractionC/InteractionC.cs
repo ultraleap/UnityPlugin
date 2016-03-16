ff46ea0fd49f64d55aa5b37a0c8a86a7e3106ccd
Amarcolina <Amarcoli@ucsc.edu>
2016-03-16 15:57:42 -0700

    Removed extra unneded constructors

--- Assets/LeapMotionModules/InteractionEngine/Scripts/InteractionC/InteractionC.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/InteractionC/InteractionC.cs
@@ -106,22 +106,12 @@ namespace InteractionEngine.CApi {
   public struct LEAP_IE_SPHERE_DESCRIPTION {
     public LEAP_IE_SHAPE_DESCRIPTION shape;
     public float radius;
-
-    public LEAP_IE_SPHERE_DESCRIPTION(SphereCollider sphereCollider) {
-      shape.type = eLeapIEShapeType.eLeapIEShape_Sphere;
-      radius = sphereCollider.transform.lossyScale.x * sphereCollider.radius;
-    }
   }
 
   [StructLayout(LayoutKind.Sequential, Pack = 1)]
   public struct LEAP_IE_OBB_DESCRIPTION {
     public LEAP_IE_SHAPE_DESCRIPTION shape;
     public LEAP_VECTOR extents;
-
-    public LEAP_IE_OBB_DESCRIPTION(BoxCollider boxCollider) {
-      shape.type = eLeapIEShapeType.eLeapIEShape_OBB;
-      extents = new LEAP_VECTOR(boxCollider.transform.TransformVector(boxCollider.size * 0.5f));
-    }
   }
 
   [StructLayout(LayoutKind.Sequential, Pack = 1)]
@@ -130,84 +120,6 @@ namespace InteractionEngine.CApi {
     public UInt32 nVerticies;
     public IntPtr pVertices; //LEAP_VECTOR*
     public float radius;
-
-    public LEAP_IE_CONVEX_POLYHEDRON_DESCRIPTION(SphereCollider sphereCollider) {
-      shape.type = eLeapIEShapeType.eLeapIEShape_Convex;
-      nVerticies = 1;
-
-      pVertices = StructAllocator.AllocateArray<LEAP_VECTOR>(1);
-      Vector3 scaledCenter = sphereCollider.transform.TransformVector(sphereCollider.center);
-      StructMarshal<LEAP_VECTOR>.CopyIntoArray(pVertices, new LEAP_VECTOR(scaledCenter), 0);
-
-      radius = sphereCollider.transform.lossyScale.x * sphereCollider.radius;
-    }
-
-    public LEAP_IE_CONVEX_POLYHEDRON_DESCRIPTION(BoxCollider boxCollider) {
-      shape.type = eLeapIEShapeType.eLeapIEShape_Convex;
-
-      pVertices = StructAllocator.AllocateArray<LEAP_VECTOR>(8);
-
-      nVerticies = 0;
-      Vector3 scaledCenter = boxCollider.transform.TransformVector(boxCollider.center);
-      for (int dx = -1; dx <= 1; dx += 2) {
-        for (int dy = -1; dy <= 1; dy += 2) {
-          for (int dz = 1; dz <= 1; dz += 2) {
-            Vector3 corner = Vector3.Scale(boxCollider.size, new Vector3(dx, dy, dz)) * 0.5f;
-            corner += scaledCenter;
-            StructMarshal<LEAP_VECTOR>.CopyIntoArray(pVertices, new LEAP_VECTOR(corner), (int)nVerticies++);
-          }
-        }
-      }
-
-      radius = 0.0f;
-    }
-
-    public LEAP_IE_CONVEX_POLYHEDRON_DESCRIPTION(CapsuleCollider capsuleCollider) {
-      shape.type = eLeapIEShapeType.eLeapIEShape_Convex;
-
-      nVerticies = 2;
-
-      pVertices = StructAllocator.AllocateArray<LEAP_VECTOR>(2);
-      Vector3 center = capsuleCollider.transform.TransformVector(capsuleCollider.center);
-      float scaledHeight = capsuleCollider.transform.lossyScale.x * capsuleCollider.height;
-      Vector3 axis;
-      switch (capsuleCollider.direction) {
-        case 0:
-          axis = Vector3.right;
-          break;
-        case 1:
-          axis = Vector3.up;
-          break;
-        case 2:
-          axis = Vector3.forward;
-          break;
-        default:
-          throw new InvalidOperationException("Unexpected capsule direction " + capsuleCollider.direction);
-      }
-
-      Vector3 p0 = center + axis * scaledHeight * 0.5f;
-      Vector3 p1 = center - axis * scaledHeight * 0.5f;
-
-      StructMarshal<LEAP_VECTOR>.CopyIntoArray(pVertices, new LEAP_VECTOR(p0), 0);
-      StructMarshal<LEAP_VECTOR>.CopyIntoArray(pVertices, new LEAP_VECTOR(p1), 1);
-
-      radius = capsuleCollider.transform.lossyScale.x * capsuleCollider.radius;
-    }
-
-    public LEAP_IE_CONVEX_POLYHEDRON_DESCRIPTION(MeshCollider meshCollider) {
-      shape.type = eLeapIEShapeType.eLeapIEShape_Convex;
-      Mesh convexMesh = meshCollider.sharedMesh;
-      Vector3[] verts = convexMesh.vertices;
-
-      nVerticies = (UInt32)convexMesh.vertexCount;
-      pVertices = StructAllocator.AllocateArray<LEAP_VECTOR>(verts.Length);
-      for (int i = 0; i < verts.Length; i++) {
-        LEAP_VECTOR v = new LEAP_VECTOR(meshCollider.transform.TransformVector(verts[i]));
-        StructMarshal<LEAP_VECTOR>.CopyIntoArray(pVertices, v, i);
-      }
-
-      radius = 0.0f;
-    }
   }
 
   [StructLayout(LayoutKind.Sequential, Pack = 1)]
@@ -216,35 +128,6 @@ namespace InteractionEngine.CApi {
     public UInt32 nShapes;
     public IntPtr pShapes; //LEAP_IE_SHAPE_DESCRIPTION**
     public IntPtr pTransforms; //LEAP_IE_TRANSFORM* 
-
-    public LEAP_IE_COMPOUND_DESCRIPTION(GameObject obj) {
-      shape.type = eLeapIEShapeType.eLeapIEShape_Compound;
-
-      Collider[] colliders = obj.GetComponentsInChildren<Collider>();
-      nShapes = (uint)colliders.Length;
-
-      pShapes = StructAllocator.AllocateArray<IntPtr>(colliders.Length);
-      pTransforms = StructAllocator.AllocateArray<LEAP_IE_TRANSFORM>(colliders.Length);
-      for (int i = 0; i < colliders.Length; i++) {
-        LEAP_IE_CONVEX_POLYHEDRON_DESCRIPTION desc;
-
-        Collider collider = colliders[i];
-        if (collider is SphereCollider) {
-          desc = new LEAP_IE_CONVEX_POLYHEDRON_DESCRIPTION(collider as SphereCollider);
-        } else if (collider is BoxCollider) {
-          desc = new LEAP_IE_CONVEX_POLYHEDRON_DESCRIPTION(collider as BoxCollider);
-        } else if (collider is CapsuleCollider) {
-          desc = new LEAP_IE_CONVEX_POLYHEDRON_DESCRIPTION(collider as CapsuleCollider);
-        } else if (collider is MeshCollider) {
-          desc = new LEAP_IE_CONVEX_POLYHEDRON_DESCRIPTION(collider as MeshCollider);
-        } else {
-          throw new InvalidOperationException("Unsupported collider type " + collider.GetType());
-        }
-
-        IntPtr descPtr = StructAllocator.AllocateStruct(desc);
-        StructMarshal<IntPtr>.CopyIntoArray(pShapes, descPtr, i);
-      }
-    }
   }
 
   [StructLayout(LayoutKind.Sequential, Pack = 1)]
