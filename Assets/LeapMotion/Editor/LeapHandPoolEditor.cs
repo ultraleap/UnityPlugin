195acae52ff3860ccb06f29dfb169e2aa9749d76
protodeep <fox@brainstormer-project.net>
2016-03-25 11:00:05 -0700

    Improving ModelCollection display with ModelPair editor script

--- Assets/LeapMotion/Editor/LeapHandPoolEditor.cs
+++ Assets/LeapMotion/Editor/LeapHandPoolEditor.cs
@@ -7,7 +7,7 @@ namespace Leap.Unity {
   public class LeapHandPoolEditor : Editor {
     public override void OnInspectorGUI() {
       serializedObject.Update();
-      LeapEditorList.Show(serializedObject.FindProperty("ModelCollection"));
+      LeapModelPairList.Show(serializedObject.FindProperty("ModelCollection"));
       EditorGUILayout.PropertyField(serializedObject.FindProperty("EnforceHandedness"));
       if (Application.isPlaying) {
         EditorGUILayout.PropertyField(serializedObject.FindProperty("ModelPool"), true);
---
+++ Assets/LeapMotion/Editor/LeapModelPairList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public static class LeapModelPairList {
+	
+	public static void Show (SerializedProperty list) {
+		EditorGUILayout.PropertyField(list);
+		EditorGUI.indentLevel += 1;
+		if (list.isExpanded) {
+			EditorGUILayout.PropertyField(list.FindPropertyRelative("Array.size"));
+			for (int i = 0; i < list.arraySize; i++) {
+				EditorGUI.indentLevel += 2;
+				EditorGUILayout.PropertyField(list.GetArrayElementAtIndex(i), new GUIContent("Model Pair: " + i),true);
+				EditorGUI.indentLevel -= 2;
+				EditorGUILayout.Space();
+			}
+		}
+		EditorGUI.indentLevel -= 1;
+
+	}
+}
