bc13626681e14ae13c36fc0c224a2259b488f0e0
Amarcolina <Amarcoli@ucsc.edu>
2016-03-29 16:04:25 -0700

    Adding kabsch implementation

--- Assets/LeapMotionModules/InteractionEngine/Scripts/InteractionObjects/KabschInteractionBehaviour.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/InteractionObjects/KabschInteractionBehaviour.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using LeapInternal;
 using Leap.Unity.Interaction.CApi;
 
 namespace Leap.Unity.Interaction {
 
   public class KabschInteractionBehaviour : InteractionBehaviour {
+    public const int NUM_FINGERS = 5;
+    public const int NUM_JOINTS = 4;
+
+    protected Dictionary<int, HandPointCollection> _handIdToPoints;
 
     protected Rigidbody _rigidbody;
     protected LEAP_IE_KABSCH _kabsch;
@@ -21,14 +26,67 @@ namespace Leap.Unity.Interaction {
 
     public override void OnHandGrasp(Hand hand) {
       base.OnHandGrasp(hand);
+
+      var newCollection = HandPointCollection.Create(_rigidbody);
+      _handIdToPoints[hand.Id] = newCollection;
+      newCollection.UpdateTransform();
+
+      for (int f = 0; f < NUM_FINGERS; f++) {
+        Finger finger = hand.Fingers[f];
+        Finger.FingerType fingerType = finger.Type;
+
+        for (int j = 0; j < NUM_JOINTS; j++) {
+          Bone.BoneType boneType = (Bone.BoneType)j;
+          Bone bone = finger.Bone(boneType);
+
+          Vector3 jointPos = bone.NextJoint.ToVector3();
+          Vector3 closestOnSurface = jointPos; //TODO: how to get closest on surface?
+
+          newCollection.SetGlobalPosition(closestOnSurface, fingerType, boneType);
+        }
+      }
     }
 
     public override void OnHandsHold(List<Hand> hands) {
       base.OnHandsHold(hands);
+
+      KabschC.Reset(ref _kabsch);
+      for (int h = 0; h < hands.Count; h++) {
+        Hand hand = hands[h];
+
+        var collection = _handIdToPoints[hand.Id];
+        collection.UpdateTransform();
+
+        for (int f = 0; f < NUM_FINGERS; f++) {
+          Finger finger = hand.Fingers[f];
+          Finger.FingerType fingerType = finger.Type;
+
+          for (int j = 0; j < NUM_JOINTS; j++) {
+            Bone.BoneType boneType = (Bone.BoneType)j;
+            Bone bone = finger.Bone(boneType);
+
+            Vector3 jointPos = bone.NextJoint.ToVector3();
+            Vector3 coresponding = collection.GetGlobalPosition(fingerType, boneType);
+
+            LEAP_VECTOR point1 = new LEAP_VECTOR(jointPos);
+            LEAP_VECTOR point2 = new LEAP_VECTOR(coresponding);
+
+            KabschC.AddPoint(ref _kabsch, ref point1, ref point2, 1.0f);
+          }
+        }
+      }
+
+      KabschC.Solve(ref _kabsch);
+
+
     }
 
     public override void OnHandRelease(Hand hand) {
       base.OnHandRelease(hand);
+
+      var collection = _handIdToPoints[hand.Id];
+      _handIdToPoints.Remove(hand.Id);
+      HandPointCollection.Return(collection);
     }
 
     public override void OnHandLostTracking(Hand oldHand) {
@@ -55,6 +113,7 @@ namespace Leap.Unity.Interaction {
 
     #region UNITY CALLBACKS
     protected virtual void Awake() {
+      _handIdToPoints = new Dictionary<int, HandPointCollection>();
       KabschC.Construct(ref _kabsch);
     }
 
@@ -75,7 +134,61 @@ namespace Leap.Unity.Interaction {
     }
     #endregion
 
+    #region CLASSES
+    protected class HandPointCollection {
+      private static Stack<HandPointCollection> _handPointCollectionPool = new Stack<HandPointCollection>();
 
+      protected Rigidbody _rigidbody;
+      protected Vector3[] _localPositions;
+
+      private Matrix4x4 _transformMatrix;
+      private Matrix4x4 _inverseTransformMatrix;
+
+      public static HandPointCollection Create(Rigidbody rigidbody) {
+        HandPointCollection collection;
+        if (_handPointCollectionPool.Count != 0) {
+          collection = _handPointCollectionPool.Pop();
+        } else {
+          collection = new HandPointCollection();
+        }
+
+        collection.init(rigidbody);
+        return collection;
+      }
+
+      public static void Return(HandPointCollection handPointCollection) {
+        handPointCollection.reset();
+        _handPointCollectionPool.Push(handPointCollection);
+      }
+
+      private HandPointCollection() { }
+
+      private void init(Rigidbody rigidbody) {
+        _localPositions = new Vector3[NUM_FINGERS * NUM_JOINTS];
+      }
+
+      private void reset() {
+        _rigidbody = null;
+      }
+
+      public void UpdateTransform() {
+        _transformMatrix = Matrix4x4.TRS(_rigidbody.position, _rigidbody.rotation, Vector3.one);
+        _inverseTransformMatrix = Matrix4x4.Inverse(_transformMatrix);
+      }
+
+      public void SetGlobalPosition(Vector3 globalPosition, Finger.FingerType fingerType, Bone.BoneType boneType) {
+        _localPositions[getIndex(fingerType, boneType)] = _inverseTransformMatrix.MultiplyPoint3x4(globalPosition);
+      }
+
+      public Vector3 GetGlobalPosition(Finger.FingerType fingerType, Bone.BoneType boneType) {
+        return _transformMatrix.MultiplyPoint3x4(_localPositions[getIndex(fingerType, boneType)]);
+      }
+
+      private int getIndex(Finger.FingerType fingerType, Bone.BoneType boneType) {
+        return (int)fingerType * 5 + (int)boneType;
+      }
+    }
+    #endregion
 
 
   }
