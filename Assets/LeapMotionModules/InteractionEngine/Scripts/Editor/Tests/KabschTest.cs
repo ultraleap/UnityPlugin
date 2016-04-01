891a7c0cbb718f7122aaceb12de80c0aaea9d5da
Amarcolina <Amarcoli@ucsc.edu>
2016-04-01 12:46:58 -0700

    Added kabsch test base

---
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/Editor/Tests/KabschTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+namespace Leap.Unity.Interaction.Test {
+
+  public class KabschTest {
+
+    [SetUp]
+    public void Setup() {
+
+    }
+
+    [TearDown]
+    public void Teardown() {
+
+    }
+
+
+
+  }
+}
