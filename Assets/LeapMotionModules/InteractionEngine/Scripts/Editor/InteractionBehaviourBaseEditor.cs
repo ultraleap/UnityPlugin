7c12665f3dfe6699c4294ad2fbd49a2af00a6692
Amarcolina <Amarcoli@ucsc.edu>
2016-04-08 15:38:35 -0700

    Split editor into two.  Added parent/sibling/child check

---
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/Editor/InteractionBehaviourBaseEditor.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Leap.Unity.Interaction {
+
+  [CustomEditor(typeof(InteractionBehaviourBase), true)]
+  public class InteractionBehaviourBaseEditor : CustomEditorBase {
+    protected InteractionBehaviour _interactionBehaviour;
+
+    protected override void OnEnable() {
+      base.OnEnable();
+
+      _interactionBehaviour = target as InteractionBehaviour;
+
+      specifyCustomDecorator("_manager", kinematicDecorator);
+    }
+
+    private void kinematicDecorator(SerializedProperty prop) {
+      Rigidbody rigidbody = _interactionBehaviour.GetComponent<Rigidbody>();
+
+      if (_interactionBehaviour.GetComponentsInParent<InteractionBehaviourBase>().Length > 1 ||
+          _interactionBehaviour.GetComponentsInChildren<InteractionBehaviourBase>().Length > 1) {
+        EditorGUILayout.HelpBox("Interaction Behaviour cannot be a child sibling or parent of another Interaction Behaviour.", MessageType.Error);
+      }
+
+      if (rigidbody == null) {
+        EditorGUILayout.HelpBox("This component requires a Rigidbody", MessageType.Error);
+      } else {
+        if (rigidbody.interpolation == RigidbodyInterpolation.None) {
+          EditorGUILayout.HelpBox("It is recommended to use interpolation on Rigidbodies to improve interaction fidelity.", MessageType.Warning);
+        }
+
+        if (rigidbody.isKinematic) {
+          if (rigidbody.useGravity) {
+            EditorGUILayout.HelpBox("Rigidbody is set as Kinematic but has gravity enabled.", MessageType.Warning);
+          } else {
+            EditorGUILayout.HelpBox("Will be simulated as Kinematic.", MessageType.Info);
+          }
+        } else {
+          if (rigidbody.useGravity) {
+            EditorGUILayout.HelpBox("Will be simulated with gravity.", MessageType.Info);
+          } else {
+            EditorGUILayout.HelpBox("Will be simulated without gravity.", MessageType.Info);
+          }
+        }
+      }
+    }
+
+    
+
+    public override void OnInspectorGUI() {
+      base.OnInspectorGUI();
+
+      if (Application.isPlaying) {
+        EditorGUILayout.Space();
+        InteractionBehaviourBase behaviour = target as InteractionBehaviourBase;
+
+        if (!behaviour.IsInteractionEnabled) {
+          EditorGUILayout.LabelField("Interaction Disabled", EditorStyles.boldLabel);
+        } else {
+          EditorGUILayout.LabelField("Interaction Info", EditorStyles.boldLabel);
+          using (new EditorGUI.DisabledGroupScope(true)) {
+            EditorGUILayout.IntField("Grasping Hand Count", behaviour.GraspingHandCount);
+            EditorGUILayout.IntField("Untracked Hand Count", behaviour.UntrackedHandCount);
+          }
+        }
+      }
+    }
+  }
+}
--- Assets/LeapMotionModules/InteractionEngine/Scripts/Editor/InteractionBehaviourEditor.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/Editor/InteractionBehaviourEditor.cs
@@ -4,43 +4,13 @@ using UnityEditor;
 namespace Leap.Unity.Interaction {
 
   [CustomEditor(typeof(InteractionBehaviour), true)]
-  public class InteractionBehaviourEditor : CustomEditorBase {
-    private InteractionBehaviour _interactionBehaviour;
+  public class InteractionBehaviourEditor : InteractionBehaviourBaseEditor {
 
     protected override void OnEnable() {
       base.OnEnable();
 
-      _interactionBehaviour = target as InteractionBehaviour;
-
-      specifyCustomDecorator("_manager", kinematicDecorator);
       specifyCustomDecorator("_graphicalAnchor", graphicalAnchor);
       specifyCustomDrawer("_pushingEnabled", pushingDrawer);
-    }
-
-    private void kinematicDecorator(SerializedProperty prop) {
-      Rigidbody rigidbody = _interactionBehaviour.GetComponent<Rigidbody>();
-
-      if (rigidbody == null) {
-        EditorGUILayout.HelpBox("This component requires a Rigidbody", MessageType.Error);
-      } else {
-        if (rigidbody.interpolation == RigidbodyInterpolation.None) {
-          EditorGUILayout.HelpBox("It is recommended to use interpolation on Rigidbodies to improve interaction fidelity.", MessageType.Warning);
-        }
-
-        if (rigidbody.isKinematic) {
-          if (rigidbody.useGravity) {
-            EditorGUILayout.HelpBox("Rigidbody is set as Kinematic but has gravity enabled.", MessageType.Warning);
-          } else {
-            EditorGUILayout.HelpBox("Will be simulated as Kinematic.", MessageType.Info);
-          }
-        } else {
-          if (rigidbody.useGravity) {
-            EditorGUILayout.HelpBox("Will be simulated with gravity.", MessageType.Info);
-          } else {
-            EditorGUILayout.HelpBox("Will be simulated without gravity.", MessageType.Info);
-          }
-        }
-      }
     }
 
     private void graphicalAnchor(SerializedProperty prop) {
@@ -54,25 +24,6 @@ namespace Leap.Unity.Interaction {
 
       if (!rigidbody.isKinematic) {
         EditorGUILayout.PropertyField(prop);
-      }
-    }
-
-    public override void OnInspectorGUI() {
-      base.OnInspectorGUI();
-
-      if (Application.isPlaying) {
-        EditorGUILayout.Space();
-        InteractionBehaviourBase behaviour = target as InteractionBehaviourBase;
-
-        if (!behaviour.IsInteractionEnabled) {
-          EditorGUILayout.LabelField("Interaction Disabled", EditorStyles.boldLabel);
-        } else {
-          EditorGUILayout.LabelField("Interaction Info", EditorStyles.boldLabel);
-          using (new EditorGUI.DisabledGroupScope(true)) {
-            EditorGUILayout.IntField("Grasping Hand Count", behaviour.GraspingHandCount);
-            EditorGUILayout.IntField("Untracked Hand Count", behaviour.UntrackedHandCount);
-          }
-        }
       }
     }
   }
