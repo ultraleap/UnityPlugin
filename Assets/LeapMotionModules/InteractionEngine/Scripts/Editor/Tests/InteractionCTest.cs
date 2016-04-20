91df3cded6fdccf54223215dbaaabeb3c18f2d62
Amarcolina <Amarcoli@ucsc.edu>
2016-04-20 15:45:45 -0700

    Adding some more checks to the test

--- Assets/LeapMotionModules/InteractionEngine/Scripts/Editor/Tests/InteractionCTest.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/Editor/Tests/InteractionCTest.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.IO;
+using System.Collections.Generic;
 using Leap.Unity.Interaction.CApi;
 
 namespace Leap.Unity.Interaction.Tests {
@@ -69,11 +70,27 @@ namespace Leap.Unity.Interaction.Tests {
       InteractionC.UpdateController(ref _scene, ref transform);
     }
 
+    [Test]
+    public void GetDebugLines() {
+      InteractionC.EnableDebugFlags(ref _scene, (uint)DebugFlags.Lines);
 
+      uint lineCount;
+      IntPtr lineBuffer;
+      InteractionC.GetDebugLines(ref _scene, out lineCount, out lineBuffer);
+    }
+
+    [Test]
+    public void GetDebugStrings() {
+      InteractionC.EnableDebugFlags(ref _scene, (uint)DebugFlags.Strings);
+
+      List<string> strings = new List<string>();
+      InteractionC.GetDebugStrings(ref _scene, strings);
+    }
   }
 
   public class InteractionCShapeTests : InteractionCTestBase {
     INTERACTION_SHAPE_DESCRIPTION_HANDLE _shapeDescriptionHandle;
+    INTERACTION_SHAPE_INSTANCE_HANDLE _shapeInstanceHandle;
 
     public override void Setup() {
       base.Setup();
@@ -85,16 +102,44 @@ namespace Leap.Unity.Interaction.Tests {
       IntPtr spherePtr = StructAllocator.AllocateStruct(ref sphere);
       
       InteractionC.AddShapeDescription(ref _scene, spherePtr, out _shapeDescriptionHandle);
+
+      INTERACTION_CREATE_SHAPE_INFO info = new INTERACTION_CREATE_SHAPE_INFO();
+      info.gravity = Vector3.zero.ToCVector();
+      info.shapeFlags = ShapeInfoFlags.GravityEnabled | ShapeInfoFlags.HasRigidBody;
+
+      INTERACTION_TRANSFORM transform = new INTERACTION_TRANSFORM();
+      transform.position = Vector3.zero.ToCVector();
+      transform.rotation = Quaternion.identity.ToCQuaternion();
+      transform.wallTime = 0;
+
+      InteractionC.CreateShapeInstance(ref _scene, ref _shapeDescriptionHandle, ref transform, ref info, out _shapeInstanceHandle);
     }
 
     public override void Teardown() {
+      InteractionC.DestroyShapeInstance(ref _scene, ref _shapeInstanceHandle);
       InteractionC.RemoveShapeDescription(ref _scene, ref _shapeDescriptionHandle);
 
       base.Teardown();
     }
 
     [Test]
-    public void AddRemoveShapeDescriptionTest() { }
+    public void CreateDestroyInstance() { }
+
+    [Test]
+    public void UpdateInstance() {
+      INTERACTION_TRANSFORM transform = new INTERACTION_TRANSFORM();
+      transform.position = Vector3.one.ToCVector();
+      transform.rotation = Quaternion.identity.ToCQuaternion();
+      transform.wallTime = 0;
+
+      INTERACTION_UPDATE_SHAPE_INFO info = new INTERACTION_UPDATE_SHAPE_INFO();
+      info.angularAcceleration = Vector3.zero.ToCVector();
+      info.angularVelocity = Vector3.zero.ToCVector();
+      info.linearAcceleration = Vector3.zero.ToCVector();
+      info.linearVelocity = Vector3.zero.ToCVector();
+
+      InteractionC.UpdateShapeInstance(ref _scene, ref transform, ref info, ref _shapeInstanceHandle);
+    }
   }
 
 
