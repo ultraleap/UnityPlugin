6dc222581aa85e8cb8ba2484106addce9c70689a
Amarcolina <Amarcoli@ucsc.edu>
2016-04-21 13:56:18 -0700

    Added helper button to manager to assign objs

--- Assets/LeapMotionModules/InteractionEngine/Scripts/Editor/InteractionManagerEditor.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/Editor/InteractionManagerEditor.cs
@@ -5,7 +5,41 @@ using UnityEditor;
 namespace Leap.Unity.Interaction {
 
   [CustomEditor(typeof(InteractionManager), true)]
-  public class InteractionManagerEditor : Editor {
+  public class InteractionManagerEditor : CustomEditorBase {
+
+    private IInteractionBehaviour[] _interactionBehaviours;
+    private bool _anyBehavioursUnregistered;
+
+    protected override void OnEnable() {
+      base.OnEnable();
+
+      specifyCustomDecorator("_leapProvider", providerDectorator);
+
+      _interactionBehaviours = FindObjectsOfType<IInteractionBehaviour>();
+      for (int i = 0; i < _interactionBehaviours.Length; i++) {
+        if (_interactionBehaviours[i].Manager == null) {
+          _anyBehavioursUnregistered = true;
+          break;
+        }
+      }
+    }
+
+    private void providerDectorator(SerializedProperty prop) {
+      if (_anyBehavioursUnregistered) {
+        EditorGUILayout.HelpBox("Some Interaction Behaviours do not have their manager assigned!  Do you want to assign them to this manager?", MessageType.Warning);
+        if (GUILayout.Button("Assign To This Manager")) {
+          for (int i = 0; i < _interactionBehaviours.Length; i++) {
+            var behaviour = _interactionBehaviours[i];
+            if (behaviour.Manager == null) {
+              behaviour.Manager = target as InteractionManager;
+              EditorUtility.SetDirty(behaviour);
+            }
+          }
+          _anyBehavioursUnregistered = false;
+        }
+        GUILayout.Space(EditorGUIUtility.singleLineHeight);
+      }
+    }
 
     public override void OnInspectorGUI() {
       base.OnInspectorGUI();
