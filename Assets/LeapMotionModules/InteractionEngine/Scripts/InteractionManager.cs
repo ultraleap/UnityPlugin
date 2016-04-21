12812829d3dfd1b62c4f1464285ce819e7c8e287
Amarcolina <Amarcoli@ucsc.edu>
2016-04-21 12:57:03 -0700

    Finished grasping and contact toggles

--- Assets/LeapMotionModules/InteractionEngine/Scripts/InteractionManager.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/InteractionManager.cs
@@ -42,15 +42,13 @@ namespace Leap.Unity.Interaction {
     [SerializeField]
     protected float _untrackedTimeout = 0.5f;
 
-    // TODO: eLeapIESceneInfoFlags_PhysicsEnabled
     [Tooltip("Allow the Interaction Engine to modify object velocities when pushing.")]
     [SerializeField]
     protected bool _enableContact = true;
 
-    //TODO: eLeapIESceneInfoFlags_GraspEnabled
-    //[Tooltip("Allow the Interaction plugin to modify object positions by grasping.")]
-    //[SerializeField]
-    //protected bool _enableGrasping = true;
+    [Tooltip("Allow the Interaction plugin to modify object positions by grasping.")]
+    [SerializeField]
+    protected bool _enableGrasping = true;
 
     [Header("Debug")]
     [Tooltip("Allows simulation to be disabled without destroying the scene in any way.")]
@@ -148,6 +146,32 @@ namespace Leap.Unity.Interaction {
     public IEnumerable<IInteractionBehaviour> GraspedObjects {
       get {
         return _graspedBehaviours;
+      }
+    }
+
+    /// <summary>
+    /// Sets or Gets whether or not the Interaction Engine can modify object velocities when pushing.
+    /// </summary>
+    public bool EnableContact {
+      get {
+        return _enableContact;
+      }
+      set {
+        _enableContact = value;
+        UpdateSceneInfo();
+      }
+    }
+
+    /// <summary>
+    /// Sets or Gets whether or not the Interaction plugin to modify object positions by grasping.
+    /// </summary>
+    public bool EnableGrasping {
+      get {
+        return _enableGrasping;
+      }
+      set {
+        _enableGrasping = value;
+        UpdateSceneInfo();
       }
     }
 
@@ -257,6 +281,8 @@ namespace Leap.Unity.Interaction {
       if (Application.isPlaying && _hasSceneBeenCreated) {
         //Allow the debug lines to be toggled while the scene is playing
         applyDebugSettings();
+        //Allow scene info to be updated while the scene is playing
+        UpdateSceneInfo();
       }
 
       //Timeout must be positive
@@ -682,7 +708,7 @@ namespace Leap.Unity.Interaction {
       INTERACTION_SHAPE_INSTANCE_HANDLE instanceHandle = interactionBehaviour.ShapeInstanceHandle;
 
       _instanceHandleToBehaviour.Remove(instanceHandle);
-      
+
       InteractionC.DestroyShapeInstance(ref _scene, ref instanceHandle);
 
       interactionBehaviour.NotifyInteractionShapeDestroyed();
@@ -719,7 +745,17 @@ namespace Leap.Unity.Interaction {
     private INTERACTION_SCENE_INFO getSceneInfo() {
       INTERACTION_SCENE_INFO info = new INTERACTION_SCENE_INFO();
       info.gravity = Physics.gravity.ToCVector();
-      info.sceneFlags = SceneInfoFlags.HasGravity | SceneInfoFlags.PhysicsEnabled | SceneInfoFlags.GraspEnabled;
+
+      info.sceneFlags = SceneInfoFlags.HasGravity;
+
+      if (_enableContact) {
+        info.sceneFlags |= SceneInfoFlags.PhysicsEnabled;
+      }
+
+      if (_enableGrasping) {
+        info.sceneFlags |= SceneInfoFlags.GraspEnabled;
+      }
+
       return info;
     }
 
