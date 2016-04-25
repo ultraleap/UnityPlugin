c25ae9839be8d98147e90f19e51744d64292702c
Joe Ward <jward@leapmotion.com>
2016-04-25 13:09:20 -0700

    wip

--- Assets/LeapMotionModules/Attachments/Scripts/CameraFacer.cs
+++ Assets/LeapMotionModules/Attachments/Scripts/CameraFacer.cs
@@ -3,17 +3,22 @@ using System.Collections;
 
 public class CameraFacer : MonoBehaviour {
 
-  public bool ConstrainX = false;
-  public bool ConstrainY = false;
-  public bool ConstrainZ = false;
+  public Vector3 objectForward = Vector3.forward;
+  public Transform RotationParent;
+  public float MaxTilt = 20;
 
-	// Update is called once per frame
+  private Quaternion offset;
+
+  void Awake(){
+    offset = transform.localRotation;
+  }
+
 	void Update () {
-    Quaternion lookRotation = Quaternion.LookRotation(Camera.main.transform.position, Camera.main.transform.up);
-    float eulerX = ConstrainX ? transform.rotation.eulerAngles.x : lookRotation.eulerAngles.x;
-    float eulerY = ConstrainY ? transform.rotation.eulerAngles.y : lookRotation.eulerAngles.y;
-    float eulerZ = ConstrainZ ? transform.rotation.eulerAngles.z : lookRotation.eulerAngles.z;
-    //transform.rotation = Quaternion.Euler(eulerX, eulerY, eulerZ);
-    transform.localRotation = Quaternion.Euler(eulerX, eulerY, eulerZ);
+    //Quaternion lookAtCamera = Quaternion.FromToRotation(objectForward, -Camera.main.transform.forward);
+
+    float tiltAngle = Quaternion.Angle(Quaternion.identity, transform.localRotation);
+    float toCameraAngle = Quaternion.Angle(Camera.main.transform.rotation, transform.localRotation);
+    if(toCameraAngle < MaxTilt) toCameraAngle = MaxTilt;
+    transform.rotation = Quaternion.RotateTowards(transform.rotation, Camera.main.transform.rotation, MaxTilt);
 	}
 }
