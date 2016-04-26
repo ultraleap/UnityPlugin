b789f0c0ffc521ce5b11c2f3dadf5c433ac32c9b
Amarcolina <Amarcoli@ucsc.edu>
2016-04-26 16:41:28 -0700

    Adding a bunch of tests

---
+++ Assets/LeapMotion/Editor/Tests/HandFactoryTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Leap.Unity.Tests {
+
+  [TestFixture(Category = "TestHandFactory")]
+  public class HandFactoryTwoHands : FrameValidator {
+    protected override Frame createFrame() {
+      return TestHandFactory.MakeTestFrame(0, true, true);
+    }
+
+    [Test]
+    public void CorrectHandCount() {
+      Assert.That(_frame.Hands.Count, Is.EqualTo(2));
+    }
+  }
+
+  [TestFixture(Category = "TestHandFactory")]
+  public class HandFactoryLeft : FrameValidator {
+    protected override Frame createFrame() {
+      return TestHandFactory.MakeTestFrame(0, true, false);
+    }
+
+    [Test]
+    public void CorrectHandCount() {
+      Assert.That(_frame.Hands.Count, Is.EqualTo(1));
+    }
+  }
+
+  [TestFixture(Category = "TestHandFactory")]
+  public class HandFactoryRight : FrameValidator {
+    protected override Frame createFrame() {
+      return TestHandFactory.MakeTestFrame(0, false, true);
+    }
+
+    [Test]
+    public void CorrectHandCount() {
+      Assert.That(_frame.Hands.Count, Is.EqualTo(1));
+    }
+  }
+}
