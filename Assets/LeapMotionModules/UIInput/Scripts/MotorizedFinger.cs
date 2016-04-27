24ec89e8f1837897f52ce915d1a62c81f93417f6
jselstad <jselstad@leapmotion.com>
2016-04-26 20:05:51 -0700

    Motorized Finger Hands; "Puppet Hands" Initial Commit
    Rigidbody Hands with Motorized Constraints for interaction and stuff
    
    Palm is still not springed....

--- Assets/LeapMotionModules/UIInput/Scripts/MotorizedFinger.cs
+++ Assets/LeapMotionModules/UIInput/Scripts/MotorizedFinger.cs
@@ -13,82 +13,135 @@ namespace Leap.Unity
     /** A physics finger model for our rigid hand made out of various cube Unity Colliders. */
     public class MotorizedFinger : SkeletalFinger
     {
-
         public float filtering = 0.5f;
-        public bool update = true;
+
         public Rigidbody Palm;
+        private bool dirty = false;
+        private HingeJoint[] hinges;
+        public bool Debug1 = false;
 
         void Start()
         {
+
+
             for (int i = 0; i < bones.Length; ++i)
             {
                 if (bones[i] != null)
                 {
-                    bones[i].GetComponent<Rigidbody>().maxAngularVelocity = 7f;
-                    bones[i].GetComponent<Rigidbody>().maxDepenetrationVelocity = 1f;
+                    bones[i].transform.parent = null;
+                }
+            }
+        }
 
-                    //transform.parent = null;
-                    //Palm.transform.parent = null;
-                    //bones[i].parent = null;
-
+        void InitializeFingerJoints()
+        {
+            for (int i = 1; i < bones.Length-1; ++i)
+            {
+                if (bones[i] != null)
+                {
                     if (i == 1)
                     {
                         HingeJoint Hinge = Palm.gameObject.AddComponent<HingeJoint>();
-                        Hinge.enableCollision = false;
                         Hinge.enablePreprocessing = true;
                         Hinge.autoConfigureConnectedAnchor = false;
                         Hinge.connectedBody = bones[i].GetComponent<Rigidbody>();
-                        Hinge.anchor = Palm.transform.InverseTransformPoint(bones[i].TransformPoint(new Vector3(0f, 0f, -bones[i].GetComponent<CapsuleCollider>().height / 2f)));
-                        Hinge.connectedAnchor = new Vector3(0f, 0f, -bones[i].GetComponent<CapsuleCollider>().height / 2f);
+                        Hinge.anchor = Palm.transform.InverseTransformPoint(bones[i].TransformPoint(new Vector3(0f, 0f, (bones[i].GetComponent<CapsuleCollider>().radius) - (bones[i].GetComponent<CapsuleCollider>().height / 2f))));
+                        Hinge.connectedAnchor = new Vector3(0f, 0f, (bones[i].GetComponent<CapsuleCollider>().radius) - (bones[i].GetComponent<CapsuleCollider>().height / 2f));
                         Hinge.axis = Palm.transform.InverseTransformDirection(bones[i].transform.right);
+                        Hinge.enableCollision = false;
+
+                        Hinge.hideFlags = HideFlags.DontSave | HideFlags.DontSaveInEditor;
+
+
+                        //Hinge.useLimits = true;
+                        //JointLimits limit = new JointLimits();
+                        //limit.min = -80f;
+                        //limit.max = 5f;
+                        //Hinge.limits = limit;
 
                         Hinge.useMotor = true;
-                        JointMotor mmotor = new JointMotor();
-                        mmotor.force = 1000f;
-                        mmotor.targetVelocity = -100f;
-                        Hinge.motor = mmotor;
+                        //JointMotor mmotor = new JointMotor();
+                        //mmotor.force = 10f;
+                        //mmotor.targetVelocity = -100f;
+                        //Hinge.motor = mmotor;
 
-
+                        hinges[0] = Hinge;
                     }
 
                     if (i + 1 < bones.Length)
                     {
                         HingeJoint Hinge = bones[i].gameObject.AddComponent<HingeJoint>();
-                        Hinge.enableCollision = false;
                         Hinge.enablePreprocessing = true;
-                        Hinge.autoConfigureConnectedAnchor = true;
+                        Hinge.autoConfigureConnectedAnchor = false;
                         Hinge.connectedBody = bones[i + 1].gameObject.GetComponent<Rigidbody>();
-                        Hinge.anchor = bones[i].InverseTransformPoint(bones[i + 1].TransformPoint(new Vector3(0f, 0f, -bones[i + 1].GetComponent<CapsuleCollider>().height / 2f)));
-                        //Hinge.connectedAnchor = new Vector3(0f, 0f, -bones[i+1].GetComponent<CapsuleCollider>().height / 2f);
+                        Hinge.anchor = bones[i].InverseTransformPoint(bones[i + 1].TransformPoint(new Vector3(0f, 0f, (bones[i + 1].GetComponent<CapsuleCollider>().radius)-(bones[i + 1].GetComponent<CapsuleCollider>().height / 2f))));
+                        Hinge.connectedAnchor = new Vector3(0f, 0f, (bones[i + 1].GetComponent<CapsuleCollider>().radius) - (bones[i + 1].GetComponent<CapsuleCollider>().height / 2f));
                         Hinge.axis = bones[i].InverseTransformDirection(bones[i + 1].transform.right);
+                        Hinge.enableCollision = false;
+
+                        Hinge.hideFlags = HideFlags.DontSave | HideFlags.DontSaveInEditor;
+
+                        //Hinge.useLimits = true;
+                        //JointLimits limit = new JointLimits();
+                        //limit.min = -70f;
+                        //limit.max = 15f;
+                        //Hinge.limits = limit;
 
                         Hinge.useMotor = true;
-                        JointMotor mmotor = new JointMotor();
-                        mmotor.force = 1000f;
-                        mmotor.targetVelocity = -100f;
-                        Hinge.motor = mmotor;
-                    }
+                        //JointMotor mmotor = new JointMotor();
+                        //mmotor.force = 10f;
+                        //mmotor.targetVelocity = -100f;
+                        //Hinge.motor = mmotor;
 
+                        hinges[i] = Hinge;
+                    }
                 }
             }
         }
-        public void Update()
+
+        void RemoveFingerJoints()
         {
+            HingeJoint[] palmhingelist = Palm.gameObject.GetComponents<HingeJoint>();
+            foreach (HingeJoint hinge in palmhingelist)
+            {
+                Destroy(hinge);
+            }
+
             for (int i = 0; i < bones.Length; ++i)
             {
                 if (bones[i] != null)
                 {
-                    Rigidbody boneBody = bones[i].GetComponent<Rigidbody>();
-                    boneBody.velocity = Vector3.zero;
-                    boneBody.angularVelocity = Vector3.zero;
+                    HingeJoint[] hingelist = bones[i].gameObject.GetComponents<HingeJoint>();
+                    foreach (HingeJoint hinge in hingelist)
+                    {
+                        Destroy(hinge);
+                    }
+
+                    bones[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
+                    bones[i].GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                    bones[i].gameObject.SetActive(false);
                 }
             }
         }
 
+        void OnEnable(){
+            hinges = new HingeJoint[3];
+            dirty = true;
+            RemoveFingerJoints();
+        }
+
+        void OnApplicationQuit()
+        {
+            RemoveFingerJoints();
+        }
+
+        void OnDisable()
+        {
+            RemoveFingerJoints();
+        }
 
         public override void UpdateFinger()
         {
-
             for (int i = 0; i < bones.Length; ++i)
             {
                 if (bones[i] != null)
@@ -98,21 +151,27 @@ namespace Leap.Unity
                     if (capsule != null)
                     {
                         // Initialization
-                        //capsule.direction = 2;
-                        // bones[i].localScale = new Vector3(1f / transform.lossyScale.x, 1f / transform.lossyScale.y, 1f / transform.lossyScale.z);
+                        capsule.direction = 2;
+                        bones[i].localScale = new Vector3(1f / transform.lossyScale.x, 1f / transform.lossyScale.y, 1f / transform.lossyScale.z);
 
                         // Update
-                        //capsule.radius = GetBoneWidth(i) / 2f;
-                        //capsule.height = GetBoneLength(i) + GetBoneWidth(i);
+                        capsule.radius = GetBoneWidth(i) / 2f;
+                        capsule.height = GetBoneLength(i) + GetBoneWidth(i);
+                        //capsule.collider.
                     }
 
                     Rigidbody boneBody = bones[i].GetComponent<Rigidbody>();
-                    if (update)
+
+                    if (!bones[i].gameObject.activeSelf)
                     {
+                        bones[i].gameObject.SetActive(true);
                         if (boneBody)
                         {
-                            boneBody.MovePosition(GetBoneCenter(i));
-                            boneBody.MoveRotation(GetBoneRotation(i));
+                            boneBody.velocity = Vector3.zero;
+                            boneBody.angularVelocity = Vector3.zero;
+
+                            bones[i].position = GetBoneCenter(i);
+                            bones[i].rotation = GetBoneRotation(i);
                         }
                         else
                         {
@@ -120,7 +179,25 @@ namespace Leap.Unity
                             bones[i].rotation = GetBoneRotation(i);
                         }
                     }
+                    else
+                    {
+                        float offset = (Quaternion.Inverse((Quaternion.Inverse(GetBoneRotation(i)) * bones[i].rotation))).eulerAngles.x;
+                        if (offset > 180)
+                        {
+                           offset -= 360f;
+                        }
+                        JointMotor mmotor = new JointMotor();
+                        mmotor.force = 10f;
+                        mmotor.targetVelocity = offset * -100f;
+                        hinges[i-1].motor = mmotor;
+                    }
                 }
+            }
+
+            if (dirty)
+            {
+                dirty = false;
+                InitializeFingerJoints();
             }
         }
     }
---
+++ Assets/LeapMotionModules/UIInput/Scripts/PuppetHand.cs
@@ -0,0 +1,128 @@
+﻿/******************************************************************************\
+* Copyright (C) Leap Motion, Inc. 2011-2016.                                   *
+* Leap Motion proprietary. Licensed under Apache 2.0                           *
+* Available at http://www.apache.org/licenses/LICENSE-2.0.html                 *
+\******************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+using Leap;
+
+namespace Leap.Unity
+{
+    /** A physics model for our rigid hand made out of various Unity Collider. */
+    public class PuppetHand : SkeletalHand
+    {
+        public override ModelType HandModelType
+        {
+            get
+            {
+                return ModelType.Physics;
+            }
+        }
+        public float filtering = 0.5f;
+
+        public override void InitHand()
+        {
+            base.InitHand();
+
+            for (int f = 0; f < fingers.Length; ++f)
+            {
+                if (fingers[f] != null)
+                {
+                     Physics.IgnoreCollision(fingers[f].bones[1].GetComponent<Collider>(), palm.GetComponent<Collider>());
+                     Physics.IgnoreCollision(fingers[f].bones[2].GetComponent<Collider>(), palm.GetComponent<Collider>());
+
+                     if (f < fingers.Length - 1)
+                     {
+                         Physics.IgnoreCollision(fingers[f].bones[1].GetComponent<Collider>(), fingers[f + 1].bones[1].GetComponent<Collider>());
+                         Physics.IgnoreCollision(fingers[f].bones[2].GetComponent<Collider>(), fingers[f + 1].bones[2].GetComponent<Collider>());
+                         Physics.IgnoreCollision(fingers[f].bones[3].GetComponent<Collider>(), fingers[f + 1].bones[3].GetComponent<Collider>());
+                         Physics.IgnoreCollision(fingers[f].bones[2].GetComponent<Collider>(), fingers[f + 1].bones[3].GetComponent<Collider>());
+                         Physics.IgnoreCollision(fingers[f].bones[1].GetComponent<Collider>(), fingers[f + 1].bones[3].GetComponent<Collider>());
+                     }
+                }
+            }
+        }
+
+        public void OnEnable()
+        {
+            for (int f = 0; f < fingers.Length; ++f)
+            {
+                if (fingers[f] != null)
+                {
+                    Physics.IgnoreCollision(fingers[f].bones[1].GetComponent<Collider>(), palm.GetComponent<Collider>());
+                    Physics.IgnoreCollision(fingers[f].bones[2].GetComponent<Collider>(), palm.GetComponent<Collider>());
+
+                    if (f < fingers.Length - 1)
+                    {
+                        Physics.IgnoreCollision(fingers[f].bones[1].GetComponent<Collider>(), fingers[f + 1].bones[1].GetComponent<Collider>());
+                        Physics.IgnoreCollision(fingers[f].bones[2].GetComponent<Collider>(), fingers[f + 1].bones[2].GetComponent<Collider>());
+                        Physics.IgnoreCollision(fingers[f].bones[3].GetComponent<Collider>(), fingers[f + 1].bones[3].GetComponent<Collider>());
+                        Physics.IgnoreCollision(fingers[f].bones[2].GetComponent<Collider>(), fingers[f + 1].bones[3].GetComponent<Collider>());
+                        Physics.IgnoreCollision(fingers[f].bones[1].GetComponent<Collider>(), fingers[f + 1].bones[3].GetComponent<Collider>());
+                    }
+                }
+            }
+        }
+
+        public override void UpdateHand()
+        {
+            if (palm != null)
+            {
+                Rigidbody palmBody = palm.GetComponent<Rigidbody>();
+                if (palmBody)
+                {
+                    palmBody.MovePosition(GetPalmCenter());
+                    palmBody.MoveRotation(GetPalmRotation());
+                    palm.position = GetPalmCenter();
+                    palm.rotation = GetPalmRotation();
+                }
+                else
+                {
+                    palm.position = GetPalmCenter();
+                    palm.rotation = GetPalmRotation();
+                }
+            }
+
+
+            for (int f = 0; f < fingers.Length; ++f)
+            {
+                if (fingers[f] != null)
+                {
+                    fingers[f].UpdateFinger();
+                }
+            }
+
+            if (forearm != null)
+            {
+                // Set arm dimensions.
+                CapsuleCollider capsule = forearm.GetComponent<CapsuleCollider>();
+                if (capsule != null)
+                {
+                    // Initialization
+                    capsule.direction = 2;
+                    forearm.localScale = new Vector3(1f / transform.lossyScale.x, 1f / transform.lossyScale.y, 1f / transform.lossyScale.z);
+
+                    // Update
+                    capsule.radius = GetArmWidth() / 2f;
+                    capsule.height = GetArmLength() + GetArmWidth();
+
+                    Physics.IgnoreCollision(capsule, palm.GetComponent<Collider>());
+                }
+
+                Rigidbody forearmBody = forearm.GetComponent<Rigidbody>();
+                if (forearmBody)
+                {
+                    forearmBody.MovePosition(GetArmCenter());
+                    forearmBody.MoveRotation(GetArmRotation());
+                }
+                else
+                {
+                    forearm.position = GetArmCenter();
+                    forearm.rotation = GetArmRotation();
+                }
+            }
+        }
+    }
+}
