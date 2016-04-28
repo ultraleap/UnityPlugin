113f598e70e61283260d30ca8bb08c8a8bed5b3d
Joe Ward <jward@leapmotion.com>
2016-04-28 12:40:39 -0700

    some demo resources

---
+++ Assets/LeapMotionModules/Attachments/Scripts/LookAtCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+    public GameObject Menu;
+    public Vector3 Zed = Vector3.zero;
+  public bool IsActive = false;
+
+  void LateUpdate () {
+        transform.rotation = Quaternion.LookRotation(transform.parent.right, Camera.main.transform.position - transform.position);
+        Zed = transform.localEulerAngles;
+        if (transform.localEulerAngles.z < 320f && transform.localEulerAngles.z > 250f)
+        {
+            Menu.SetActive(true);
+            IsActive = true;
+        }
+        else
+        {
+            Menu.SetActive(false);
+            IsActive = false;
+        }
+  }
+}
---
+++ Assets/LeapMotionModules/DetectionUtilities/Scripts/ZoneDetector.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+namespace Leap.Unity.DetectionUtilities {
+
+  /** Detects when the hand has entered a rectangular zone. */
+  public class ZoneDetector : Detector {
+    public Vector3 zones = Vector3.one;
+    public Vector3 TargetZone = Vector3.zero;
+
+    private Device _device = null;
+    private Controller _controller;
+    private float[] _xDivisions;
+    private float[] _yDivisions;
+    private float[] _zDivisions;
+
+    //Get the interaction volume, divide into zones and report which zone the hand is in.
+
+    void Start(){
+      if(_controller == null){
+        _controller = new Controller();
+      }
+      _controller.Device += OnDevice;
+    }
+    void OnDevice(object sender, DeviceEventArgs deviceArgs){
+      _device = deviceArgs.Device;
+      if(_device.IsStreaming){
+        int yDivs = (int)(_device.Range/zones.y + 0.5f);
+      }
+    }
+  }
+}
