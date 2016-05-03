97a0d1803ece5b64df4c3334fd873e05dfb4ed94
Veronica Zheng <vzheng@leapmotion.com>
2016-05-02 19:33:03 -0700

    fix Leap_Hands_Demo_HMD to use FpsLabel

--- Assets/LeapMotionModules/Android/FpsLabel/Scripts/FpsLabel.cs
+++ Assets/LeapMotionModules/Android/FpsLabel/Scripts/FpsLabel.cs
@@ -4,22 +4,17 @@ using System.Collections;
 
 namespace Leap.Unity {
   public class FpsLabel : MonoBehaviour {
-	public LeapProvider Provider { get; set; }
-
-	public Text frameRateText;
+    public LeapProvider Provider;
+  	public Text frameRateText;
     private float fps;
 
-	void Start () {
-	  Provider = GetComponent<LeapProvider>();
-	}
-
-	void Update () {
-	  Frame frame = Provider.CurrentFrame;
-	  float interp = Time.deltaTime / (0.5f + Time.deltaTime);
-	  float currentFPS = 1.0f / Time.deltaTime;
-	  fps = Mathf.Lerp (fps, currentFPS, interp);
-	  frameRateText.text = "Data FPS:" + frame.CurrentFramesPerSecond.ToString ("f2") +
-		System.Environment.NewLine + "Render FPS:" + Mathf.RoundToInt (fps).ToString ("f2");
-	}
+  	void Update () {
+  	  Frame frame = Provider.CurrentFrame;
+  	  float interp = Time.deltaTime / (0.5f + Time.deltaTime);
+  	  float currentFPS = 1.0f / Time.deltaTime;
+  	  fps = Mathf.Lerp (fps, currentFPS, interp);
+  	  frameRateText.text = "Data FPS:" + frame.CurrentFramesPerSecond.ToString ("f2") +
+  		System.Environment.NewLine + "Render FPS:" + Mathf.RoundToInt (fps).ToString ("f2");
+  	}
   }
 }
