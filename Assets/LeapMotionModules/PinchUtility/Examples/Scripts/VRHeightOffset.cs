6b11f6580cd7dcf6a8aa3dcda9345b5f27928438
Amarcolina <Amarcoli@ucsc.edu>
2016-05-05 16:57:01 -0700

    Adding VR height offset

---
+++ Assets/LeapMotionModules/PinchUtility/Examples/Scripts/VRHeightOffset.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+using System;
+using System.Linq;
+
+public class VRHeightOffset : MonoBehaviour {
+
+  [Serializable]
+  public class DeviceHeightPair {
+    public string deviceName;
+    public float heightOffset;
+
+    public DeviceHeightPair(string deviceName, float heightOffset) {
+      this.deviceName = deviceName;
+      this.heightOffset = heightOffset;
+    }
+  }
+
+  public DeviceHeightPair[] _deviceOffsets;
+
+  void Reset() {
+    _deviceOffsets = new DeviceHeightPair[2];
+    _deviceOffsets[0] = new DeviceHeightPair("Oculus", -0.1f);
+    _deviceOffsets[1] = new DeviceHeightPair("Vive", 1.0f);
+  }
+
+  void Start() {
+    if (VRDevice.isPresent && VRSettings.enabled && _deviceOffsets != null) {
+      string deviceName = VRSettings.loadedDeviceName;
+      var deviceHeightPair = _deviceOffsets.FirstOrDefault(d => d.deviceName == deviceName);
+      if (deviceHeightPair != null) {
+        transform.Translate(Vector3.up * deviceHeightPair.heightOffset);
+      }
+    }
+  }
+
+}
