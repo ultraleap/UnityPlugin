5f4bc2d65797e14caa05ca0211e00881140f3e49
jselstad <jselstad@leapmotion.com>
2016-05-09 18:48:28 -0700

    Small UI Changes
    Beginning to add old style UI widgets in (and sound effects for the
    layers)

--- Assets/LeapMotionModules/UIInput/Scripts/CompressibleUI.cs
+++ Assets/LeapMotionModules/UIInput/Scripts/CompressibleUI.cs
@@ -15,9 +15,12 @@ public class CompressibleUI : MonoBehaviour, ILeapWidget
         public RectTransform LayerTransform;
         public float MaxFloatDistance;
         public float MinFloatDistance;
+        public bool TriggerLayerEvent;
 
         [HideInInspector]
         public float CurrentFloatingDistance;
+        [HideInInspector]
+        public bool touchingFinger;
     }
 
     [SerializeField]
@@ -26,6 +29,8 @@ public class CompressibleUI : MonoBehaviour, ILeapWidget
     public float ContractSpeed = 0.1f;
     [SerializeField]
     public float PushPaddingDistance = 0.01f;
+    [SerializeField]
+    public UnityEvent LayerCollapseStateChange;
 
     //How quickly the button layers are Lerping
     private float curLerpSpeed = 0.1f;
@@ -86,14 +91,38 @@ public class CompressibleUI : MonoBehaviour, ILeapWidget
                     if (HoveringDistance < Layers[i].MaxFloatDistance && HoveringDistance > Layers[i].MinFloatDistance)
                     {
                         Layers[i].CurrentFloatingDistance = Mathf.Lerp(Layers[i].CurrentFloatingDistance, HoveringDistance, 0.2f); //Set to 1f for responsive touching...
+                        if (Layers[i].TriggerLayerEvent && !Layers[i].touchingFinger) {
+                            Layers[i].touchingFinger = true;
+                            Graphic image = Layers[i].LayerTransform.GetComponent<Graphic>();
+                            if (image != null) {
+                                image.color = new Color(image.color.r - 0.175f, image.color.g - 0.175f, image.color.b - 0.175f, image.color.a);
+                            }
+                            LayerCollapseStateChange.Invoke();
+                        }
                     }
                     else if (HoveringDistance < Layers[i].MinFloatDistance)
                     {
                         Layers[i].CurrentFloatingDistance = Mathf.Lerp(Layers[i].CurrentFloatingDistance, Layers[i].MinFloatDistance, curLerpSpeed);
+                        if (Layers[i].TriggerLayerEvent && !Layers[i].touchingFinger) {
+                            Layers[i].touchingFinger = true;
+                            Graphic image = Layers[i].LayerTransform.GetComponent<Graphic>();
+                            if (image != null) {
+                                image.color = new Color(image.color.r - 0.175f, image.color.g - 0.175f, image.color.b - 0.175f, image.color.a);
+                            }
+                            LayerCollapseStateChange.Invoke();
+                        }
                     }
                     else
                     {
                         Layers[i].CurrentFloatingDistance = Mathf.Lerp(Layers[i].CurrentFloatingDistance, Layers[i].MaxFloatDistance, curLerpSpeed);
+                        if (Layers[i].TriggerLayerEvent && Layers[i].touchingFinger) {
+                            Layers[i].touchingFinger = false;
+                            Graphic image = Layers[i].LayerTransform.GetComponent<Graphic>();
+                            if (image != null) {
+                                image.color = new Color(image.color.r + 0.175f, image.color.g + 0.175f, image.color.b + 0.175f, image.color.a);
+                            }
+                            LayerCollapseStateChange.Invoke();
+                        }
                     }
 
                     Vector3 LocalPosition = transform.InverseTransformPoint(Layers[i].LayerTransform.position);
@@ -101,6 +130,7 @@ public class CompressibleUI : MonoBehaviour, ILeapWidget
                 }
             }
         }
+        //else Just lay them flat so they're not bothering any cursors.
         else
         {
             for (int i = 0; i < Layers.Length; i++)
@@ -108,6 +138,13 @@ public class CompressibleUI : MonoBehaviour, ILeapWidget
                 if (Layers[i].LayerTransform != null)
                 {
                     Layers[i].CurrentFloatingDistance = Mathf.Lerp(Layers[i].CurrentFloatingDistance, 0f, curLerpSpeed);
+                    if (Layers[i].TriggerLayerEvent && Layers[i].touchingFinger) {
+                        Layers[i].touchingFinger = false;
+                        Graphic image = Layers[i].LayerTransform.GetComponent<Graphic>();
+                        if (image != null) {
+                            image.color = new Color(image.color.r + 0.175f, image.color.g + 0.175f, image.color.b + 0.175f, image.color.a);
+                        }
+                    }
 
                     Vector3 LocalPosition = transform.InverseTransformPoint(Layers[i].LayerTransform.position);
                     Layers[i].LayerTransform.position = transform.TransformPoint(new Vector3(LocalPosition.x, LocalPosition.y, -Layers[i].CurrentFloatingDistance / transform.lossyScale.z));
