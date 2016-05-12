192c3eb0b91fb093c036bb5f68562d964f572517
Joe Ward <jward@leapmotion.com>
2016-05-11 17:40:14 -0700

    facing work

--- Assets/LeapMotionModules/Attachments/Scripts/CameraFacer.cs
+++ Assets/LeapMotionModules/Attachments/Scripts/CameraFacer.cs
@@ -5,20 +5,38 @@ public class CameraFacer : MonoBehaviour {
 
   public Vector3 objectForward = Vector3.forward;
   public Transform RotationParent;
-  public float MaxTilt = 20;
+  public Vector3 MaxTilt = Vector3.one * 15;//degrees
 
   private Quaternion offset;
+  private Quaternion originalLocalRotation;
 
   void Awake(){
-    offset = transform.localRotation;
+    transform.rotation = Quaternion.LookRotation(objectForward);
+    offset = transform.rotation;
+    originalLocalRotation = transform.localRotation;
   }
 
-	void Update () {
-    //Quaternion lookAtCamera = Quaternion.FromToRotation(objectForward, -Camera.main.transform.forward);
+  void Update () {
+    Vector3 cameraDirection = (Camera.main.transform.position - transform.position).normalized;
+    Vector3 objectDirection = transform.TransformDirection(objectForward);
+    Quaternion towardCamera = Quaternion.LookRotation(cameraDirection);
+    towardCamera *= offset;
+    transform.rotation = Quaternion.Slerp(towardCamera, Quaternion.LookRotation(objectDirection), Time.deltaTime / 5);
+    float angleX = ClampAngle(transform.localEulerAngles.x, -MaxTilt.x, MaxTilt.x);
+    float angleY = ClampAngle(transform.localEulerAngles.y, -MaxTilt.y, MaxTilt.y);
+    float angleZ = ClampAngle(transform.localEulerAngles.z, -MaxTilt.z, MaxTilt.z);
+    transform.localEulerAngles = new Vector3(angleX, angleY, angleZ);
+  }
 
-    float tiltAngle = Quaternion.Angle(Quaternion.identity, transform.localRotation);
-    float toCameraAngle = Quaternion.Angle(Camera.main.transform.rotation, transform.localRotation);
-    if(toCameraAngle < MaxTilt) toCameraAngle = MaxTilt;
-    transform.rotation = Quaternion.RotateTowards(transform.rotation, Camera.main.transform.rotation, MaxTilt);
-	}
+  /* Attribution: aldonaletto http://answers.unity.com/questions/141775/limit-local-rotation.html */
+  float ClampAngle(float angle, float min, float max) {
+    if (angle < 90 || angle > 270){       // if angle in the critic region...
+        if (angle > 180) angle -= 360;  // convert all angles to -180..+180
+        if (max > 180) max -= 360;
+        if (min > 180) min -= 360;
+    }    
+    angle = Mathf.Clamp(angle, min, max);
+    if (angle<0) angle += 360;  // if angle negative, convert to 0..360
+    return angle;
+  }
 }
