73d17c8c6371babdc60a7d2710d276b8f85f46af
Joe Ward <jward@leapmotion.com>
2016-05-12 10:07:32 -0700

    facing tweaks

--- Assets/LeapMotionModules/Attachments/Scripts/CameraFacer.cs
+++ Assets/LeapMotionModules/Attachments/Scripts/CameraFacer.cs
@@ -6,6 +6,10 @@ public class CameraFacer : MonoBehaviour {
   public Vector3 objectForward = Vector3.forward;
   public Transform RotationParent;
   public Vector3 MaxTilt = Vector3.one * 15;//degrees
+  public Vector3 MinTilt = Vector3.one * 15;//degrees
+  public bool FreezeX = false;
+  public bool FreezeY = false;
+  public bool FreezeZ = false;
 
   private Quaternion offset;
   private Quaternion originalLocalRotation;
@@ -22,9 +26,24 @@ public class CameraFacer : MonoBehaviour {
     Quaternion towardCamera = Quaternion.LookRotation(cameraDirection);
     towardCamera *= offset;
     transform.rotation = Quaternion.Slerp(towardCamera, Quaternion.LookRotation(objectDirection), Time.deltaTime / 5);
-    float angleX = ClampAngle(transform.localEulerAngles.x, -MaxTilt.x, MaxTilt.x);
-    float angleY = ClampAngle(transform.localEulerAngles.y, -MaxTilt.y, MaxTilt.y);
-    float angleZ = ClampAngle(transform.localEulerAngles.z, -MaxTilt.z, MaxTilt.z);
+    Vector3 startEulers = originalLocalRotation.eulerAngles;
+
+    float angleX, angleY, angleZ;
+    if(FreezeX){
+      angleX = startEulers.x;
+    } else {
+      angleX = ClampAngle(transform.localEulerAngles.x, startEulers.x - MinTilt.x, startEulers.x + MaxTilt.x);
+    }
+    if(FreezeY){
+      angleY = startEulers.y;
+    } else {
+      angleY = ClampAngle(transform.localEulerAngles.y, startEulers.y - MinTilt.y, startEulers.y + MaxTilt.y);
+    }
+    if(FreezeZ){
+      angleZ = startEulers.z;
+    } else {
+      angleZ = ClampAngle(transform.localEulerAngles.z, startEulers.z - MinTilt.z, startEulers.z + MaxTilt.z);
+    }
     transform.localEulerAngles = new Vector3(angleX, angleY, angleZ);
   }
 
