cb09b014f7c0ab223ad038a07bf96f7e690624c0
Joe Ward <jward@leapmotion.com>
2016-05-13 12:29:46 -0700

    Added easing curve to facer

--- Assets/LeapMotionModules/Attachments/Scripts/CameraFacer.cs
+++ Assets/LeapMotionModules/Attachments/Scripts/CameraFacer.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class CameraFacer : MonoBehaviour {
 
-  [Range(.1f,20)]
-  public float Speed = 10;
   public Vector3 objectForward = Vector3.forward;
+  public AnimationCurve Ease = AnimationCurve.EaseInOut(0, 0, 1, 1);
+  [Range(1, 20)]
+  public float Speed = 10;
   public bool FreezeX = false;
   public bool FreezeY = false;
   public bool FreezeZ = false;
@@ -20,9 +21,12 @@ public class CameraFacer : MonoBehaviour {
 
   void Update () {
     Vector3 cameraDirection = (Camera.main.transform.position - transform.position).normalized;
+    Vector3 objectFacing = transform.TransformDirection(objectForward);
+    float deltaAngle = Vector3.Angle(objectFacing, cameraDirection);
+    float easing = Ease.Evaluate(Speed * deltaAngle / 360);
     Quaternion towardCamera = Quaternion.LookRotation(cameraDirection);
     towardCamera *= offset;
-    transform.rotation = Quaternion.Slerp(transform.rotation, towardCamera, Time.deltaTime * Speed);
+    transform.rotation = Quaternion.Slerp(transform.rotation, towardCamera, easing);
     Vector3 startingEulers = startingLocalRotation.eulerAngles;
     Vector3 targetEulers = transform.localEulerAngles;
     float angleX, angleY, angleZ;
