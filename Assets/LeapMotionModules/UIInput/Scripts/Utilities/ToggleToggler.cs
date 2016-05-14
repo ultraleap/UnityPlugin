a935acd4ea09b37ccefce8b1f0925828d62416ab
jselstad <jselstad@leapmotion.com>
2016-05-13 18:25:17 -0700

    Integrating Alex's Suggestions
    Virtual Reality Supported kind of has to be on since we're a VR company
    now...

--- Assets/LeapMotionModules/UIInput/Scripts/Utilities/ToggleToggler.cs
+++ Assets/LeapMotionModules/UIInput/Scripts/Utilities/ToggleToggler.cs
@@ -2,26 +2,23 @@
 using UnityEngine.UI;
 using System.Collections;
 
-namespace Leap.Unity.InputModule
-{
-    public class ToggleToggler : MonoBehaviour
-    {
-        public Text text;
-        public UnityEngine.UI.Image image;
-        public Color OnColor;
-        public Color OffColor;
+namespace Leap.Unity.InputModule {
+  public class ToggleToggler : MonoBehaviour {
+    public Text text;
+    public UnityEngine.UI.Image image;
+    public Color OnColor;
+    public Color OffColor;
 
-        public void SetToggle(Toggle toggle)
-        {
-            if (toggle.isOn) {
-                text.text = "On";
-                text.color = Color.white;
-                image.color = OnColor;
-            } else {
-                text.text = "Off";
-                text.color = Color.black;
-                image.color = OffColor;
-            }
-        }
+    public void SetToggle(Toggle toggle) {
+      if (toggle.isOn) {
+        text.text = "On";
+        text.color = Color.white;
+        image.color = OnColor;
+      } else {
+        text.text = "Off";
+        text.color = Color.black;
+        image.color = OffColor;
+      }
     }
+  }
 }
