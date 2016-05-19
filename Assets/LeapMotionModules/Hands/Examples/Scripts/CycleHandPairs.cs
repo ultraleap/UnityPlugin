549e0cc0424eed8df04195e4167648e7829d2f51
protodeep <fox@brainstormer-project.net>
2016-05-18 19:22:46 -0700

    First pass at cycling pairs script for demo scene

---
+++ Assets/LeapMotionModules/Hands/Examples/Scripts/CycleHandPairs.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap.Unity;
+
+[RequireComponent(typeof(HandPool))]
+public class CycleHandPairs : MonoBehaviour {
+  private HandPool handPool;
+  public string[] GroupNames;
+  private int currentGroup;
+  public int CurrentGroup {
+    get { return currentGroup; }
+    set {
+      handPool.DisableGroup(GroupNames[CurrentGroup]);
+      currentGroup = value;
+      handPool.EnableGroup(GroupNames[value]);
+    }
+  }
+
+  // Use this for initialization
+  void Start () {
+    handPool = GetComponent<HandPool>();
+  }
+  
+  // Update is called once per frame
+  void Update () {
+    if (Input.GetKeyUp(KeyCode.RightArrow)) {
+      if (CurrentGroup < GroupNames.Length -1) {
+        CurrentGroup++;
+      }
+    }
+    if (Input.GetKeyUp(KeyCode.LeftArrow)) {
+      if (CurrentGroup > 0) {
+        CurrentGroup--;
+      }
+    }
+  }
+  //show group name on change
+}
