4288bb323789b895febec82bb0e829e1462b8845
protodeep <fox@brainstormer-project.net>
2016-05-19 10:47:07 -0700

    Corrected capitalization in comments and added a comment

--- Assets/LeapMotion/Scripts/HandPool.cs
+++ Assets/LeapMotion/Scripts/HandPool.cs
@@ -25,11 +25,11 @@ namespace Leap.Unity {
     private Dictionary<IHandModel, HandRepresentation> modelToHandRepMapping = new Dictionary<IHandModel, HandRepresentation>();
 
     /**
-     * ModelGroup contains a left/right pair of iHandModel's 
-     * @param modelList The iHandModels available for use by HandRepresentations
-     * @param modelsCheckedOut The iHandModels currently in use by active HandRepresentations
+     * ModelGroup contains a left/right pair of IHandModel's 
+     * @param modelList The IHandModels available for use by HandRepresentations
+     * @param modelsCheckedOut The IHandModels currently in use by active HandRepresentations
      * @param IsEnabled determines whether the ModelGroup is active at app Start(), though ModelGroup's are controlled with the EnableGroup() & DisableGroup methods.
-     * @param CanDuplicate Allows a iHandModels in the ModelGroup to be cloned at runtime if a suitable iHandModel isn't available.
+     * @param CanDuplicate Allows a IHandModels in the ModelGroup to be cloned at runtime if a suitable IHandModel isn't available.
      */
     [System.Serializable]
     public class ModelGroup {
@@ -49,7 +49,8 @@ namespace Leap.Unity {
       public List<IHandModel> modelsCheckedOut;
       public bool IsEnabled = true;
       public bool CanDuplicate;
-      /*Looks for suitable iHandModel is the ModelGroup's modelList, if found, it is added to modelsCheckedOut */
+      /*Looks for suitable IHandModel is the ModelGroup's modelList, if found, it is added to modelsCheckedOut.
+       * If not, one can be cloned*/
       public IHandModel TryGetModel(Chirality chirality, ModelType modelType) {
         for (int i = 0; i < modelList.Count; i++) {
           if (modelList[i].HandModelType == modelType
@@ -147,7 +148,7 @@ namespace Leap.Unity {
       return handRep;
     }
     /**
-    * EnableGroup finds suitable HandRepresentations and adds iHandModels from the ModelGroup, returns them to their ModelGroup and sets the groups IsEnabled to true.
+    * EnableGroup finds suitable HandRepresentations and adds IHandModels from the ModelGroup, returns them to their ModelGroup and sets the groups IsEnabled to true.
      * @param groupName Takes a string that matches the ModelGroup's groupName serialized in the Inspector
     */
     public void EnableGroup(string groupName) {
@@ -167,7 +168,7 @@ namespace Leap.Unity {
       }
     }
     /**
-     * DisableGroup finds and removes the ModelGroup's iHandModels from their HandRepresentations, returns them to their ModelGroup and sets the groups IsEnabled to false.
+     * DisableGroup finds and removes the ModelGroup's IHandModels from their HandRepresentations, returns them to their ModelGroup and sets the groups IsEnabled to false.
      * @param groupName Takes a string that matches the ModelGroup's groupName serialized in the Inspector
      */
     public void DisableGroup(string groupName) {
