0de9301b39e1007b8648def063fe9f50e5c6a851
Amarcolina <Amarcoli@ucsc.edu>
2016-05-18 19:29:54 -0700

    added bounciness

--- Assets/LeapMotionModules/InteractionEngine/Scripts/InteractionBehaviour.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/InteractionBehaviour.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Assertions;
 using Leap.Unity.Interaction.CApi;
 using LeapInternal;
 
