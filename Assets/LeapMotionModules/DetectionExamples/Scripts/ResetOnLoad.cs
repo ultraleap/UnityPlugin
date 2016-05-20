76fbf9cb0c288a21ba28a5478bedffa499711e70
Joe Ward <jward@leapmotion.com>
2016-05-20 11:27:33 -0700

    Only adjust y position

---
+++ Assets/LeapMotionModules/DetectionExamples/Scripts/ResetOnLoad.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+using System.Collections;
+
+public class ResetOnLoad : MonoBehaviour {
+    public Transform HeadTrackingNode;
+    private Vector3 offset;
+    void Awake() {
+        offset = transform.position - HeadTrackingNode.transform.position;
+    }
+
+	void Start () {
+        transform.position = new Vector3(transform.position.x, HeadTrackingNode.transform.position.y + offset.y, transform.position.z);
+        InputTracking.Recenter();
+    }
+}
