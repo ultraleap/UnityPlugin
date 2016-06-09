9808338da340ff3b5d85e9b5e5ff804f388adead
Amarcolina <Amarcoli@ucsc.edu>
2016-06-09 13:03:22 -0700

    Renamed folder and added OnHold callback to throwin g

--- Assets/LeapMotionModules/InteractionEngine/Scripts/ControllerInterfaces/IControllerBase.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/ControllerInterfaces/IControllerBase.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 namespace Leap.Unity.Interaction {
 
--- Assets/LeapMotionModules/InteractionEngine/Scripts/ControllerInterfaces/IThrowingController.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/ControllerInterfaces/IThrowingController.cs
@@ -3,6 +3,7 @@
 namespace Leap.Unity.Interaction {
 
   public abstract class IThrowingController : IControllerBase {
+    public abstract void OnHold();
     public abstract void OnThrow();
   }
 }
