5261e1f7eea3fcb51b8078e33e037c1696cd6212
Amarcolina <Amarcoli@ucsc.edu>
2016-06-09 16:41:20 -0700

    Put correct logic into physics controller

--- Assets/LeapMotionModules/InteractionEngine/Scripts/ControllerImplementations/PhysicsControllerVelocity.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/ControllerImplementations/PhysicsControllerVelocity.cs
@@ -16,26 +16,31 @@ namespace Leap.Unity.Interaction {
         _obj.rigidbody.position = solvedPosition;
         _obj.rigidbody.rotation = solvedRotation;
       } else {
-        Vector3 deltaPos = solvedPosition - _obj.rigidbody.position;
-        Quaternion deltaRot = solvedRotation * Quaternion.Inverse(_obj.rigidbody.rotation);
+        Vector3 deltaPos = solvedPosition - _obj.warper.RigidbodyPosition;
+        Quaternion deltaRot = solvedRotation * Quaternion.Inverse(_obj.warper.RigidbodyRotation);
 
         Vector3 deltaAxis;
         float deltaAngle;
         deltaRot.ToAngleAxis(out deltaAngle, out deltaAxis);
+
+        if (float.IsInfinity(deltaAxis.x)) {
+          deltaAxis = Vector3.zero;
+          deltaAngle = 0;
+        }
 
         Vector3 targetVelocity = deltaPos / Time.fixedDeltaTime;
         Vector3 targetAngularVelocity = deltaAxis * deltaAngle * Mathf.Deg2Rad / Time.fixedDeltaTime;
 
         if (targetVelocity.sqrMagnitude > float.Epsilon) {
           float targetSpeed = targetVelocity.magnitude;
-          float actualSpeed = Mathf.Min(_maxVelocity, targetSpeed);
+          float actualSpeed = Mathf.Min(_maxVelocity * _obj.Manager.SimulationScale, targetSpeed);
           float targetPercent = actualSpeed / targetSpeed;
 
           targetVelocity *= targetPercent;
           targetAngularVelocity *= targetPercent;
         }
 
-        float followStrength = _strengthByDistance.Evaluate(info.remainingDistanceLastFrame);
+        float followStrength = _strengthByDistance.Evaluate(info.remainingDistanceLastFrame / _obj.Manager.SimulationScale);
         _obj.rigidbody.velocity = Vector3.Lerp(_obj.rigidbody.velocity, targetVelocity, followStrength);
         _obj.rigidbody.angularVelocity = Vector3.Lerp(_obj.rigidbody.angularVelocity, targetAngularVelocity, followStrength);
       }
