afc45fb33690eb576c9dd6cee49c931a3514bfab
Amarcolina <Amarcoli@ucsc.edu>
2016-06-10 13:23:10 -0700

    Added layer controller and throwing controller

---
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/ControllerImplementations/ThrowingControllerPalmBased.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace Leap.Unity.Interaction {
+
+  public class ThrowingControllerPalmBased : IThrowingController {
+
+    [SerializeField]
+    private AnimationCurve _throwingVelocityCurve;
+
+    public override void OnHold(ReadonlyList<Hand> hands) { }
+
+    public override void OnThrow(Hand throwingHand) {
+      Vector3 palmVel = throwingHand.PalmVelocity.ToVector3();
+      float speed = palmVel.magnitude;
+      float multiplier = _throwingVelocityCurve.Evaluate(speed / _obj.Manager.SimulationScale);
+      _obj.rigidbody.velocity = palmVel * multiplier;
+    }
+  }
+}
--- Assets/LeapMotionModules/InteractionEngine/Scripts/ControllerInterfaces/IThrowingController.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/ControllerInterfaces/IThrowingController.cs
@@ -3,7 +3,7 @@
 namespace Leap.Unity.Interaction {
 
   public abstract class IThrowingController : IControllerBase {
-    public abstract void OnHold();
-    public abstract void OnThrow();
+    public abstract void OnHold(ReadonlyList<Hand> hands);
+    public abstract void OnThrow(Hand throwingHand);
   }
 }
