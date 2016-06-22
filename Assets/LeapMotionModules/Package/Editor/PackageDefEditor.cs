7480ebe0a3e3cdb23b39122702b9e921a2da919c
Amarcolina <Amarcoli@ucsc.edu>
2016-06-22 15:59:17 -0700

    working on package manager

---
+++ Assets/LeapMotionModules/Package/Editor/PackageDefEditor.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using System;
+using System.IO;
+using System.Collections;
+
+namespace Leap.Unity.Packaging {
+
+  [CustomEditor(typeof(PackageDef))]
+  public class PackageDefEditor : CustomEditorBase {
+
+    private PackageDef _def;
+
+    protected override void OnEnable() {
+      base.OnEnable();
+
+      _def = target as PackageDef;
+
+      createList("_dependantFolders", drawFolderElement);
+      createList("_dependantFiles", drawFileElement);
+      createList("_dependantPackages", drawPackageElement);
+
+      specifyCustomDecorator("_dependantFolders", drawPackageExportFolder);
+    }
+
+    private void drawPackageExportFolder(SerializedProperty property) {
+      string folder;
+      EditorGUILayout.BeginHorizontal();
+
+      if (_def.TryGetPackageExportFolder(out folder, prompIfNotDefined: false)) {
+        EditorGUILayout.TextField("Package Export Folder", folder);
+        if (GUILayout.Button("Change")) {
+          _def.PrompUserToSetExportPath();
+        }
+      } else {
+        EditorGUILayout.LabelField("Package Export Folder");
+        if (GUILayout.Button("Change")) {
+          _def.PrompUserToSetExportPath();
+        }
+      }
+
+      EditorGUILayout.EndHorizontal();
+    }
+
+    private void drawFolderElement(Rect rect, SerializedProperty property) {
+      drawExplorerElement(rect, property, EditorUtility.OpenFolderPanel);
+    }
+
+    private void drawFileElement(Rect rect, SerializedProperty property) {
+      drawExplorerElement(rect, property, EditorUtility.OpenFilePanel);
+    }
+
+    private void drawPackageElement(Rect rect, SerializedProperty property) {
+      EditorGUI.PropertyField(rect, property, GUIContent.none);
+    }
+
+    private ReorderableList createList(string propertyName, Action<Rect, SerializedProperty> drawMethod) {
+      SerializedProperty listProperty = serializedObject.FindProperty(propertyName);
+
+      var list = new ReorderableList(serializedObject, listProperty,
+                                     draggable: true,
+                                     displayHeader: true,
+                                     displayAddButton: true,
+                                     displayRemoveButton: true);
+
+      list.drawElementCallback += (rect, index, isActive, isFocused) => {
+        SerializedProperty property = list.serializedProperty.GetArrayElementAtIndex(index);
+        drawMethod(rect, property);
+      };
+
+      list.drawHeaderCallback += (rect) => {
+        GUI.Label(rect, listProperty.displayName);
+      };
+
+      list.elementHeight = EditorGUIUtility.singleLineHeight;
+
+
+      specifyCustomDrawer(propertyName, p => list.DoLayoutList());
+
+      return list;
+    }
+
+    private void drawExplorerElement(Rect rect, SerializedProperty property, Func<string, string, string, string> openAction) {
+      Rect leftRect = rect;
+      Rect rightRect = rect;
+
+      leftRect.width -= 100;
+
+      rightRect.x = leftRect.x + leftRect.width;
+      rightRect.width = 100;
+
+      EditorGUI.TextField(leftRect, property.stringValue);
+
+      if (GUI.Button(rightRect, "Change")) {
+        string chosenFolder = openAction("Select Folder", Application.dataPath, "");
+        string relativePath = makeRelativePath(Application.dataPath, chosenFolder);
+        if (!string.IsNullOrEmpty(relativePath) && !relativePath.StartsWith("..")) {
+          property.stringValue = relativePath;
+        }
+      }
+    }
+
+    private static string makeRelativePath(string fromPath, string toPath) {
+      if (string.IsNullOrEmpty(fromPath)) throw new ArgumentNullException("fromPath");
+      if (string.IsNullOrEmpty(toPath)) throw new ArgumentNullException("toPath");
+
+      Uri fromUri = new Uri(fromPath);
+      Uri toUri = new Uri(toPath);
+
+      if (fromUri.Scheme != toUri.Scheme) { return toPath; } // path can't be made relative.
+
+      Uri relativeUri = fromUri.MakeRelativeUri(toUri);
+      string relativePath = Uri.UnescapeDataString(relativeUri.ToString());
+
+      if (toUri.Scheme.Equals("file", StringComparison.InvariantCultureIgnoreCase)) {
+        relativePath = relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+      }
+
+      return relativePath;
+    }
+
+  }
+}
---
+++ Assets/LeapMotionModules/Package/PackageDef.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Leap.Unity.Packaging {
+
+  public class PackageDef : ScriptableObject {
+    private const string PACKAGE_EXPORT_FOLDER_KEY = "LeapPackageDefExportFolder";
+
+    [SerializeField]
+    protected string _packageName;
+
+    [SerializeField]
+    protected string[] _dependantFolders;
+
+    [SerializeField]
+    protected string[] _dependantFiles;
+
+    [SerializeField]
+    protected PackageDef[] _dependantPackages;
+
+    [ContextMenu("Reset Export Folder")]
+    public void ResetExportFolder() {
+      EditorPrefs.DeleteKey(getExportFolderKey());
+    }
+
+    public bool PrompUserToSetExportPath() {
+      string chosenFolder = EditorUtility.SaveFilePanel("Select export path for " + _packageName, Application.dataPath, _packageName, "unitypackage");
+      if (string.IsNullOrEmpty(chosenFolder)) {
+        return false;
+      }
+
+      EditorPrefs.SetString(getExportFolderKey(), chosenFolder);
+      return true;
+    }
+
+    public bool HasExportFolderBeenDefined() {
+      string key = getExportFolderKey();
+      return EditorPrefs.HasKey(key);
+    }
+
+    public bool TryGetPackageExportFolder(out string folder, bool prompIfNotDefined) {
+      string key = getExportFolderKey();
+      if (!EditorPrefs.HasKey(key)) {
+        folder = null;
+        return false;
+      }
+
+      folder = EditorPrefs.GetString(key);
+      return true;
+    }
+
+    public void BuildPackage(ExportPackageOptions options) {
+      string exportFolder;
+      if (!TryGetPackageExportFolder(out exportFolder, prompIfNotDefined: true)) {
+        Debug.LogWarning("Did not build package " + _packageName + " because no path was defined.");
+        return;
+      }
+
+      HashSet<string> assets = new HashSet<string>();
+
+      HashSet<PackageDef> totalPackages = new HashSet<PackageDef>();
+      totalPackages.Add(this);
+      buildPackageSet(totalPackages);
+
+      foreach (var package in totalPackages) {
+        assets.UnionWith(package._dependantFiles);
+        assets.UnionWith(package._dependantFolders);
+      }
+
+      var filteredAssets = assets.Where(path => File.Exists(path) || Directory.Exists(path)).ToArray();
+
+      AssetDatabase.ExportPackage(filteredAssets, exportFolder, options);
+    }
+
+    /// <summary>
+    /// Builds this package in addition to all packages that depend on this package in some way.
+    /// </summary>
+    public void BuildAllParentPackages(ExportPackageOptions options) {
+      List<PackageDef> parentPackages = findParentPackages();
+      parentPackages.Add(this);
+
+      foreach (var package in parentPackages) {
+        package.BuildPackage(options);
+      }
+    }
+
+    private void buildPackageSet(HashSet<PackageDef> packages) {
+      for (int i = 0; i < _dependantPackages.Length; i++) {
+        PackageDef package = _dependantPackages[i];
+        if (package == null) {
+          continue;
+        }
+
+        if (!packages.Contains(package)) {
+          packages.Add(package);
+          package.buildPackageSet(packages);
+        }
+      }
+    }
+
+    /// <summary>
+    /// Finds all packages that depend on this package in some way.
+    /// </summary>
+    private List<PackageDef> findParentPackages() {
+      List<PackageDef> parents = new List<PackageDef>();
+      var allPackages = Resources.FindObjectsOfTypeAll<PackageDef>();
+
+      HashSet<PackageDef> packages = new HashSet<PackageDef>();
+      foreach (var package in allPackages) {
+        package.buildPackageSet(packages);
+        if (packages.Contains(this)) {
+          parents.Add(package);
+        }
+        packages.Clear();
+      }
+
+      return parents;
+    }
+
+    private string getExportFolderKey() {
+      return PACKAGE_EXPORT_FOLDER_KEY + "_" + AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(this));
+    }
+
+    [MenuItem("Menu/MakeOne")]
+    public static void doTheTHing() {
+      var instance = CreateInstance<PackageDef>();
+      AssetDatabase.CreateAsset(instance, "Assets/packageDef.asset");
+      AssetDatabase.SaveAssets();
+    }
+  }
+}
