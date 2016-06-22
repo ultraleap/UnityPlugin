5374e50864af5092ea5673e4445c2308b1e5d7c5
Amarcolina <Amarcoli@ucsc.edu>
2016-06-21 17:14:10 -0700

    optimized dequeue and added some tests

--- Assets/LeapMotionModules/RealtimeGraph/Scripts/Dequeue.cs
+++ Assets/LeapMotionModules/RealtimeGraph/Scripts/Dequeue.cs
@@ -4,17 +4,19 @@ namespace Leap.Unity.Graphing {
 
   public class Dequeue<T> {
     private T[] _array;
-    private int _back, _count;
+    private uint _back, _count;
+    private uint _indexMask;
 
-    public Dequeue(int capacity = 8) {
-      _array = new T[capacity];
+    public Dequeue(int capacityPower = 3) {
+      _array = new T[(int)Math.Pow(2, capacityPower)];
+      recalculateIndexMask();
       _back = 0;
       _count = 0;
     }
 
     public int Count {
       get {
-        return _count;
+        return (int)_count;
       }
     }
 
@@ -33,101 +35,151 @@ namespace Leap.Unity.Graphing {
     public void PushBack(T t) {
       expandIfNeeded();
       _count++;
-      _back = (_back + _array.Length - 1) % _array.Length;
+      _back = (_back - 1) & _indexMask;
       Back = t;
     }
 
     public void PopFront() {
-      if (_count == 0) {
-        throw new InvalidOperationException("Cannot pop a value because the Dequeue is empty.");
-      }
+      checkForEmpty("pop front");
 
       Front = default(T);
-      _count--;
+      --_count;
     }
 
     public void PopBack() {
-      if (_count == 0) {
-        throw new InvalidOperationException("Cannot pop a value because the Dequeue is empty.");
-      }
+      checkForEmpty("pop back");
 
       Back = default(T);
       _count--;
-      _back = (_back + 1) % _array.Length;
+      _back = (_back + 1) & _indexMask;
     }
 
     public void PopFront(out T front) {
-      if (_count == 0) {
-        throw new InvalidOperationException("Cannot pop a value because the Dequeue is empty.");
-      }
+      checkForEmpty("pop front");
 
-      int frontIndex = getFrontIndex();
+      uint frontIndex = getFrontIndex();
       front = _array[frontIndex];
       _array[frontIndex] = default(T);
-      _count--;
+      --_count;
     }
 
     public void PopBack(out T back) {
-      if (_count == 0) {
-        throw new InvalidOperationException("Cannot pop a value because the Dequeue is empty.");
-      }
+      checkForEmpty("pop back");
 
       back = _array[_back];
       _array[_back] = default(T);
-      _count--;
-      _back = (_back + 1) % _array.Length;
+      _back = (_back + 1) & _indexMask;
+      --_count;
     }
 
     public T Back {
       get {
-        if (_count == 0) {
-          throw new InvalidOperationException("Cannot get the back of the Dequeue because it is empty.");
-        }
-
+        checkForEmpty("get back");
         return _array[_back];
       }
       set {
-        if (_count == 0) {
-          throw new InvalidOperationException("Cannot set the back of the Dequeue because it is empty.");
-        }
-
+        checkForEmpty("set back");
         _array[_back] = value;
       }
     }
 
     public T Front {
       get {
+        checkForEmpty("get front");
         return _array[getFrontIndex()];
       }
       set {
+        checkForEmpty("set front");
         _array[getFrontIndex()] = value;
       }
     }
 
     public T this[int index] {
       get {
-        return _array[getIndex(index)];
+        uint uindex = (uint)index;
+        checkForValidIndex(uindex);
+        return _array[getIndex(uindex)];
       }
       set {
-        _array[getIndex(index)] = value;
+        uint uindex = (uint)index;
+        checkForValidIndex(uindex);
+        _array[getIndex(uindex)] = value;
       }
     }
 
-    private int getFrontIndex() {
-      return (_back + _count - 1) % _array.Length;
+    public string ToDebugString() {
+      string debug = "[";
+      uint front = (_back + _count - 1) & _indexMask;
+      for (uint i = 0; i < _array.Length; i++) {
+        bool isEmpty;
+        if (_count == 0) {
+          isEmpty = true;
+        } else if (_count == 1) {
+          isEmpty = i != _back;
+        } else if (_back < front) {
+          isEmpty = (i < _back) || (i > front);
+        } else {
+          isEmpty = (i < _back) && (i > front);
+        }
+
+        string element = "";
+        if (i == _back) {
+          element = "{";
+        } else {
+          element = " ";
+        }
+
+        if (isEmpty) {
+          element += ".";
+        } else {
+          element += _array[i].ToString();
+        }
+
+        if (i == front) {
+          element += "}";
+        } else {
+          element += " ";
+        }
+
+        debug += element;
+      }
+      debug += "]";
+      return debug;
     }
 
-    private int getIndex(int index) {
-      return (_back + index) % _array.Length;
+    private uint getFrontIndex() {
+      return (_back + _count - 1) & _indexMask;
+    }
+
+    private uint getIndex(uint index) {
+      return (_back + index) & _indexMask;
     }
 
     private void expandIfNeeded() {
       if (_count >= _array.Length) {
         T[] newArray = new T[_array.Length * 2];
-        for (int i = 0; i < _count; i++) {
-          newArray[i] = _array[(_back + i) % _array.Length];
+        for (int i = 0; i < _count; ++i) {
+          newArray[i] = _array[(_back + i) & _indexMask];
         }
+        _back = 0;
         _array = newArray;
+        recalculateIndexMask();
+      }
+    }
+
+    private void recalculateIndexMask() {
+      _indexMask = (uint)_array.Length - 1;
+    }
+
+    private void checkForValidIndex(uint index) {
+      if (index >= _count) {
+        throw new IndexOutOfRangeException("The index " + index + " was out of range for the Dequeue with size " + _count + ".");
+      }
+    }
+
+    private void checkForEmpty(string actionName) {
+      if (_count == 0) {
+        throw new InvalidOperationException("Cannot " + actionName + " because the Dequeue is empty.");
       }
     }
   }
--- Assets/LeapMotionModules/RealtimeGraph/Scripts/Editor/Tests/DequeueTest.cs
+++ Assets/LeapMotionModules/RealtimeGraph/Scripts/Editor/Tests/DequeueTest.cs
@@ -22,6 +22,7 @@ namespace Leap.Unity.Graphing.Tests {
     public void PushFront() {
       for (int i = 0; i < 100; i++) {
         _dequeue.PushFront(i);
+        Assert.That(_dequeue.Front, Is.EqualTo(i));
         for (int j = 0; j <= i; j++) {
           Assert.That(j, Is.EqualTo(_dequeue[j]));
         }
@@ -29,7 +30,29 @@ namespace Leap.Unity.Graphing.Tests {
 
       for (int i = 0; i < 100; i++) {
         int value;
+        Assert.That(_dequeue.Back, Is.EqualTo(i));
         _dequeue.PopBack(out value);
+        Assert.That(i, Is.EqualTo(value));
+      }
+    }
+
+    [Test]
+    public void PushBack() {
+      Debug.Log(_dequeue.ToDebugString());
+      for (int i = 0; i < 30; i++) {
+        _dequeue.PushBack(i);
+        Assert.That(_dequeue.Back, Is.EqualTo(i));
+        for (int j = 0; j <= i; j++) {
+          //Debug.Log((i - j) + " : " + _dequeue[j]);
+          //Assert.That(i - j, Is.EqualTo(_dequeue[j]));
+        }
+        Debug.Log(_dequeue.ToDebugString());
+      }
+
+      for (int i = 0; i < 30; i++) {
+        int value;
+        Assert.That(_dequeue.Front, Is.EqualTo(i));
+        _dequeue.PopFront(out value);
         Assert.That(i, Is.EqualTo(value));
       }
     }
