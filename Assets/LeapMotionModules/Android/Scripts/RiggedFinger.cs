fa73d69b5d5932113ae9d6db6845c718947c863f
jselstad <jselstad@leapmotion.com>
2016-06-28 13:46:50 -0700

    Moved Asset Dependencies to Android Folder

---
+++ Assets/LeapMotionModules/Android/Scripts/RiggedFinger.cs
@@ -0,0 +1,70 @@
+﻿/******************************************************************************\
+* Copyright (C) Leap Motion, Inc. 2011-2014.                                   *
+* Leap Motion proprietary. Licensed under Apache 2.0                           *
+* Available at http://www.apache.org/licenses/LICENSE-2.0.html                 *
+\******************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+using Leap;
+
+namespace Leap.Unity {
+  /**
+* Manages the orientation of the bones in a model rigged for skeletal animation.
+* 
+* The class expects that the graphics model bones corresponding to bones in the Leap Motion 
+* hand model are in the same order in the bones array.
+*/
+  public class RiggedFinger : FingerModel {
+
+    /** Allows the mesh to be stretched to align with finger joint positions
+     * Only set to true when mesh is not visible
+     */
+    public bool deformPosition = false;
+
+    public Vector3 modelFingerPointing = Vector3.forward;
+    public Vector3 modelPalmFacing = -Vector3.up;
+
+    public Quaternion Reorientation() {
+      return Quaternion.Inverse(Quaternion.LookRotation(modelFingerPointing, -modelPalmFacing));
+    }
+
+    private RiggedHand riggedHand;
+
+    /** Updates the bone rotations. */
+    public override void UpdateFinger() {
+      for (int i = 0; i < bones.Length; ++i) {
+        if (bones[i] != null) {
+          bones[i].rotation = GetBoneRotation(i) * Reorientation();
+          if (deformPosition) {
+            bones[i].position = GetJointPosition(i);
+          }
+        }
+      }
+    }
+    public void SetupRiggedFinger (bool useMetaCarpals) {
+      findBoneTransforms(useMetaCarpals);
+      modelFingerPointing = calulateModelFingerPointing();
+    }
+
+    private void findBoneTransforms(bool useMetaCarpals) {
+      if (!useMetaCarpals || fingerType == Finger.FingerType.TYPE_THUMB) {
+        bones[1] = transform;
+        bones[2] = transform.GetChild(0).transform;
+        bones[3] = transform.GetChild(0).transform.GetChild(0).transform;
+      }
+      else {
+        bones[0] = transform;
+        bones[1] = transform.GetChild(0).transform;
+        bones[2] = transform.GetChild(0).transform.GetChild(0).transform;
+        bones[3] = transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform;
+
+      }
+    }
+    private Vector3 calulateModelFingerPointing() {
+      Vector3 distance = transform.InverseTransformPoint(transform.position) -  transform.InverseTransformPoint(transform.GetChild(0).transform.position);
+      Vector3 zeroed = RiggedHand.CalculateZeroedVector(distance);
+      return zeroed;
+    }
+  } 
+}
---
+++ Assets/LeapMotionModules/Android/Scripts/RiggedHand.cs
@@ -0,0 +1,145 @@
+﻿/******************************************************************************\
+* Copyright (C) Leap Motion, Inc. 2011-2014.                                   *
+* Leap Motion proprietary. Licensed under Apache 2.0                           *
+* Available at http://www.apache.org/licenses/LICENSE-2.0.html                 *
+\******************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+using Leap;
+
+namespace Leap.Unity {
+  // Class to setup a rigged hand based on a model.
+  public class RiggedHand : HandModel {
+    public override ModelType HandModelType {
+      get {
+        return ModelType.Graphics;
+      }
+    }
+    public override bool SupportsEditorPersistence() {
+      return true;
+    }
+    [Tooltip("Hands are typically rigged in 3D packages with the palm transform near the wrist. Uncheck this is your model's palm transform is at the center of the palm similar to Leap's API drives")]
+    public bool ModelPalmAtLeapWrist = true;
+    public bool UseMetaCarpals;
+    public Vector3 modelFingerPointing = new Vector3(0, 0, 0);
+    public Vector3 modelPalmFacing = new Vector3(0, 0, 0);
+
+    public override void InitHand() {
+      UpdateHand();
+    }
+
+    public Quaternion Reorientation() {
+      return Quaternion.Inverse(Quaternion.LookRotation(modelFingerPointing, -modelPalmFacing));
+    }
+    public override void UpdateHand() {
+      if (palm != null) {
+        if (ModelPalmAtLeapWrist) {
+          palm.position = GetWristPosition();
+        }
+        else {
+          palm.position = GetPalmPosition();
+          if (wristJoint) {
+            wristJoint.position = GetWristPosition();
+          }
+        }
+        palm.rotation = GetRiggedPalmRotation() * Reorientation();
+      }
+
+      if (forearm != null) {
+        forearm.rotation = GetArmRotation() * Reorientation();
+      }
+
+      for (int i = 0; i < fingers.Length; ++i) {
+        if (fingers[i] != null) {
+          fingers[i].fingerType = (Finger.FingerType)i;
+          fingers[i].UpdateFinger();
+        }
+      }
+    }
+
+    //These versions of GetPalmRotation & CalculateRotation return the opposite vector compared to LeapUnityExtension.CalculateRotation
+    //This will be deprecated once LeapUnityExtension.CalculateRotation is flipped in the next release of LeapMotion Core Assets
+    public Quaternion GetRiggedPalmRotation() {
+      if (hand_ != null) {
+        LeapTransform trs = hand_.Basis;
+        return CalculateRotation(trs);
+      }
+      if (palm) {
+        return palm.rotation;
+      }
+      return Quaternion.identity;
+    }
+
+    private Quaternion CalculateRotation(this LeapTransform trs) {
+      Vector3 up = trs.yBasis.ToVector3();
+      Vector3 forward = trs.zBasis.ToVector3();
+      return Quaternion.LookRotation(forward, up);
+    }
+
+    [ContextMenu("Setup Rigged Hand")]
+    public void SetupRiggedHand() {
+      modelFingerPointing = new Vector3(0, 0, 0);
+      modelPalmFacing = new Vector3(0, 0, 0);
+      findFingerModels();
+      modelPalmFacing = calculateModelPalmFacing();
+      modelFingerPointing = calculateModelFingerPointing();
+      setFingerPalmFacing();
+    }
+
+    private void findFingerModels() {
+      RiggedFinger[] fingerModelList = GetComponentsInChildren<RiggedFinger>();
+      for (int i = 0; i < 5; i++) {
+        int fingersIndex = fingerModelList[i].fingerType.indexOf();
+        fingers[fingersIndex] = fingerModelList[i];
+        fingerModelList[i].SetupRiggedFinger(UseMetaCarpals);
+      }
+    }
+    private void setFingerPalmFacing() {
+      RiggedFinger[] fingerModelList = GetComponentsInChildren<RiggedFinger>();
+      for (int i = 0; i < 5; i++) {
+        int fingersIndex = fingerModelList[i].fingerType.indexOf();
+        fingers[fingersIndex] = fingerModelList[i];
+        fingerModelList[i].modelPalmFacing = modelPalmFacing;
+      }
+    }
+
+    private Vector3 calculateModelPalmFacing() {
+      Vector3 a = transform.InverseTransformPoint(palm.position);
+      Vector3 b = transform.InverseTransformPoint(fingers[2].transform.position);
+      Vector3 c = transform.InverseTransformPoint(fingers[1].transform.position);
+
+      Vector3 side1 = b - a;
+      Vector3 side2 = c - a;
+      Vector3 perpendicular;
+
+      if (Handedness == Chirality.Left) {
+        perpendicular = Vector3.Cross(side2, side1);
+      }
+      else perpendicular = Vector3.Cross(side1, side2);
+      Vector3 calculatedPalmFacing = CalculateZeroedVector(perpendicular);
+      return calculatedPalmFacing;
+    }
+
+    private Vector3 calculateModelFingerPointing() {
+      Vector3 distance = transform.InverseTransformPoint(fingers[2].transform.GetChild(0).transform.position) - transform.InverseTransformPoint(palm.position);
+      Vector3 calculatedFingerPointing = CalculateZeroedVector(distance);
+      return calculatedFingerPointing;
+    }
+
+    public static Vector3 CalculateZeroedVector(Vector3 vectorToZero) {
+      var zeroed = new Vector3();
+      float max = Mathf.Max(Mathf.Abs(vectorToZero.x), Mathf.Abs(vectorToZero.y), Mathf.Abs(vectorToZero.z));
+      if (Mathf.Abs(vectorToZero.x) == max) {
+        zeroed = (vectorToZero.x < 0) ? new Vector3(1, 0, 0) : new Vector3(-1, 0, 0);
+      }
+      if (Mathf.Abs(vectorToZero.y) == max) {
+        zeroed = (vectorToZero.y < 0) ? new Vector3(0, 1, 0) : new Vector3(0, -1, 0);
+      }
+      if (Mathf.Abs(vectorToZero.z) == max) {
+        zeroed = (vectorToZero.y < 0) ? new Vector3(0, 0, 1) : new Vector3(0, 0, -1);
+      }
+      return zeroed;
+    }
+  }
+}
