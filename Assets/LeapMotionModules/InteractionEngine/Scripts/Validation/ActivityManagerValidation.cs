8904ed1703a5791d64b559691f65378685977032
Amarcolina <Amarcoli@ucsc.edu>
2016-07-21 14:38:37 -0700

    Added activity manager validation

--- Assets/LeapMotionModules/InteractionEngine/Scripts/Validation/ActivityManagerValidation.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/Validation/ActivityManagerValidation.cs
@@ -1,13 +1,48 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using UnityEngine.Assertions;
 
 namespace Leap.Unity.Interaction {
 
   public partial class ActivityManager {
-    
+
     public void Validate() {
 
+      Assert.IsTrue(_overlapRadius > 0.0f,
+                    "Overlap radius must be positive and non-zero.");
+
+      Assert.IsTrue(_maxDepth >= 1,
+                    "Max depth must be positive and non-zero.");
+
+      foreach (var pair in _registeredBehaviours) {
+        var interactionObj = pair.Key;
+        var monitor = pair.Value;
+
+        Assert.IsTrue(interactionObj.IsRegisteredWithManager,
+                      "All registered behaviours must be reported as registered.");
+
+        Assert.IsTrue(interactionObj.isActiveAndEnabled,
+                      "All registered behaviours must be active and enabled.");
+
+        Assert.IsTrue(IsRegistered(interactionObj),
+                      "Registration status must match the reported status.");
+
+        Assert.AreEqual(monitor != null, IsActive(interactionObj),
+                        "Monitor must be non-null for objects reported as active.");
+
+        if (monitor != null) {
+          Assert.IsTrue(monitor.isActiveAndEnabled,
+                        "Monitor must be active and enabled.");
+        }
+      }
+
+      foreach (var activeObj in _activeBehaviours) {
+        Assert.IsTrue(_registeredBehaviours.ContainsKey(activeObj),
+                      "Active behaviour must be registered with this activity manager.");
+      }
+
+      foreach (var misbehavingObj in _misbehavingBehaviours) {
+        Assert.IsTrue(_registeredBehaviours.ContainsKey(misbehavingObj),
+                      "All misbehaving objects must be still considered registered.");
+      }
     }
-    
   }
 }
