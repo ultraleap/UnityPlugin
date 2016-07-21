3d2782129934888f04c7e0865c63b044ccc14a39
Adrian Johnston <ajohnston@leapmotion.com>
2016-07-21 16:44:58 -0700

    UnityTestTools set up.

---
+++ Assets/LeapMotionTests/InteractionEngine/Scripts/TestSpawnCube.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityTest;
+
+namespace Leap.Unity.InteractionTest {
+
+public class TestSpawnCube : UnityTest.TestComponent
+{
+
+  void Start() {
+    IntegrationTest.Pass( gameObject);
+  }
+
+
+
+
+
+
+};
+
+}; // namespace Leap.Unity.Interaction
--- Assets/UnityTestTools/IntegrationTestsFramework/TestRunner/TestRunnerConfigurator.cs
+++ Assets/UnityTestTools/IntegrationTestsFramework/TestRunner/TestRunnerConfigurator.cs
@@ -49,23 +49,23 @@ namespace UnityTest
             {
                 text = GetTextFromTempFile(previousScenes);
             }
-                
+
             if(text != null)
             {
                 var serializer = new System.Xml.Serialization.XmlSerializer(typeof(UnityEditor.EditorBuildSettingsScene[]));
                 using(var textReader = new StringReader(text))
                 {
-                    try 
+                    try
                     {
                         return (UnityEditor.EditorBuildSettingsScene[] )serializer.Deserialize(textReader);
                     }
-                    catch (System.Xml.XmlException e)
+                    catch (System.Xml.XmlException)
                     {
                         return null;
                     }
                 }
             }
-                
+
             return null;
         }
 #endif
@@ -154,7 +154,7 @@ namespace UnityTest
         public static List<string> GetAvailableNetworkIPs()
         {
 #if UTT_SOCKETS_SUPPORTED
-            if (!NetworkInterface.GetIsNetworkAvailable()) 
+            if (!NetworkInterface.GetIsNetworkAvailable())
                 return new List<String>{IPAddress.Loopback.ToString()};
 
             var ipList = new List<UnicastIPAddressInformation>();
