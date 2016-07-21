b9aeba0558108f68df0658969ef2855eea8a6a40
Amarcolina <Amarcoli@ucsc.edu>
2016-07-21 15:42:59 -0700

    Added validation logic for shadow state

--- Assets/LeapMotionModules/InteractionEngine/Scripts/Validation/InteractionBehaviourValidation.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/Validation/InteractionBehaviourValidation.cs
@@ -1,13 +1,31 @@
-﻿using UnityEngine;
+﻿using UnityEngine.Assertions;
 using System.Collections;
 
 namespace Leap.Unity.Interaction {
 
   public partial class InteractionBehaviour {
-    
+
     public override void Validate() {
       base.Validate();
 
+      bool shouldShadowStateMatch = true;
+      //If being grasped, actual state might be different than shadow state.
+      if (IsBeingGrasped) {
+        shouldShadowStateMatch = false;
+      }
+
+      //If in soft contact mode, actual state might be different than shadow state.
+      if (_contactMode == ContactMode.SOFT) {
+        shouldShadowStateMatch = false;
+      }
+
+      if (shouldShadowStateMatch) {
+        Assert.AreEqual(_rigidbody.isKinematic, _isKinematic,
+                        "Kinematic shadow state must match actual kinematic state.");
+
+        Assert.AreEqual(_rigidbody.useGravity, _useGravity,
+                        "Gravity shadow state must match actual gravity state.");
+      }
     }
 
   }
