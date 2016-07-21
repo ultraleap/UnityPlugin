4a8f5aefe02ac40ef7065e42080766cff1e76eda
Amarcolina <Amarcoli@ucsc.edu>
2016-07-21 14:03:29 -0700

    Added basic scene validation to manager

--- Assets/LeapMotionModules/InteractionEngine/Scripts/Validation/InteractionManagerValidation.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/Validation/InteractionManagerValidation.cs
@@ -1,12 +1,31 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using UnityEngine.Assertions;
+using System;
 
 namespace Leap.Unity.Interaction {
 
   public partial class InteractionManager {
 
     public void Validate() {
+      Assert.AreEqual(isActiveAndEnabled, _hasSceneBeenCreated,
+                      "Activation status should always be equal to scene creation status.");
 
+      Assert.AreEqual(isActiveAndEnabled, _scene.pScene != IntPtr.Zero,
+                      "Scene ptr should always be non-null when manager is active.");
+
+      assertNonNullWhenActive(_activityManager, "Activity Manager");
+      assertNonNullWhenActive(_shapeDescriptionPool, "Shape Description Pool");
+      assertNonNullWhenActive(_instanceHandleToBehaviour, "Instance Handle mapping");
+      assertNonNullWhenActive(_idToInteractionHand, "Id To Hand mapping");
+      assertNonNullWhenActive(_graspedBehaviours, "Grasped behaviour list");
+
+
+
+
+    }
+
+    private void assertNonNullWhenActive(object obj, string name) {
+      Assert.AreEqual(isActiveAndEnabled, obj != null,
+                      name + " should always be non-null when manager is active.");
     }
 
   }
