78f7b4b780a6bbd2c4962f2403a5f411ca49b984
Amarcolina <Amarcoli@ucsc.edu>
2016-07-21 15:36:35 -0700

    Added validation for the base object

--- Assets/LeapMotionModules/InteractionEngine/Scripts/Validation/InteractionBehaviourBaseValidation.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/Validation/InteractionBehaviourBaseValidation.cs
@@ -1,12 +1,28 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using UnityEngine.Assertions;
+using System;
 
 namespace Leap.Unity.Interaction {
 
   public partial class InteractionBehaviourBase {
-    
-    public virtual void Validate() {
 
+    public virtual void Validate() {
+      Assert.AreEqual(isActiveAndEnabled, _isRegisteredWithManager,
+                     "Must only active and enabled if registered with manager.");
+
+      if (_hasShapeDescriptionBeenCreated) {
+        Assert.IsTrue(_isRegisteredWithManager,
+                      "If shape description is enabled, must be registered with manager.");
+      }
+
+      if (_hasShapeInstanceHandle) {
+        Assert.IsTrue(_isRegisteredWithManager,
+                      "If has a shape instance, must be registered with manager.");
+      }
+
+      foreach (var untrackedId in _untrackedIds) {
+        Assert.IsTrue(_graspingIds.Contains(untrackedId),
+                      "All untracked ids must be considered grasping.");
+      }
     }
 
   }
