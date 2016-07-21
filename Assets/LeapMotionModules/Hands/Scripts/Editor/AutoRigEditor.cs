f477400d1dc18fcac26d72306d1e61858a5cd02f
protodeep <fox@brainstormer-project.net>
2016-07-21 15:49:03 -0700

    Adding instructions to inspector for LeapHandsAutoRig

--- Assets/LeapMotionModules/Hands/Scripts/Editor/AutoRigEditor.cs
+++ Assets/LeapMotionModules/Hands/Scripts/Editor/AutoRigEditor.cs
@@ -6,8 +6,13 @@ namespace Leap.Unity {
   [CustomEditor(typeof(LeapHandsAutoRig))]
   public class ObjectBuilderEditor : Editor {
     public override void OnInspectorGUI() {
-      DrawDefaultInspector();
+      GUILayout.Space(10);
+      GUILayout.Label("Leap Hand Auto Rigger", EditorStyles.largeLabel);
+      GUILayout.Label("Guidelines for creating FBX hand assets and instructions for auto rigging are at:", EditorStyles.wordWrappedLabel);
+      GUILayout.Label("https://developer.leapmotion.com/documentation/unity/unity/Unity_Hand_Assets.html", EditorStyles.wordWrappedLabel);
+      GUILayout.Space(10);
 
+      DrawDefaultInspector();
       LeapHandsAutoRig autoRigger = (LeapHandsAutoRig)target;
       if (GUILayout.Button("AutoRig")) {
         autoRigger.AutoRig();
