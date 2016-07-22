53dcc2d14eeeee01b01e9e611c8c9a012840c2b2
Amarcolina <Amarcoli@ucsc.edu>
2016-07-22 16:28:31 -0700

    Adding automatic test generation

--- Assets/LeapMotionTests/InteractionEngine/Scripts/SadisticTest.cs
+++ Assets/LeapMotionTests/InteractionEngine/Scripts/SadisticTest.cs
@@ -5,6 +5,8 @@ using System.Collections;
 namespace Leap.Unity.Interaction.Testing {
 
   public class SadisticTest : TestComponent {
-    public int sadisticTestIndex = 0;
+
+    [Header("Test Settings")]
+    public SadisticInteractionBehaviour.SadisticDef sadisticDefinition;
   }
 }
--- Assets/LeapMotionTests/InteractionEngine/Scripts/SadisticTestManager.cs
+++ Assets/LeapMotionTests/InteractionEngine/Scripts/SadisticTestManager.cs
@@ -1,16 +1,22 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using UnityTest;
+using System;
+using Leap.Unity.Attributes;
 
 namespace Leap.Unity.Interaction.Testing {
-  
+
   [ExecuteInEditMode]
   public class SadisticTestManager : TestComponent {
 
-    [SerializeField]
-    private SadisticInteractionBehaviour.SadisticAction _actions;
-
+    [Header("Sadistic Settings")]
+    [EnumFlags]
     [SerializeField]
     private SadisticInteractionBehaviour.Callback _callbacks;
+
+    [EnumFlags]
+    [SerializeField]
+    private SadisticInteractionBehaviour.SadisticAction _actions;
 
     private bool _update = false;
 
@@ -19,6 +25,7 @@ namespace Leap.Unity.Interaction.Testing {
 
       if (!Application.isPlaying) {
         _update = true;
+        gameObject.SetActive(true);
       }
     }
 
@@ -37,16 +44,34 @@ namespace Leap.Unity.Interaction.Testing {
         }
       }
 
-      /*
+      var actionType = typeof(SadisticInteractionBehaviour.SadisticAction);
+      var callbackType = typeof(SadisticInteractionBehaviour.Callback);
 
-      for (int i = 0; i < SadisticInteractionBehaviour.definitions.Count; i++) {
-        GameObject childObj = new GameObject("Sadistic Test " + i);
-        childObj.transform.parent = transform;
-        var childTest = childObj.AddComponent<SadisticTest>();
-        childTest.sadisticTestIndex = i;
-        childTest.timeout = timeout;
+      int[] actionValues = (int[])Enum.GetValues(actionType);
+      int[] callbackValues = (int[])Enum.GetValues(callbackType);
+      string[] actionNames = Enum.GetNames(actionType);
+      string[] callbackNames = Enum.GetNames(callbackType);
+
+      for (int i = actionValues.Length; i-- != 0;) {
+        var actionValue = actionValues[i];
+        if (((int)_actions & actionValue) == 0) continue;
+
+        for (int j = callbackValues.Length; j-- != 0;) {
+          var callbackValue = callbackValues[j];
+          if (((int)_callbacks & callbackValue) == 0) continue;
+
+          GameObject testObj = new GameObject(ObjectNames.NicifyVariableName(callbackNames[j]) +
+                                             " " +
+                                             ObjectNames.NicifyVariableName(actionNames[i]));
+          testObj.transform.parent = transform;
+
+          var test = testObj.AddComponent<SadisticTest>();
+          test.sadisticDefinition = new SadisticInteractionBehaviour.SadisticDef(
+                                           (SadisticInteractionBehaviour.Callback)callbackValue,
+                                           (SadisticInteractionBehaviour.SadisticAction)actionValue);
+          test.timeout = timeout;
+        }
       }
-      */
     }
   }
 }
