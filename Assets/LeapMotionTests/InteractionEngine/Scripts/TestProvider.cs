c58116e1957f340bcaa0fa9064766fec847e6cc4
Amarcolina <Amarcoli@ucsc.edu>
2016-07-22 17:00:07 -0700

    working on files

---
+++ Assets/LeapMotionTests/InteractionEngine/Scripts/TestProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Leap.Unity.Interaction.Testing {
+
+  public class TestProvider : LeapProvider {
+
+    public override Frame CurrentFixedFrame {
+      get {
+        return null;
+      }
+    }
+
+    public override Frame CurrentFrame {
+      get {
+        return null;
+      }
+    }
+
+    public override Image CurrentImage {
+      get {
+        return null;
+      }
+    }
+
+  }
+}
