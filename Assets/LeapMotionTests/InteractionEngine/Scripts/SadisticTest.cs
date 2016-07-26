94b40807fbd88d07358f72a67b3a8c8a57d5ac22
Amarcolina <Amarcoli@ucsc.edu>
2016-07-26 13:15:29 -0700

    Validate every frame

--- Assets/LeapMotionTests/InteractionEngine/Scripts/SadisticTest.cs
+++ Assets/LeapMotionTests/InteractionEngine/Scripts/SadisticTest.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 using UnityTest;
+using System;
 
 namespace Leap.Unity.Interaction.Testing {
 
@@ -9,15 +11,26 @@ namespace Leap.Unity.Interaction.Testing {
     public InteractionTestRecording recording;
     public SadisticInteractionBehaviour.SadisticDef sadisticDefinition;
 
+    private InteractionManager _manager;
     private InteractionTestProvider _provider;
 
     void Start() {
+      _manager = FindObjectOfType<InteractionManager>();
       _provider = FindObjectOfType<InteractionTestProvider>();
+
       _provider.recording = recording;
       _provider.Play();
+
+      Assert.raiseExceptions = true;
     }
 
     void Update() {
+      try {
+        _manager.Validate();
+      } catch (Exception e) {
+        IntegrationTest.Fail(e.Message);
+      }
+
       //If we reach the end of the recording, we pass!
       if (!_provider.IsPlaying) {
         IntegrationTest.Pass();
