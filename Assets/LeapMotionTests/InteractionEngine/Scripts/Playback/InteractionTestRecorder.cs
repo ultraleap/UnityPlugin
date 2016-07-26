9581c60034d0776e0aa42913630c5eb77a3f0144
Amarcolina <Amarcoli@ucsc.edu>
2016-07-26 13:22:02 -0700

    Trimmed recording

--- Assets/LeapMotionTests/InteractionEngine/Scripts/Playback/InteractionTestRecorder.cs
+++ Assets/LeapMotionTests/InteractionEngine/Scripts/Playback/InteractionTestRecorder.cs
@@ -4,10 +4,19 @@ using Leap.Unity.Playback;
 namespace Leap.Unity.Interaction.Testing {
 
   public class InteractionTestRecorder : PlaybackRecorder {
+
     public override void StartRecording() {
       var testRecording = ScriptableObject.CreateInstance<InteractionTestRecording>();
       testRecording.CaptureCurrentShapes();
       _currentRecording = testRecording;
+    }
+
+    public override Recording EndRecording() {
+      var testRecording = _currentRecording as InteractionTestRecording;
+      testRecording.TrimStartOfEmptyFrames(2);
+      testRecording.TrimEndOfEmptyFrames(2);
+
+      return base.EndRecording();
     }
   }
 }
