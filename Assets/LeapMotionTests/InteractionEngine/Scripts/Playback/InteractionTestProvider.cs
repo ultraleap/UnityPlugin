461c82cc9a679140a756caf0dab7d1d4900d40b7
Amarcolina <Amarcoli@ucsc.edu>
2016-07-26 13:03:19 -0700

    Updated provider and recorder

---
+++ Assets/LeapMotionTests/InteractionEngine/Scripts/Playback/InteractionTestProvider.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Leap.Unity.Playback;
+
+namespace Leap.Unity.Interaction.Testing {
+
+  public class InteractionTestProvider : PlaybackProvider {
+
+    [Header("Interaction Test Settings")]
+    [SerializeField]
+    protected Transform _testRoot;
+
+    public override Recording recording {
+      set {
+        clearShapes();
+        base.recording = value;
+        createShapes();
+      }
+    }
+
+    protected override void Start() {
+      base.Start();
+      createShapes();
+    }
+
+    protected void createShapes() {
+      if (base.recording is InteractionTestRecording) {
+        var interactionRecording = base.recording as InteractionTestRecording;
+        interactionRecording.CreateInitialShapes(_testRoot);
+      }
+    }
+
+    protected void clearShapes() {
+      var objs = _testRoot.GetComponentsInChildren<IInteractionBehaviour>();
+      foreach (var obj in objs) {
+        DestroyImmediate(obj.gameObject);
+      }
+    }
+  }
+}
---
+++ Assets/LeapMotionTests/InteractionEngine/Scripts/Playback/InteractionTestRecorder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Leap.Unity.Playback;
+
+namespace Leap.Unity.Interaction.Testing {
+
+  public class InteractionTestRecorder : PlaybackRecorder {
+    public override void StartRecording() {
+      var testRecording = ScriptableObject.CreateInstance<InteractionTestRecording>();
+      testRecording.CaptureCurrentShapes();
+      _currentRecording = testRecording;
+    }
+  }
+}
