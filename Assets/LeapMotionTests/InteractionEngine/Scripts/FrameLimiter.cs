bb82f906658a3c4dad14a939e681f0d4ee60dd83
Amarcolina <Amarcoli@ucsc.edu>
2016-07-26 17:04:14 -0700

    Cleaned up frame limiter

--- Assets/LeapMotionTests/InteractionEngine/Scripts/FrameLimiter.cs
+++ Assets/LeapMotionTests/InteractionEngine/Scripts/FrameLimiter.cs
@@ -3,12 +3,14 @@ using System.Collections;
 using System.Diagnostics;
 
 public class FrameLimiter : MonoBehaviour {
-  
+
+  public int frameRate = 75;
+
   IEnumerator Start() {
     Stopwatch stopwatch = new Stopwatch();
     stopwatch.Start();
     long lastTick = stopwatch.ElapsedTicks;
-    long ticks = Stopwatch.Frequency * 13 / 1000;
+    long ticks = Stopwatch.Frequency / frameRate;
     while (true) {
       yield return null;
       while((stopwatch.ElapsedTicks - lastTick) < ticks) { }
