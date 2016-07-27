5a28d3e9a06e0dfe849ec31b92f4609c9297008c
Amarcolina <Amarcoli@ucsc.edu>
2016-07-27 13:52:24 -0700

    Added conditional attribute to assert helper

--- Assets/LeapMotion/Scripts/Utils/AssertHelper.cs
+++ Assets/LeapMotion/Scripts/Utils/AssertHelper.cs
@@ -1,32 +1,38 @@
 ﻿using UnityEngine.Assertions;
 using System;
 using System.Linq;
+using System.Diagnostics;
 using System.Collections.Generic;
 
 public static class AssertHelper {
 
+  [Conditional("UNITY_ASSERTIONS")]
   public static void Implies(bool condition, bool result, string message = "") {
     if (condition) {
       Assert.IsTrue(result, message);
     }
   }
 
+  [Conditional("UNITY_ASSERTIONS")]
   public static void Implies(bool condition, Func<bool> result, string message = "") {
     if (condition) {
       Implies(condition, result(), message);
     }
   }
 
+  [Conditional("UNITY_ASSERTIONS")]
   public static void Implies(string conditionName, bool condition, string resultName, bool result) {
     Implies(condition, result, "When " + conditionName + " is true, " + resultName + " must always be true.");
   }
 
+  [Conditional("UNITY_ASSERTIONS")]
   public static void Implies(string conditionName, bool condition, string resultName, Func<bool> result) {
     if (condition) {
       Implies(conditionName, condition, resultName, result());
     }
   }
 
+  [Conditional("UNITY_ASSERTIONS")]
   public static void Contains<T>(T value, IEnumerable<T> collection, string message = "") {
     if (!collection.Contains(value)) {
       string result = "The value " + value + " was not found in the collection [";
