a590593fb1fc3046ab4c5ea2d04ffe424b603314
Amarcolina <Amarcoli@ucsc.edu>
2016-08-04 19:52:11 -0700

    Moved code because apparently it makes a difference

--- Assets/LeapMotion/Scripts/Utils/RuntimeGizmoDrawer.cs
+++ Assets/LeapMotion/Scripts/Utils/RuntimeGizmoDrawer.cs
@@ -14,6 +14,258 @@ namespace Leap.Unity.RuntimeGizmos {
   public interface IRuntimeGizmoDrawer {
     void OnDrawRuntimeGizmos();
   }
+  
+  [ExecuteInEditMode]
+  public class RuntimeGizmoDrawer : MonoBehaviour {
+    public const int CIRCLE_RESOLUTION = 32;
+
+    [Tooltip("Should the gizmos be visible in the game view.")]
+    [SerializeField]
+    protected bool _displayInGameView = true;
+
+    [Tooltip("Should the gizmos be visible in a build.")]
+    [SerializeField]
+    protected bool _enabledForBuild = true;
+
+    [Tooltip("The mesh to use for the filled sphere gizmo.")]
+    [SerializeField]
+    protected Mesh _sphereMesh;
+
+    [Tooltip("The shader to use for rendering wire gizmos.")]
+    [SerializeField]
+    protected Shader _wireShader;
+
+    [Tooltip("The shader to use for rendering filled gizmos.")]
+    [SerializeField]
+    protected Shader _filledShader;
+
+    protected Material _wireMaterial, _filledMaterial;
+    protected Mesh _cubeMesh, _wireCubeMesh, _wireSphereMesh;
+
+    protected void onPostRender(Camera camera) {
+#if UNITY_EDITOR
+      if (camera.gameObject.name == "PreRenderCamera") {
+        return;
+      }
+
+      bool isSceneCamera = camera.gameObject.hideFlags == HideFlags.HideAndDontSave;
+      if (!isSceneCamera && !_displayInGameView) {
+        return;
+      }
+#endif
+
+      RGizmos.DrawAllGizmosToScreen(_wireMaterial, _filledMaterial);
+    }
+
+    protected virtual void OnValidate() {
+      if (_wireMaterial != null) {
+        _wireMaterial.shader = _wireShader;
+      }
+      if (_filledMaterial != null) {
+        _filledMaterial.shader = _filledShader;
+      }
+    }
+
+    protected virtual void OnEnable() {
+#if !UNITY_EDITOR
+      if (_enabledForBuild) {
+        enabled = false;
+      }
+#endif
+
+      generateMeshes();
+      assignMeshes();
+
+      Camera.onPostRender -= onPostRender;
+      Camera.onPostRender += onPostRender;
+
+      if (Application.isPlaying) {
+        StartCoroutine(clearGizmoCoroutine());
+      }
+    }
+
+    protected virtual void OnDisable() {
+      Camera.onPostRender -= onPostRender;
+    }
+
+    private List<GameObject> _objList = new List<GameObject>();
+    private List<IRuntimeGizmoDrawer> _gizmoList = new List<IRuntimeGizmoDrawer>();
+    protected virtual void Update() {
+      if (_wireMaterial == null) {
+        if (_wireShader == null) {
+          return;
+        }
+        _wireMaterial = new Material(_wireShader);
+        _wireMaterial.name = "Runtime Gizmos Wire Material";
+        _wireMaterial.hideFlags = HideFlags.HideAndDontSave;
+      }
+
+      if (_filledMaterial == null) {
+        if (_filledShader == null) {
+          return;
+        }
+        _filledMaterial = new Material(_filledShader);
+        _filledMaterial.name = "Runtime Gizmos Filled Material";
+        _filledMaterial.hideFlags = HideFlags.HideAndDontSave;
+      }
+
+#if UNITY_EDITOR
+      //If the application is playing, gizmos are cleared at the end of frame instead
+      if (!Application.isPlaying) {
+        RGizmos.ClearAllGizmos();
+      }
+#endif
+
+      Scene scene = SceneManager.GetActiveScene();
+      scene.GetRootGameObjects(_objList);
+      for (int i = 0; i < _objList.Count; i++) {
+        GameObject obj = _objList[i];
+        obj.GetComponentsInChildren(false, _gizmoList);
+        for (int j = 0; j < _gizmoList.Count; j++) {
+          Transform componentTransform = (_gizmoList[j] as Component).transform;
+
+          bool isDisabled = false;
+          do {
+            var toggle = componentTransform.GetComponentInParent<RuntimeGizmoToggle>();
+            if (toggle == null) {
+              break;
+            }
+
+            if (!toggle.enabled) {
+              isDisabled = true;
+              break;
+            }
+
+            componentTransform = componentTransform.parent;
+          } while (componentTransform != null);
+
+          if (isDisabled) {
+            continue;
+          }
+
+          assignMeshes();
+
+          if (RGizmos.matrix != Matrix4x4.identity) {
+            RGizmos.matrix = Matrix4x4.identity;
+          }
+          _gizmoList[j].OnDrawRuntimeGizmos();
+        }
+      }
+    }
+
+    private IEnumerator clearGizmoCoroutine() {
+      WaitForEndOfFrame endOfFrameWaiter = new WaitForEndOfFrame();
+      while (true) {
+        yield return endOfFrameWaiter;
+        RGizmos.ClearAllGizmos();
+      }
+    }
+
+    private void assignMeshes() {
+      RGizmos.sphereMesh = _sphereMesh;
+      RGizmos.cubeMesh = _cubeMesh;
+      RGizmos.wireSphereMesh = _wireSphereMesh;
+      RGizmos.wireCubeMesh = _wireCubeMesh;
+    }
+
+    private void generateMeshes() {
+      _cubeMesh = new Mesh();
+      _cubeMesh.name = "RuntimeGizmoCube";
+      _cubeMesh.hideFlags = HideFlags.HideAndDontSave;
+
+      List<Vector3> verts = new List<Vector3>();
+      List<int> indexes = new List<int>();
+
+      addQuad(verts, indexes, Vector3.forward, -Vector3.right, Vector3.up);
+      addQuad(verts, indexes, -Vector3.forward, Vector3.right, Vector3.up);
+      addQuad(verts, indexes, Vector3.right, -Vector3.up, Vector3.forward);
+      addQuad(verts, indexes, -Vector3.right, Vector3.up, Vector3.forward);
+      addQuad(verts, indexes, Vector3.up, -Vector3.forward, Vector3.right);
+      addQuad(verts, indexes, -Vector3.up, Vector3.forward, Vector3.right);
+
+      _cubeMesh.SetVertices(verts);
+      _cubeMesh.SetIndices(indexes.ToArray(), MeshTopology.Quads, 0);
+      _cubeMesh.RecalculateNormals();
+      _cubeMesh.RecalculateBounds();
+      _cubeMesh.UploadMeshData(true);
+
+      _wireCubeMesh = new Mesh();
+      _wireCubeMesh.name = "RuntimeWireCubeMesh";
+      _wireCubeMesh.hideFlags = HideFlags.HideAndDontSave;
+
+      verts.Clear();
+      indexes.Clear();
+
+      verts.Add(0.5f * new Vector3(1, 1, 1));
+      verts.Add(0.5f * new Vector3(1, 1, -1));
+      verts.Add(0.5f * new Vector3(1, -1, 1));
+      verts.Add(0.5f * new Vector3(1, -1, -1));
+      verts.Add(0.5f * new Vector3(-1, 1, 1));
+      verts.Add(0.5f * new Vector3(-1, 1, -1));
+      verts.Add(0.5f * new Vector3(-1, -1, 1));
+      verts.Add(0.5f * new Vector3(-1, -1, -1));
+
+      addCorner(indexes, 0, 1, 2, 4);
+      addCorner(indexes, 3, 1, 2, 7);
+      addCorner(indexes, 5, 1, 4, 7);
+      addCorner(indexes, 6, 2, 4, 7);
+
+      _wireCubeMesh.SetVertices(verts);
+      _wireCubeMesh.SetIndices(indexes.ToArray(), MeshTopology.Lines, 0);
+      _wireCubeMesh.RecalculateBounds();
+      _wireCubeMesh.UploadMeshData(true);
+
+      _wireSphereMesh = new Mesh();
+      _wireSphereMesh.name = "RuntimeWireSphereMesh";
+      _wireSphereMesh.hideFlags = HideFlags.HideAndDontSave;
+
+      verts.Clear();
+      indexes.Clear();
+
+      int totalVerts = CIRCLE_RESOLUTION * 3;
+      for (int i = 0; i < CIRCLE_RESOLUTION; i++) {
+        float angle = Mathf.PI * 2 * i / CIRCLE_RESOLUTION;
+        float dx = 0.5f * Mathf.Cos(angle);
+        float dy = 0.5f * Mathf.Sin(angle);
+
+        indexes.Add((i * 3 + 0) % totalVerts);
+        indexes.Add((i * 3 + 3) % totalVerts);
+
+        indexes.Add((i * 3 + 1) % totalVerts);
+        indexes.Add((i * 3 + 4) % totalVerts);
+
+        indexes.Add((i * 3 + 2) % totalVerts);
+        indexes.Add((i * 3 + 5) % totalVerts);
+
+        verts.Add(new Vector3(dx, dy, 0));
+        verts.Add(new Vector3(0, dx, dy));
+        verts.Add(new Vector3(dx, 0, dy));
+      }
+
+      _wireSphereMesh.SetVertices(verts);
+      _wireSphereMesh.SetIndices(indexes.ToArray(), MeshTopology.Lines, 0);
+      _wireSphereMesh.RecalculateBounds();
+      _wireSphereMesh.UploadMeshData(true);
+    }
+
+    private void addQuad(List<Vector3> verts, List<int> indexes, Vector3 normal, Vector3 axis1, Vector3 axis2) {
+      indexes.Add(verts.Count + 0);
+      indexes.Add(verts.Count + 1);
+      indexes.Add(verts.Count + 2);
+      indexes.Add(verts.Count + 3);
+
+      verts.Add(0.5f * (normal + axis1 + axis2));
+      verts.Add(0.5f * (normal + axis1 - axis2));
+      verts.Add(0.5f * (normal - axis1 - axis2));
+      verts.Add(0.5f * (normal - axis1 + axis2));
+    }
+
+    private void addCorner(List<int> indexes, int a, int b, int c, int d) {
+      indexes.Add(a); indexes.Add(b);
+      indexes.Add(a); indexes.Add(c);
+      indexes.Add(a); indexes.Add(d);
+    }
+  }
 
   public static class RGizmos {
     private static List<OperationType> _operations = new List<OperationType>();
@@ -301,258 +553,6 @@ namespace Leap.Unity.RuntimeGizmos {
         this.a = a;
         this.b = b;
       }
-    }
-  }
-
-  [ExecuteInEditMode]
-  public class RuntimeGizmoDrawer : MonoBehaviour {
-    public const int CIRCLE_RESOLUTION = 32;
-
-    [Tooltip("Should the gizmos be visible in the game view.")]
-    [SerializeField]
-    protected bool _displayInGameView = true;
-
-    [Tooltip("Should the gizmos be visible in a build.")]
-    [SerializeField]
-    protected bool _enabledForBuild = true;
-
-    [Tooltip("The mesh to use for the filled sphere gizmo.")]
-    [SerializeField]
-    protected Mesh _sphereMesh;
-
-    [Tooltip("The shader to use for rendering wire gizmos.")]
-    [SerializeField]
-    protected Shader _wireShader;
-
-    [Tooltip("The shader to use for rendering filled gizmos.")]
-    [SerializeField]
-    protected Shader _filledShader;
-
-    protected Material _wireMaterial, _filledMaterial;
-    protected Mesh _cubeMesh, _wireCubeMesh, _wireSphereMesh;
-
-    protected void onPostRender(Camera camera) {
-#if UNITY_EDITOR
-      if (camera.gameObject.name == "PreRenderCamera") {
-        return;
-      }
-
-      bool isSceneCamera = camera.gameObject.hideFlags == HideFlags.HideAndDontSave;
-      if (!isSceneCamera && !_displayInGameView) {
-        return;
-      }
-#endif
-
-      RGizmos.DrawAllGizmosToScreen(_wireMaterial, _filledMaterial);
-    }
-
-    protected virtual void OnValidate() {
-      if (_wireMaterial != null) {
-        _wireMaterial.shader = _wireShader;
-      }
-      if (_filledMaterial != null) {
-        _filledMaterial.shader = _filledShader;
-      }
-    }
-
-    protected virtual void OnEnable() {
-#if !UNITY_EDITOR
-      if (_enabledForBuild) {
-        enabled = false;
-      }
-#endif
-
-      generateMeshes();
-      assignMeshes();
-
-      Camera.onPostRender -= onPostRender;
-      Camera.onPostRender += onPostRender;
-
-      if (Application.isPlaying) {
-        StartCoroutine(clearGizmoCoroutine());
-      }
-    }
-
-    protected virtual void OnDisable() {
-      Camera.onPostRender -= onPostRender;
-    }
-
-    private List<GameObject> _objList = new List<GameObject>();
-    private List<IRuntimeGizmoDrawer> _gizmoList = new List<IRuntimeGizmoDrawer>();
-    protected virtual void Update() {
-      if (_wireMaterial == null) {
-        if (_wireShader == null) {
-          return;
-        }
-        _wireMaterial = new Material(_wireShader);
-        _wireMaterial.name = "Runtime Gizmos Wire Material";
-        _wireMaterial.hideFlags = HideFlags.HideAndDontSave;
-      }
-
-      if (_filledMaterial == null) {
-        if (_filledShader == null) {
-          return;
-        }
-        _filledMaterial = new Material(_filledShader);
-        _filledMaterial.name = "Runtime Gizmos Filled Material";
-        _filledMaterial.hideFlags = HideFlags.HideAndDontSave;
-      }
-
-#if UNITY_EDITOR
-      //If the application is playing, gizmos are cleared at the end of frame instead
-      if (!Application.isPlaying) {
-        RGizmos.ClearAllGizmos();
-      }
-#endif
-
-      Scene scene = SceneManager.GetActiveScene();
-      scene.GetRootGameObjects(_objList);
-      for (int i = 0; i < _objList.Count; i++) {
-        GameObject obj = _objList[i];
-        obj.GetComponentsInChildren(false, _gizmoList);
-        for (int j = 0; j < _gizmoList.Count; j++) {
-          Transform componentTransform = (_gizmoList[j] as Component).transform;
-
-          bool isDisabled = false;
-          do {
-            var toggle = componentTransform.GetComponentInParent<RuntimeGizmoToggle>();
-            if (toggle == null) {
-              break;
-            }
-
-            if (!toggle.enabled) {
-              isDisabled = true;
-              break;
-            }
-
-            componentTransform = componentTransform.parent;
-          } while (componentTransform != null);
-
-          if (isDisabled) {
-            continue;
-          }
-
-          assignMeshes();
-
-          if (RGizmos.matrix != Matrix4x4.identity) {
-            RGizmos.matrix = Matrix4x4.identity;
-          }
-          _gizmoList[j].OnDrawRuntimeGizmos();
-        }
-      }
-    }
-
-    private IEnumerator clearGizmoCoroutine() {
-      WaitForEndOfFrame endOfFrameWaiter = new WaitForEndOfFrame();
-      while (true) {
-        yield return endOfFrameWaiter;
-        RGizmos.ClearAllGizmos();
-      }
-    }
-
-    private void assignMeshes() {
-      RGizmos.sphereMesh = _sphereMesh;
-      RGizmos.cubeMesh = _cubeMesh;
-      RGizmos.wireSphereMesh = _wireSphereMesh;
-      RGizmos.wireCubeMesh = _wireCubeMesh;
-    }
-
-    private void generateMeshes() {
-      _cubeMesh = new Mesh();
-      _cubeMesh.name = "RuntimeGizmoCube";
-      _cubeMesh.hideFlags = HideFlags.HideAndDontSave;
-
-      List<Vector3> verts = new List<Vector3>();
-      List<int> indexes = new List<int>();
-
-      addQuad(verts, indexes, Vector3.forward, -Vector3.right, Vector3.up);
-      addQuad(verts, indexes, -Vector3.forward, Vector3.right, Vector3.up);
-      addQuad(verts, indexes, Vector3.right, -Vector3.up, Vector3.forward);
-      addQuad(verts, indexes, -Vector3.right, Vector3.up, Vector3.forward);
-      addQuad(verts, indexes, Vector3.up, -Vector3.forward, Vector3.right);
-      addQuad(verts, indexes, -Vector3.up, Vector3.forward, Vector3.right);
-
-      _cubeMesh.SetVertices(verts);
-      _cubeMesh.SetIndices(indexes.ToArray(), MeshTopology.Quads, 0);
-      _cubeMesh.RecalculateNormals();
-      _cubeMesh.RecalculateBounds();
-      _cubeMesh.UploadMeshData(true);
-
-      _wireCubeMesh = new Mesh();
-      _wireCubeMesh.name = "RuntimeWireCubeMesh";
-      _wireCubeMesh.hideFlags = HideFlags.HideAndDontSave;
-
-      verts.Clear();
-      indexes.Clear();
-
-      verts.Add(0.5f * new Vector3(1, 1, 1));
-      verts.Add(0.5f * new Vector3(1, 1, -1));
-      verts.Add(0.5f * new Vector3(1, -1, 1));
-      verts.Add(0.5f * new Vector3(1, -1, -1));
-      verts.Add(0.5f * new Vector3(-1, 1, 1));
-      verts.Add(0.5f * new Vector3(-1, 1, -1));
-      verts.Add(0.5f * new Vector3(-1, -1, 1));
-      verts.Add(0.5f * new Vector3(-1, -1, -1));
-
-      addCorner(indexes, 0, 1, 2, 4);
-      addCorner(indexes, 3, 1, 2, 7);
-      addCorner(indexes, 5, 1, 4, 7);
-      addCorner(indexes, 6, 2, 4, 7);
-
-      _wireCubeMesh.SetVertices(verts);
-      _wireCubeMesh.SetIndices(indexes.ToArray(), MeshTopology.Lines, 0);
-      _wireCubeMesh.RecalculateBounds();
-      _wireCubeMesh.UploadMeshData(true);
-
-      _wireSphereMesh = new Mesh();
-      _wireSphereMesh.name = "RuntimeWireSphereMesh";
-      _wireSphereMesh.hideFlags = HideFlags.HideAndDontSave;
-
-      verts.Clear();
-      indexes.Clear();
-
-      int totalVerts = CIRCLE_RESOLUTION * 3;
-      for (int i = 0; i < CIRCLE_RESOLUTION; i++) {
-        float angle = Mathf.PI * 2 * i / CIRCLE_RESOLUTION;
-        float dx = 0.5f * Mathf.Cos(angle);
-        float dy = 0.5f * Mathf.Sin(angle);
-
-        indexes.Add((i * 3 + 0) % totalVerts);
-        indexes.Add((i * 3 + 3) % totalVerts);
-
-        indexes.Add((i * 3 + 1) % totalVerts);
-        indexes.Add((i * 3 + 4) % totalVerts);
-
-        indexes.Add((i * 3 + 2) % totalVerts);
-        indexes.Add((i * 3 + 5) % totalVerts);
-
-        verts.Add(new Vector3(dx, dy, 0));
-        verts.Add(new Vector3(0, dx, dy));
-        verts.Add(new Vector3(dx, 0, dy));
-      }
-
-      _wireSphereMesh.SetVertices(verts);
-      _wireSphereMesh.SetIndices(indexes.ToArray(), MeshTopology.Lines, 0);
-      _wireSphereMesh.RecalculateBounds();
-      _wireSphereMesh.UploadMeshData(true);
-    }
-
-    private void addQuad(List<Vector3> verts, List<int> indexes, Vector3 normal, Vector3 axis1, Vector3 axis2) {
-      indexes.Add(verts.Count + 0);
-      indexes.Add(verts.Count + 1);
-      indexes.Add(verts.Count + 2);
-      indexes.Add(verts.Count + 3);
-
-      verts.Add(0.5f * (normal + axis1 + axis2));
-      verts.Add(0.5f * (normal + axis1 - axis2));
-      verts.Add(0.5f * (normal - axis1 - axis2));
-      verts.Add(0.5f * (normal - axis1 + axis2));
-    }
-
-    private void addCorner(List<int> indexes, int a, int b, int c, int d) {
-      indexes.Add(a); indexes.Add(b);
-      indexes.Add(a); indexes.Add(c);
-      indexes.Add(a); indexes.Add(d);
     }
   }
 }
