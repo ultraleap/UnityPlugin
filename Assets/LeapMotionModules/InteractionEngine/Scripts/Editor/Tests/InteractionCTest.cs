1e520ec4631f8243b2f5e77d98cd346267e11a07
Amarcolina <Amarcoli@ucsc.edu>
2016-08-05 15:12:33 -0700

    Removed unused vars

--- Assets/LeapMotionModules/InteractionEngine/Scripts/Editor/Tests/InteractionCTest.cs
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/Editor/Tests/InteractionCTest.cs
@@ -72,9 +72,6 @@ namespace Leap.Unity.Interaction.Tests {
     [Test]
     public void GetDebugLines() {
       InteractionC.EnableDebugFlags(ref _scene, (uint)DebugFlags.Lines);
-
-      uint lineCount;
-      IntPtr lineBuffer;
       InteractionC.GetDebugLines(ref _scene, new List<INTERACTION_DEBUG_LINE>());
     }
 
