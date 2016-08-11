5e7d2a199bd9b9388a82f02e68ff61b6272b703d
Joe Ward <jward@leapmotion.com>
2016-08-11 16:19:59 -0700

    Added custom editor for Transition component.

---
+++ Assets/LeapMotionModules/Attachments/Editor/TransitionEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Leap.Unity;
+using Leap.Unity.Attachments;
+
+[CustomEditor (typeof(Transition))]
+public class TransitionEditor : CustomEditorBase {
+  protected override void OnEnable() {
+    base.OnEnable();
+
+    specifyConditionalDrawing("AnimatePosition",
+                              "OnPosition",
+                              "OffPosition",
+                              "PositionCurve");
+    specifyConditionalDrawing("AnimateRotation",
+                          "OnRotation",
+                          "OffRotation",
+                          "RotationCurve");
+    specifyConditionalDrawing("AnimateScale",
+                              "OnScale",
+                              "OffScale",
+                              "ScaleCurve");
+    specifyConditionalDrawing("AnimateColor",
+                              "TransitionColor",
+                              "ColorShaderPropertyName",
+                              "ColorCurve");
+  }
+
+}
