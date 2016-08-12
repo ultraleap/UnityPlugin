a3efc8a85d5558abcdcac7fa0c456ac1e860dc54
Amarcolina <Amarcoli@ucsc.edu>
2016-08-12 11:46:00 -0700

    Added behaviour event class

---
+++ Assets/LeapMotionModules/InteractionEngine/Scripts/InteractionBehaviours/InteractionBehaviourEvents.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+namespace Leap.Unity.Interaction {
+
+  [RequireComponent(typeof(InteractionBehaviourBase))]
+  public class InteractionBehaviourEvents : MonoBehaviour {
+
+    [Serializable]
+    public class HandEvent : UnityEvent<Hand> { }
+
+    public UnityEvent onGraspBegin;
+    public UnityEvent onGraspEnd;
+    public HandEvent onHandGrasp;
+    public HandEvent onHandRelease;
+
+    private InteractionBehaviourBase _interactionBehaviour;
+
+    void Awake() {
+      _interactionBehaviour = GetComponent<InteractionBehaviourBase>();
+
+      if (_interactionBehaviour != null) {
+        _interactionBehaviour.OnGraspBeginEvent += onGraspBegin.Invoke;
+        _interactionBehaviour.OnGraspEndEvent += onGraspEnd.Invoke;
+        _interactionBehaviour.OnHandGraspedEvent += onHandGrasp.Invoke;
+        _interactionBehaviour.OnHandReleasedEvent += onHandRelease.Invoke;
+      }
+    }
+
+    void OnDestroy() {
+      if (_interactionBehaviour != null) {
+        _interactionBehaviour.OnGraspBeginEvent -= onGraspBegin.Invoke;
+        _interactionBehaviour.OnGraspEndEvent -= onGraspEnd.Invoke;
+        _interactionBehaviour.OnHandGraspedEvent -= onHandGrasp.Invoke;
+        _interactionBehaviour.OnHandReleasedEvent -= onHandRelease.Invoke;
+      }
+    }
+  }
+}
