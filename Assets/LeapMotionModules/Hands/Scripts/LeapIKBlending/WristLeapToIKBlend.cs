8fe5e3fbe9b3a92daae7b667a4afec97e8e96996
protodeep <fox@brainstormer-project.net>
2016-08-26 15:50:25 -0700

    Tuning elbow drops

--- Assets/LeapMotionModules/Hands/Scripts/LeapIKBlending/WristLeapToIKBlend.cs
+++ Assets/LeapMotionModules/Hands/Scripts/LeapIKBlending/WristLeapToIKBlend.cs
@@ -48,14 +48,11 @@ namespace Leap.Unity {
     public AnimationCurve DropCurveZ;
     public float ArmDropDuration = .25f;
 
-
     protected override void Awake() {
       base.Awake();
       animator = transform.root.GetComponentInChildren<Animator>();
       handModel = transform.GetComponent<HandModel>();
       palm = GetComponent<HandModel>().palm;
-      startingPalmPosition = palm.localPosition;
-      startingOrientation = palm.localRotation;
       Neck = animator.GetBoneTransform(HumanBodyBones.Neck);
       if(Handedness == Chirality.Left){
         Scapula = animator.GetBoneTransform(HumanBodyBones.LeftShoulder);
@@ -132,7 +129,6 @@ namespace Leap.Unity {
           animator.SetIKHintPositionWeight(AvatarIKHint.LeftElbow, positionIKWeight);
           animator.SetIKHintPosition(AvatarIKHint.LeftElbow, ElbowIKTarget.position);
 
-          //Debug.Log(("ElbowMarker_L.position.y: " + ElbowMarker_L.position.y + " - Shoulder_L.position.y: " + Shoulder_L.position.y));
           if (ElbowMarker.position.y > Scapula.position.y) {
             shoulder_up_target_weight = (ElbowMarker.position.y - Shoulder.position.y) * 10f;
             animator.SetFloat("shoulder_up_left", shoulder_up_weight);
@@ -141,8 +137,8 @@ namespace Leap.Unity {
             shoulder_up_target_weight = 0.0f;
             animator.SetFloat("shoulder_up_left", shoulder_up_weight);
           }
+
           if (ElbowMarker.position.z > Scapula.position.z) {
-            //Debug.Log("Left Forward: " + shoulder_forward_target_weight);
             shoulder_forward_target_weight = (ElbowMarker.position.z - Shoulder.position.z) * 10f;
             animator.SetFloat("shoulder_forward_left", shoulder_forward_weight);
           }
@@ -156,7 +152,7 @@ namespace Leap.Unity {
           animator.SetIKPosition(AvatarIKGoal.LeftHand, UntrackedIKPosition);
           animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, rotationIKWeight);
           animator.SetIKRotation(AvatarIKGoal.LeftHand, PalmRotationAtLateUpdate);
-          animator.SetIKHintPositionWeight(AvatarIKHint.LeftElbow, 0);
+          animator.SetIKHintPositionWeight(AvatarIKHint.LeftElbow, 1);
           animator.SetIKHintPosition(AvatarIKHint.LeftElbow, ElbowIKTarget.position);
         }
       }
@@ -169,7 +165,6 @@ namespace Leap.Unity {
           animator.SetIKHintPositionWeight(AvatarIKHint.RightElbow, positionIKWeight);
           animator.SetIKHintPosition(AvatarIKHint.RightElbow, ElbowIKTarget.position);
           if (ElbowMarker.position.y > Scapula.position.y + 0f) {
-            //.Log("Right Above");
             shoulder_up_target_weight = (ElbowMarker.position.y - Shoulder.position.y) * 10f;
             animator.SetFloat("shoulder_up_right", shoulder_up_weight);
           }
@@ -177,9 +172,9 @@ namespace Leap.Unity {
             shoulder_up_target_weight = 0.0f;
             animator.SetFloat("shoulder_up_right", shoulder_up_weight);
           }
+
           if (ElbowMarker.position.z > Scapula.position.z) {
             shoulder_forward_target_weight = (ElbowMarker.position.z - Shoulder.position.z) * 10f;
-            //Debug.Log("Right Forward: " + shoulder_forward_target_weight);
             animator.SetFloat("shoulder_forward_right", shoulder_forward_weight);
           }
           else {
@@ -192,7 +187,7 @@ namespace Leap.Unity {
           animator.SetIKPosition(AvatarIKGoal.RightHand, UntrackedIKPosition);
           animator.SetIKRotationWeight(AvatarIKGoal.RightHand, rotationIKWeight);
           animator.SetIKRotation(AvatarIKGoal.RightHand, PalmRotationAtLateUpdate);
-          animator.SetIKHintPositionWeight(AvatarIKHint.RightElbow, 0);
+          animator.SetIKHintPositionWeight(AvatarIKHint.RightElbow, 1);
           animator.SetIKHintPosition(AvatarIKHint.RightElbow, ElbowIKTarget.position);
         }
       }
