7502f74c6faca4ef44a2960665295f27f0ca95e7
Amarcolina <Amarcoli@ucsc.edu>
2016-08-29 11:37:02 -0700

    Upgraded disableif logic

--- Assets/LeapMotion/Scripts/Attributes/DisableIf.cs
+++ Assets/LeapMotion/Scripts/Attributes/DisableIf.cs
@@ -1,22 +1,48 @@
-﻿#if UNITY_EDITOR
+﻿using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
 #endif
+using System;
 
 namespace Leap.Unity.Attributes {
 
   public class DisableIf : CombinablePropertyAttribute, IPropertyDisabler {
     public readonly string propertyName;
-    public readonly bool equalTo;
+    public readonly object testValue;
+    public readonly bool disableResult;
 
-    public DisableIf(string propertyName, bool equalTo = true) {
+    public DisableIf(string propertyName, object equalTo = null, object notEqualTo = null) {
       this.propertyName = propertyName;
-      this.equalTo = equalTo;
+
+      if ((equalTo != null) == (notEqualTo != null)) {
+        throw new ArgumentException("Must specify exactly one of 'equalTo' or 'notEqualTo'.");
+      }
+
+      if (equalTo != null) {
+        testValue = equalTo;
+        disableResult = true;
+      } else if (notEqualTo != null) {
+        testValue = notEqualTo;
+        disableResult = false;
+      }
+
+      if (!(testValue is bool) && !(testValue is Enum)) {
+        throw new ArgumentException("Only values of bool or Enum are allowed in comparisons using DisableIf.");
+      }
     }
 
 #if UNITY_EDITOR
     public bool ShouldDisable(SerializedProperty property) {
       SerializedProperty prop = property.serializedObject.FindProperty(propertyName);
-      return prop.boolValue == equalTo;
+
+      if (prop.propertyType == SerializedPropertyType.Boolean) {
+        return (prop.boolValue == (bool)testValue) == disableResult;
+      } else if (prop.propertyType == SerializedPropertyType.Enum) {
+        return (prop.intValue == (int)testValue) == disableResult;
+      } else {
+        Debug.LogError("Can only conditionally disable based on boolean or enum types.");
+        return false;
+      }
     }
 #endif
   }
