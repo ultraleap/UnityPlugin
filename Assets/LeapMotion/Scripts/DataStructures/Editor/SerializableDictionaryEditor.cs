8ea955afd53bb08173540ced9a6304ece7542d54
Amarcolina <Amarcoli@ucsc.edu>
2016-09-12 10:59:00 -0700

    Added namespace

--- Assets/LeapMotion/Scripts/DataStructures/Editor/SerializableDictionaryEditor.cs
+++ Assets/LeapMotion/Scripts/DataStructures/Editor/SerializableDictionaryEditor.cs
@@ -3,206 +3,209 @@ using UnityEditor;
 using UnityEditorInternal;
 using System.Collections.Generic;
 
-[CustomPropertyDrawer(typeof(SDictionary))]
-public class SerializableDictionaryEditor : PropertyDrawer {
+namespace Leap.Unity {
 
-  private ReorderableList _list;
-  private SerializedProperty _currProperty;
+  [CustomPropertyDrawer(typeof(SDictionaryAttribute))]
+  public class SerializableDictionaryEditor : PropertyDrawer {
 
-  private List<Pair> _pairs = new List<Pair>();
-  private class Pair {
-    public int index;
-    public bool isDuplicate;
-    public SerializedProperty a;
-    public SerializedProperty b;
+    private ReorderableList _list;
+    private SerializedProperty _currProperty;
 
-    public Pair(int index, bool isDuplicate, SerializedProperty a, SerializedProperty b) {
-      this.index = index;
-      this.isDuplicate = isDuplicate;
-      this.a = a;
-      this.b = b;
-    }
-  }
+    private List<Pair> _pairs = new List<Pair>();
+    private class Pair {
+      public int index;
+      public bool isDuplicate;
+      public SerializedProperty a;
+      public SerializedProperty b;
 
-  public SerializableDictionaryEditor() {
-    _list = new ReorderableList(_pairs, typeof(Pair),
-                                draggable: true,
-                                displayHeader: true,
-                                displayAddButton: true,
-                                displayRemoveButton: true);
-
-    _list.drawElementCallback = drawElementCallback;
-    _list.elementHeightCallback = elementHeightCallback;
-    _list.drawHeaderCallback = drawHeader;
-    _list.onAddCallback = onAddCallback;
-    _list.onRemoveCallback = onRemoveCallback;
-    _list.onReorderCallback = onReorderCallback;
-  }
-
-
-  public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-    _currProperty = property;
-
-    updatePairsFromProperty(property);
-
-    EditorGUIUtility.labelWidth /= 2;
-    _list.DoList(position);
-    EditorGUIUtility.labelWidth *= 2;
-  }
-
-  public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
-    updatePairsFromProperty(property);
-    return _list.GetHeight();
-  }
-
-  private void updatePairsFromProperty(SerializedProperty property) {
-    SerializedProperty keys = property.FindPropertyRelative("_keys");
-    SerializedProperty values = property.FindPropertyRelative("_values");
-
-    var dup = (fieldInfo.GetValue(property.serializedObject.targetObject) as ICanReportDuplicateInformation).GetDuplicationInformation();
-
-    _pairs.Clear();
-    int count = keys.arraySize;
-    for (int i = 0; i < count; i++) {
-      SerializedProperty key = keys.GetArrayElementAtIndex(i);
-      SerializedProperty value = values.GetArrayElementAtIndex(i);
-
-      bool isDup = false;
-      if (i < dup.Count) {
-        isDup = dup[i] > 1;
+      public Pair(int index, bool isDuplicate, SerializedProperty a, SerializedProperty b) {
+        this.index = index;
+        this.isDuplicate = isDuplicate;
+        this.a = a;
+        this.b = b;
       }
-
-      _pairs.Add(new Pair(i, isDup, key, value));
-    }
-  }
-
-  private void drawHeader(Rect rect) {
-    EditorGUI.LabelField(rect, _currProperty.displayName);
-    rect.x += rect.width - 110;
-    rect.width = 110;
-    if (GUI.Button(rect, "Clear Duplicates")) {
-      Undo.RecordObject(_currProperty.serializedObject.targetObject, "Cleared duplicates");
-      (fieldInfo.GetValue(_currProperty.serializedObject.targetObject) as ICanReportDuplicateInformation).ClearDuplicates();
-    }
-  }
-
-  private void drawElementCallback(Rect rect, int index, bool isActive, bool isFocused) {
-    Rect leftRect = rect;
-    leftRect.width *= 0.5f;
-
-    Rect rightRect = leftRect;
-    rightRect.x += rightRect.width;
-
-    Pair pair = _pairs[index];
-
-    if (pair.isDuplicate) {
-      GUI.contentColor = new Color(1, 0.7f, 0);
-      GUI.color = new Color(1, 0.7f, 0.5f);
     }
 
-    drawProp(pair.a, leftRect);
+    public SerializableDictionaryEditor() {
+      _list = new ReorderableList(_pairs, typeof(Pair),
+                                  draggable: true,
+                                  displayHeader: true,
+                                  displayAddButton: true,
+                                  displayRemoveButton: true);
 
-    GUI.contentColor = Color.white;
-    GUI.color = Color.white;
-    GUI.backgroundColor = Color.white;
+      _list.drawElementCallback = drawElementCallback;
+      _list.elementHeightCallback = elementHeightCallback;
+      _list.drawHeaderCallback = drawHeader;
+      _list.onAddCallback = onAddCallback;
+      _list.onRemoveCallback = onRemoveCallback;
+      _list.onReorderCallback = onReorderCallback;
+    }
 
-    drawProp(pair.b, rightRect);
-  }
 
-  private void onAddCallback(ReorderableList list) {
-    SerializedProperty keys = _currProperty.FindPropertyRelative("_keys");
-    SerializedProperty values = _currProperty.FindPropertyRelative("_values");
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+      _currProperty = property;
 
-    keys.arraySize++;
-    values.arraySize++;
+      updatePairsFromProperty(property);
 
-    updatePairsFromProperty(_currProperty);
-  }
+      EditorGUIUtility.labelWidth /= 2;
+      _list.DoList(position);
+      EditorGUIUtility.labelWidth *= 2;
+    }
 
-  private void onRemoveCallback(ReorderableList list) {
-    SerializedProperty keys = _currProperty.FindPropertyRelative("_keys");
-    SerializedProperty values = _currProperty.FindPropertyRelative("_values");
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+      updatePairsFromProperty(property);
+      return _list.GetHeight();
+    }
 
-    keys.DeleteArrayElementAtIndex(list.index);
-    values.DeleteArrayElementAtIndex(list.index);
+    private void updatePairsFromProperty(SerializedProperty property) {
+      SerializedProperty keys = property.FindPropertyRelative("_keys");
+      SerializedProperty values = property.FindPropertyRelative("_values");
 
-    updatePairsFromProperty(_currProperty);
-  }
+      var dup = (fieldInfo.GetValue(property.serializedObject.targetObject) as ICanReportDuplicateInformation).GetDuplicationInformation();
 
-  private void onReorderCallback(ReorderableList list) {
-    SerializedProperty keys = _currProperty.FindPropertyRelative("_keys");
-    SerializedProperty values = _currProperty.FindPropertyRelative("_values");
+      _pairs.Clear();
+      int count = keys.arraySize;
+      for (int i = 0; i < count; i++) {
+        SerializedProperty key = keys.GetArrayElementAtIndex(i);
+        SerializedProperty value = values.GetArrayElementAtIndex(i);
 
-    int startIndex = -1, endIndex = -1;
-    bool isForward = true;
-
-    for (int i = 0; i < _pairs.Count; i++) {
-      if (i != _pairs[i].index) {
-        if (_pairs[i].index - i > 1) {
-          isForward = false;
+        bool isDup = false;
+        if (i < dup.Count) {
+          isDup = dup[i] > 1;
         }
-        startIndex = i;
-        break;
+
+        _pairs.Add(new Pair(i, isDup, key, value));
       }
     }
 
-    for (int i = _pairs.Count; i-- != 0;) {
-      if (i != _pairs[i].index) {
-        endIndex = i;
-        break;
+    private void drawHeader(Rect rect) {
+      EditorGUI.LabelField(rect, _currProperty.displayName);
+      rect.x += rect.width - 110;
+      rect.width = 110;
+      if (GUI.Button(rect, "Clear Duplicates")) {
+        Undo.RecordObject(_currProperty.serializedObject.targetObject, "Cleared duplicates");
+        (fieldInfo.GetValue(_currProperty.serializedObject.targetObject) as ICanReportDuplicateInformation).ClearDuplicates();
       }
     }
 
-    if (isForward) {
-      keys.MoveArrayElement(startIndex, endIndex);
-      values.MoveArrayElement(startIndex, endIndex);
-    } else {
-      keys.MoveArrayElement(endIndex, startIndex);
-      values.MoveArrayElement(endIndex, startIndex);
+    private void drawElementCallback(Rect rect, int index, bool isActive, bool isFocused) {
+      Rect leftRect = rect;
+      leftRect.width *= 0.5f;
+
+      Rect rightRect = leftRect;
+      rightRect.x += rightRect.width;
+
+      Pair pair = _pairs[index];
+
+      if (pair.isDuplicate) {
+        GUI.contentColor = new Color(1, 0.7f, 0);
+        GUI.color = new Color(1, 0.7f, 0.5f);
+      }
+
+      drawProp(pair.a, leftRect);
+
+      GUI.contentColor = Color.white;
+      GUI.color = Color.white;
+      GUI.backgroundColor = Color.white;
+
+      drawProp(pair.b, rightRect);
     }
 
-    updatePairsFromProperty(_currProperty);
-  }
+    private void onAddCallback(ReorderableList list) {
+      SerializedProperty keys = _currProperty.FindPropertyRelative("_keys");
+      SerializedProperty values = _currProperty.FindPropertyRelative("_values");
 
-  private float elementHeightCallback(int index) {
-    Pair pair = _pairs[index];
-    float size = Mathf.Max(getSize(pair.a), getSize(pair.b));
-    _list.elementHeight = size;
-    return size;
-  }
+      keys.arraySize++;
+      values.arraySize++;
 
-  private float getSize(SerializedProperty prop) {
-
-    float size = 0;
-    if (prop.propertyType == SerializedPropertyType.Generic) {
-      SerializedProperty copy = prop.Copy();
-      SerializedProperty endProp = copy.GetEndProperty(false);
-
-      copy.NextVisible(true);
-      while (!SerializedProperty.EqualContents(copy, endProp)) {
-        size += EditorGUI.GetPropertyHeight(copy);
-        copy.NextVisible(false);
-      }
-    } else {
-      size = EditorGUI.GetPropertyHeight(prop, GUIContent.none, false);
+      updatePairsFromProperty(_currProperty);
     }
-    return size;
-  }
 
-  private void drawProp(SerializedProperty prop, Rect r) {
-    if (prop.propertyType == SerializedPropertyType.Generic) {
-      SerializedProperty copy = prop.Copy();
-      SerializedProperty endProp = copy.GetEndProperty(false);
-      copy.NextVisible(true);
-      while (!SerializedProperty.EqualContents(copy, endProp)) {
-        r.height = EditorGUI.GetPropertyHeight(copy);
-        EditorGUI.PropertyField(r, copy, true);
-        r.y += r.height;
-        copy.NextVisible(false);
+    private void onRemoveCallback(ReorderableList list) {
+      SerializedProperty keys = _currProperty.FindPropertyRelative("_keys");
+      SerializedProperty values = _currProperty.FindPropertyRelative("_values");
+
+      keys.DeleteArrayElementAtIndex(list.index);
+      values.DeleteArrayElementAtIndex(list.index);
+
+      updatePairsFromProperty(_currProperty);
+    }
+
+    private void onReorderCallback(ReorderableList list) {
+      SerializedProperty keys = _currProperty.FindPropertyRelative("_keys");
+      SerializedProperty values = _currProperty.FindPropertyRelative("_values");
+
+      int startIndex = -1, endIndex = -1;
+      bool isForward = true;
+
+      for (int i = 0; i < _pairs.Count; i++) {
+        if (i != _pairs[i].index) {
+          if (_pairs[i].index - i > 1) {
+            isForward = false;
+          }
+          startIndex = i;
+          break;
+        }
       }
-    } else {
-      r.height = EditorGUI.GetPropertyHeight(prop);
-      EditorGUI.PropertyField(r, prop, GUIContent.none, false);
+
+      for (int i = _pairs.Count; i-- != 0;) {
+        if (i != _pairs[i].index) {
+          endIndex = i;
+          break;
+        }
+      }
+
+      if (isForward) {
+        keys.MoveArrayElement(startIndex, endIndex);
+        values.MoveArrayElement(startIndex, endIndex);
+      } else {
+        keys.MoveArrayElement(endIndex, startIndex);
+        values.MoveArrayElement(endIndex, startIndex);
+      }
+
+      updatePairsFromProperty(_currProperty);
+    }
+
+    private float elementHeightCallback(int index) {
+      Pair pair = _pairs[index];
+      float size = Mathf.Max(getSize(pair.a), getSize(pair.b));
+      _list.elementHeight = size;
+      return size;
+    }
+
+    private float getSize(SerializedProperty prop) {
+
+      float size = 0;
+      if (prop.propertyType == SerializedPropertyType.Generic) {
+        SerializedProperty copy = prop.Copy();
+        SerializedProperty endProp = copy.GetEndProperty(false);
+
+        copy.NextVisible(true);
+        while (!SerializedProperty.EqualContents(copy, endProp)) {
+          size += EditorGUI.GetPropertyHeight(copy);
+          copy.NextVisible(false);
+        }
+      } else {
+        size = EditorGUI.GetPropertyHeight(prop, GUIContent.none, false);
+      }
+      return size;
+    }
+
+    private void drawProp(SerializedProperty prop, Rect r) {
+      if (prop.propertyType == SerializedPropertyType.Generic) {
+        SerializedProperty copy = prop.Copy();
+        SerializedProperty endProp = copy.GetEndProperty(false);
+        copy.NextVisible(true);
+        while (!SerializedProperty.EqualContents(copy, endProp)) {
+          r.height = EditorGUI.GetPropertyHeight(copy);
+          EditorGUI.PropertyField(r, copy, true);
+          r.y += r.height;
+          copy.NextVisible(false);
+        }
+      } else {
+        r.height = EditorGUI.GetPropertyHeight(prop);
+        EditorGUI.PropertyField(r, prop, GUIContent.none, false);
+      }
     }
   }
 }
--- Assets/LeapMotion/Scripts/DataStructures/SerializableDictionary.cs
+++ Assets/LeapMotion/Scripts/DataStructures/SerializableDictionary.cs
@@ -1,89 +1,101 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class SDictionary : PropertyAttribute { }
+namespace Leap.Unity {
 
-public interface ICanReportDuplicateInformation {
-  List<int> GetDuplicationInformation();
-  void ClearDuplicates();
-}
+  /// <summary>
+  /// You must mark a serializable dictionary with this attribute in order to 
+  /// use the custom inspector editor.
+  /// </summary>
+  public class SDictionaryAttribute : PropertyAttribute { }
 
-public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ICanReportDuplicateInformation, ISerializationCallbackReceiver {
-
-  [SerializeField]
-  private List<TKey> _keys;
-
-  [SerializeField]
-  private List<TValue> _values;
-
-  public void OnAfterDeserialize() {
-    Clear();
-
-    if (_keys != null && _values != null) {
-      int count = Mathf.Min(_keys.Count, _values.Count);
-      for (int i = 0; i < count; i++) {
-        this[_keys[i]] = _values[i];
-      }
-    }
+  public interface ICanReportDuplicateInformation {
+    List<int> GetDuplicationInformation();
+    void ClearDuplicates();
   }
 
-  public List<int> GetDuplicationInformation() {
-    Dictionary<TKey, int> info = new Dictionary<TKey, int>();
+  /// <summary>
+  /// In order to have this class be serialized, you will always need to create your own
+  /// non-generic version specific to your needs.  This is the same workflow that exists
+  /// for using the UnityEvent class as well. 
+  /// </summary>
+  public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ICanReportDuplicateInformation, ISerializationCallbackReceiver {
+    
+    [SerializeField]
+    private List<TKey> _keys;
+    
+    [SerializeField]
+    private List<TValue> _values;
 
-    for (int i = 0; i < _keys.Count; i++) {
-      TKey key = _keys[i];
-      if (info.ContainsKey(key)) {
-        info[key]++;
-      } else {
-        info[key] = 1;
+    public void OnAfterDeserialize() {
+      Clear();
+
+      if (_keys != null && _values != null) {
+        int count = Mathf.Min(_keys.Count, _values.Count);
+        for (int i = 0; i < count; i++) {
+          this[_keys[i]] = _values[i];
+        }
       }
     }
 
-    List<int> dups = new List<int>();
-    for (int i = 0; i < _keys.Count; i++) {
-      dups.Add(info[_keys[i]]);
-    }
+    public List<int> GetDuplicationInformation() {
+      Dictionary<TKey, int> info = new Dictionary<TKey, int>();
 
-    return dups;
-  }
-
-  public void ClearDuplicates() {
-    HashSet<TKey> takenKeys = new HashSet<TKey>();
-    for (int i = 0; i < _keys.Count; i++) {
-      TKey key = _keys[i];
-      if (takenKeys.Contains(key)) {
-        _keys.RemoveAt(i);
-        _values.RemoveAt(i);
-        i--;
-      } else {
-        takenKeys.Add(key);
+      for (int i = 0; i < _keys.Count; i++) {
+        TKey key = _keys[i];
+        if (info.ContainsKey(key)) {
+          info[key]++;
+        } else {
+          info[key] = 1;
+        }
       }
-    }
-  }
 
-  public void OnBeforeSerialize() {
-    if (_keys == null) {
-      _keys = new List<TKey>();
-    }
+      List<int> dups = new List<int>();
+      for (int i = 0; i < _keys.Count; i++) {
+        dups.Add(info[_keys[i]]);
+      }
 
-    if (_values == null) {
-      _values = new List<TValue>();
+      return dups;
     }
 
-    for (int i = _keys.Count; i-- != 0;) {
-      if (!ContainsKey(_keys[i])) {
-        _keys.RemoveAt(i);
-        _values.RemoveAt(i);
+    public void ClearDuplicates() {
+      HashSet<TKey> takenKeys = new HashSet<TKey>();
+      for (int i = 0; i < _keys.Count; i++) {
+        TKey key = _keys[i];
+        if (takenKeys.Contains(key)) {
+          _keys.RemoveAt(i);
+          _values.RemoveAt(i);
+          i--;
+        } else {
+          takenKeys.Add(key);
+        }
       }
     }
 
-    Enumerator enumerator = GetEnumerator();
-    while (enumerator.MoveNext()) {
-      var pair = enumerator.Current;
+    public void OnBeforeSerialize() {
+      if (_keys == null) {
+        _keys = new List<TKey>();
+      }
 
-      if (!_keys.Contains(pair.Key)) {
-        _keys.Add(pair.Key);
-        _values.Add(pair.Value);
+      if (_values == null) {
+        _values = new List<TValue>();
+      }
+
+      for (int i = _keys.Count; i-- != 0;) {
+        if (!ContainsKey(_keys[i])) {
+          _keys.RemoveAt(i);
+          _values.RemoveAt(i);
+        }
+      }
+
+      Enumerator enumerator = GetEnumerator();
+      while (enumerator.MoveNext()) {
+        var pair = enumerator.Current;
+
+        if (!_keys.Contains(pair.Key)) {
+          _keys.Add(pair.Key);
+          _values.Add(pair.Value);
+        }
       }
     }
   }
