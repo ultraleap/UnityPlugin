74e3cc72d296dc39dc05bf18586d3a20ccef7ab5
Amarcolina <Amarcoli@ucsc.edu>
2016-12-14 13:43:54 -0800

    Updated packages

--- Assets/LeapMotionModules/Package/Editor/PackageDefinitionBuildMenuItems.cs
+++ Assets/LeapMotionModules/Package/Editor/PackageDefinitionBuildMenuItems.cs
@@ -4,16 +4,52 @@ namespace Leap.Unity.Packaging {
 
   public class PackageDefinitionBuildMenuItems {
 
+    // Detection Examples
+    [MenuItem("Build/Detection Examples")]
+    public static void Build904a61d077ec8a6408978b1184f66599() {
+      PackageDefinition.BuildPackage("904a61d077ec8a6408978b1184f66599");
+    }
+
     // Interaction Engine
     [MenuItem("Build/Interaction Engine")]
     public static void Buildbffac24abeb9a8e48b10867fa36b5dfc() {
       PackageDefinition.BuildPackage("bffac24abeb9a8e48b10867fa36b5dfc");
     }
 
+    // Hands
+    [MenuItem("Build/Hands")]
+    public static void Build0270504144afc6248ba5d4114c5feddf() {
+      PackageDefinition.BuildPackage("0270504144afc6248ba5d4114c5feddf");
+    }
+
+    // UIInput
+    [MenuItem("Build/UIInput")]
+    public static void Buildee63a291f059d0c4a86b5b232ab19fae() {
+      PackageDefinition.BuildPackage("ee63a291f059d0c4a86b5b232ab19fae");
+    }
+
+    // Android
+    [MenuItem("Build/Android")]
+    public static void Buildf1acba439a0394b43a768a06a42c039f() {
+      PackageDefinition.BuildPackage("f1acba439a0394b43a768a06a42c039f");
+    }
+
+    // Attachments
+    [MenuItem("Build/Attachments")]
+    public static void Build75ae930456fc07049858fdc6fc70393b() {
+      PackageDefinition.BuildPackage("75ae930456fc07049858fdc6fc70393b");
+    }
+
     // Core
     [MenuItem("Build/Core")]
     public static void Build39b6898d05b13f54082394c350c88ed1() {
       PackageDefinition.BuildPackage("39b6898d05b13f54082394c350c88ed1");
+    }
+
+    // RealtimeGraph
+    [MenuItem("Build/RealtimeGraph")]
+    public static void Buildb48faf9dc13e83b429b454fe72d8690a() {
+      PackageDefinition.BuildPackage("b48faf9dc13e83b429b454fe72d8690a");
     }
   }
 }
