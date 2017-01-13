9d651ba1c53058964c70857a0d27ac452f0b8442
Nicholas Benson <nbenson@leapmotion.com>
2017-01-13 13:39:35 -0800

    Added missing files. Sorry, includes extension API.

---
+++ Assets/LeapMotionModules/UI/LeapMotionHandsExtentionAPI/FingerType.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Leap.Unity {
+
+  public static class FingerType {
+
+    public static int Thumb {
+      get {
+        return (int)Leap.Finger.FingerType.TYPE_THUMB;
+      }
+    }
+
+    public static int Index {
+      get {
+        return (int)Leap.Finger.FingerType.TYPE_INDEX;
+      }
+    }
+
+    public static int Middle {
+      get {
+        return (int)Leap.Finger.FingerType.TYPE_MIDDLE;
+      }
+    }
+
+    public static int Ring {
+      get {
+        return (int)Leap.Finger.FingerType.TYPE_MIDDLE;
+      }
+    }
+
+    public static int Pinky {
+      get {
+        return (int)Leap.Finger.FingerType.TYPE_MIDDLE;
+      }
+    }
+
+  }
+
+}
---
+++ Assets/LeapMotionModules/UI/LeapMotionHandsExtentionAPI/Util.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Leap.Unity {
+
+  public static class Util {
+
+    /// <summary>
+    /// Similar to Unity's Transform.LookAt(), but resolves the forward vector of this
+    /// Transform to point away from the argument Transform.
+    /// 
+    /// Useful for billboarding Quads and UI elements whose forward vectors should match
+    /// rather than oppose the Main Camera.
+    /// </summary>
+    public static void LookAwayFrom(this Transform thisTransform, Transform transform) {
+      thisTransform.rotation = Quaternion.LookRotation(thisTransform.position - transform.position, Vector3.up);
+    }
+
+  }
+
+}
