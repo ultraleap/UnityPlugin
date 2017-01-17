1eacec11c7cc454a987e49a33ccf1ee843523547
Nicholas Benson <nbenson@leapmotion.com>
2017-01-17 12:04:36 -0800

    Added RingBuffer, underlies Delta and DelayBuffer

---
+++ Assets/LeapMotion/Scripts/DataStructures/RingBuffer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingBuffer<T> {
+
+  private T[] arr;
+  private int firstIdx = 0;
+  private int lastIdx = 0;
+
+  public RingBuffer(int bufferSize) {
+    arr = new T[bufferSize];
+  }
+
+  public int Length {
+    get {
+      if (lastIdx == -1) return 0;
+      int diff = (lastIdx + 1) - firstIdx;
+      if (diff <= 0) return diff + arr.Length;
+      return diff;
+    }
+  }
+
+  public bool isFull {
+    get { return Length == arr.Length; }
+  }
+
+  public void Clear() {
+    lastIdx = -1;
+    firstIdx = 0;
+  }
+
+  public void Add(T t) {
+    if (isFull) {
+      firstIdx += 1;
+      firstIdx %= arr.Length;
+    }
+    lastIdx += 1;
+    lastIdx %= arr.Length;
+
+    arr[lastIdx] = t;
+  }
+
+  /// <summary>
+  /// Oldest element is at index 0, youngest is at Length - 1.
+  /// </summary>
+  public T Get(int idx) {
+    return arr[(firstIdx + idx) % arr.Length];
+  }
+
+  public T GetLatest() {
+    return Get(Length - 1);
+  }
+
+  public T GetOldest() {
+    return Get(0);
+  }
+
+  public void Set(int idx, T t) {
+    int actualIdx = (firstIdx + idx) % arr.Length;
+    arr[actualIdx] = t;
+  }
+
+  public void SetLatest(T t) {
+    Set(Length - 1, t);
+  }
+
+}
