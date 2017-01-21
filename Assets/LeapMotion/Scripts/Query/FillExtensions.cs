46d52cedb4c2c9579b5b9d690d8c2d879b81a7ea
Amarcolina <Amarcoli@ucsc.edu>
2017-01-20 19:36:23 -0800

    Return values for better coding ya

--- Assets/LeapMotion/Scripts/Query/FillExtensions.cs
+++ Assets/LeapMotion/Scripts/Query/FillExtensions.cs
@@ -6,37 +6,42 @@ namespace Leap.Unity.Query {
 
   public static class FillExtensions {
 
-    public static void Fill<T>(this T[] array, T value) {
+    public static T[] Fill<T>(this T[] array, T value) {
       for (int i = 0; i < array.Length; i++) {
         array[i] = value;
       }
+      return array;
     }
 
-    public static void Fill<T>(this T[,] array, T value) {
+    public static T[,] Fill<T>(this T[,] array, T value) {
       for (int i = 0; i < array.GetLength(0); i++) {
         for (int j = 0; j < array.GetLength(1); j++) {
           array[i, j] = value;
         }
       }
+      return array;
     }
 
-    public static void Fill<T>(this List<T> list, T value) {
+    public static List<T> Fill<T>(this List<T> list, T value) {
       for (int i = 0; i < list.Count; i++) {
         list[i] = value;
       }
+      return list;
     }
 
-    public static void Fill<T>(this List<T> list, int count, T value) {
+    public static List<T> Fill<T>(this List<T> list, int count, T value) {
       list.Clear();
       for (int i = 0; i < count; i++) {
         list.Add(value);
       }
+      return list;
     }
 
-    public static void Append<T>(this List<T> list, int count, T value) {
+    public static List<T> Append<T>(this List<T> list, int count, T value) {
       for (int i = 0; i < count; i++) {
         list.Add(value);
       }
+      return list;
     }
   }
 }
