2331ac659d7c031dfff6c6a20b8fbbc0fdacdebd
Nicholas Benson <nbenson@leapmotion.com>
2017-01-23 13:27:09 -0800

    Working Rigidbody parenting; needs more testing.

--- Assets/LeapMotionModules/UI/Scripts/RigidbodyParenting.cs
+++ Assets/LeapMotionModules/UI/Scripts/RigidbodyParenting.cs
@@ -2,69 +2,137 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody))]
 public class RigidbodyParenting : MonoBehaviour, IRuntimeGizmoComponent {
 
-  [SerializeField]
+  [Header("Debug")]
+  public Text outputText;
+
   private Rigidbody _parentBody;
-  [SerializeField]
-  private Rigidbody _body;
+  private Rigidbody _childBody;
 
   void Start() {
-    _body = GetComponent<Rigidbody>();
-    _parentBody = _body.transform.parent.GetComponent<Rigidbody>();
-    if (_parentBody == null) {
-      Debug.LogError("[RigidbodyParenting] Must be attached to a Rigidbody that is the child of another Rigidbody.");
-    }
-    _body.sleepThreshold = -1F;
+    _childBody = GetComponent<Rigidbody>();
+    _parentBody = _childBody.transform.parent.GetComponent<Rigidbody>();
+    if (_parentBody == null) { Debug.LogError("[RigidbodyParenting] Must be attached to a Rigidbody that is the child of another Rigidbody."); }
+    InitializeBodies();
   }
 
-  private Vector3 _parentPositionPrePhysics;
-  private Quaternion _parentRotationPrePhysics;
-  private Vector3 _bodyPositionInParentSpacePrePhysics;
-  private bool _didPhysicsMetrics;
+  private bool _physicsUpdated = false;
 
   void FixedUpdate() {
-    if (!_didPhysicsMetrics) {
-      _parentPositionPrePhysics = _parentBody.position;
-      _parentRotationPrePhysics = _parentBody.rotation;
-      _bodyPositionInParentSpacePrePhysics = _parentBody.transform.InverseTransformPoint(_body.position);
-      _didPhysicsMetrics = true;
+    if (!_physicsUpdated) {
+      OnPrePhysics();
+      _physicsUpdated = true;
     }
-    _body.velocity = Vector3.zero;
-    _body.angularVelocity = Vector3.zero;
   }
 
   void Update() {
-    Vector3 parentPositionDelta = _parentBody.position - _parentPositionPrePhysics;
+    OnPostPhysics();
+    _physicsUpdated = false;
 
-    Quaternion parentRotationDelta = _parentBody.rotation * Quaternion.Inverse(_parentRotationPrePhysics);
-    float rotationDeltaAngle;
-    Vector3 rotationDeltaAxis;
-    parentRotationDelta.ToAngleAxis(out rotationDeltaAngle, out rotationDeltaAxis);
-
-    Vector3 positionDeltaDueToParentRotation = (parentRotationDelta * _parentBody.transform.TransformVector(_bodyPositionInParentSpacePrePhysics)) - _parentBody.transform.TransformVector(_bodyPositionInParentSpacePrePhysics);
-
-    _body.position = _body.position + Vector3.zero /*parentPositionDelta*/ + positionDeltaDueToParentRotation;
-
-    _body.rotation = parentRotationDelta * _body.rotation;
-
-    _didPhysicsMetrics = false;
+    if (outputText != null) {
+      outputText.text = "Child Local Position: \n" + _childBody.transform.localPosition.ToString("G4") + "      "
+                      + "Child Body Velocity: \n" + _childBody.velocity;
+    }
   }
+
+  #region World Space Calculations (really really bad rotational drift)
+
+  //private Vector3 _prePhysicsParentPos;
+  //private Quaternion _prePhysicsParentRot;
+  //private Vector3 _prePhysicsChildPos;
+
+  //private Vector3 _childPosUpdate = Vector3.zero;
+  //private Vector3 _childPosFromRotUpdate = Vector3.zero;
+
+  //private void OnPrePhysics() {
+  //  _childBody.WakeUp();
+
+  //  _prePhysicsParentPos = _parentBody.position;
+  //  _prePhysicsParentRot = _parentBody.rotation;
+  //  _prePhysicsChildPos = _childBody.position;
+
+  //  _childBody.position = _childBody.position + _childPosUpdate + _childPosFromRotUpdate;
+  //}
+
+  //private void OnPostPhysics() {
+  //  Vector3 parentPosDelta = _parentBody.position - _prePhysicsParentPos;
+  //  _childPosUpdate = parentPosDelta;
+
+  //  Quaternion parentRotDelta = _parentBody.rotation * Quaternion.Inverse(_prePhysicsParentRot);
+  //  _childPosFromRotUpdate = parentRotDelta * (_prePhysicsChildPos - _prePhysicsParentPos) - (_prePhysicsChildPos - _prePhysicsParentPos);
+  //}
+
+  #endregion
+
+  #region Transform Calculations
+
+  private Transform _parentT;
+  private Transform _childT;
+
+  private void InitializeBodies() {
+    _parentT = new GameObject("Transform Sim: " + _parentBody.gameObject.name).transform;
+    _childT  = new GameObject("Transform Sim: " + _childBody.gameObject.name).transform;
+
+    _parentT.transform.position = _parentBody.transform.position;
+    _parentT.transform.rotation = _parentBody.transform.rotation;
+    _parentT.transform.localScale = _parentBody.transform.localScale;
+
+    _childT.transform.parent = _parentT;
+    _childT.transform.position = _childBody.transform.position;
+    _childT.transform.rotation = _childBody.transform.rotation;
+    _childT.transform.localScale = _childBody.transform.localScale;
+  }
+
+  private Vector3    _prePhysicsParentPosition;
+  private Quaternion _prePhysicsParentRotation;
+  private Vector3    _prePhysicsChildTransformPosition;
+  private Quaternion _prePhysicsChildTransformRotation;
+
+  private Vector3    _childPosUpdate = Vector3.zero;
+  private Quaternion _childRotUpdate = Quaternion.identity;
+
+  // called once per frame, on the first FixedUpdate
+  private void OnPrePhysics() {
+    _prePhysicsParentPosition = _parentBody.position;
+    _prePhysicsParentRotation = _parentBody.rotation;
+
+    _childBody.position = _childBody.position + _childPosUpdate;
+    _childBody.rotation = _childBody.rotation * _childRotUpdate;
+
+    _childT.position = _childBody.position;
+    _childT.rotation = _childBody.rotation;
+    _prePhysicsChildTransformPosition = _childT.position;
+    _prePhysicsChildTransformRotation = _childT.rotation;
+  }
+
+  // called on Update
+  private void OnPostPhysics() {
+    _parentT.position = _parentBody.position;
+    _parentT.rotation = _parentBody.rotation;
+
+    _childPosUpdate = _childT.position - _prePhysicsChildTransformPosition;
+    _childRotUpdate = Quaternion.Inverse(_prePhysicsChildTransformRotation) * _childT.rotation;
+  }
+
+  #endregion
 
   #region Gizmos
 
   public void OnDrawRuntimeGizmos(RuntimeGizmoDrawer drawer) {
+    if (_childBody == null || _parentBody == null) return;
 
     drawer.PushMatrix();
     drawer.color = Color.red;
-    drawer.matrix = _body.transform.localToWorldMatrix;
+    drawer.matrix = _childBody.transform.localToWorldMatrix;
     drawer.DrawWireCube(Vector3.zero, Vector3.one * 0.04F);
     drawer.PopMatrix();
 
     drawer.color = new Color(0.9F, 0.45F, 0.1F);
-    drawer.DrawLine(_body.position, _body.position + _body.rotation * Vector3.up * 0.1F);
+    drawer.DrawLine(_childBody.position, _childBody.position + _childBody.rotation * Vector3.up * 0.1F);
   }
 
   #endregion
