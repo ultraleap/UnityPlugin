964478f2e3c88669b4cdaf08b833cc7e10ad8e2c
Amarcolina <Amarcoli@ucsc.edu>
2017-01-23 12:00:53 -0800

    Defined procedural mesh abstract class

---
+++ Assets/LeapMotionModules/UI/GuiSpace/Scripts/Meshes/ProceduralMeshSource.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Leap.Unity.Gui.Space {
+
+  public abstract class ProceduralMeshSource : MonoBehaviour {
+    public abstract Mesh GetMesh();
+  }
+}
