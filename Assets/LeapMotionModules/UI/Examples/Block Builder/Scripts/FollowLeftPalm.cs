3cf8b1ecdd1e19ac3e647ebfd3ba20007949aadd
Nicholas Benson <nbenson@leapmotion.com>
2017-01-24 12:48:36 -0800

    Progress on practical 'builder' example

---
+++ Assets/LeapMotionModules/UI/Examples/Block Builder/Scripts/FollowLeftPalm.cs
@@ -0,0 +1,17 @@
+﻿using Leap;
+using Leap.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowLeftPalm : MonoBehaviour {
+
+  void Update() {
+    Hand lHand = Hands.Left;
+    if (lHand != null) {
+      this.transform.position = lHand.PalmPosition.ToVector3();
+      this.transform.rotation = lHand.Rotation.ToQuaternion();
+    }
+  }
+
+}
