7a10d236d50510a1421c1ee235ae694bf41fe041
Amarcolina <Amarcolina@gmail.com>
2017-02-01 14:31:59 -0800

    IMplemented abstract class

--- Assets/LeapMotionModules/UI/LeapGui/Scripts/Renderers/LeapGuiDynamicRenderer.cs
+++ Assets/LeapMotionModules/UI/LeapGui/Scripts/Renderers/LeapGuiDynamicRenderer.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.Rendering;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using Leap.Unity.Query;
+using Leap.Unity.Attributes;
 
-public class LeapGuiDynamicRenderer : MonoBehaviour {
+public class LeapGuiDynamicRenderer : LeapGuiRenderer {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  public override void OnEnableRenderer() {
+    throw new NotImplementedException();
+  }
+
+  public override void OnDisableRenderer() {
+    throw new NotImplementedException();
+  }
+
+  public override void OnUpdateRenderer() {
+    throw new NotImplementedException();
+  }
+
+  public override void OnEnableRendererEditor() {
+    throw new NotImplementedException();
+  }
+
+  public override void OnDisableRendererEditor() {
+    throw new NotImplementedException();
+  }
+
+  public override void OnUpdateRendererEditor() {
+    throw new NotImplementedException();
+  }
 }
