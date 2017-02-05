29ad99a9acff22f33f1491bee4e2a12fc5275a9b
jselstad <jselstad@leapmotion.com>
2017-02-04 16:37:59 -0800

    Also add bendy hands script for post processes

---
+++ Assets/BendyHands.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Leap.Unity.Interaction;
+using Leap.Unity;
+using Leap;
+
+public class BendyHands : MonoBehaviour {
+  public InteractionBrushHand leftHand;
+  public InteractionBrushHand rightHand;
+  public void Start() {}
+  public void setHeldHand(Hand inHand) {
+    if (enabled) {
+      if (inHand.IsLeft) {
+        leftHand.fillBones(inHand);
+      } else {
+        rightHand.fillBones(inHand);
+      }
+    }
+  }
+}
