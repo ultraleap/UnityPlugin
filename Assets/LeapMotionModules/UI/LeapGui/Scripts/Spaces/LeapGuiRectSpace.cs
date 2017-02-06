b748f523182592a4a05284bdf9b85aacd927a07b
Amarcolina <Amarcolina@gmail.com>
2017-02-06 14:27:00 -0800

    Added the rect space

---
+++ Assets/LeapMotionModules/UI/LeapGui/Scripts/Spaces/LeapGuiRectSpace.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LeapGuiRectSpace : LeapGuiSpace, ISupportsAddRemove {
+
+  public void OnAddElements(List<LeapGuiElement> element, List<int> indexes) { }
+  public void OnRemoveElements(List<int> toRemove) { }
+
+  public override void BuildElementData(Transform root) { }
+  public override void RefreshElementData(Transform root) { }
+
+  public override Vector3 InverseTransformPoint(Vector3 worldGuiPos) {
+    return worldGuiPos;
+  }
+
+  public override Vector3 InverseTransformPoint(LeapGuiElement element, Vector3 worldGuiPos) {
+    return worldGuiPos;
+  }
+
+  public override Vector3 TransformPoint(Vector3 worldRectPos) {
+    return worldRectPos;
+  }
+
+  public override Vector3 TransformPoint(LeapGuiElement element, Vector3 localRectPos) {
+    return localRectPos;
+  }
+}
