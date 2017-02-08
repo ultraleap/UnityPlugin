1413eee893bc8773622b472a5decd07ab9574a7a
Amarcolina <Amarcolina@gmail.com>
2017-02-07 16:17:08 -0800

    Adding sprite feature

---
+++ Assets/LeapMotionModules/UI/LeapGui/Scripts/Features/Sprite/LeapGuiSpriteData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Leap.Unity.Query;
+
+public static class LeapGuiSpriteExtensions {
+  public static LeapGuiSpriteData Texture(this LeapGuiElement element) {
+    return element.data.Query().OfType<LeapGuiSpriteData>().FirstOrDefault();
+  }
+}
+
+[AddComponentMenu("")]
+public class LeapGuiSpriteData : LeapGuiElementData {
+  public Sprite sprite;
+}
---
+++ Assets/LeapMotionModules/UI/LeapGui/Scripts/Features/Sprite/LeapGuiSpriteFeature.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[AddComponentMenu("")]
+[LeapGuiTag("Sprite")]
+public class LeapGuiSpriteFeature : LeapGuiFeature<LeapGuiSpriteData> {
+  public string propertyName = "_MainTex";
+  public UVChannelFlags channel = UVChannelFlags.UV0;
+
+#if UNITY_EDITOR
+  public override void DrawFeatureEditor(Rect rect, bool isActive, bool isFocused) {
+    Rect line = rect.SingleLine();
+
+    propertyName = EditorGUI.TextField(line, "Property Name", propertyName);
+    line = line.NextLine();
+
+    channel = (UVChannelFlags)EditorGUI.EnumPopup(line, "Uv Channel", channel);
+  }
+
+  public override float GetEditorHeight() {
+    return EditorGUIUtility.singleLineHeight * 2;
+  }
+#endif
+}
