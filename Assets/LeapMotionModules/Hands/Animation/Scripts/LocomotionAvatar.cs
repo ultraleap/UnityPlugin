f6c3642ec2421dc7f38f4ad9f0a1744d6bc0a81b
protodeep <fox@brainstormer-project.net>
2017-02-15 16:48:38 -0800

    Simplifying LocomotionAvatar

--- Assets/LeapMotionModules/Hands/Animation/Scripts/LocomotionAvatar.cs
+++ Assets/LeapMotionModules/Hands/Animation/Scripts/LocomotionAvatar.cs
@@ -13,37 +13,31 @@ namespace Leap.Unity {
     private Queue<float> speedList = new Queue<float>(10);
     private float direction = 0;
     private Locomotion locomotion = null;
-
     private Vector3 moveDirection;
-    
     private Vector3 distanceToRoot;
     private Vector3 rootDirection;
+    [HideInInspector]
     public Transform LMRig;
+    [HideInInspector]
     public Transform Target;
-
-    public Text standWalkStateText;
-    public Text m_AnimatorStateText;
-    public Text CenteringText;
-    public Text DistanceText;
-    public Text SpeedText;
-    private bool isDisplayState = false;
-
-    //For debugging - - - - - - - - - - - - - - - - 
-    public Transform AnimatorRoot;
-    public Transform CameraOnGround;
     [HideInInspector]
     public bool IsCentering = false;
 
     public bool WalkingEnabled = true;
     public bool crouchEnabled = false;
     private float userHeight = 1.63241f;
+    private bool standing = true;
+
+    [Header("For Debugging")] 
+    public Transform AnimatorRoot;
+    public Transform CameraOnGround;
+
 
     void Awake() {
       LMRig = GameObject.FindObjectOfType<LeapHandController>().transform.root;
     }
 
     void Start() {
-      //CenteringText.text = "";
       InputTracking.Recenter();
       animator = GetComponent<Animator>();
       locomotion = new Locomotion(animator);
@@ -58,38 +52,29 @@ namespace Leap.Unity {
     }
 
     void Update() {
-      AnimatorLocomotion();
-      
-      if (isDisplayState) {
-        StateDisplay();
+      if (WalkingEnabled) {
+        AnimatorLocomotion();
       }
-      //if (Input.GetKeyUp(KeyCode.Space)) {
-      //  CenterUnderCamera();
-      //  userHeight = Camera.main.transform.position.y;
-      //}
     }
    
-   
-    Vector3 MoveDirectionCameraDirection() {
-      // Get camera rotation.
-      rootDirection = transform.forward;// +transform.position;
-      Vector3 CameraDirection = Camera.main.transform.forward;
-      CameraDirection.y = 0.0f;
-      return CameraDirection;
-    }
-    Vector3 MoveDirectionTowardCamera() {
-      // Get camera rotation.
-      rootDirection = transform.forward;// +transform.position;
-      Vector3 DirectionToCamera = Camera.main.transform.position - transform.position;
-      DirectionToCamera.y = 0.0f;
-      Quaternion referentialShift = Quaternion.FromToRotation(Vector3.forward, DirectionToCamera);
-      // Convert joystick input in Worldspace coordinates
-      return DirectionToCamera;
-    }
-    bool standing = true;
+    void OnAnimatorIK() {
+      //Floating crouch
+      if (crouchEnabled && !WalkingEnabled) {
+        float heightOffset = 0;
+        if (Camera.main.transform.position.y < userHeight) {
+          heightOffset = Camera.main.transform.position.y - userHeight;
+          animator.transform.position = new Vector3(animator.transform.position.x, heightOffset, animator.transform.position.z);
+        }
+      }
+      Vector3 placeAnimatorUnderCam = new Vector3(Camera.main.transform.position.x, transform.position.y, Camera.main.transform.position.z);
 
-    public void CenterUnderCamera() {
-      animator.transform.position = new Vector3(Camera.main.transform.position.x, transform.position.y, Camera.main.transform.position.z);
+      if (IsCentering || !WalkingEnabled) {
+        animator.transform.position = Vector3.Lerp(animator.rootPosition, placeAnimatorUnderCam, .05f);
+        var lookPos = Target.position - transform.position;
+        lookPos.y = 0;
+        var rotation = Quaternion.LookRotation(lookPos);
+        animator.transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 1.5f);
+      }
     }
 
     void AnimatorLocomotion() {
@@ -112,9 +97,7 @@ namespace Leap.Unity {
         averageSpeed += s;
       }
       averageSpeed = (averageSpeed / 10);
-
-
-
+      
       if (!standing && averageSpeed < .15f) {
         standing = true;
       }
@@ -127,12 +110,6 @@ namespace Leap.Unity {
       }
       else {
         moveDirection = MoveDirectionTowardCamera();
-        //if (transform.InverseTransformPoint(Camera.main.transform.position).z < -.1f
-        //  && direction > -20 && direction < 20) {
-        //  Debug.Log("Reversing");
-        //  moveDirection = MoveDirectionCameraDirection();
-        //  reverse = -1;
-        //}
       }
 
       //Vector3 moveDirection = referentialShift * CameraDirection;
@@ -143,47 +120,28 @@ namespace Leap.Unity {
         Debug.DrawLine(transform.position, moveDirection * 2, Color.red);
       }
     }
-    void OnAnimatorIK() {
-      //Floating crouch
-      if (crouchEnabled && !WalkingEnabled) {
-        float heightOffset = 0;
-        if (Camera.main.transform.position.y < userHeight) {
-          heightOffset = Camera.main.transform.position.y - userHeight;
-          animator.transform.position = new Vector3(animator.transform.position.x, heightOffset, animator.transform.position.z);
-        }
-      }
-      Vector3 placeAnimatorUnderCam = new Vector3(Camera.main.transform.position.x, transform.position.y, Camera.main.transform.position.z);
 
-      if (IsCentering || !WalkingEnabled) {
-        animator.transform.position = Vector3.Lerp(animator.rootPosition, placeAnimatorUnderCam, .05f);
-        var lookPos = Target.position - transform.position;
-        lookPos.y = 0;
-        var rotation = Quaternion.LookRotation(lookPos);
-        animator.transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 1.5f);
-
-        //Optional: lock animator.transform rotation to camera y
-        //float CameraRotationY = Camera.main.transform.rotation.y;
-        //animator.transform.rotation = new Quaternion(animator.transform.rotation.x, CameraRotationY, animator.transform.rotation.z, animator.transform.rotation.w); 
-      }
+    Vector3 MoveDirectionCameraDirection() {
+      // Get camera rotation.
+      rootDirection = transform.forward;// +transform.position;
+      Vector3 CameraDirection = Camera.main.transform.forward;
+      CameraDirection.y = 0.0f;
+      return CameraDirection;
     }
 
-    void StateDisplay() {
-      if (standing) {
-        standWalkStateText.text = "Idle/Turning";
-      }
-      else standWalkStateText.text = "WalkRun";
-      AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
-      if (state.IsName("Locomotion.Idle")) {
-        m_AnimatorStateText.text = "State: Idle";
-      }
-      if (state.IsName("Locomotion.TurnOnSpot")) {
-        m_AnimatorStateText.text = "State: TurnOnSpot";
-      }
-      if (state.IsName("Locomotion.WalkRun")) {
-        m_AnimatorStateText.text = "State: WalkRun";
-      }
-      DistanceText.text = distanceToRoot.magnitude.ToString("F2");
-      SpeedText.text = animator.GetFloat("Speed").ToString("F2");
+    Vector3 MoveDirectionTowardCamera() {
+      // Get camera rotation.
+      rootDirection = transform.forward;// +transform.position;
+      Vector3 DirectionToCamera = Camera.main.transform.position - transform.position;
+      DirectionToCamera.y = 0.0f;
+      Quaternion referentialShift = Quaternion.FromToRotation(Vector3.forward, DirectionToCamera);
+      // Convert joystick input in Worldspace coordinates
+      return DirectionToCamera;
+    }
+
+    //Called by Mecanim StateMachineBehavior
+    public void CenterUnderCamera() {
+      animator.transform.position = new Vector3(Camera.main.transform.position.x, transform.position.y, Camera.main.transform.position.z);
     }
   }
 }
