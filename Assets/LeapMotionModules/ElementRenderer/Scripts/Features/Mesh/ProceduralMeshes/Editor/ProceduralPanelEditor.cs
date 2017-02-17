1c1a52e5a4705cd5d368bdd8d895af729763e8e4
Amarcolina <Amarcolina@gmail.com>
2017-02-17 11:58:15 -0800

    Added an editor

---
+++ Assets/LeapMotionModules/ElementRenderer/Scripts/Features/Mesh/ProceduralMeshes/Editor/ProceduralPanelEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using Leap.Unity;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(ProceduralPanel))]
+public class ProceduralPanelEditor : CustomEditorBase<ProceduralPanel> {
+
+  protected override void OnEnable() {
+    base.OnEnable();
+
+    specifyConditionalDrawing("_resolutionMode", 
+                              (int)ProceduralPanel.ResolutionMode.Explicit, 
+                              "_resolutionX", 
+                              "_resolutionY");
+    specifyConditionalDrawing("_resolutionMode",
+                              (int)ProceduralPanel.ResolutionMode.Implicit,
+                              "_vertexPerMeterX",
+                              "_vertexPerMeterY");
+  }
+}
