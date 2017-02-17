cbaa2d642e8a8a7892452943f8266570026c8a36
Amarcolina <Amarcolina@gmail.com>
2017-02-17 11:19:28 -0800

    adding a custom editor

---
+++ Assets/LeapMotionModules/ElementRenderer/Scripts/Features/Mesh/Editor/LeapGuiMeshDataEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Leap.Unity;
+
+[CustomEditor(typeof(LeapGuiMeshData))]
+public class LeapGuiMeshDataEditor : CustomEditorBase {
+  
+
+
+
+
+
+}
