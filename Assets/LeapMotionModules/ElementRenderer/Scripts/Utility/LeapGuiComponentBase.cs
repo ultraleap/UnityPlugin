897869b9dbfd5a3d238dea0297c6e7e467bab067
Amarcolina <Amarcolina@gmail.com>
2017-03-09 13:07:32 -0800

    Disable component hiding for now

--- Assets/LeapMotionModules/ElementRenderer/Scripts/Utility/LeapGuiComponentBase.cs
+++ Assets/LeapMotionModules/ElementRenderer/Scripts/Utility/LeapGuiComponentBase.cs
@@ -14,7 +14,7 @@ public class LeapGuiComponentBase<AttachedComponent> : MonoBehaviour
       InternalUtility.Destroy(this);
     }
 
-    //hideFlags = HideFlags.None;
-    hideFlags = HideFlags.HideInInspector | HideFlags.HideInHierarchy;
+    hideFlags = HideFlags.None;
+    //hideFlags = HideFlags.HideInInspector | HideFlags.HideInHierarchy;
   }
 }
