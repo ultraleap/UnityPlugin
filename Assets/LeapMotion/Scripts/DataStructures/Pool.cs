f8bf4d910bdbae44c94347e7e04ab98eb23c1f83
Amarcolina <Amarcolina@gmail.com>
2017-03-23 17:19:42 -0700

    Added tween to core

---
+++ Assets/LeapMotion/Scripts/DataStructures/Pool.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leap.Unity {
+
+  public interface IPoolable {
+    void OnSpawn();
+  }
+
+  public static class Pool<T> where T : new() {
+    [ThreadStatic]
+    private static Stack<T> _pool = new Stack<T>();
+
+    public static T Spawn() {
+      if (_pool == null) _pool = new Stack<T>();
+
+      T value;
+      if (_pool.Count > 0) {
+        value = _pool.Pop();
+      } else {
+        value = new T();
+      }
+
+      if (value is IPoolable) {
+        (value as IPoolable).OnSpawn();
+      }
+
+      return value;
+    }
+
+    public static void Recycle(T t) {
+      _pool.Push(t);
+    }
+  }
+}
