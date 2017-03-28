0005627fed070f0b116187872be009955c6ff799
Nicholas Benson <nbenson@leapmotion.com>
2017-03-28 15:35:03 -0700

    sure Actions are fine

--- Assets/LeapMotionModules/Interaction/InteractionBehaviour.cs
+++ Assets/LeapMotionModules/Interaction/InteractionBehaviour.cs
@@ -8,7 +8,6 @@ using UnityEngine.Assertions;
 
 namespace Leap.Unity.UI.Interaction {
 
-  // TODO: Put this somewhere better than here!
   public delegate void Action<T1, T2, T3, T4, T5>(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5);
 
   [RequireComponent(typeof(Rigidbody))]
