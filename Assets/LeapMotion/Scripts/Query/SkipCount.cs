989845ad60ff65853ad2522565efb60e5407d39b
Amarcolina <Amarcolina@gmail.com>
2017-04-28 16:42:31 -0700

    Catch LeapMotionTests as well

--- Assets/LeapMotion/Scripts/Query/SkipCount.cs
+++ Assets/LeapMotion/Scripts/Query/SkipCount.cs
@@ -1,10 +1,10 @@
-/****************************************************************************** 
- * Copyright (C) Leap Motion, Inc. 2011-2017.                                 * 
- * Leap Motion proprietary and  confidential.                                 * 
- *                                                                            * 
- * Use subject to the terms of the Leap Motion SDK Agreement available at     * 
- * https://developer.leapmotion.com/sdk_agreement, or another agreement       * 
- * between Leap Motion and you, your company or other organization.           * 
+/******************************************************************************
+ * Copyright (C) Leap Motion, Inc. 2011-2017.                                 *
+ * Leap Motion proprietary and  confidential.                                 *
+ *                                                                            *
+ * Use subject to the terms of the Leap Motion SDK Agreement available at     *
+ * https://developer.leapmotion.com/sdk_agreement, or another agreement       *
+ * between Leap Motion and you, your company or other organization.           *
  ******************************************************************************/
 
 namespace Leap.Unity.Query {
