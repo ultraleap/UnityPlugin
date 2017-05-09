b5ed40d8c4e980c198d7c4fd38f0ce587d6a30fd
Amarcolina <Amarcolina@gmail.com>
2017-05-09 12:13:40 -0700

    Added docs and helpers

--- Assets/LeapMotionModules/GraphicRenderer/Scripts/Features/CustomChannel/CustomChannelDataBase.cs
+++ Assets/LeapMotionModules/GraphicRenderer/Scripts/Features/CustomChannel/CustomChannelDataBase.cs
@@ -7,9 +7,32 @@
  * between Leap Motion and you, your company or other organization.           *
  ******************************************************************************/
 
+using System;
 using UnityEngine;
+using Leap.Unity.Query;
 
 namespace Leap.Unity.GraphicalRenderer {
+
+  public partial class LeapGraphic {
+
+    private T getCustomChannelFeature<T>(string channelName) where T : CustomChannelDataBase {
+      int index = _featureData.Query().
+                               Select(d => d.feature).
+                               Cast<ICustomChannelFeature>().
+                               IndexOf(f => f.channelName == channelName);
+
+      if (index == -1) {
+        throw new Exception("No custom channel of the name " + channelName + " could be found.");
+      }
+
+      T featureDataObj = _featureData[index] as T;
+      if (featureDataObj == null) {
+        throw new Exception("The channel name " + channelName + " did not match to a custom channel of type " + typeof(T).Name + ".");
+      }
+
+      return featureDataObj;
+    }
+  }
 
   public abstract class CustomChannelDataBase : LeapFeatureData { }
 
