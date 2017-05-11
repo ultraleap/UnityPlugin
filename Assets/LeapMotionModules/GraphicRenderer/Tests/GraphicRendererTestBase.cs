27289bed17ca2a3dcc3f94206aa80ac0d42e2752
Amarcolina <Amarcolina@gmail.com>
2017-05-11 15:24:12 -0700

    Improving testing api

--- Assets/LeapMotionModules/GraphicRenderer/Tests/GraphicRendererTestBase.cs
+++ Assets/LeapMotionModules/GraphicRenderer/Tests/GraphicRendererTestBase.cs
@@ -4,14 +4,25 @@ namespace Leap.Unity.GraphicalRenderer.Tests {
 
   public abstract class GraphicRendererTestBase {
 
-    protected LeapGraphicRenderer renderer;
+    protected LeapGraphicRenderer renderer { get; private set; }
 
-    protected LeapGraphic oneGraphic;
+    protected LeapGraphic oneGraphic { get; private set; }
 
-    protected LeapGraphicGroup firstGroup;
+    protected LeapGraphicGroup firstGroup { get; private set; }
 
-    protected LeapGraphicGroup secondGroup;
+    protected LeapGraphicGroup secondGroup { get; private set; }
 
+    /// <summary>
+    /// Should be called at the start of the test.  The argument is 
+    /// the name of the prefab to spawn.  This prefab should have a
+    /// graphic renderer component attached to the root of the prefab.
+    /// 
+    /// This method will automatically populate the following fields:
+    ///  - renderer : with the reference to the renderer on the base of the prefab
+    ///  - oneGraphic : with a reference to one graphic that exists in the prefab, if any
+    ///  - firstGroup : the first group attached to the renderer
+    ///  - secondGroup : the second group attached to the renderer, if any
+    /// </summary>
     protected void InitTest(string prefabName) {
       var prefab = Resources.Load<GameObject>(prefabName);
       var obj = Object.Instantiate(prefab);
@@ -25,6 +36,11 @@ namespace Leap.Unity.GraphicalRenderer.Tests {
       secondGroup = renderer.groups.Count > 1 ? renderer.groups[1] : null;
     }
 
+    /// <summary>
+    /// Should be called to spawn a graphic prefab and child it to the 
+    /// renderer.  If the oneGraphic property has not yet been assigned to,
+    /// this method will assign the newly spawned graphic to it.
+    /// </summary>
     protected LeapGraphic CreateGraphic(string prefabName) {
       var prefab = Resources.Load<GameObject>(prefabName);
       var obj = Object.Instantiate(prefab);
