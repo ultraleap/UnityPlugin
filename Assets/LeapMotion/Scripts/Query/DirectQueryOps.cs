138c4cb404d2448d58e27bcc5f36d1e7c5b20eaf
Amarcolina <Amarcolina@gmail.com>
2017-05-12 12:54:10 -0700

    Added docs to direct query ops

--- Assets/LeapMotion/Scripts/Query/DirectQueryOps.cs
+++ Assets/LeapMotion/Scripts/Query/DirectQueryOps.cs
@@ -14,6 +14,9 @@ namespace Leap.Unity.Query {
 
   public partial struct QueryWrapper<QueryType, QueryOp> where QueryOp : IQueryOp<QueryType> {
 
+    /// <summary>
+    /// Returns true if all elements in the sequence satisfy the predicate.
+    /// </summary>
     public bool All(Func<QueryType, bool> predicate) {
       var op = _op;
 
@@ -26,6 +29,10 @@ namespace Leap.Unity.Query {
       return true;
     }
 
+    /// <summary>
+    /// Returns true if all elements in the sequence are equal to the same value.
+    /// Will always return true for sequences with one or zero elements.
+    /// </summary>
     public bool AllEqual() {
       var op = _op;
 
@@ -52,6 +59,9 @@ namespace Leap.Unity.Query {
       return true;
     }
 
+    /// <summary>
+    /// Returns true if the sequence has any elements in it.
+    /// </summary>
     public bool Any() {
       var op = _op;
 
@@ -59,10 +69,16 @@ namespace Leap.Unity.Query {
       return op.TryGetNext(out obj);
     }
 
+    /// <summary>
+    /// Returns true if any elements in the sequence satisfy the predicate.
+    /// </summary>
     public bool Any(Func<QueryType, bool> predicate) {
       return Where(predicate).Any();
     }
 
+    /// <summary>
+    /// Returns true if any element in the sequence is equal to a specific value.
+    /// </summary>
     public bool Contains(QueryType instance) {
       var op = _op;
 
@@ -75,6 +91,9 @@ namespace Leap.Unity.Query {
       return false;
     }
 
+    /// <summary>
+    /// Returns the number of elements in the sequence.
+    /// </summary>
     public int Count() {
       var op = _op;
 
@@ -86,18 +105,33 @@ namespace Leap.Unity.Query {
       return count;
     }
 
+    /// <summary>
+    /// Returns the number of elements in the sequence that satisfy a predicate.
+    /// </summary>
     public int Count(Func<QueryType, bool> predicate) {
       return Where(predicate).Count();
     }
 
+    /// <summary>
+    /// Returns the element at a specific index in the sequence.  Will throw an error
+    /// if the sequence has no element at that index.
+    /// </summary>
     public QueryType ElementAt(int index) {
       return Skip(index).First();
     }
 
+    /// <summary>
+    /// Returns the element at a specific index in the sequence.  Will return
+    /// the default value if the sequence has no element at that index.
+    /// </summary>
     public QueryType ElementAtOrDefault(int index) {
       return Skip(index).FirstOrDefault();
     }
 
+    /// <summary>
+    /// Returns the first element in the sequence.  Will throw an error if there are
+    /// no elements in the sequence.
+    /// </summary>
     public QueryType First() {
       var op = _op;
 
@@ -109,10 +143,18 @@ namespace Leap.Unity.Query {
       return obj;
     }
 
+    /// <summary>
+    /// Returns the first element in the sequence that satisfies a predicate.  Will
+    /// throw an error if there is no such element.
+    /// </summary>
     public QueryType First(Func<QueryType, bool> predicate) {
       return Where(predicate).First();
     }
 
+    /// <summary>
+    /// Returns the first element in the sequence.  Will return the default value
+    /// if the sequence is empty.
+    /// </summary>
     public QueryType FirstOrDefault() {
       var op = _op;
 
@@ -121,10 +163,24 @@ namespace Leap.Unity.Query {
       return obj;
     }
 
+    /// <summary>
+    /// Returns the first element in the sequence that satisfies a predicate.  Will return
+    /// the default value if there is no such element.
+    /// </summary>
     public QueryType FirstOrDefault(Func<QueryType, bool> predicate) {
       return Where(predicate).FirstOrDefault();
     }
 
+    /// <summary>
+    /// Folds all of the elements in the sequence into a single element, using a fold function.
+    /// Will throw an error if there are no elements in the sequence.
+    /// 
+    /// The fold function takes in the current folded value, and the next item to fold in.
+    /// It returns the result of folding the item into the current folded value.  For example,
+    /// you can use the Fold operation to implement a sum:
+    /// 
+    /// var sum = numbers.Query().Fold((a,b) => a + b);
+    /// </summary>
     public QueryType Fold(Func<QueryType, QueryType, QueryType> foldFunc) {
       var op = _op;
 
@@ -141,6 +197,10 @@ namespace Leap.Unity.Query {
       return value;
     }
 
+    /// <summary>
+    /// Returns the index of the first element that is equal to a specific value.  Will return
+    /// a negative index if there is no such element.
+    /// </summary>
     public int IndexOf(QueryType value) {
       var op = _op;
 
@@ -155,6 +215,10 @@ namespace Leap.Unity.Query {
       return -1;
     }
 
+    /// <summary>
+    /// Returns the index of the first element to satisfy a predicate.  Will return a negative
+    /// index if there is no such element.
+    /// </summary>
     public int IndexOf(Func<QueryType, bool> predicate) {
       var op = _op;
 
@@ -169,6 +233,9 @@ namespace Leap.Unity.Query {
       return -1;
     }
 
+    /// <summary>
+    /// Returns the last element in the sequence.  Will throw an error if the sequence is empty.
+    /// </summary>
     public QueryType Last() {
       var op = _op;
 
@@ -184,10 +251,17 @@ namespace Leap.Unity.Query {
       return obj;
     }
 
+    /// <summary>
+    /// Returns the last element in the sequence that satisfies a predicate.  Will throw an error
+    /// if there is no such element.
+    /// </summary>
     public QueryType Last(Func<QueryType, bool> predicate) {
       return Where(predicate).Last();
     }
 
+    /// <summary>
+    /// Returns the last element in the sequence.  Will return the default value if the sequence is empty.
+    /// </summary>
     public QueryType LastOrDefault() {
       var op = _op;
 
@@ -196,10 +270,18 @@ namespace Leap.Unity.Query {
       return obj;
     }
 
+    /// <summary>
+    /// Returns the last element in the sequence that satisfies a predicate.  Will return the default
+    /// value if there is no such element.
+    /// </summary>
     public QueryType LastOrDefault(Func<QueryType, bool> predicate) {
       return Where(predicate).LastOrDefault();
     }
 
+    /// <summary>
+    /// Returns the first and only element in the sequence.  Will throw an error if the length of the 
+    /// sequence is anything other than 1.
+    /// </summary>
     public QueryType Single() {
       var op = _op;
 
@@ -216,11 +298,19 @@ namespace Leap.Unity.Query {
       return obj;
     }
 
+    /// <summary>
+    /// Returns the first and only element in the sequence that satisfies the predicate.  Will throw
+    /// an error if the number of such elements is anything other than 1.
+    /// </summary>
     public QueryType Single(Func<QueryType, bool> predicate) {
       return Where(predicate).Single();
     }
 
     private static List<QueryType> _utilityList = new List<QueryType>();
+
+    /// <summary>
+    /// Converts the sequence into an array.
+    /// </summary>
     public QueryType[] ToArray() {
       try {
         AppendList(_utilityList);
@@ -230,6 +320,10 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Copies the elements of the sequence into an array.  Can optionally specify the offset into the array
+    /// where to copy.
+    /// </summary>
     public void FillArray(QueryType[] array, int offset = 0) {
       var op = _op;
 
@@ -239,17 +333,26 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Converts the sequence into a list.
+    /// </summary>
     public List<QueryType> ToList() {
       List<QueryType> list = new List<QueryType>();
       AppendList(list);
       return list;
     }
 
+    /// <summary>
+    /// Fills a given list with the elements in this sequence.  The list is cleared before the fill happens.
+    /// </summary>
     public void FillList(List<QueryType> list) {
       list.Clear();
       AppendList(list);
     }
 
+    /// <summary>
+    /// Appends the elements in this sequence to the end of a given list.
+    /// </summary>
     public void AppendList(List<QueryType> list) {
       var op = _op;
 
