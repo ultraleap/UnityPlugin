58cf60610470efca8b4e5894000714bb89c96ecb
Amarcolina <Amarcolina@gmail.com>
2017-06-09 14:02:00 -0700

    Added a preferences menu for interaction engine

---
+++ Assets/LeapMotion/Modules/InteractionEngine/Scripts/Editor/InteractionPreferences.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Leap.Unity.Interaction {
+
+  public static class InteractionPreferences {
+    public const string PROMPT_FOR_GRAVITY_KEY = "InteractionEngine_ShouldPrompForGravity";
+    public const string PROMPT_FOR_PHYSICS_TIMESTEP = "InteractionEngine_ShouldPrompForTimestep";
+
+    private static GUIContent _gravityPrompContent;
+    private static GUIContent _timestepPrompContent;
+
+    static InteractionPreferences() {
+      _gravityPrompContent = new GUIContent("Validate Gravity Magnitude", "Should prompt the user if the magnitude of the gravity vector is higher than the recommended amount?");
+      _timestepPrompContent = new GUIContent("Validate Physics Timestep", "Should prompt the user if the physics timestep is larger then the recommended value?");
+    }
+
+    public static bool shouldPrompForGravity {
+      get {
+        return EditorPrefs.GetBool(PROMPT_FOR_GRAVITY_KEY, defaultValue: true);
+      }
+      set {
+        EditorPrefs.SetBool(PROMPT_FOR_GRAVITY_KEY, value);
+      }
+    }
+
+    public static bool shouldPrompForPhysicsTimestep {
+      get {
+        return EditorPrefs.GetBool(PROMPT_FOR_PHYSICS_TIMESTEP, defaultValue: true);
+      }
+      set {
+        EditorPrefs.SetBool(PROMPT_FOR_PHYSICS_TIMESTEP, value);
+      }
+    }
+
+    [PreferenceItem("Leap Interaction")]
+    private static void preferencesGUI() {
+      bool newGravityValue = EditorGUILayout.Toggle(_gravityPrompContent, shouldPrompForGravity);
+      if (newGravityValue != shouldPrompForGravity) {
+        shouldPrompForGravity = newGravityValue;
+      }
+
+      bool newTimestepValue = EditorGUILayout.Toggle(_timestepPrompContent, shouldPrompForPhysicsTimestep);
+      if (newTimestepValue != shouldPrompForPhysicsTimestep) {
+        shouldPrompForPhysicsTimestep = newTimestepValue;
+      }
+    }
+  }
+}
