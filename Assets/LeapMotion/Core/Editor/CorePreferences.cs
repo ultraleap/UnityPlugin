023b5dfa5770e17a2a7a5647ad3440e477987749
Alex Marcolina <amarcolina@gmail.com>
2017-06-20 18:54:34 -0700

    working on core preferences

---
+++ Assets/LeapMotion/Core/Editor/CorePreferences.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Leap.Unity {
+
+  public static class CorePreferences {
+
+    private const string ALLOW_CLEAR_TRANSFORM_HOTKEY_KEY = "LeapMotion_AllowClearTransformHotkey";
+    private const string ALLOW_GROUP_OBJECTS_HOTKEY_KEY = "LeapMotion_AllowGroupObjectsHotkey";
+    private const string ALLOW_REMOVE_OBJECTS_HOTKEY_KEY = "LeapMotion_AllowRemoveObjectsHotkey";
+
+    public static bool allowCleartransformHotkey {
+
+    }
+
+
+  }
+}
