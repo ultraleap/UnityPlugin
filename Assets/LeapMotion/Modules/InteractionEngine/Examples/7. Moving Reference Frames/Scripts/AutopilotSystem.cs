fc90699bb4e0bf67481ec24026185d4872a4edcf
Nicholas Benson <nbenson@leapmotion.com>
2017-06-27 12:45:13 -0700

    Add moving reference frame example

---
+++ Assets/LeapMotion/Modules/InteractionEngine/Examples/7. Moving Reference Frames/Scripts/AutopilotSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Leap.Unity.Examples {
+
+  [AddComponentMenu("")]
+  public class AutopilotSystem : MonoBehaviour {
+
+    public Spaceship spaceship;
+
+    private float _targetSpeed = 0F;
+    private Vector3 _targetTorque = Vector3.zero;
+
+    private float _currentSpeed = 0F;
+    public float currentSpeed { get { return _currentSpeed; } }
+
+    public void IncreaseSpeed() {
+      _targetSpeed += 5F;
+    }
+
+    public void IncreaseTorque() {
+      _targetTorque += 36f * Vector3.one;
+    }
+
+    public void Stop() {
+      _targetSpeed = 0F;
+      _targetTorque = Vector3.zero;
+    }
+
+    void Update() {
+      spaceship.velocity = _targetSpeed * Vector3.forward;
+      spaceship.angularVelocity = _targetTorque;
+    }
+
+  }
+
+}
---
+++ Assets/LeapMotion/Modules/InteractionEngine/Examples/7. Moving Reference Frames/Scripts/LinearReferenceSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Leap.Unity.Examples {
+  
+  /// <summary>
+  /// This script keeps a GameObject in front of the ship, off to the side a bit. The
+  /// skybox cannot provide a frame of reference for the linear motion of the ship, so
+  /// the spawned object provides one instead.
+  /// 
+  /// This script assumes the ship is moving along the world forward axis.
+  /// </summary>
+  [AddComponentMenu("")]
+  public class LinearReferenceSpawner : MonoBehaviour {
+
+    public Spaceship spaceship;
+    public GameObject toSpawn;
+
+    private GameObject _spawnedObj;
+
+    void Update() {
+      bool justSpawned = false;
+      if (_spawnedObj == null) {
+        _spawnedObj = GameObject.Instantiate(toSpawn);
+        justSpawned = true;
+      }
+
+      if (justSpawned
+          || (_spawnedObj.transform.position - spaceship.transform.position).z < -1F) {
+        Vector3 spawnPos = spaceship.transform.position;
+        spawnPos += spaceship.velocity;
+        spawnPos += Vector3.left * 1.33F;
+        _spawnedObj.transform.position = spawnPos;
+      }
+    }
+
+  }
+
+}
