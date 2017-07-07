91405f524623a2e22cadfa0cd3a308e865866ef1
Johnathon Selstad <jselstad@leapmotion.com>
2017-07-06 20:33:40 -0700

    Add a fix for .NET 4.6 in Unity 2017

--- Assets/LeapMotion/Modules/InteractionEngine/Scripts/Utility/FiveParameterAction.cs
+++ Assets/LeapMotion/Modules/InteractionEngine/Scripts/Utility/FiveParameterAction.cs
@@ -8,7 +8,7 @@
  ******************************************************************************/
 
 namespace Leap.Unity.Interaction.Internal {
-
-  public delegate void Action<T1, T2, T3, T4, T5>(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5);
-
+#if !NET_4_6
+     public delegate void Action<T1, T2, T3, T4, T5>(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5);
+#endif
 }
