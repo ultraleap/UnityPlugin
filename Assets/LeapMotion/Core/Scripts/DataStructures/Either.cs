fd22b958fe1d02182e70f246688327db772d749d
Amarcolina <Amarcolina@gmail.com>
2017-07-18 15:35:36 -0700

    Added either and maybe

---
+++ Assets/LeapMotion/Core/Scripts/DataStructures/Either.cs
@@ -0,0 +1,208 @@
+﻿using System;
+
+namespace Leap.Unity {
+
+  public struct Either<A, B> : IEquatable<Either<A, B>>, IComparable, IComparable<Either<A, B>> {
+    private bool _isA;
+    private readonly A _a;
+    private readonly B _b;
+
+    public Maybe<A> a {
+      get {
+        if (_isA) {
+          return Maybe<A>.Some(_a);
+        } else {
+          return Maybe<A>.None;
+        }
+      }
+    }
+
+    public Maybe<B> b {
+      get {
+        if (_isA) {
+          return Maybe<B>.None;
+        } else {
+          return Maybe<B>.Some(_b);
+        }
+      }
+    }
+
+    public Either(A a) {
+      _isA = true;
+      _a = a;
+      _b = default(B);
+    }
+
+    public Either(ref A a) {
+      _isA = true;
+      _a = a;
+      _b = default(B);
+    }
+
+    public Either(B b) {
+      _isA = false;
+      _b = b;
+      _a = default(A);
+    }
+
+    public Either(ref B b) {
+      _isA = false;
+      _b = b;
+      _a = default(A);
+    }
+
+    public void IfA(Action<A> ifA) {
+      if (_isA) {
+        ifA(_a);
+      }
+    }
+
+    public void IfA(Action ifA) {
+      if (_isA) {
+        ifA();
+      }
+    }
+
+    public void IfB(Action<B> ifB) {
+      if (!_isA) {
+        ifB(_b);
+      }
+    }
+
+    public void IfB(Action ifB) {
+      if (!_isA) {
+        ifB();
+      }
+    }
+
+    public void Match(Action<A> ifA, Action<B> ifB) {
+      if (_isA) {
+        ifA(_a);
+      } else {
+        ifB(_b);
+      }
+    }
+
+    public void Match(Action ifA, Action<B> ifB) {
+      if (_isA) {
+        ifA();
+      } else {
+        ifB(_b);
+      }
+    }
+
+    public void Match(Action<A> ifA, Action ifB) {
+      if (_isA) {
+        ifA(_a);
+      } else {
+        ifB();
+      }
+    }
+
+    public bool TryGetA(out A a) {
+      a = _a;
+      return _isA;
+    }
+
+    public bool TryGetB(out B b) {
+      b = _b;
+      return !_isA;
+    }
+
+    public override int GetHashCode() {
+      if (_isA) {
+        return _a.GetHashCode();
+      } else {
+        return _b.GetHashCode();
+      }
+    }
+
+    public override bool Equals(object obj) {
+      if (obj is Either<A, B>) {
+        return Equals((Either<A, B>)obj);
+      } else {
+        return false;
+      }
+    }
+
+    public bool Equals(Either<A, B> other) {
+      if (_isA != other._isA) {
+        return false;
+      } else if (_isA) {
+        return _a.Equals(other._a);
+      } else {
+        return _b.Equals(other._b);
+      }
+    }
+
+    public int CompareTo(object obj) {
+      if (!(obj is Either<A, B>)) {
+        throw new ArgumentException();
+      } else {
+        return CompareTo((Either<A, B>)obj);
+      }
+    }
+
+    public int CompareTo(Either<A, B> other) {
+      if (_isA != other._isA) {
+        return _isA ? -1 : 1;
+      } else if (_isA) {
+        IComparable<A> ca = _a as IComparable<A>;
+        if (ca != null) {
+          return ca.CompareTo(other._a);
+        } else {
+          IComparable c = _a as IComparable;
+          if (c != null) {
+            return c.CompareTo(other._b);
+          } else {
+            return 0;
+          }
+        }
+      } else {
+        IComparable<B> cb = _b as IComparable<B>;
+        if (cb != null) {
+          return cb.CompareTo(other._b);
+        } else {
+          IComparable c = _b as IComparable;
+          if (c != null) {
+            return c.CompareTo(other._b);
+          } else {
+            return 0;
+          }
+        }
+      }
+    }
+
+    public static bool operator ==(Either<A, B> either0, Either<A, B> either1) {
+      return either0.Equals(either1);
+    }
+
+    public static bool operator !=(Either<A, B> either0, Either<A, B> either1) {
+      return !either0.Equals(either1);
+    }
+
+    public static bool operator >(Either<A, B> either0, Either<A, B> either1) {
+      return either0.CompareTo(either1) > 0;
+    }
+
+    public static bool operator >=(Either<A, B> either0, Either<A, B> either1) {
+      return either0.CompareTo(either1) >= 0;
+    }
+
+    public static bool operator <(Either<A, B> either0, Either<A, B> either1) {
+      return either0.CompareTo(either1) < 0;
+    }
+
+    public static bool operator <=(Either<A, B> either0, Either<A, B> either1) {
+      return either0.CompareTo(either1) <= 0;
+    }
+
+    public static implicit operator Either<A, B>(A a) {
+      return new Either<A, B>(ref a);
+    }
+
+    public static implicit operator Either<A, B>(B b) {
+      return new Either<A, B>(ref b);
+    }
+  }
+}
---
+++ Assets/LeapMotion/Core/Scripts/DataStructures/Maybe.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace Leap.Unity {
+
+  public struct Maybe<T> : IEquatable<Maybe<T>>, IComparable, IComparable<Maybe<T>> {
+    public static readonly Maybe<T> None = new Maybe<T>();
+
+    public readonly bool hasValue;
+
+    private readonly T _t;
+
+    public Maybe(T t) {
+      hasValue = t != null;
+      _t = t;
+    }
+
+    public Maybe(ref T t) {
+      hasValue = t != null;
+      _t = t;
+    }
+
+    public static Maybe<T> Some(T t) {
+      return new Maybe<T>(ref t);
+    }
+
+    public static Maybe<T> Some(ref T t) {
+      return new Maybe<T>(ref t);
+    }
+
+    public bool TryGetValue(out T t) {
+      t = _t;
+      return hasValue;
+    }
+
+    public void Match(Action<T> ifValue) {
+      if (hasValue) {
+        ifValue(_t);
+      }
+    }
+
+    public void Match(Action<T> ifValue, Action ifNot) {
+      if (hasValue) {
+        ifValue(_t);
+      } else {
+        ifNot();
+      }
+    }
+
+    public override int GetHashCode() {
+      return hasValue ? _t.GetHashCode() : 0;
+    }
+
+    public override bool Equals(object obj) {
+      if (obj is Maybe<T>) {
+        return Equals((Maybe<T>)obj);
+      } else {
+        return false;
+      }
+    }
+
+    public bool Equals(Maybe<T> other) {
+      if (hasValue != other.hasValue) {
+        return false;
+      } else if (hasValue) {
+        return _t.Equals(other._t);
+      } else {
+        return true;
+      }
+    }
+
+    public int CompareTo(object obj) {
+      if (!(obj is Maybe<T>)) {
+        throw new ArgumentException();
+      } else {
+        return CompareTo((Maybe<T>)obj);
+      }
+    }
+
+    public int CompareTo(Maybe<T> other) {
+      if (hasValue != other.hasValue) {
+        return hasValue ? 1 : -1;
+      } else if (hasValue) {
+        IComparable<T> ct = _t as IComparable<T>;
+        if (ct != null) {
+          return ct.CompareTo(other._t);
+        } else {
+          IComparable c = _t as IComparable;
+          if (c != null) {
+            return c.CompareTo(other._t);
+          } else {
+            return 0;
+          }
+        }
+      } else {
+        return 0;
+      }
+    }
+
+    public static bool operator ==(Maybe<T> maybe0, Maybe<T> maybe1) {
+      return maybe0.Equals(maybe1);
+    }
+
+    public static bool operator !=(Maybe<T> maybe0, Maybe<T> maybe1) {
+      return !maybe0.Equals(maybe1);
+    }
+
+    public static bool operator >(Maybe<T> maybe0, Maybe<T> maybe1) {
+      return maybe0.CompareTo(maybe1) > 0;
+    }
+
+    public static bool operator >=(Maybe<T> maybe0, Maybe<T> maybe1) {
+      return maybe0.CompareTo(maybe1) >= 0;
+    }
+
+    public static bool operator <(Maybe<T> maybe0, Maybe<T> maybe1) {
+      return maybe0.CompareTo(maybe1) < 0;
+    }
+
+    public static bool operator <=(Maybe<T> maybe0, Maybe<T> maybe1) {
+      return maybe0.CompareTo(maybe1) <= 0;
+    }
+
+    public static implicit operator Maybe<T>(T t) {
+      return new Maybe<T>(ref t);
+    }
+  }
+}
