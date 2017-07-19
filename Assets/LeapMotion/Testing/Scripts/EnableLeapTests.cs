6bb7d0243e119964ed18c730e651391aed8ac80b
Amarcolina <Amarcolina@gmail.com>
2017-07-19 16:46:59 -0700

    Added ability to enable leap tests

---
+++ Assets/LeapMotion/Testing/Scripts/EnableLeapTests.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public static class EnableLeapTests {
+
+  [MenuItem("Assets/Enable Leap Tests")]
+  public static void enableTests() {
+    string defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+    PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, defines + " LEAP_TESTS");
+  }
+}
