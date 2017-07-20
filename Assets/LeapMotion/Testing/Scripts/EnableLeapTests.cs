d16be94cd3a090a035fec6412c26ddddf075f874
Amarcolina <Amarcolina@gmail.com>
2017-07-19 17:15:12 -0700

    Add scenes whenever we enable tests

--- Assets/LeapMotion/Testing/Scripts/EnableLeapTests.cs
+++ Assets/LeapMotion/Testing/Scripts/EnableLeapTests.cs
@@ -1,10 +1,31 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
 
-public static class EnableLeapTests {
+namespace Leap.Unity.Testing {
 
-  [MenuItem("Assets/Enable Leap Tests")]
-  public static void enableTests() {
-    string defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
-    PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, defines + " LEAP_TESTS");
+  [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+  public class SetupLeapTestsAttribute : Attribute { }
+
+  public static class EnableLeapTests {
+
+    [MenuItem("Assets/Enable Leap Tests")]
+    public static void enableTests() {
+      var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+      foreach (var type in assemblies.SelectMany(a => a.GetTypes())) {
+        foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static)) {
+          var attributes = method.GetCustomAttributes(typeof(SetupLeapTestsAttribute), inherit: true);
+          if (attributes.Length == 0) {
+            continue;
+          }
+
+          method.Invoke(null, null);
+        }
+      }
+
+      string defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+      PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, defines + " LEAP_TESTS");
+    }
   }
 }
