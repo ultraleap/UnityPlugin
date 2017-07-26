1ae96d974dbce8a9a1ed1d0838655b7c56595d11
Amarcolina <Amarcolina@gmail.com>
2017-07-26 12:51:33 -0700

    Updating and fixing bugs with curve optimizer

--- Assets/CurveOptimizer.cs
+++ Assets/CurveOptimizer.cs
@@ -1,40 +1,21 @@
 using System;
 using UnityEngine;
+using Leap.Unity.Query;
 
 public static class AnimationCurveUtil {
 
   public static AnimationCurve Compress(AnimationCurve curve, float maxDelta = 0.005f, int checkSteps = 8) {
-    AnimationCurve compressedCurve = new AnimationCurve(curve.keys);
+    var curveArray = new AnimationCurve[] { curve };
 
-    var keys = curve.keys;
-    Keyframe nextKeyFrame = keys[keys.Length - 1];
-
-    for (int i = compressedCurve.keys.Length - 2; i > 0; i--) {
-      Keyframe currKeyframe = keys[i];
-      Keyframe prevKeyframe = keys[i - 1];
-
-      compressedCurve.RemoveKey(i);
-
-      for (int k = 0; k < checkSteps; k++) {
-        float percent = k / (checkSteps - 1.0f);
-        float prevTime = Mathf.Lerp(currKeyframe.time, prevKeyframe.time, percent);
-        float nextTime = Mathf.Lerp(currKeyframe.time, nextKeyFrame.time, percent);
-
-        float prevValue_c = compressedCurve.Evaluate(prevTime);
-        float prevValue_o = curve.Evaluate(prevTime);
-
-        float nextValue_c = compressedCurve.Evaluate(nextTime);
-        float nextValue_o = curve.Evaluate(nextTime);
-
-        if (Mathf.Abs(prevValue_c - prevValue_o) > maxDelta || Mathf.Abs(nextValue_c - nextValue_o) > maxDelta) {
-          compressedCurve.AddKey(currKeyframe);
-          nextKeyFrame = currKeyframe;
-          break;
-        }
-      }
-    }
-
-    return compressedCurve;
+    var result = CompressCurves(curveArray,
+                                (src, dst, t) => {
+                                  float originalValue = src[0].Evaluate(t);
+                                  float compressedValue = dst[0].Evaluate(t);
+                                  return Mathf.Abs(originalValue - compressedValue);
+                                },
+                                maxDelta,
+                                checkSteps);
+    return result[0];
   }
 
   //public static void CompressPositions(AnimationCurve xCurve,
@@ -64,34 +45,36 @@ public static class AnimationCurveUtil {
       nextFrame[i] = keys.Length - 1;
     }
 
-    for (int i = 0; i < curves.Length; i++) {
+    do {
+      for (int i = 0; i < curves.Length; i++) {
 
-      if (position[i] > 0) {
-        Keyframe nextKeyframe = keyframes[i][nextFrame[i]];
-        Keyframe currKeyframe = keyframes[i][position[i]];
-        Keyframe prevKeyframe = keyframes[i][position[i] - 1];
+        if (position[i] > 0) {
+          Keyframe nextKeyframe = keyframes[i][nextFrame[i]];
+          Keyframe currKeyframe = keyframes[i][position[i]];
+          Keyframe prevKeyframe = keyframes[i][position[i] - 1];
 
-        compressedCurves[i].RemoveKey(position[i]);
+          compressedCurves[i].RemoveKey(position[i]);
 
-        for (int k = 0; k < checkSteps; k++) {
-          float percent = k / (checkSteps - 1.0f);
+          for (int k = 0; k < checkSteps; k++) {
+            float percent = k / (checkSteps - 1.0f);
 
-          float prevTime = Mathf.Lerp(currKeyframe.time, prevKeyframe.time, percent);
-          float nextTime = Mathf.Lerp(currKeyframe.time, nextKeyframe.time, percent);
+            float prevTime = Mathf.Lerp(currKeyframe.time, prevKeyframe.time, percent);
+            float nextTime = Mathf.Lerp(currKeyframe.time, nextKeyframe.time, percent);
 
-          float prevCost = costFunc(curves, compressedCurves, prevTime);
-          float nextCost = costFunc(curves, compressedCurves, nextTime);
+            float prevCost = costFunc(curves, compressedCurves, prevTime);
+            float nextCost = costFunc(curves, compressedCurves, nextTime);
 
-          if (prevCost > maxCost || nextCost > maxCost) {
-            compressedCurves[i].AddKey(currKeyframe);
-            nextFrame[i] = position[i];
-            break;
+            if (prevCost > maxCost || nextCost > maxCost) {
+              compressedCurves[i].AddKey(currKeyframe);
+              nextFrame[i] = position[i];
+              break;
+            }
           }
-        }
 
-        position[i]--;
+          position[i]--;
+        }
       }
-    }
+    } while (position.Query().Any(p => p > 0));
 
     return compressedCurves;
   }
