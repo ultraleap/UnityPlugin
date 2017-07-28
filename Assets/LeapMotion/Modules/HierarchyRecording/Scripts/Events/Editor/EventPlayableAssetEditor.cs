1409698df270d137d6f8e6aff8cb9901291e10ea
Nick Benson <nickjbenson@gmail.com>
2017-07-27 18:22:24 -0700

    Woops forgot editor script for added Quat, Vec4 args

--- Assets/LeapMotion/Modules/HierarchyRecording/Scripts/Events/Editor/EventPlayableAssetEditor.cs
+++ Assets/LeapMotion/Modules/HierarchyRecording/Scripts/Events/Editor/EventPlayableAssetEditor.cs
@@ -11,18 +11,22 @@ namespace Leap.Unity.Recording {
     protected override void OnEnable() {
       base.OnEnable();
 
-      specifyConditionalDrawing(() => target.argumentType == SerializedArgumentType.Int,
-                                "intArg");
+      specifyConditionalDrawing(() => target.argumentType == SerializedArgumentType.Color,
+                                "colorArg");
       specifyConditionalDrawing(() => target.argumentType == SerializedArgumentType.Float,
                                 "floatArg");
+      specifyConditionalDrawing(() => target.argumentType == SerializedArgumentType.Int,
+                                "intArg");
+      specifyConditionalDrawing(() => target.argumentType == SerializedArgumentType.Quaternion,
+                                "quaternionArg");
+      specifyConditionalDrawing(() => target.argumentType == SerializedArgumentType.String,
+                                "stringArg");
       specifyConditionalDrawing(() => target.argumentType == SerializedArgumentType.Vector2,
                                 "vector2Arg");
       specifyConditionalDrawing(() => target.argumentType == SerializedArgumentType.Vector3,
                                 "vector3Arg");
-      specifyConditionalDrawing(() => target.argumentType == SerializedArgumentType.String,
-                                "stringArg");
-      specifyConditionalDrawing(() => target.argumentType == SerializedArgumentType.Color,
-                                "colorArg");
+      specifyConditionalDrawing(() => target.argumentType == SerializedArgumentType.Vector4,
+                                "vector4Arg");
     }
 
   }
