1f6d4c60e23f6608c7ee33beb90c474c8c0daf45
Amarcolina <Amarcolina@gmail.com>
2017-08-04 14:45:48 -0700

    Added recording interface and a raw recording implementation

---
+++ Assets/LeapMotion/Modules/HierarchyRecording/Scripts/LeapData/LeapRecording.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Leap.Unity.Recording {
+
+  public abstract class LeapRecording : ScriptableObject {
+
+    /// <summary>
+    /// Returns the length of the recording measured in seconds.
+    /// </summary>
+    public abstract float Length { get; }
+
+    /// <summary>
+    /// Samples the recording at the given time.  Must provide a frame object
+    /// that will be filled with the sampled frame data.  Returns true if
+    /// the frame was filled with recording data, and false if it was not.
+    /// 
+    /// The user can also optionally configure if the input time is allowed 
+    /// to be clamped to a valid timestamp.  When true, even if the requested
+    /// time does not exist, the recording will still fill the frame with data
+    /// taken from the closest valid timestamp.
+    /// </summary>
+    public abstract bool Sample(float time, Frame toFill, bool clampTimeToValid = true);
+  }
+}
---
+++ Assets/LeapMotion/Modules/HierarchyRecording/Scripts/LeapData/RawLeapRecording.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace Leap.Unity.Recording {
+
+  public class RawLeapRecording : LeapRecording {
+    public const double NS_TO_S = 1e-6;
+    public const double S_TO_NS = 1e6;
+
+    public List<Frame> frameList = new List<Frame>();
+
+    public long EarliestTimestamp {
+      get {
+        if (frameList.Count != 0) {
+          return frameList[0].Timestamp;
+        } else {
+          return 0;
+        }
+      }
+    }
+
+    public long LatestTimestamp {
+      get {
+        if (frameList.Count != 0) {
+          return frameList[frameList.Count - 1].Timestamp;
+        } else {
+          return 0;
+        }
+      }
+    }
+
+    public override float Length {
+      get {
+        if (frameList.Count != 0) {
+          return (float)((frameList[frameList.Count - 1].Timestamp - frameList[0].Timestamp) * NS_TO_S);
+        } else {
+          return 0;
+        }
+      }
+    }
+
+    public override bool Sample(float time, Frame toFill, bool clampTimeToValid = true) {
+      if (frameList.Count == 0) {
+        return false;
+      }
+
+      if (!clampTimeToValid && time < 0 || time > Length) {
+        return false;
+      }
+
+      long timestap = (long)(time * S_TO_NS) + EarliestTimestamp;
+
+      int index = 0;
+      while (index < frameList.Count && frameList[index].Timestamp > timestap) {
+        index++;
+      }
+
+      toFill.CopyFrom(frameList[index]);
+      return true;
+    }
+  }
+}
