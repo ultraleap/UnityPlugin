ee75a016d8b52a50200447e686d92fb29e92a443
Amarcolina <Amarcolina@gmail.com>
2017-08-11 12:29:07 -0700

    Adding empty folder utility

---
+++ Assets/LeapMotion/Core/Editor/EmptyFolderUtility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public static class EmptyFolderUtility {
+
+  [MenuItem("Assets/Delete Empty Folders")]
+  public static void DeleteEmptyFolders() {
+    string[] directories = Directory.GetDirectories("Assets", "*", SearchOption.AllDirectories);
+
+    foreach (var directory in directories) {
+      try {
+        if (!Directory.Exists(directory)) {
+          continue;
+        }
+
+        if (Directory.GetFiles(directory, "*", SearchOption.AllDirectories).Count(p => Path.GetExtension(p) != ".meta") > 0) {
+          continue;
+        }
+      } catch (Exception e) {
+        Debug.LogException(e);
+      }
+
+      try {
+        Directory.Delete(directory, recursive: true);
+      } catch (Exception e) {
+        Debug.LogError("Could not delete directory " + directory);
+        Debug.LogException(e);
+      }
+
+    }
+
+    AssetDatabase.Refresh();
+  }
+}
