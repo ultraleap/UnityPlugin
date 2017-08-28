b07fab5129d23934a66f16105bc36bad3de0f62e
Amarcolina <Amarcolina@gmail.com>
2017-08-28 13:21:35 -0700

    Added the IValueProxy interface

---
+++ Assets/LeapMotion/Core/Scripts/Animation/IValueProxy.cs
@@ -0,0 +1,22 @@
+﻿
+/// <summary>
+/// A simple interface that allows an object to act as a 'proxy'
+/// interface to another object.  The proxy can store a serialized
+/// representation of a value on another object.  The value of
+/// the proxy can either be updated from the object (pull), or
+/// be pushed out to the object (push).
+/// </summary>
+public interface IValueProxy {
+
+  /// <summary>
+  /// Called when this proxy should push its serialized representation
+  /// out to the target object.
+  /// </summary>
+  void OnPushValue();
+
+  /// <summary>
+  /// Called when this proxy should pull from the target object into
+  /// its serialized representation.
+  /// </summary>
+  void OnPullValue();
+}
