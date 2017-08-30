924966b5105ccb9dd9fe8bc376d17f586b951f41
Amarcolina <Amarcolina@gmail.com>
2017-08-30 12:00:36 -0700

    Added repeat operator

---
+++ Assets/LeapMotion/Core/Scripts/Query/Repeat.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Leap.Unity.Query {
+
+  public struct RepeatOp<SourceType, SourceOp> : IQueryOp<SourceType>
+    where SourceOp : IQueryOp<SourceType> {
+    private SourceOp _source;
+    private int _repeatTimes;
+    private int _currTimes;
+    private bool _didReturnValue;
+
+    public RepeatOp(SourceOp source, int times) {
+      _source = source;
+      _repeatTimes = times;
+      _currTimes = 0;
+      _didReturnValue = false;
+    }
+
+    public bool TryGetNext(out SourceType t) {
+      if (_currTimes == _repeatTimes) {
+        t = default(SourceType);
+        return false;
+      }
+
+      if (_source.TryGetNext(out t)) {
+        _didReturnValue = true;
+        return true;
+      }
+
+      _currTimes++;
+      if (_currTimes == _repeatTimes) {
+        return false;
+      }
+
+      _source.Reset();
+      if (_source.TryGetNext(out t)) {
+        return true;
+      }
+
+      return false;
+    }
+
+    public void Reset() {
+      _source.Reset();
+      _currTimes = 0;
+    }
+  }
+
+  public partial struct QueryWrapper<QueryType, QueryOp> where QueryOp : IQueryOp<QueryType> {
+
+    /// <summary>
+    /// Returns a new query operation representing the current sequence repeated a number of
+    /// times.  If the number of repetitions is less than zero, the sequence will be repeated
+    /// forever.
+    /// 
+    /// For example:
+    ///   (1, 2, 3).Query().Repeat(3)
+    /// Would result in:
+    ///   (1, 2, 3, 1, 2, 3, 1, 2, 3)
+    /// </summary>
+    public QueryWrapper<QueryType, RepeatOp<QueryType, QueryOp>> Repeat(int times = -1) {
+      return new QueryWrapper<QueryType, RepeatOp<QueryType, QueryOp>>(new RepeatOp<QueryType, QueryOp>(_op, times));
+    }
+  }
+}
