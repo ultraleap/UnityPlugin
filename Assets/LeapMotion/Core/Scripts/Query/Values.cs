2146d1f4ba4389a499a3bb26589bf1a7d0ea1367
Amarcolina <Amarcolina@gmail.com>
2017-08-30 12:11:25 -0700

    working on query values

---
+++ Assets/LeapMotion/Core/Scripts/Query/Values.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Leap.Unity.Query {
+
+  public static class Values {
+
+    public static QueryWrapper<T, SingleOp<T>> Single<T>(T value) {
+      return new QueryWrapper<T, SingleOp<T>>(new SingleOp<T>(value));
+    }
+
+    public struct SingleOp<T> : IQueryOp<T> {
+      private T _t;
+      private bool _hasReturned;
+
+      public SingleOp(T t) {
+        _t = t;
+        _hasReturned = false;
+      }
+
+      public bool TryGetNext(out T t) {
+        if (_hasReturned) {
+          t = default(T);
+          return false;
+        } else {
+          t = _t;
+          _hasReturned = true;
+          return true;
+        }
+      }
+
+      public void Reset() {
+        _hasReturned = false;
+      }
+    }
+
+  }
+}
