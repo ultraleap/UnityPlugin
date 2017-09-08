eadc1f01205c10c8d22863c74da417457353fff8
Amarcolina <Amarcolina@gmail.com>
2017-09-08 10:37:40 -0700

    removed default curve in exchange for simple constants

--- Assets/LeapMotion/Core/Scripts/Utils/GuiRectUtil.cs
+++ Assets/LeapMotion/Core/Scripts/Utils/GuiRectUtil.cs
@@ -32,6 +32,30 @@ namespace Leap.Unity {
       return new Vector3(rect.x + rect.width, rect.y + rect.height);
     }
 
+    public static Vector2 Map(this Vector2 point, Rect inRect, Rect outRect) {
+      point.x = Mathf.Lerp(outRect.x, outRect.x + outRect.width, Mathf.InverseLerp(inRect.x, inRect.x + inRect.width, point.x));
+      point.y = Mathf.Lerp(outRect.y, outRect.y + outRect.height, Mathf.InverseLerp(inRect.y, inRect.y + inRect.height, point.y));
+      return point;
+    }
+
+    public static Rect Encapsulate(this Rect rect, Vector2 point) {
+      if (point.x < rect.x) {
+        rect.width += rect.x - point.x;
+        rect.x = point.x;
+      } else if (point.x > rect.x + rect.width) {
+        rect.width = point.x - rect.x;
+      }
+
+      if (point.y < rect.y) {
+        rect.height += rect.y - point.y;
+        rect.y = point.y;
+      } else if (point.y > rect.y + rect.height) {
+        rect.height = point.y - rect.y;
+      }
+
+      return rect;
+    }
+
     public static void SplitHorizontally(this Rect rect, out Rect left, out Rect right) {
       left = rect;
       left.width /= 2;
