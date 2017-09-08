4ee0c98dc3691cf8d6e9f3d7c39413c271f06612
Amarcolina <Amarcolina@gmail.com>
2017-09-08 10:42:55 -0700

    Added some small docs

--- Assets/LeapMotion/Core/Scripts/Attributes/CurveBounds.cs
+++ Assets/LeapMotion/Core/Scripts/Attributes/CurveBounds.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 
 namespace Leap.Unity.Attributes {
 
+  /// <summary>
+  /// You can use this attribute to mark that an AnimationCurve can only have
+  /// values that fall within specific bounds.  The user will be prevented from
+  /// entering a curve that lies outside of these bounds.
+  /// </summary>
   public class CurveBounds : CombinablePropertyAttribute, IFullPropertyDrawer {
     public readonly Rect bounds;
     public readonly Color color;
@@ -43,6 +48,11 @@ namespace Leap.Unity.Attributes {
 #endif
   }
 
+  /// <summary>
+  /// You can use this attribute to mark that an AnimationCurve can only have values
+  /// that range from 0 to 1.  The user will be prevented from entering a curve that
+  /// lies outside of these bounds.
+  /// </summary>
   public class UnitCurve : CurveBounds {
     public UnitCurve() : base(new Rect(0, 0, 1, 1), Color.green) { }
     public UnitCurve(Color color) : base(new Rect(0, 0, 1, 1), color) { }
