bda5c28a8662b62a2e196c4e77ef00d39a433cdf
Amarcolina <Amarcolina@gmail.com>
2017-09-08 12:53:54 -0700

    Removed color

--- Assets/LeapMotion/Core/Scripts/Attributes/CurveBounds.cs
+++ Assets/LeapMotion/Core/Scripts/Attributes/CurveBounds.cs
@@ -13,31 +13,18 @@ namespace Leap.Unity.Attributes {
   /// </summary>
   public class CurveBoundsAttribute : CombinablePropertyAttribute, IFullPropertyDrawer {
     public readonly Rect bounds;
-    public readonly Color color;
 
     public CurveBoundsAttribute(Rect bounds) {
       this.bounds = bounds;
-      color = Color.green;
-    }
-
-    public CurveBoundsAttribute(Rect bounds, Color color) {
-      this.bounds = bounds;
-      this.color = color;
     }
 
     public CurveBoundsAttribute(float width, float height) {
       bounds = new Rect(0, 0, width, height);
-      color = Color.green;
-    }
-
-    public CurveBoundsAttribute(float width, float height, Color color) {
-      bounds = new Rect(0, 0, width, height);
-      this.color = color;
     }
 
 #if UNITY_EDITOR
     public void DrawProperty(Rect rect, SerializedProperty property, GUIContent label) {
-      EditorGUI.CurveField(rect, property, color, bounds);
+      EditorGUI.CurveField(rect, property, Color.green, bounds);
     }
 
     public override IEnumerable<SerializedPropertyType> SupportedTypes {
@@ -54,7 +41,6 @@ namespace Leap.Unity.Attributes {
   /// lies outside of these bounds.
   /// </summary>
   public class UnitCurveAttribute : CurveBoundsAttribute {
-    public UnitCurveAttribute() : base(new Rect(0, 0, 1, 1), Color.green) { }
-    public UnitCurveAttribute(Color color) : base(new Rect(0, 0, 1, 1), color) { }
+    public UnitCurveAttribute() : base(new Rect(0, 0, 1, 1)) { }
   }
 }
