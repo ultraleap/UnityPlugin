431166a2dd55a9335fa37c869957ecf060920beb
Amarcolina <Amarcolina@gmail.com>
2017-09-19 18:19:49 -0700

    Improved save utility

--- Assets/LeapMotion/Testing/Scripts/Editor/ResaveScenesUtility.cs
+++ Assets/LeapMotion/Testing/Scripts/Editor/ResaveScenesUtility.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using System.IO;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 
-public static class ResaveScenesUtility {
+namespace Leap.Unity {
+  using Query;
 
+  public static class ResaveScenesUtility {
 
-  [MenuItem("Assets/Re-Save All Scenes")]
-  public static void ReSaveAllScenes() {
-    foreach (var scenePath in AssetDatabase.FindAssets("t:Scene")) {
-      var scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
-      EditorSceneManager.SaveScene(scene);
+    [MenuItem("Assets/Re-Save All Scenes")]
+    public static void ReSaveAllScenes() {
+      EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+
+      var paths = AssetDatabase.FindAssets("t:Scene").
+                                Query().
+                                Select(guid => AssetDatabase.GUIDToAssetPath(guid)).
+                                ToList();
+
+      try {
+        for (int i = 0; i < paths.Count; i++) {
+          var scenePath = paths[i];
+          if (EditorUtility.DisplayCancelableProgressBar("Saving Scenes", "Saving " + Path.GetFileNameWithoutExtension(scenePath), i / (float)paths.Count)) {
+            return;
+          }
+
+          var scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
+          EditorSceneManager.SaveScene(scene);
+        }
+      } finally {
+        EditorUtility.ClearProgressBar();
+      }
     }
   }
 }
