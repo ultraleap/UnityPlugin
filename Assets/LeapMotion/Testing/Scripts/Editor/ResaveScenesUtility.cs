a557d0a9f2d3e3f84e37d2987c27b08e9db07a22
Amarcolina <Amarcolina@gmail.com>
2017-09-19 18:15:48 -0700

    Added resave utility

---
+++ Assets/LeapMotion/Testing/Scripts/Editor/ResaveScenesUtility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public static class ResaveScenesUtility {
+
+
+  [MenuItem("Assets/Re-Save All Scenes")]
+  public static void ReSaveAllScenes() {
+    foreach (var scenePath in AssetDatabase.FindAssets("t:Scene")) {
+      var scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
+      EditorSceneManager.SaveScene(scene);
+    }
+  }
+}
