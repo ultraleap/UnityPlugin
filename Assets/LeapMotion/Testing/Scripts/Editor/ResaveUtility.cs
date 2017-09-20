d8ab2b410e3692e482c7566f6d941a023f56edb3
Amarcolina <Amarcolina@gmail.com>
2017-09-19 18:27:53 -0700

    Adding asset saving to the mix

--- Assets/LeapMotion/Testing/Scripts/Editor/ResaveUtility.cs
+++ Assets/LeapMotion/Testing/Scripts/Editor/ResaveUtility.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using UnityEngine;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 
@@ -7,8 +8,13 @@ namespace Leap.Unity {
 
   public static class ResaveUtility {
 
-    [MenuItem("Assets/Save All Scenes/Assets")]
-    public static void ReSaveAllScenes() {
+    [MenuItem("Assets/Save All Scenes\\Assets")]
+    public static void SaveAllScenesAndAssets() {
+      SaveAllScenes();
+      SaveAllAssets();
+    }
+
+    public static void SaveAllScenes() {
       EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 
       var paths = AssetDatabase.FindAssets("t:Scene").
@@ -19,14 +25,41 @@ namespace Leap.Unity {
       try {
         for (int i = 0; i < paths.Count; i++) {
           var scenePath = paths[i];
+
           if (EditorUtility.DisplayCancelableProgressBar("Saving Scenes", "Saving " + Path.GetFileNameWithoutExtension(scenePath), i / (float)paths.Count)) {
-            return;
+            break;
           }
 
           var scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
           EditorSceneManager.SaveScene(scene);
         }
       } finally {
+        EditorUtility.ClearProgressBar();
+      }
+    }
+
+    [MenuItem("Assets/Save All Assets For Reals")]
+    public static void SaveAllAssets() {
+      var paths = AssetDatabase.FindAssets("t:ScriptableObject").
+                                Query().
+                                Concat(AssetDatabase.FindAssets("t:GameObject").
+                                                     Query()).
+                                Select(guid => AssetDatabase.GUIDToAssetPath(guid)).
+                                ToList();
+
+      try {
+        for (int i = 0; i < paths.Count; i++) {
+          var assetPath = paths[i];
+
+          if (EditorUtility.DisplayCancelableProgressBar("Saving Assets", "Saving " + Path.GetFileNameWithoutExtension(assetPath), i / (float)paths.Count)) {
+            break;
+          }
+
+          var obj = AssetDatabase.LoadAssetAtPath<Object>(assetPath);
+          EditorUtility.SetDirty(obj);
+        }
+      } finally {
+        AssetDatabase.SaveAssets();
         EditorUtility.ClearProgressBar();
       }
     }
