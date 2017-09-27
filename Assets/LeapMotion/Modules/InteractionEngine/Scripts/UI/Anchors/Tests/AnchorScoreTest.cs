00050492c16585c41c5b4c8f304662c0c0515063
Nicholas Benson <nbenson@leapmotion.com>
2017-09-27 15:10:10 -0700

    Add alwaysAttachDistance for AnchorableBehaviour

--- Assets/LeapMotion/Modules/InteractionEngine/Scripts/UI/Anchors/Tests/AnchorScoreTest.cs
+++ Assets/LeapMotion/Modules/InteractionEngine/Scripts/UI/Anchors/Tests/AnchorScoreTest.cs
@@ -27,6 +27,9 @@ namespace Leap.Unity.Interaction {
     [Range(5F, 90F)]
     public float maxAngle = 60F;
 
+    [Range(0f, 1f)]
+    public float alwaysAttachDistance = 0f;
+
     [Header("Generated")]
     public Texture2D textureXY;
     public Texture2D textureXZ;
@@ -68,7 +71,7 @@ namespace Leap.Unity.Interaction {
                            + (dir1 * i * widthPerPixel - (dir1 * center))
                            + (dir2 * j * widthPerPixel - (dir2 * center));
 
-        float score = AnchorableBehaviour.GetAnchorScore(anchObjPos, anchObjVel, anchorPos, maxRange, maxRange / 2F, Mathf.Cos(maxAngle * Mathf.Deg2Rad));
+        float score = AnchorableBehaviour.GetAnchorScore(anchObjPos, anchObjVel, anchorPos, maxRange, maxRange * 0.40F, Mathf.Cos(maxAngle * Mathf.Deg2Rad), alwaysAttachDistance);
         Color pixel = Color.HSVToRGB(Mathf.Lerp(1F, 0F, score), Mathf.Lerp(0F, 1F, score), Mathf.Lerp(0F, 1F, score));
         _pixels[i + j * tex.width] = pixel;
       }
