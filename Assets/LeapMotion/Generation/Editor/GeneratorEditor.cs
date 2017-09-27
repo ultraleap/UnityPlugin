151e90518e584a2e2bb70cb80af3f9ad3a9de310
Amarcolina <Amarcolina@gmail.com>
2017-09-27 16:49:37 -0700

    Added splines and basic generator structure

---
+++ Assets/LeapMotion/Generation/Editor/GeneratorEditor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+namespace Leap.Unity.Generation {
+
+  [CanEditMultipleObjects]
+  [CustomEditor(typeof(GeneratorBase), editorForChildClasses: true)]
+  public class GeneratorEditor : CustomEditorBase<GeneratorBase> {
+
+    [MenuItem("Assets/Run All Generators")]
+    public static void TriggerGeneration() {
+      foreach (var gen in Resources.FindObjectsOfTypeAll<GeneratorBase>()) {
+        try {
+          gen.Generate();
+        } catch (Exception e) {
+          Debug.LogException(e);
+        }
+      }
+    }
+
+    protected override void OnEnable() {
+      base.OnEnable();
+
+      dontShowScriptField();
+    }
+
+    public override void OnInspectorGUI() {
+      drawScriptField();
+
+      if (GUILayout.Button("Generate")) {
+        foreach (var target in targets) {
+          target.Generate();
+        }
+      }
+
+      base.OnInspectorGUI();
+    }
+  }
+}
---
+++ Assets/LeapMotion/Generation/GeneratorBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+namespace Leap.Unity.Generation {
+
+  public abstract class GeneratorBase : ScriptableObject {
+    public abstract void Generate();
+  }
+}
