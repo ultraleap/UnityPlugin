3f6ab1ee9ad4f10a2475bbe4bf6bbde3be453245
Nicholas Benson <nbenson@leapmotion.com>
2017-10-23 13:37:05 -0700

    reduce FaceTargetWithoutTwist to sugar on LookRotation

--- Assets/LeapMotion/Core/Scripts/Utils/Utils.cs
+++ Assets/LeapMotion/Core/Scripts/Utils/Utils.cs
@@ -839,6 +839,38 @@ namespace Leap.Unity {
       thisTransform.rotation = Quaternion.LookRotation(thisTransform.position - transform.position, upwards);
     }
 
+    /// <summary>
+    /// Returns the rotation that makes a transform at objectPosition point its forward
+    /// vector at targetPosition and keep its rightward vector parallel with the horizon
+    /// defined by a normal of Vector3.up.
+    /// 
+    /// For example, this will point an interface panel at a user camera while
+    /// maintaining the alignment of text and other elements with the horizon line.
+    /// </summary>
+    /// <returns></returns>
+    public static Quaternion FaceTargetWithoutTwist(Vector3 fromPosition,
+                                                    Vector3 targetPosition,
+                                                    bool flip180 = false) {
+      return FaceTargetWithoutTwist(fromPosition, targetPosition, Vector3.up, flip180);
+    }
+
+    /// <summary>
+    /// Returns the rotation that makes a transform at objectPosition point its forward
+    /// vector at targetPosition and keep its rightward vector parallel with the horizon
+    /// defined by the upwardDirection normal.
+    /// 
+    /// For example, this will point an interface panel at a user camera while
+    /// maintaining the alignment of text and other elements with the horizon line.
+    /// </summary>
+    public static Quaternion FaceTargetWithoutTwist(Vector3 objectPosition,
+                                                    Vector3 targetPosition,
+                                                    Vector3 upwardDirection,
+                                                    bool flip180 = false) {
+      Vector3 objToTarget = targetPosition - objectPosition;
+      return Quaternion.LookRotation((flip180 ? -1 : 1) * objToTarget,
+                                     upwardDirection);
+    }
+
     #endregion
 
     #region Quaternion Utils
@@ -1577,43 +1609,6 @@ namespace Leap.Unity {
     }
 
     #endregion
-
-    #endregion
-
-    #region Pose Utils
-
-    /// <summary>
-    /// Returns the rotation that makes a transform at objectPosition point its forward
-    /// vector at targetPosition and keep its rightward vector parallel with the horizon
-    /// defined by a normal of Vector3.up.
-    /// 
-    /// For example, this will point an interface panel at a user camera while
-    /// maintaining the alignment of text and other elements with the horizon line.
-    /// </summary>
-    /// <returns></returns>
-    public static Quaternion FaceTargetWithoutTwist(Vector3 fromPosition,
-                                                    Vector3 targetPosition,
-                                                    bool flip180 = false) {
-      return FaceTargetWithoutTwist(fromPosition, targetPosition, Vector3.up, flip180);
-    }
-
-    /// <summary>
-    /// Returns the rotation that makes a transform at objectPosition point its forward
-    /// vector at targetPosition and keep its rightward vector parallel with the horizon
-    /// defined by the upwardDirection normal.
-    /// 
-    /// For example, this will point an interface panel at a user camera while
-    /// maintaining the alignment of text and other elements with the horizon line.
-    /// </summary>
-    public static Quaternion FaceTargetWithoutTwist(Vector3 objectPosition,
-                                                    Vector3 targetPosition,
-                                                    Vector3 upwardDirection,
-                                                    bool flip180 = false) {
-      Vector3 objToTarget = -1f * (targetPosition - objectPosition);
-      Vector3 horizonRight = Vector3.Cross(upwardDirection, objToTarget);
-      Vector3 objUp = Vector3.Cross(objToTarget.normalized, horizonRight.normalized);
-      return Quaternion.LookRotation((flip180 ? -1 : 1) * objToTarget, objUp);
-    }
 
     #endregion
 
