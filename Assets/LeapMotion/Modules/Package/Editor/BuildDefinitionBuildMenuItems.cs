57ec2d94a6da8020d649396c3c5b59383b185b42
Alex Marcolina <amarcolina@gmail.com>
2017-11-16 10:23:05 -0800

    Cleared out definitions

--- Assets/LeapMotion/Modules/Package/Editor/BuildDefinitionBuildMenuItems.cs
+++ Assets/LeapMotion/Modules/Package/Editor/BuildDefinitionBuildMenuItems.cs
@@ -3,12 +3,6 @@ using UnityEditor;
 namespace Leap.Unity.Packaging {
 
   public class BuildDefinitionBuildMenuItems { 
-
-    // My Super Build
-    [MenuItem("Build/My Super Build", priority = 20)]
-    public static void Build_a568da3389816734e866c3c97cfce958() {
-      BuildDefinition.Build("a568da3389816734e866c3c97cfce958");
-    }
   }
 }
 
