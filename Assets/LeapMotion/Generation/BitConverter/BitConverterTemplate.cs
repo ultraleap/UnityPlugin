8588e4acbf6339fe4afd944f4d70ae46af7edf68
Alex Marcolina <amarcolina@gmail.com>
2017-11-17 11:31:53 -0800

    working towards tests

--- Assets/LeapMotion/Generation/BitConverter/BitConverterTemplate.cs
+++ Assets/LeapMotion/Generation/BitConverter/BitConverterTemplate.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using NUnit.Framework;
 
 namespace Leap.Unity.Generation {
 
-  public static class BitConverterNonAlloc {
+  public class BitConverterNonAllocTests {
     //BEGIN
 
-    public static _Primitive_ To_Primitive_(byte[] bytes, int offset = 0) {
-      unsafe {
-        fixed (byte* ptr = &bytes[offset]) {
-          _Primitive_* primitivePtr = (_Primitive_*)ptr;
-          return *primitivePtr;
-        }
+    public byte[] randomBytes;
+
+    [SetUp]
+    void Setup() {
+      randomBytes = new byte[128];
+      for (int i = 0; i < randomBytes.Length; i++) {
+        randomBytes[i] = (byte)UnityEngine.Random.Range(int.MinValue, int.MaxValue);
       }
+    }
+
+    [Test]
+    public void TestToSingle() {
+      Single expected = BitConverter.ToSingle(randomBytes, 0);
+      Single actual = BitConverterNonAlloc.ToSingle(randomBytes, 0);
+      Assert.That(actual, Is.EqualTo(expected));
     }
 
     public static _Primitive_ To_Primitive_(byte[] bytes, ref int offset) {
