509361bb830fae6e52f14c2c58c2f00ca2d24243
Alex Marcolina <amarcolina@gmail.com>
2017-11-17 10:23:00 -0800

    Adding generic enum helper

---
+++ Assets/LeapMotion/Core/Scripts/Utils/Enum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class Enum<T> {
+  public static readonly string[] names;
+  public static readonly T[] values;
+
+  static Enum() {
+    names = (string[])Enum.GetNames(typeof(T));
+    values = (T[])Enum.GetValues(typeof(T));
+  }
+}
