4773989366c792f0433e807f0d8755494f159d86
Alex Marcolina <amarcolina@gmail.com>
2017-11-17 10:23:21 -0800

    Added namespace

--- Assets/LeapMotion/Core/Scripts/Utils/Enum.cs
+++ Assets/LeapMotion/Core/Scripts/Utils/Enum.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 
-public static class Enum<T> {
-  public static readonly string[] names;
-  public static readonly T[] values;
+namespace Leap.Unity {
 
-  static Enum() {
-    names = (string[])Enum.GetNames(typeof(T));
-    values = (T[])Enum.GetValues(typeof(T));
+  public static class Enum<T> {
+    public static readonly string[] names;
+    public static readonly T[] values;
+
+    static Enum() {
+      names = (string[])Enum.GetNames(typeof(T));
+      values = (T[])Enum.GetValues(typeof(T));
+    }
   }
 }
