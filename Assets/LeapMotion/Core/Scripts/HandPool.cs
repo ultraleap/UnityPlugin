695fc973b373fc4d775ec48d0a050367e5e70de4
Nick Benson <nickjbenson@gmail.com>
2017-11-22 10:31:36 -0800

    Change modelList and modelsCheckedOut to NonSerialized

--- Assets/LeapMotion/Core/Scripts/HandPool.cs
+++ Assets/LeapMotion/Core/Scripts/HandPool.cs
@@ -12,6 +12,7 @@ using UnityEngine.Assertions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Serialization;
+using System;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -61,10 +62,10 @@ namespace Leap.Unity {
       public IHandModel RightModel;
       [HideInInspector]
       public bool IsRightToBeSpawned;
-      [HideInInspector]
       public List<IHandModel> modelList = new List<IHandModel>();
-      [HideInInspector]
       public List<IHandModel> modelsCheckedOut = new List<IHandModel>();
+      [NonSerialized]
+      [NonSerialized]
       public bool IsEnabled = true;
       public bool CanDuplicate;
 
