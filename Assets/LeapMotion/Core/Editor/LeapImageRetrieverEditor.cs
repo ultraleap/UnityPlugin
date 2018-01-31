08e14a6786c041a525f3b4b9e877a8e2501d44ac
Alex Marcolina <amarcolina@gmail.com>
2018-01-31 15:21:05 -0800

    Removed bright from keywords

--- Assets/LeapMotion/Core/Editor/LeapImageRetrieverEditor.cs
+++ Assets/LeapMotion/Core/Editor/LeapImageRetrieverEditor.cs
@@ -14,13 +14,13 @@ namespace Leap.Unity{
   [CustomEditor(typeof(LeapImageRetriever))]
   public class LeapImageRetrieverEditor : CustomEditorBase<LeapImageRetriever> {
 
-    private GUIContent _brightTextureGUIContent;
+    private GUIContent _TextureGUIContent;
     private GUIContent _distortionTextureGUIContent;
 
     protected override void OnEnable() {
       base.OnEnable();
 
-      _brightTextureGUIContent = new GUIContent("Bright Texture");
+      _TextureGUIContent = new GUIContent("Sensor Texture");
       _distortionTextureGUIContent = new GUIContent("Distortion Texture");
     }
 
@@ -32,7 +32,7 @@ namespace Leap.Unity{
         var dataType = typeof(Object);
 
         EditorGUI.BeginDisabledGroup(true);
-        EditorGUILayout.ObjectField(_brightTextureGUIContent, data.TextureData.CombinedTexture, dataType, true);
+        EditorGUILayout.ObjectField(_TextureGUIContent, data.TextureData.CombinedTexture, dataType, true);
         EditorGUILayout.ObjectField(_distortionTextureGUIContent, data.Distortion.CombinedTexture, dataType, true);
         EditorGUI.EndDisabledGroup();
       }
