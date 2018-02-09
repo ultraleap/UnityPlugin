fd9d5134260dfc2940ce1a9d448e1d2f3e77af54
Alex Marcolina <amarcolina@gmail.com>
2018-02-08 16:07:07 -0800

    Working on improving the dislocator

--- Assets/LeapMotion/Core/Scripts/XR/LeapEyeDislocator.cs
+++ Assets/LeapMotion/Core/Scripts/XR/LeapEyeDislocator.cs
@@ -22,17 +22,16 @@ namespace Leap.Unity {
   public class LeapEyeDislocator : MonoBehaviour {
 
     [SerializeField]
-    private EyeType _eyeType = new EyeType(EyeType.OrderType.CENTER);
-
-    [SerializeField]
     private bool _useCustomBaseline = false;
 
     [MinValue(0), Units("MM"), InspectorName("Baseline")]
     [SerializeField]
     private float _customBaselineValue = 64;
 
+    [SerializeField]
+    private bool _showEyePositions = false;
+
     private LeapXRServiceProvider _provider;
-    private Matrix4x4 _finalCenterMatrix;
     private Maybe<float> _deviceBaseline = Maybe.None;
 
     private Camera _cachedCamera;
@@ -60,20 +59,23 @@ namespace Leap.Unity {
     }
 
     private void OnDisable() {
+      _camera.ResetStereoViewMatrices();
+
       _provider.OnDeviceSafe -= onDevice;
     }
 
     private void Update() {
-      _eyeType.Reset();
-    }
+      _camera.ResetStereoViewMatrices();
+      Matrix4x4 leftMat = _camera.GetStereoViewMatrix(Camera.StereoscopicEye.Left);
+      Matrix4x4 rightMat = _camera.GetStereoViewMatrix(Camera.StereoscopicEye.Right);
 
-    private void OnPreCull() {
-      _camera.ResetWorldToCameraMatrix();
-      _finalCenterMatrix = _camera.worldToCameraMatrix;
-    }
+      Pose leftPose = leftMat.inverse.GetPose();
+      Pose rightPose = rightMat.inverse.GetPose();
 
-    private void OnPreRender() {
-      _eyeType.BeginCamera(); // swaps eye
+      Pose centerPose = new Pose() {
+        position = Vector3.Lerp(leftPose.position, rightPose.position, 0.5f),
+        rotation = Quaternion.Slerp(leftPose.rotation, rightPose.rotation, 0.5f)
+      };
 
       Maybe<float> baselineToUse = Maybe.None;
       if (_useCustomBaseline) {
@@ -82,15 +84,43 @@ namespace Leap.Unity {
         baselineToUse = _deviceBaseline;
       }
 
-      float baseline;
-      if (baselineToUse.TryGetValue(out baseline)) {
-        Vector3 ipdOffset = (_eyeType.IsLeftEye ? 1 : -1) * Vector3.right * baseline * 0.5f * 1e-3f;
-        Vector3 providerOffset = Vector3.forward * _provider.deviceOffsetZAxis - Vector3.up * _provider.deviceOffsetYAxis;
-        Quaternion providerRotation = Quaternion.AngleAxis(_provider.deviceTiltXAxis, Vector3.right);
+      float baselineValue;
+      if (baselineToUse.TryGetValue(out baselineValue)) {
+        var centerViewMatrix = Matrix4x4.TRS(centerPose.position, centerPose.rotation, Vector3.one).inverse;
 
-        _camera.worldToCameraMatrix = Matrix4x4.Translate(providerOffset + ipdOffset) *
-                                      Matrix4x4.Rotate(providerRotation) *
-                                      _finalCenterMatrix;
+        adjustViewMatrix(Camera.StereoscopicEye.Left, centerViewMatrix, baselineValue);
+        adjustViewMatrix(Camera.StereoscopicEye.Right, centerViewMatrix, baselineValue);
+      }
+    }
+
+    private void adjustViewMatrix(Camera.StereoscopicEye eye, Matrix4x4 centerMatrix, float baseline) {
+      float eyeOffset = eye == Camera.StereoscopicEye.Left ? 1 : -1;
+      Vector3 ipdOffset = eyeOffset * Vector3.right * baseline * 0.5f * 1e-3f;
+      Vector3 providerForwardOffset = Vector3.forward * _provider.deviceOffsetZAxis;
+      Vector3 providerVerticalOffset = -Vector3.up * _provider.deviceOffsetYAxis;
+      Quaternion providerRotation = Quaternion.AngleAxis(_provider.deviceTiltXAxis, Vector3.right);
+
+      _camera.SetStereoViewMatrix(eye, 
+                                       Matrix4x4.Rotate(providerRotation) *
+                                       Matrix4x4.Translate(providerForwardOffset + ipdOffset) *
+                                       Matrix4x4.Translate(providerVerticalOffset) *
+                                       centerMatrix);
+    }
+
+    private void OnDrawGizmos() {
+      if (_showEyePositions && Application.isPlaying) {
+        Matrix4x4 leftMat = _camera.GetStereoViewMatrix(Camera.StereoscopicEye.Left);
+        Matrix4x4 rightMat = _camera.GetStereoViewMatrix(Camera.StereoscopicEye.Right);
+
+        Vector3 leftPos = leftMat.inverse.MultiplyPoint3x4(Vector3.zero);
+        Vector3 rightPos = rightMat.inverse.MultiplyPoint3x4(Vector3.zero);
+
+        Gizmos.color = Color.white;
+        Gizmos.DrawSphere(leftPos, 0.02f);
+        Gizmos.DrawSphere(rightPos, 0.02f);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(leftPos, rightPos);
       }
     }
   }
