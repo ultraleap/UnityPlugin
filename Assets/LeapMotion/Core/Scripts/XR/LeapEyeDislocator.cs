bcb112234169af176a485a31b0d408f1418f242b
Alex Marcolina <amarcolina@gmail.com>
2018-02-08 16:55:32 -0800

    Drastically improved LeapEyeDislocator

--- Assets/LeapMotion/Core/Scripts/XR/LeapEyeDislocator.cs
+++ Assets/LeapMotion/Core/Scripts/XR/LeapEyeDislocator.cs
@@ -33,6 +33,7 @@ namespace Leap.Unity {
 
     private LeapXRServiceProvider _provider;
     private Maybe<float> _deviceBaseline = Maybe.None;
+    private bool _hasVisitedPreCull = false;
 
     private Camera _cachedCamera;
     private Camera _camera {
@@ -66,16 +67,14 @@ namespace Leap.Unity {
 
     private void Update() {
       _camera.ResetStereoViewMatrices();
-      Matrix4x4 leftMat = _camera.GetStereoViewMatrix(Camera.StereoscopicEye.Left);
-      Matrix4x4 rightMat = _camera.GetStereoViewMatrix(Camera.StereoscopicEye.Right);
+      _hasVisitedPreCull = false;
+    }
 
-      Pose leftPose = leftMat.inverse.GetPose();
-      Pose rightPose = rightMat.inverse.GetPose();
-
-      Pose centerPose = new Pose() {
-        position = Vector3.Lerp(leftPose.position, rightPose.position, 0.5f),
-        rotation = Quaternion.Slerp(leftPose.rotation, rightPose.rotation, 0.5f)
-      };
+    private void OnPreCull() {
+      if (_hasVisitedPreCull) {
+        return;
+      }
+      _hasVisitedPreCull = true;
 
       Maybe<float> baselineToUse = Maybe.None;
       if (_useCustomBaseline) {
@@ -86,25 +85,34 @@ namespace Leap.Unity {
 
       float baselineValue;
       if (baselineToUse.TryGetValue(out baselineValue)) {
-        var centerViewMatrix = Matrix4x4.TRS(centerPose.position, centerPose.rotation, Vector3.one).inverse;
+        baselineValue *= 1e-3f;
 
-        adjustViewMatrix(Camera.StereoscopicEye.Left, centerViewMatrix, baselineValue);
-        adjustViewMatrix(Camera.StereoscopicEye.Right, centerViewMatrix, baselineValue);
+        Matrix4x4 leftMat = _camera.GetStereoViewMatrix(Camera.StereoscopicEye.Left);
+        Matrix4x4 rightMat = _camera.GetStereoViewMatrix(Camera.StereoscopicEye.Right);
+
+        Vector3 leftPos = leftMat.inverse.MultiplyPoint3x4(Vector3.zero);
+        Vector3 rightPos = rightMat.inverse.MultiplyPoint3x4(Vector3.zero);
+        float existingBaseline = Vector3.Distance(leftPos, rightPos);
+
+        float baselineAdjust = baselineValue - existingBaseline;
+
+        adjustViewMatrix(Camera.StereoscopicEye.Left, baselineAdjust);
+        adjustViewMatrix(Camera.StereoscopicEye.Right, baselineAdjust);
       }
     }
 
-    private void adjustViewMatrix(Camera.StereoscopicEye eye, Matrix4x4 centerMatrix, float baseline) {
+    private void adjustViewMatrix(Camera.StereoscopicEye eye, float baselineAdjust) {
       float eyeOffset = eye == Camera.StereoscopicEye.Left ? 1 : -1;
-      Vector3 ipdOffset = eyeOffset * Vector3.right * baseline * 0.5f * 1e-3f;
+      Vector3 ipdOffset = eyeOffset * Vector3.right * baselineAdjust * 0.5f;
       Vector3 providerForwardOffset = Vector3.forward * _provider.deviceOffsetZAxis;
       Vector3 providerVerticalOffset = -Vector3.up * _provider.deviceOffsetYAxis;
       Quaternion providerRotation = Quaternion.AngleAxis(_provider.deviceTiltXAxis, Vector3.right);
 
-      _camera.SetStereoViewMatrix(eye, 
-                                       Matrix4x4.Rotate(providerRotation) *
+      var existingMatrix = _camera.GetStereoViewMatrix(eye);
+      _camera.SetStereoViewMatrix(eye, Matrix4x4.Rotate(providerRotation) *
                                        Matrix4x4.Translate(providerForwardOffset + ipdOffset) *
                                        Matrix4x4.Translate(providerVerticalOffset) *
-                                       centerMatrix);
+                                       existingMatrix);
     }
 
     private void OnDrawGizmos() {
