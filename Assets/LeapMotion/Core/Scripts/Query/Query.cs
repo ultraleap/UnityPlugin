7eaaae8de4b603e80705bd8be3b2cb03246cc8dc
Alex Marcolina <amarcolina@gmail.com>
2018-03-21 15:07:58 -0700

    Added documentation and extra operators

--- Assets/LeapMotion/Core/Scripts/Query/Query.cs
+++ Assets/LeapMotion/Core/Scripts/Query/Query.cs
@@ -6,10 +6,16 @@ namespace Leap.Unity.Query {
 
   public static class QueryConversionExtensions {
 
+    /// <summary>
+    /// Constructs a new Query from the given Collection.
+    /// </summary>
     public static Query<T> Query<T>(this ICollection<T> collection) {
       return new Query<T>(collection);
     }
 
+    /// <summary>
+    /// Constructs a new Query from the given IEnumerable
+    /// </summary>
     public static Query<T> Query<T>(this IEnumerable<T> enumerable) {
       List<T> list = Pool<List<T>>.Spawn();
       try {
@@ -21,6 +27,9 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Constructs a new Query from the given IEnumerator
+    /// </summary>
     public static Query<T> Query<T>(this IEnumerator<T> enumerator) {
       List<T> list = Pool<List<T>>.Spawn();
       try {
@@ -33,6 +42,9 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Constructs a new Query from the given two dimensional array.
+    /// </summary>
     public static Query<T> Query<T>(this T[,] array) {
       var dst = ArrayPool<T>.Spawn(array.GetLength(0) * array.GetLength(1));
       int dstIndex = 0;
@@ -46,18 +58,58 @@ namespace Leap.Unity.Query {
     }
   }
 
-  public partial struct Query<T> {
+  /// <summary>
+  /// A Query object is a type of immutable ordered collection of elements that can be 
+  /// used to perform useful queries.  Thesse queries are very similar to LINQ style
+  /// queries, providing useful methods such as Where, Select, Concat, etc....
+  /// 
+  /// The Query struct and its interfaces use a pooling strategy backed by ArrayPool
+  /// to incur an amortized cost of zero GC allocations.
+  /// 
+  /// A Query struct is immutable, and so cannot be modified once it has been created.
+  /// You can use a query in few ways:
+  ///  - The simplest way is to call an operator method such as Where or Select.  These
+  ///    methods CONSUME the query to produce a new query.  Trying to use the original
+  ///    query after it has been consumed will cause a runtime error.
+  ///  - The next way os to call a collapsing operator, which will consume the query
+  ///    and produce a non-query value or other side-effect.  Examples of collapsing 
+  ///    operators are First, Last, or ElementAt.
+  ///  - The last way to use a query is to Deconstruct it, by calling a Deconstruct
+  ///    method to destroy the query and get access to its underlying data.  You
+  ///    will be responsible for cleaning up or disposing the data you get.
+  /// </summary>
+  public struct Query<T> {
     private T[] _array;
     private int _count;
 
     private Validator _validator;
 
+    /// <summary>
+    /// Consutructs a new query given a source array and a count.  The query assumes
+    /// ownership of the array, so you should not use it or store it 
+    /// after the query is constructed.
+    /// </summary>
     public Query(T[] array, int count) {
+      if (array == null) {
+        throw new ArgumentNullException("array");
+      }
+
+      if (count < 0) {
+        throw new ArgumentException("Count must be non-negative, but was " + count);
+      }
+
+      if (count < array.Length) {
+        throw new ArgumentException("Count was " + count + " but the provided array had a length of " + array.Length);
+      }
+
       _array = array;
       _count = count;
       _validator = Validator.Spawn();
     }
 
+    /// <summary>
+    /// Constructs a new query of the given collection.
+    /// </summary>
     public Query(ICollection<T> collection) {
       _array = ArrayPool<T>.Spawn(collection.Count);
       _count = collection.Count;
@@ -66,6 +118,9 @@ namespace Leap.Unity.Query {
       _validator = Validator.Spawn();
     }
 
+    /// <summary>
+    /// Constructs a query that is an exact copy of another query.
+    /// </summary>
     public Query(Query<T> other) {
       other._validator.Validate();
 
@@ -76,6 +131,48 @@ namespace Leap.Unity.Query {
       _validator = Validator.Spawn();
     }
 
+    //Certain operators cannot be implemented as extension methods due to the way
+    //the generic arguments are to be consumed by the user, so there are implemented
+    //directly here in the Query class.
+    #region DIRECT IMPLEMENTED OPERATORS
+
+    /// <summary>
+    /// Returns a new Query representing only the items of the current Query that
+    /// are of a specific type.
+    /// 
+    /// For example
+    ///   ("A", 1, null, 5.0f, 900, "hello").Query().OfType<string>()
+    /// would result in
+    ///   ("A", "hello")
+    /// </summary>
+    public Query<K> OfType<K>() where K : T {
+      var dstArray = ArrayPool<K>.Spawn(_count);
+
+      int dstCount = 0;
+      for (int i = 0; i < _count; i++) {
+        if (_array[i] is K) {
+          dstArray[dstCount++] = (K)_array[i];
+        }
+      }
+
+      Dispose();
+      return new Query<K>(dstArray, dstCount);
+    }
+
+    /// <summary>
+    /// Returns a new Query representing the current query sequence where each element is cast
+    /// to a new type.
+    /// </summary>
+    public Query<K> Cast<K>() where K : class {
+      return this.Select(item => item as K);
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Disposes of the resources that this query holds.  The Query cannot be 
+    /// used after this method is called.
+    /// </summary>
     public void Dispose() {
       _validator.Validate();
 
@@ -86,7 +183,12 @@ namespace Leap.Unity.Query {
       _count = 0;
     }
 
-    public void Dispose(out T[] array, out int count) {
+    /// <summary>
+    /// Deconstructs this Query into its base elements, the array and the count.
+    /// The caller assumes ownership of the array and is responsible for managing
+    /// its lifecycle.  The Query cannot be used after this method is called.
+    /// </summary>
+    public void Deconstruct(out T[] array, out int count) {
       _validator.Validate();
 
       array = _array;
@@ -97,10 +199,16 @@ namespace Leap.Unity.Query {
       _count = 0;
     }
 
+    /// <summary>
+    /// Deconstructs this Query into a simple QuerySlice construct.  This is
+    /// simply a utility overload of the regular Deconstruct method.  The
+    /// user is stil responsible for managing the memory lifecycle of the returned
+    /// slice.  The Query cannot be used after this method is called.
+    /// </summary>
     public QuerySlice Deconstruct() {
       T[] array;
       int count;
-      Dispose(out array, out count);
+      Deconstruct(out array, out count);
 
       return new QuerySlice(array, count);
     }
@@ -110,7 +218,7 @@ namespace Leap.Unity.Query {
 
       T[] array;
       int count;
-      Dispose(out array, out count);
+      Deconstruct(out array, out count);
 
       return new Enumerator(array, count);
     }
@@ -157,7 +265,6 @@ namespace Leap.Unity.Query {
     }
 
     public struct QuerySlice : IDisposable {
-
       public readonly T[] BackingArray;
       public readonly int Count;
 
--- Assets/LeapMotion/Core/Scripts/Query/QueryCollapseExtensions.cs
+++ Assets/LeapMotion/Core/Scripts/Query/QueryCollapseExtensions.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 
 namespace Leap.Unity.Query {
 
-  public static class ExtensionSingles {
+  public static class QueryCollapseExtensions {
 
+    /// <summary>
+    /// Returns true if all elements in the Query satisfy the predicate.
+    /// </summary>
     public static bool All<T>(this Query<T> query, Func<T, bool> predicate) {
       using (var slice = query.Deconstruct()) {
 
@@ -18,6 +21,10 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns true if all elements in the Query are equal to the same value.
+    /// Will always return true for Queries with one or zero elements.
+    /// </summary>
     public static bool AllEqual<T>(this Query<T> query) {
       using (var slice = query.Deconstruct()) {
         if (slice.Count <= 1) {
@@ -45,10 +52,16 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns true if the Query has any elements in it.
+    /// </summary>
     public static bool Any<T>(this Query<T> query) {
       return query.Count() > 0;
     }
 
+    /// <summary>
+    /// Returns true if any elements in the Query satisfy the predicate.
+    /// </summary>
     public static bool Any<T>(this Query<T> query, Func<T, bool> predicate) {
       using (var slice = query.Deconstruct()) {
         for (int i = 0; i < slice.Count; i++) {
@@ -61,10 +74,13 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns true if any element in the Query is equal to a specific value.
+    /// </summary>
     public static bool Contains<T>(this Query<T> query, T item) {
       T[] array;
       int count;
-      query.Dispose(out array, out count);
+      query.Deconstruct(out array, out count);
 
       for (int i = 0; i < count; i++) {
         if (array[i].Equals(item)) {
@@ -77,16 +93,25 @@ namespace Leap.Unity.Query {
       return false;
     }
 
+    /// <summary>
+    /// Returns the number of elements in the Query.
+    /// </summary>
     public static int Count<T>(this Query<T> query) {
       using (var slice = query.Deconstruct()) {
         return slice.Count;
       }
     }
 
+    /// <summary>
+    /// Returns the number of elements in the Query that satisfy a predicate.
+    /// </summary>
     public static int Count<T>(this Query<T> query, Func<T, bool> predicate) {
       return query.Where(predicate).Count();
     }
 
+    /// <summary>
+    /// Counts the number of distinct elements in the Query.
+    /// </summary>
     public static int CountUnique<T>(this Query<T> query) {
       var slice = query.Deconstruct();
       var set = Pool<HashSet<T>>.Spawn();
@@ -104,10 +129,18 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns the number of distinct elements in the Query once it has been mapped
+    /// using a selector function.
+    /// </summary>
     public static int CountUnique<T, K>(this Query<T> query, Func<T, K> selector) {
       return query.Select(selector).CountUnique();
     }
 
+    /// <summary>
+    /// Returns the element at a specific index in the Query.  Will throw an error
+    /// if the Query has no element at that index.
+    /// </summary>
     public static T ElementAt<T>(this Query<T> query, int index) {
       using (var slice = query.Deconstruct()) {
         if (index < 0 || index >= slice.Count) {
@@ -117,6 +150,10 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns the element at a specific index in the Query.  Will return
+    /// the default value if the Query has no element at that index.
+    /// </summary>
     public static T ElementAtOrDefault<T>(this Query<T> query, int index) {
       using (var slice = query.Deconstruct()) {
         if (index < 0 || index >= slice.Count) {
@@ -126,6 +163,10 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns the first element in the Query.  Will throw an error if there are
+    /// no elements in the Query.
+    /// </summary>
     public static T First<T>(this Query<T> query) {
       using (var slice = query.Deconstruct()) {
         if (slice.Count == 0) {
@@ -135,10 +176,18 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns the first element in the Query that satisfies a predicate.  Will
+    /// throw an error if there is no such element.
+    /// </summary>
     public static T First<T>(this Query<T> query, Func<T, bool> predicate) {
       return query.Where(predicate).First();
     }
 
+    /// <summary>
+    /// Returns the first element in the Query.  Will return the default value
+    /// if the Query is empty.
+    /// </summary>
     public static T FirstOrDefault<T>(this Query<T> query) {
       using (var slice = query.Deconstruct()) {
         if (slice.Count == 0) {
@@ -148,10 +197,18 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns the first element in the Query that satisfies a predicate.  Will return
+    /// the default value if there is no such element.
+    /// </summary>
     public static T FirstOrDefault<T>(this Query<T> query, Func<T, bool> predicate) {
       return query.Where(predicate).FirstOrDefault();
     }
 
+    /// <summary>
+    /// Returns Some value that represents the first value in the Query, or None
+    /// if there is no such value.
+    /// </summary>
     public static Maybe<T> FirstOrNone<T>(this Query<T> query) {
       using (var slice = query.Deconstruct()) {
         if (slice.Count == 0) {
@@ -161,10 +218,24 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns the Some value representing the first value that satisfies the predicate,
+    /// or None if there is no such value.
+    /// </summary>
     public static Maybe<T> FirstOrNone<T>(this Query<T> query, Func<T, bool> predicate) {
       return query.Where(predicate).FirstOrNone();
     }
 
+    /// <summary>
+    /// Folds all of the elements in the Query into a single element, using a fold function.
+    /// Will throw an error if there are no elements in the Query.
+    /// 
+    /// The fold function takes in the current folded value, and the next item to fold in.
+    /// It returns the result of folding the item into the current folded value.  For example,
+    /// you can use the Fold operation to implement a sum:
+    /// 
+    /// var sum = numbers.Query().Fold((a,b) => a + b);
+    /// </summary>
     public static T Fold<T>(this Query<T> query, Func<T, T, T> foldFunc) {
       using (var slice = query.Deconstruct()) {
         if (slice.Count == 0) {
@@ -180,6 +251,10 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns the index of the first element that is equal to a specific value.  Will return
+    /// a negative index if there is no such element.
+    /// </summary>
     public static int IndexOf<T>(this Query<T> query, T t) {
       using (var slice = query.Deconstruct()) {
         for (int i = 0; i < slice.Count; i++) {
@@ -196,6 +271,10 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns the index of the first element to satisfy a predicate.  Will return a negative
+    /// index if there is no such element.
+    /// </summary>
     public static int IndexOf<T>(this Query<T> query, Func<T, bool> predicate) {
       using (var slice = query.Deconstruct()) {
         for (int i = 0; i < slice.Count; i++) {
@@ -207,6 +286,9 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns the last element in the Query.  Will throw an error if the Query is empty.
+    /// </summary>
     public static T Last<T>(this Query<T> query) {
       using (var slice = query.Deconstruct()) {
         if (slice.Count == 0) {
@@ -217,10 +299,17 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns the last element in the Query that satisfies a predicate.  Will throw an error
+    /// if there is no such element.
+    /// </summary>
     public static T Last<T>(this Query<T> query, Func<T, bool> predicate) {
       return query.Where(predicate).Last();
     }
 
+    /// <summary>
+    /// Returns the last element in the Query.  Will return the default value if the Query is empty.
+    /// </summary>
     public static T LastOrDefault<T>(this Query<T> query) {
       using (var slice = query.Deconstruct()) {
         if (slice.Count == 0) {
@@ -231,6 +320,18 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns the last element in the Query that satisfies a predicate.  Will return
+    /// the default value if there is no such element.
+    /// </summary>
+    public static T LastOrDefault<T>(this Query<T> query, Func<T, bool> predicate) {
+      return query.Where(predicate).LastOrDefault();
+    }
+
+    /// <summary>
+    /// Returns Some value that represents the last value in the Query, or None
+    /// if there is no such value.
+    /// </summary>
     public static Maybe<T> LastOrNone<T>(this Query<T> query) {
       using (var slice = query.Deconstruct()) {
         if (slice.Count == 0) {
@@ -241,26 +342,46 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns the Some value representing the last value that satisfies the predicate,
+    /// or None if there is no such value.
+    /// </summary>
     public static Maybe<T> LastOrNone<T>(this Query<T> query, Func<T, bool> predicate) {
       return query.Where(predicate).LastOrNone();
     }
 
+    /// <summary>
+    /// Returns the largest element in the Query.
+    /// </summary>
     public static T Max<T>(this Query<T> query) where T : IComparable<T> {
       return query.Fold((a, b) => a.CompareTo(b) > 0 ? a : b);
     }
 
+    /// <summary>
+    /// Returns the largest element in the Query after it has been mapped using a selector function.
+    /// </summary>
     public static K Max<T, K>(this Query<T> query, Func<T, K> selector) where K : IComparable<K> {
       return query.Select(selector).Max();
     }
 
+    /// <summary>
+    /// Returns the smallest element in the Query.
+    /// </summary>
     public static T Min<T>(this Query<T> query) where T : IComparable<T> {
       return query.Fold((a, b) => a.CompareTo(b) < 0 ? a : b);
     }
 
+    /// <summary>
+    /// Returns the smallest element in the Query after it has been mapped using a selector function.
+    /// </summary>
     public static K Min<T, K>(this Query<T> query, Func<T, K> selector) where K : IComparable<K> {
       return query.Select(selector).Min();
     }
 
+    /// <summary>
+    /// Returns the first and only element in the Query.  Will throw an error if the length of the 
+    /// Query is anything other than 1.
+    /// </summary>
     public static T Single<T>(this Query<T> query) {
       using (var slice = query.Deconstruct()) {
         if (slice.Count != 1) {
@@ -271,10 +392,18 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns the first and only element in the Query that satisfies the predicate.  Will throw
+    /// an error if the number of such elements is anything other than 1.
+    /// </summary>
     public static T Single<T>(this Query<T> query, Func<T, bool> predicate) {
       return query.Where(predicate).Single();
     }
 
+    /// <summary>
+    /// Returns the first and only element in the Query.  Will return the default value if the number
+    /// of elements is anything other than 1.
+    /// </summary>
     public static T SingleOrDefault<T>(this Query<T> query) {
       using (var slice = query.Deconstruct()) {
         if (slice.Count != 1) {
@@ -285,10 +414,18 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns the first and only element in the Query that satisfies the predicate.  Will return
+    /// the default value if the number of such elements is anything other than 1.
+    /// </summary>
     public static T SingleOrDefault<T>(this Query<T> query, Func<T, bool> predicate) {
       return query.Where(predicate).SingleOrDefault();
     }
 
+    /// <summary>
+    /// Returns the first and only element in the Query.  Will return None if the number
+    /// of elements is anything other than 1.
+    /// </summary>
     public static Maybe<T> SingleOrNone<T>(this Query<T> query) {
       using (var slice = query.Deconstruct()) {
         if (slice.Count != 1) {
@@ -299,14 +436,28 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Returns the first and only element in the Query that satisfies the predicate.  Will return
+    /// None if the number of such elements is anything other than 1.
+    /// </summary>
     public static Maybe<T> SingleOrNone<T>(this Query<T> query, Func<T, bool> predicate) {
       return query.Where(predicate).SingleOrNone();
     }
 
+    /// <summary>
+    /// Returns the single value that is present in the entire Query.  If there is more
+    /// than one value in the Query or there are no values at all, this method will return
+    /// the default value.
+    /// </summary>
     public static T UniformOrDefault<T>(this Query<T> query) {
       return query.UniformOrNone().valueOrDefault;
     }
 
+    /// <summary>
+    /// Returns Some single value that is present in the entire Query.  If there is more
+    /// than one value in the Query or there are no values at all, this method will return
+    /// None.
+    /// </summary>
     public static Maybe<T> UniformOrNone<T>(this Query<T> query) {
       using (var slice = query.Deconstruct()) {
         if (slice.Count == 0) {
@@ -330,6 +481,9 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Converts the Query into an array.
+    /// </summary>
     public static T[] ToArray<T>(this Query<T> query) {
       using (var slice = query.Deconstruct()) {
         T[] result = new T[slice.Count];
@@ -338,12 +492,19 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Copies the elements of the Query into an array.  Can optionally specify the offset into the array
+    /// where to copy.
+    /// </summary>
     public static void FillArray<T>(this Query<T> query, T[] array, int offset = 0) {
       using (var slice = query.Deconstruct()) {
         Array.Copy(slice.BackingArray, 0, array, offset, slice.Count);
       }
     }
 
+    /// <summary>
+    /// Converts the Query into a list.
+    /// </summary>
     public static List<T> ToList<T>(this Query<T> query) {
       using (var slice = query.Deconstruct()) {
         List<T> result = new List<T>(slice.Count);
@@ -354,11 +515,17 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Fills a given list with the elements in this Query.  The list is cleared before the fill happens.
+    /// </summary>
     public static void FillList<T>(this Query<T> query, List<T> list) {
       list.Clear();
       query.AppendList(list);
     }
 
+    /// <summary>
+    /// Appends the elements in this Query to the end of a given list.
+    /// </summary>
     public static void AppendList<T>(this Query<T> query, List<T> list) {
       using (var slice = query.Deconstruct()) {
         for (int i = 0; i < slice.Count; i++) {
@@ -367,17 +534,26 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Converts the Query into a HashSet.
+    /// </summary>
     public static HashSet<T> ToHashSet<T>(this Query<T> query) {
       HashSet<T> set = new HashSet<T>();
       query.AppendHashSet(set);
       return set;
     }
 
+    /// <summary>
+    /// Fills a given HashSet with the elements in the Query.  The set is cleared before the fill happens.
+    /// </summary>
     public static void FillHashSet<T>(this Query<T> query, HashSet<T> set) {
       set.Clear();
       query.AppendHashSet(set);
     }
 
+    /// <summary>
+    /// Appends the elements in this Query into the given HashSet.
+    /// </summary>
     public static void AppendHashSet<T>(this Query<T> query, HashSet<T> set) {
       using (var slice = query.Deconstruct()) {
         for (int i = 0; i < slice.Count; i++) {
@@ -386,6 +562,9 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Converts the Query into a Dictionary using a specific key selector and value selector.
+    /// </summary>
     public static Dictionary<K, V> ToDictionary<T, K, V>(this Query<T> query, Func<T, K> keySelector, Func<T, V> valueSelector) {
       using (var slice = query.Deconstruct()) {
         Dictionary<K, V> dictionary = new Dictionary<K, V>();
@@ -398,6 +577,10 @@ namespace Leap.Unity.Query {
       }
     }
 
+    /// <summary>
+    /// Converst the Query into a Dictionary using the query elements as keys, and using a value selector
+    /// to select the value tied to the key.
+    /// </summary>
     public static Dictionary<T, V> ToDictionary<T, V>(this Query<T> query, Func<T, V> valueSelector) {
       return query.ToDictionary(t => t, valueSelector);
     }
