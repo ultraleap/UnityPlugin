0dcc62de3f05d475f7eddc56dfd25629a05c6979
Alex Marcolina <amarcolina@gmail.com>
2018-03-27 17:29:28 -0700

    Updated copywrite headers

--- Assets/LeapMotion/Core/Scripts/LeapProfiling.cs
+++ Assets/LeapMotion/Core/Scripts/LeapProfiling.cs
@@ -1,4 +1,13 @@
-﻿using System.Threading;
+/******************************************************************************
+ * Copyright (C) Leap Motion, Inc. 2011-2018.                                 *
+ * Leap Motion proprietary and  confidential.                                 *
+ *                                                                            *
+ * Use subject to the terms of the Leap Motion SDK Agreement available at     *
+ * https://developer.leapmotion.com/sdk_agreement, or another agreement       *
+ * between Leap Motion and you, your company or other organization.           *
+ ******************************************************************************/
+
+using System.Threading;
 using System.Collections.Generic;
 using UnityEngine.Profiling;
 
