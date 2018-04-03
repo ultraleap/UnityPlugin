264d5c4adad29d915fcd5a7ff6e69f07c50bcf51
Johnathon Selstad <jselstad@leapmotion.com>
2018-04-03 16:04:01 -0700

    Initial Commit of the Post Process Provider

---
+++ Assets/LeapMotion/Core/Examples/Example Assets/InertiaProvider.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+namespace Leap.Unity.Examples {
+  public class InertiaProvider : PostProcessProvider {
+    [Range(0f, 1f)]
+    public float _stiffness = 0.01f;
+    [Range(0f, 1f)]
+    public float _damping = 0.05f;
+
+    private Pose leftHand = Pose.identity,
+                 rightHand = Pose.identity,
+                 previousLeftHand = Pose.identity,
+                 previousRightHand = Pose.identity;
+
+    public override void ProcessFrame(ref Frame inputFrame) {
+      //Record the current pose states of the left and right hands
+      int leftHandId = -1, rightHandId = -1;
+      Pose currentLeftHand = Pose.identity,
+           currentRightHand = Pose.identity;
+      foreach (Hand hand in inputFrame.Hands) {
+        if (hand.IsLeft) {
+          leftHandId = hand.Id;
+          currentLeftHand = new Pose(hand.PalmPosition.ToVector3(),
+                                     hand.Rotation.ToQuaternion());
+          if (hand.TimeVisible == 0f) {
+            leftHand = currentLeftHand;
+            previousLeftHand = currentLeftHand;
+          }
+        } else {
+          rightHandId = hand.Id;
+          currentRightHand = new Pose(hand.PalmPosition.ToVector3(),
+                                      hand.Rotation.ToQuaternion());
+          if (hand.TimeVisible == 0f) {
+            rightHand = currentRightHand;
+            previousRightHand = currentRightHand;
+          }
+        }
+      }
+
+      //Integrate the hands forward in time and apply their transforms to the frame
+      if (leftHandId != -1) {
+        integrateHand(ref leftHand, ref previousLeftHand, currentLeftHand);
+        inputFrame.Hand(leftHandId).SetTransform(leftHand.position, leftHand.rotation);
+      }
+      if (rightHandId != -1) {
+        integrateHand(ref rightHand, ref previousRightHand, currentRightHand);
+        inputFrame.Hand(rightHandId).SetTransform(rightHand.position, rightHand.rotation);
+      }
+    }
+
+    //Integrate's the hand's inertia to give it bouncy feeling physics
+    void integrateHand(ref Pose hand, ref Pose previousHand, Pose currentHand) {
+      //Verlet Integration
+      Pose tempHand = hand;
+      hand *= Pose.Lerp(previousHand.inverse * hand, Pose.identity, _damping); //5% damping/frame
+      previousHand = tempHand;
+      //Pull the integrated hand toward the original one a little bit every frame
+      hand = Pose.Lerp(hand, currentHand, _stiffness);
+    }
+  }
+}
---
+++ Assets/LeapMotion/Core/Scripts/Utils/PostProcessProvider.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using Leap.Unity.Attributes;
+namespace Leap.Unity {
+  public abstract class PostProcessProvider : LeapProvider {
+    [Tooltip("The LeapProvider to use to drive hand representations in the defined "
+           + "model pool groups.")]
+    [SerializeField]
+    [OnEditorChange("inputLeapProvider")]
+    private LeapProvider _leapProvider;
+    public LeapProvider inputLeapProvider {
+      get { return _leapProvider; }
+      set {
+        if (_leapProvider != null) {
+          _leapProvider.OnFixedFrame -= ProcessFixedFrame;
+          _leapProvider.OnUpdateFrame -= ProcessUpdateFrame;
+        }
+
+        _leapProvider = value;
+
+        if (_leapProvider != null) {
+          _leapProvider.OnFixedFrame += ProcessFixedFrame;
+          _leapProvider.OnUpdateFrame += ProcessUpdateFrame;
+        }
+      }
+    }
+
+    protected virtual void OnEnable() {
+      if (_leapProvider == null) {
+        _leapProvider = Hands.Provider;
+      }
+
+      _leapProvider.OnUpdateFrame -= ProcessUpdateFrame;
+      _leapProvider.OnUpdateFrame += ProcessUpdateFrame;
+
+      _leapProvider.OnFixedFrame -= ProcessFixedFrame;
+      _leapProvider.OnFixedFrame += ProcessFixedFrame;
+    }
+
+    public override Frame CurrentFrame {
+      get {
+        #if UNITY_EDITOR
+        if (!Application.isPlaying) {
+          ProcessUpdateFrame(_leapProvider.CurrentFrame);
+        }
+        #endif
+        return _cachedUpdateFrame;
+      }
+    }
+    public override Frame CurrentFixedFrame {
+      get {
+        #if UNITY_EDITOR
+        if (!Application.isPlaying) {
+          ProcessUpdateFrame(_leapProvider.CurrentFixedFrame);
+        }
+        #endif
+        return _cachedFixedFrame;
+      }
+    }
+
+    private Frame _cachedUpdateFrame = new Frame();
+    private Frame _cachedFixedFrame = new Frame();
+
+    private void ProcessUpdateFrame(Frame inputFrame) {
+      _cachedUpdateFrame.CopyFrom(inputFrame);
+      ProcessFrame(ref _cachedUpdateFrame);
+      DispatchUpdateFrameEvent(_cachedUpdateFrame);
+    }
+    private void ProcessFixedFrame(Frame inputFrame) {
+      _cachedFixedFrame.CopyFrom(inputFrame);
+      ProcessFrame(ref _cachedFixedFrame);
+      DispatchFixedFrameEvent(_cachedFixedFrame);
+    }
+
+    public abstract void ProcessFrame(ref Frame inputFrame);
+  }
+}
