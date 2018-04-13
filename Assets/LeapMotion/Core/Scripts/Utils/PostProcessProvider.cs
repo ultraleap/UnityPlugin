f258c97216b1b106499d798e7b01c4286fae70e7
Nicholas Benson <nbenson@leapmotion.com>
2018-04-13 15:21:31 -0700

    PostProcessProvider: +UM style, +passthrough only, +DataUpdateMode

--- Assets/LeapMotion/Core/Scripts/Utils/PostProcessProvider.cs
+++ Assets/LeapMotion/Core/Scripts/Utils/PostProcessProvider.cs
@@ -9,9 +9,13 @@
 
 using UnityEngine;
 using Leap.Unity.Attributes;
+
 namespace Leap.Unity {
+
   public abstract class PostProcessProvider : LeapProvider {
-    [Tooltip("The LeapProvider to augment with this post-process.")]
+
+    [Tooltip("The LeapProvider whose output hand data will be copied, modified, and "
+           + "output by this post-processing provider.")]
     [SerializeField]
     [OnEditorChange("inputLeapProvider")]
     protected LeapProvider _inputLeapProvider;
@@ -21,48 +25,56 @@ namespace Leap.Unity {
         OnValidate();
 
         if (_inputLeapProvider != null) {
-          _inputLeapProvider.OnFixedFrame -= ProcessFixedFrame;
-          _inputLeapProvider.OnUpdateFrame -= ProcessUpdateFrame;
+          _inputLeapProvider.OnFixedFrame -= processFixedFrame;
+          _inputLeapProvider.OnUpdateFrame -= processUpdateFrame;
         }
 
         _inputLeapProvider = value;
 
         if (_inputLeapProvider != null) {
-          _inputLeapProvider.OnFixedFrame += ProcessFixedFrame;
-          _inputLeapProvider.OnUpdateFrame += ProcessUpdateFrame;
+          _inputLeapProvider.OnFixedFrame -= processFixedFrame; // safeguard double-subscription
+          _inputLeapProvider.OnFixedFrame += processFixedFrame;
+          _inputLeapProvider.OnUpdateFrame -= processUpdateFrame; // safeguard double-subscription
+          _inputLeapProvider.OnUpdateFrame += processUpdateFrame;
         }
       }
     }
 
-    [Tooltip("Whether this step should apply its post-process or " +
-  "pass the frames through un-modified.")]
-    public bool postProcessingEnabled = true;
+    public enum DataUpdateMode { UpdateOnly, FixedUpdateOnly, UpdateAndFixedUpdate }
+    [Tooltip("Whether this post-processing provider should process data received from "
+           + "Update frames, FixedUpdate frames, or both. Processing both kinds of frames "
+           + "is only recommended if your post-process is stateless.")]
+    public DataUpdateMode dataUpdateMode = DataUpdateMode.UpdateOnly;
+
+    [Tooltip("When this setting is enabled, frame data is passed from this provider's "
+           + "input directly to its output without performing any post-processing.")]
+    public bool passthroughOnly = false;
 
     protected virtual void OnEnable() {
       if (_inputLeapProvider == null && Hands.Provider != this) {
         _inputLeapProvider = Hands.Provider;
       }
 
-      _inputLeapProvider.OnUpdateFrame -= ProcessUpdateFrame;
-      _inputLeapProvider.OnUpdateFrame += ProcessUpdateFrame;
+      _inputLeapProvider.OnUpdateFrame -= processUpdateFrame;
+      _inputLeapProvider.OnUpdateFrame += processUpdateFrame;
 
-      _inputLeapProvider.OnFixedFrame -= ProcessFixedFrame;
-      _inputLeapProvider.OnFixedFrame += ProcessFixedFrame;
+      _inputLeapProvider.OnFixedFrame -= processFixedFrame;
+      _inputLeapProvider.OnFixedFrame += processFixedFrame;
     }
 
     protected virtual void OnValidate() {
-#if UNITY_EDITOR
+      #if UNITY_EDITOR
       if (!Application.isPlaying) {
-        if (checkForCycles()) {
+        if (detectCycle()) {
           _inputLeapProvider = null;
-          Debug.LogError("Post-Process Cycle Detected!  " +
-            "Nulling "+gameObject.name+"'s InputProvider...", this);
+          Debug.LogError("The input to the post-process provider on " + gameObject.name
+                       + " causes an infinite cycle, so its input has been set to null.");
         }
       }
-#endif
+      #endif
     }
-
-    bool checkForCycles() {
+    
+    private bool detectCycle() {
       LeapProvider providerA = _inputLeapProvider, providerB = _inputLeapProvider;
       while (providerA is PostProcessProvider) {
         providerB = (providerB as PostProcessProvider).inputLeapProvider;
@@ -79,7 +91,7 @@ namespace Leap.Unity {
       get {
         #if UNITY_EDITOR
         if (!Application.isPlaying && _inputLeapProvider != null) {
-          ProcessUpdateFrame(_inputLeapProvider.CurrentFrame);
+          processUpdateFrame(_inputLeapProvider.CurrentFrame);
         }
         #endif
         return _cachedUpdateFrame;
@@ -89,7 +101,7 @@ namespace Leap.Unity {
       get {
         #if UNITY_EDITOR
         if (!Application.isPlaying && _inputLeapProvider != null) {
-          ProcessUpdateFrame(_inputLeapProvider.CurrentFixedFrame);
+          processUpdateFrame(_inputLeapProvider.CurrentFixedFrame);
         }
         #endif
         return _cachedFixedFrame;
@@ -99,14 +111,23 @@ namespace Leap.Unity {
     private Frame _cachedUpdateFrame = new Frame();
     private Frame _cachedFixedFrame = new Frame();
 
-    private void ProcessUpdateFrame(Frame inputFrame) {
+    private void processUpdateFrame(Frame inputFrame) {
+      if (dataUpdateMode == DataUpdateMode.FixedUpdateOnly) {
+        return;
+      }
+
       _cachedUpdateFrame.CopyFrom(inputFrame);
-      if (postProcessingEnabled) { ProcessFrame(ref _cachedUpdateFrame); }
+      if (!passthroughOnly) { ProcessFrame(ref _cachedUpdateFrame); }
       DispatchUpdateFrameEvent(_cachedUpdateFrame);
     }
-    private void ProcessFixedFrame(Frame inputFrame) {
+
+    private void processFixedFrame(Frame inputFrame) {
+      if (dataUpdateMode == DataUpdateMode.UpdateOnly) {
+        return;
+      }
+
       _cachedFixedFrame.CopyFrom(inputFrame);
-      if (postProcessingEnabled) { ProcessFrame(ref _cachedFixedFrame); }
+      if (!passthroughOnly) { ProcessFrame(ref _cachedFixedFrame); }
       DispatchFixedFrameEvent(_cachedFixedFrame);
     }
 
