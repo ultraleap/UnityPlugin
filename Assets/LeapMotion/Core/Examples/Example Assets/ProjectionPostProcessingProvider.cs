c1e7c6e04b3d05f0d0f7e4966baf9fcaa8d68a09
Nicholas Benson <nbenson@leapmotion.com>
2018-04-13 15:49:06 -0700

    Apply UM style to ProjectionPostProcessingProvider

--- Assets/LeapMotion/Core/Examples/Example Assets/ProjectionPostProcessingProvider.cs
+++ Assets/LeapMotion/Core/Examples/Example Assets/ProjectionPostProcessingProvider.cs
@@ -8,22 +8,38 @@
  ******************************************************************************/
 
 using UnityEngine;
+
 namespace Leap.Unity.Examples {
+
   public class ProjectionPostProcessingProvider : PostProcessProvider {
+
     [Range(0f, 5f)]
-    public float _extensionAmount = 3.5f;
+    public float extensionAmount = 3.5f;
+
     [Range(0f, 1f)]
-    public float _handMergeDistance = 0.65f;
+    public float handMergeDistance = 0.65f;
+
     public override void ProcessFrame(ref Frame inputFrame) {
-      //Calculate the position of the head and the rotation of the shoulders
+      // Calculate the position of the head and the basis to calculate shoulder position.
       Vector3 headPos = Camera.main.transform.position;
-      Quaternion shoulderRotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up));
-      foreach (Hand hand in inputFrame.Hands) {
-        //Approximate shoulder positions with magic values
-        Vector3 shoulderPos = headPos + (shoulderRotation * (new Vector3(0f, -0.2f, -0.1f) + Vector3.left * 0.1f * (hand.IsLeft ? 1f : -1f)));
-        //Calculate the scaling amount and scale the motion of the hands
-        float scalingAmount = Mathf.Max(1f, Mathf.Pow(Vector3.Distance(hand.PalmPosition.ToVector3(), shoulderPos) + _handMergeDistance, _extensionAmount));
-        hand.SetTransform(((hand.PalmPosition.ToVector3() - shoulderPos) * scalingAmount) + shoulderPos, hand.Rotation.ToQuaternion());
+      Quaternion shoulderBasis = Quaternion.LookRotation(
+        Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up),
+        Vector3.up);
+
+      foreach (var hand in inputFrame.Hands) {
+        // Approximate shoulder position with magic values.
+        Vector3 shoulderPos = headPos
+                              + (shoulderBasis * (new Vector3(0f, -0.2f, -0.1f)
+                              + Vector3.left * 0.1f * (hand.IsLeft ? 1f : -1f)));
+
+        // Calculate the projection amount and projection the position of the hand.
+        float projectionAmount = Mathf.Max(1f,
+          Mathf.Pow(Vector3.Distance(hand.PalmPosition.ToVector3(), shoulderPos)
+                      + handMergeDistance,
+                    extensionAmount));
+        hand.SetTransform(
+          ((hand.PalmPosition.ToVector3() - shoulderPos) * projectionAmount) + shoulderPos,
+          hand.Rotation.ToQuaternion());
       }
     }
   }
