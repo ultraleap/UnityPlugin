c46afb2c608cd82b248ccfa55d16e993121008f1
Nicholas Benson <nbenson@leapmotion.com>
2018-04-16 16:37:35 -0700

    Add warning: button wobble fix only works w/o autoSyncTransforms

--- Assets/LeapMotion/Modules/InteractionEngine/Examples/2. Basic UI/MovePoseExample.cs
+++ Assets/LeapMotion/Modules/InteractionEngine/Examples/2. Basic UI/MovePoseExample.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Leap.Unity.Interaction.Examples {
 
@@ -11,6 +9,16 @@ namespace Leap.Unity.Interaction.Examples {
 
     private void OnEnable() {
       _selfToTargetPose = this.transform.ToPose().inverse * target.transform.ToPose();
+    }
+
+    private void Start() {
+      if (Physics.autoSyncTransforms) {
+        Debug.LogWarning(
+          "Physics.autoSyncTransforms is enabled. This will cause Interaction "
+        + "Buttons and similar elements to 'wobble' when this script is used to "
+        + "move a parent transform. You can modify this setting in "
+        + "Edit->Project Settings->Physics.");
+      }
     }
 
     private void Update() {
