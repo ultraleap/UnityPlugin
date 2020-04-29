7c6e5ac22bd3d56ecbe2e376229127412dcc2fea
Nick Benson <nickjbenson@gmail.com>
2020-04-28 17:47:11 -0700

    update copyright headers

--- Assets/LeapMotion/Core/Scripts/Geometry/Collision.cs
+++ Assets/LeapMotion/Core/Scripts/Geometry/Collision.cs
@@ -1,4 +1,13 @@
-﻿using Leap.Unity.Infix;
+/******************************************************************************
+ * Copyright (C) Leap Motion, Inc. 2011-2018.                                 *
+ * Leap Motion proprietary and confidential.                                  *
+ *                                                                            *
+ * Use subject to the terms of the Leap Motion SDK Agreement available at     *
+ * https://developer.leapmotion.com/sdk_agreement, or another agreement       *
+ * between Leap Motion and you, your company or other organization.           *
+ ******************************************************************************/
+
+using Leap.Unity.Infix;
 using Leap.Unity.Swizzle;
 using System;
 using UnityEngine;
--- Assets/LeapMotion/Core/Scripts/Splines/CatmullRom.cs
+++ Assets/LeapMotion/Core/Scripts/Splines/CatmullRom.cs
@@ -1,4 +1,13 @@
-﻿using Leap.Unity.Animation;
+/******************************************************************************
+ * Copyright (C) Leap Motion, Inc. 2011-2018.                                 *
+ * Leap Motion proprietary and confidential.                                  *
+ *                                                                            *
+ * Use subject to the terms of the Leap Motion SDK Agreement available at     *
+ * https://developer.leapmotion.com/sdk_agreement, or another agreement       *
+ * between Leap Motion and you, your company or other organization.           *
+ ******************************************************************************/
+
+using Leap.Unity.Animation;
 using UnityEngine;
 
 namespace Leap.Unity.Splines {
