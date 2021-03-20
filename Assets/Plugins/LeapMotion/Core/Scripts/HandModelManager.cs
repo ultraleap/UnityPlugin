19c80047f6d7b468bd55733eddd855c3370298a4
Matt Gray <matt.gray@ultraleap.com>
2021-03-20 12:32:19 +0000

    Added persistent hands * Added an option in HandModelManager to reuse hands rather than destroy and recreate them * Added some toys to the Articulation Hands Simple scene * Added PersistentHandsProvider which reuses old frames of hands

--- Assets/Plugins/LeapMotion/Core/Scripts/HandModelManager.cs
+++ Assets/Plugins/LeapMotion/Core/Scripts/HandModelManager.cs
@@ -83,36 +83,57 @@ namespace Leap.Unity {
     protected virtual void UpdateHandRepresentations(Dictionary<int, HandRepresentation> all_hand_reps, ModelType modelType, Frame frame) {
       for (int i = 0; i < frame.Hands.Count; i++) {
         var curHand = frame.Hands[i];
-        HandRepresentation rep;
-        if (!all_hand_reps.TryGetValue(curHand.Id, out rep)) {
-          rep = MakeHandRepresentation(curHand, modelType);
-          all_hand_reps.Add(curHand.Id, rep);
-        }
-        if (rep != null) {
+        HandRepresentation rep = null;
+
+            if (persistentHands) {
+                foreach (var handrep in all_hand_reps.Values) {
+                    if ((curHand.IsLeft && handrep.RepChirality == Chirality.Left) ||
+                        (curHand.IsRight && handrep.RepChirality == Chirality.Right)) {
+                        rep = handrep;
+                        break;
+                    }
+                }
+            }
+
+            if (rep == null){
+                if (!all_hand_reps.TryGetValue(curHand.Id, out rep)) {
+                    rep = MakeHandRepresentation(curHand, modelType);
+                    all_hand_reps.Add(curHand.Id, rep);
+                }
+            }
+            if (rep != null) {
           rep.IsMarked = true;
           rep.UpdateRepresentation(curHand);
           rep.LastUpdatedTime = (int)frame.Timestamp;
-        }
-      }
+                }
+            }
 
       /** Mark-and-sweep to finish unused HandRepresentations */
       HandRepresentation toBeDeleted = null;
-      for (var it = all_hand_reps.GetEnumerator(); it.MoveNext();) {
-        var r = it.Current;
-        if (r.Value != null) {
-          if (r.Value.IsMarked) {
-            r.Value.IsMarked = false;
-          }
-          else {
-            /** Initialize toBeDeleted with a value to be deleted */
-            //Debug.Log("Finishing");
-            toBeDeleted = r.Value;
-          }
-        }
-      }
-      /**Inform the representation that we will no longer be giving it any hand updates 
-       * because the corresponding hand has gone away */
-      if (toBeDeleted != null) {
+            if (!persistentHands)
+            {
+                for (var it = all_hand_reps.GetEnumerator(); it.MoveNext();)
+                {
+                    var r = it.Current;
+                    if (r.Value != null)
+                    {
+                        if (r.Value.IsMarked)
+                        {
+                            r.Value.IsMarked = false;
+                        }
+                        else
+                        {
+                            /** Initialize toBeDeleted with a value to be deleted */
+                            //Debug.Log("Finishing");
+                            toBeDeleted = r.Value;
+                            Debug.Log("This happened and needs to not!");
+                        }
+                    }
+                }
+            }
+            /**Inform the representation that we will no longer be giving it any hand updates 
+             * because the corresponding hand has gone away */
+    if (toBeDeleted != null) {
         all_hand_reps.Remove(toBeDeleted.HandID);
         toBeDeleted.Finish();
       }
@@ -143,6 +164,8 @@ namespace Leap.Unity {
         }
       }
     }
+
+    [SerializeField] private bool persistentHands = false;
 
     [SerializeField]
     [Tooltip("To add a new set of Hand Models, first add the Left and Right objects as "
