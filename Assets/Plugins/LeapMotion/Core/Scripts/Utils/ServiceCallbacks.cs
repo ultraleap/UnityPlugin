977f195ac30f1ae44154da3a42439c1f07ad6775
MattFiler <matt.filer@ultraleap.com>
2021-05-13 17:50:30 +0100

    Added service binding code (requires UltraleapTrackingServiceBinder AAR)

---
+++ Assets/Plugins/LeapMotion/Core/Scripts/Utils/ServiceCallbacks.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_ANDROID
+public class ServiceCallbacks : AndroidJavaProxy
+{
+    public ServiceCallbacks() :
+        base("com.ultraleap.tracking.service_binder.ServiceBinder$Callbacks")
+    { }
+    public void onBound()
+    {
+        Debug.Log("ServiceCallbacks.onBound");
+    }
+
+    public void onUnbound()
+    {
+        Debug.Log("ServiceCallbacks.onUnbound");
+        // Ensure we disconnect if the service becomes unbound to prevent
+        // a TIME_WAIT state on the Service TCP socket/address
+        foreach (var provider in GameObject.FindObjectsOfType<Leap.Unity.LeapServiceProvider>())
+        {
+            provider.destroyController();
+        }
+    }
+}
+#endif
