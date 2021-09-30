f24ed3ff0a393a7c2893391c1cadfe13e710e29f
Elliot Padfield <75795822+ElliotPadfieldUL@users.noreply.github.com>
2021-09-30 16:32:40 +0100

    Formatting applied

--- Core/Editor/Scripts/LeapXRServiceProviderEditor.cs
+++ Core/Editor/Scripts/LeapXRServiceProviderEditor.cs
@@ -6,13 +6,15 @@
  * between Ultraleap and you, your company or other organization.             *
  ******************************************************************************/
 
- using UnityEditor;
- using UnityEngine;
+using UnityEditor;
+using UnityEngine;
 
-namespace Leap.Unity {
+namespace Leap.Unity
+{
 
-  [CustomEditor(typeof(LeapXRServiceProvider))]
-  public class LeapXRServiceProviderEditor : LeapServiceProviderEditor {
+    [CustomEditor(typeof(LeapXRServiceProvider))]
+    public class LeapXRServiceProviderEditor : LeapServiceProviderEditor
+    {
 
         SerializedProperty _camera;
 
@@ -23,102 +25,115 @@ namespace Leap.Unity {
         }
         public XRTestHandPose TestHandPose;
 
-        protected override void OnEnable() {
-
-      _camera = serializedObject.FindProperty("_camera");
-
-      base.OnEnable();
-      isVRProvider = true;
-
-      specifyCustomDrawer("editTimePose", DrawCustomEnum);
-
-      specifyConditionalDrawing(() => { return serializedObject
-                                                 .FindProperty("_temporalWarpingMode")
-                                                   .enumValueIndex == 1; },
-                                "_customWarpAdjustment");
-
-      specifyConditionalDrawing(() => { return serializedObject
-                                                 .FindProperty("_deviceOffsetMode")
-                                                   .enumValueIndex == 1; },
-                                "_deviceOffsetYAxis", 
-                                "_deviceOffsetZAxis",
-                                "_deviceTiltXAxis");
-
-      specifyConditionalDrawing(() => { return serializedObject
-                                                 .FindProperty("_deviceOffsetMode")
-                                                   .enumValueIndex == 2; },
-                                "_deviceOrigin");
-
-      addPropertyToFoldout("_deviceOffsetMode"    , "Advanced Options");
-      addPropertyToFoldout("_temporalWarpingMode" , "Advanced Options");
-      addPropertyToFoldout("_customWarpAdjustment", "Advanced Options");
-      addPropertyToFoldout("_deviceOffsetYAxis"   , "Advanced Options");
-      addPropertyToFoldout("_deviceOffsetZAxis"   , "Advanced Options");
-      addPropertyToFoldout("_deviceTiltXAxis"     , "Advanced Options");
-      addPropertyToFoldout("_deviceOrigin"        , "Advanced Options");
-      addPropertyToFoldout("_updateHandInPrecull" , "Advanced Options");
-    }
-
-    private void DrawCustomEnum(SerializedProperty property)
-    {
-        TestHandPose = (XRTestHandPose)EditorGUILayout.EnumPopup("Edit Time Pose", TestHandPose);
-
-        switch (TestHandPose)
+        protected override void OnEnable()
         {
-            case XRTestHandPose.HeadMountedA:
+
+            _camera = serializedObject.FindProperty("_camera");
+
+            base.OnEnable();
+            isVRProvider = true;
+
+            specifyCustomDrawer("editTimePose", DrawCustomEnum);
+
+            specifyConditionalDrawing(() =>
+            {
+                return serializedObject
+                         .FindProperty("_temporalWarpingMode")
+                           .enumValueIndex == 1;
+            },
+                                      "_customWarpAdjustment");
+
+            specifyConditionalDrawing(() =>
+            {
+                return serializedObject
+                         .FindProperty("_deviceOffsetMode")
+                           .enumValueIndex == 1;
+            },
+                                      "_deviceOffsetYAxis",
+                                      "_deviceOffsetZAxis",
+                                      "_deviceTiltXAxis");
+
+            specifyConditionalDrawing(() =>
+            {
+                return serializedObject
+                         .FindProperty("_deviceOffsetMode")
+                           .enumValueIndex == 2;
+            },
+                                      "_deviceOrigin");
+
+            addPropertyToFoldout("_deviceOffsetMode", "Advanced Options");
+            addPropertyToFoldout("_temporalWarpingMode", "Advanced Options");
+            addPropertyToFoldout("_customWarpAdjustment", "Advanced Options");
+            addPropertyToFoldout("_deviceOffsetYAxis", "Advanced Options");
+            addPropertyToFoldout("_deviceOffsetZAxis", "Advanced Options");
+            addPropertyToFoldout("_deviceTiltXAxis", "Advanced Options");
+            addPropertyToFoldout("_deviceOrigin", "Advanced Options");
+            addPropertyToFoldout("_updateHandInPrecull", "Advanced Options");
+        }
+
+        private void DrawCustomEnum(SerializedProperty property)
+        {
+            TestHandPose = (XRTestHandPose)EditorGUILayout.EnumPopup("Edit Time Pose", TestHandPose);
+
+            switch (TestHandPose)
+            {
+                case XRTestHandPose.HeadMountedA:
                     property.enumValueIndex = (int)TestHandFactory.TestHandPose.HeadMountedA;
-                break;
-            case XRTestHandPose.HeadMountedB:
+                    break;
+                case XRTestHandPose.HeadMountedB:
                     property.enumValueIndex = (int)TestHandFactory.TestHandPose.HeadMountedB;
                     break;
+            }
         }
-    }
 
 
-        private void decorateAllowManualTimeAlignment(SerializedProperty property) {
-      bool pcOrAndroidPlatformDetected = false;
-      string targetPlatform = "";
+        private void decorateAllowManualTimeAlignment(SerializedProperty property)
+        {
+            bool pcOrAndroidPlatformDetected = false;
+            string targetPlatform = "";
 #if UNITY_STANDALONE
-      pcOrAndroidPlatformDetected = true;
-      targetPlatform = "Standalone (Desktop)";
+            pcOrAndroidPlatformDetected = true;
+            targetPlatform = "Standalone (Desktop)";
 #elif UNITY_ANDROID
       pcOrAndroidPlatformDetected = true;
       targetPlatform = "Android";
 #endif
 
-      if (pcOrAndroidPlatformDetected && property.boolValue) {
-        EditorGUILayout.HelpBox(targetPlatform + " target platform detected; "
-                              + "manual time alignment should not be enabled under most "
-                              + "circumstances.", MessageType.Warning);
-      }
-    }
-
-    public override void OnInspectorGUI()
-    {
-        if (_camera.objectReferenceValue == null)
-        {
-            EditorGUILayout.PropertyField(_camera);
-            serializedObject.ApplyModifiedProperties();
-            return;
+            if (pcOrAndroidPlatformDetected && property.boolValue)
+            {
+                EditorGUILayout.HelpBox(targetPlatform + " target platform detected; "
+                                      + "manual time alignment should not be enabled under most "
+                                      + "circumstances.", MessageType.Warning);
+            }
         }
 
-        base.OnInspectorGUI();
+        public override void OnInspectorGUI()
+        {
+            if (_camera.objectReferenceValue == null)
+            {
+                EditorGUILayout.PropertyField(_camera);
+                serializedObject.ApplyModifiedProperties();
+                return;
+            }
+
+            base.OnInspectorGUI();
+        }
+
+        public override void OnSceneGUI()
+        {
+            LeapXRServiceProvider xrProvider = target as LeapXRServiceProvider;
+
+            if (xrProvider.Camera == null) { return; }
+
+            controllerOffset = new Vector3(0f,
+                                xrProvider.deviceOffsetYAxis,
+                                xrProvider.deviceOffsetZAxis);
+
+            deviceRotation = xrProvider.Camera.transform.InverseTransformRotation(xrProvider.Camera.transform.TransformRotation(Quaternion.Euler(xrProvider.deviceTiltXAxis, 0f, 0f))) * Quaternion.Euler(90f, 0f, 0f);
+
+
+            base.OnSceneGUI();
+
+        }
     }
-
-    public override void OnSceneGUI() {
-    LeapXRServiceProvider xrProvider = target as LeapXRServiceProvider;
-
-    if(xrProvider.Camera == null){ return; }
-
-    controllerOffset = new Vector3(0f,
-                        xrProvider.deviceOffsetYAxis,
-                        xrProvider.deviceOffsetZAxis);
-
-    deviceRotation = xrProvider.Camera.transform.InverseTransformRotation(xrProvider.Camera.transform.TransformRotation(Quaternion.Euler(xrProvider.deviceTiltXAxis, 0f, 0f))) * Quaternion.Euler(90f, 0f, 0f);
-
-
-    base.OnSceneGUI();
-
-    }
-  }
 }
