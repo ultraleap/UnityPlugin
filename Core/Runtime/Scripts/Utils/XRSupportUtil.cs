ffd61f0a9d37c8ab82246a481c013deea342ac3f
MaxPalmer-UH <53301524+MaxPalmer-UH@users.noreply.github.com>
2021-11-05 11:23:00 +0000

    UNITY-373 Reformatted XRSupportUtil, added null checks around SVRManager.Instance

--- Core/Runtime/Scripts/Utils/XRSupportUtil.cs
+++ Core/Runtime/Scripts/Utils/XRSupportUtil.cs
@@ -23,124 +23,137 @@ namespace Leap.Unity
   	/// via #ifdefs.
   	/// </summary>
     public static class XRSupportUtil
-	{
+    {
 
-	     // There is no support planned for Unity's XR plugin system on XR2 devices using the Unity SnapDragon XR SDK
-	     // The XR2 implementation therefore calls into the SVR plugin directly
+        // There is no support planned for Unity's XR plugin system on XR2 devices using the Unity SnapDragon XR SDK
+        // The XR2 implementation therefore calls into the SVR plugin directly
 
-	     #if UNITY_ANDROID // In future we should target XR2 devices only with this. However, at the moment Android support is limited to XR2 devices only
+#if UNITY_ANDROID // In future we should target XR2 devices only with this. However, at the moment Android support is limited to XR2 devices only
 
-	     public static bool IsXREnabled() 
-		 {
-	         return SvrManager.Instance.status.running && SvrManager.Instance.status.pose == 3;
-	     }
+        public static bool IsXREnabled()
+        {
+            if (SvrManager.Instance != null)
+            {
+                return SvrManager.Instance.status.running && SvrManager.Instance.status.pose == 3;
+            }
 
-	     public static bool IsXRDevicePresent() 
-		 {
-	         return SvrManager.Instance.status.running;
-	     }
+            return false;
+        }
 
-	     public static bool IsUserPresent(bool defaultPresence = true) 
-		 {
-	         return true; // TODO user presence can be queried with recent versions of the SVR SDK (>= v4.0.4) - a proximity detector is fitted to some devices.
-	     }
+        public static bool IsXRDevicePresent()
+        {
+            if (SvrManager.Instance != null)
+            {
+                return SvrManager.Instance.status.running;
+            }
 
-	     public static Vector3 GetXRNodeCenterEyeLocalPosition() 
-		 {
-	         if (XRSupportUtil.IsXREnabled()) 
-			 {
-	             return SvrManager.Instance.head.transform.localPosition;
-	         } 
-			 else 
-			 {
-	             Debug.LogWarning("Cannot read XRNodeCenterEyeLocalPosition as XR is not enabled");
-	             return Vector3.zero;
-	         }
-	     }
+            return false;
+        }
 
-	     public static Quaternion GetXRNodeCenterEyeLocalRotation() 
-		 {
-	         if (XRSupportUtil.IsXREnabled()) 
-			 {
-	             return SvrManager.Instance.head.transform.localRotation;
-	         } 
-			 else 
-			 {
-	             Debug.LogWarning("Cannot read XRNodeCenterEyeLocalRotation as XR is not enabled");
-	             return Quaternion.identity;
-	         }
-	     }
+        public static bool IsUserPresent(bool defaultPresence = true)
+        {
+            return true; // TODO user presence can be queried with recent versions of the SVR SDK (>= v4.0.4) - a proximity detector is fitted to some devices.
+        }
 
-	     public static Vector3 GetXRNodeHeadLocalPosition() 
-		 {
-	         if (XRSupportUtil.IsXREnabled())
-	         {
-	             return SvrManager.Instance.head.localPosition;
-	         }
-	         else
-	         {
-	             Debug.LogWarning("Cannot read XRNodeHeadLocalPosition as XR is not enabled");
-	             return Vector3.zero;
-	         }
-	     }
+        public static Vector3 GetXRNodeCenterEyeLocalPosition()
+        {
+            if (XRSupportUtil.IsXREnabled())
+            {
+                return SvrManager.Instance.head.transform.localPosition;
+            }
+            else
+            {
+                Debug.LogWarning("Cannot read XRNodeCenterEyeLocalPosition as XR is not enabled");
+                return Vector3.zero;
+            }
+        }
 
-	     public static Quaternion GetXRNodeHeadLocalRotation() 
-		 {
-	         if (XRSupportUtil.IsXREnabled())
-	         {
-	             return SvrManager.Instance.head.transform.localRotation;
-	         }
-	         else
-	         {
-	             Debug.LogWarning("Cannot read XRNodeHeadLocalRotation as XR is not enabled");
-	             return Quaternion.identity;
-	         }
-	     }
+        public static Quaternion GetXRNodeCenterEyeLocalRotation()
+        {
+            if (XRSupportUtil.IsXREnabled())
+            {
+                return SvrManager.Instance.head.transform.localRotation;
+            }
+            else
+            {
+                Debug.LogWarning("Cannot read XRNodeCenterEyeLocalRotation as XR is not enabled");
+                return Quaternion.identity;
+            }
+        }
 
-	     public static Vector3 GetXRNodeLocalPosition(int node) 
-		 {
-	          Debug.LogWarning("Not implemented yet");
-	          return Vector3.zero;
-	     }
+        public static Vector3 GetXRNodeHeadLocalPosition()
+        {
+            if (XRSupportUtil.IsXREnabled())
+            {
+                return SvrManager.Instance.head.localPosition;
+            }
+            else
+            {
+                Debug.LogWarning("Cannot read XRNodeHeadLocalPosition as XR is not enabled");
+                return Vector3.zero;
+            }
+        }
 
-	     public static Quaternion GetXRNodeLocalRotation(int node) 
-		 {
-	          Debug.LogWarning("Not implemented yet");
-	          return Quaternion.identity;
-	     }
+        public static Quaternion GetXRNodeHeadLocalRotation()
+        {
+            if (XRSupportUtil.IsXREnabled())
+            {
+                return SvrManager.Instance.head.transform.localRotation;
+            }
+            else
+            {
+                Debug.LogWarning("Cannot read XRNodeHeadLocalRotation as XR is not enabled");
+                return Quaternion.identity;
+            }
+        }
 
-	     public static void Recenter() 
-		 {
-	         SvrInput.Instance.HandleRecenter();
-	     }
+        public static Vector3 GetXRNodeLocalPosition(int node)
+        {
+            Debug.LogWarning("Not implemented yet");
+            return Vector3.zero;
+        }
 
-	     public static float GetGPUTime() 
-		 {
-	          Debug.LogWarning("Not implemented for android");
-	          return 0;
-	     }
+        public static Quaternion GetXRNodeLocalRotation(int node)
+        {
+            Debug.LogWarning("Not implemented yet");
+            return Quaternion.identity;
+        }
 
-	     public static string GetLoadedDeviceName() 
-		 {
-	         return "XR2_Device";
-	     }
+        public static void Recenter()
+        {
+            if (SvrInput.Instance != null)
+            {
+                SvrInput.Instance.HandleRecenter();
+            }
+        }
 
-	     public static bool IsRoomScale() 
-		 {
-	         return true;
-	     }
+        public static float GetGPUTime()
+        {
+            Debug.LogWarning("Not implemented for android");
+            return 0;
+        }
 
-	     public static bool IsLargePlayspace() 
-		 {
-	         // The current SVR support for reading the playspace dimensions is crude. Likely to be improved on by 3rd parties
-	         // By default, we should consider the current playspace as 'large'
-	         return true;
-	     }
+        public static string GetLoadedDeviceName()
+        {
+            return "XR2_Device";
+        }
 
-	     // The following methods / fields are part of the XRSupportUtil 'interface' but are not currently used. They have not been implemented for the XR2/Android
-	     // public static float GetGPUTime() 
-		 // {
-	     // }
+        public static bool IsRoomScale()
+        {
+            return true;
+        }
+
+        public static bool IsLargePlayspace()
+        {
+            // The current SVR support for reading the playspace dimensions is crude. Likely to be improved on by 3rd parties
+            // By default, we should consider the current playspace as 'large'
+            return true;
+        }
+
+        // The following methods / fields are part of the XRSupportUtil 'interface' but are not currently used. They have not been implemented for the XR2/Android
+        // public static float GetGPUTime() 
+        // {
+        // }
 
 #else
 
@@ -460,6 +473,6 @@ namespace Leap.Unity
             return gpuTime;
         }
 
-    #endif
+#endif
     }
 }
