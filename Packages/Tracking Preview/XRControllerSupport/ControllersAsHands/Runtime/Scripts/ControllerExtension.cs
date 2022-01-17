b55cd3fe846746a2805eca3ffe61f1b2d80e6d8e
Pip Turner <pip.turner@ultrahaptics.com>
2022-01-17 15:57:02 +0000

    Update ControllerExtension.cs to add copyright

--- Packages/Tracking Preview/XRControllerSupport/ControllersAsHands/Runtime/Scripts/ControllerExtension.cs
+++ Packages/Tracking Preview/XRControllerSupport/ControllersAsHands/Runtime/Scripts/ControllerExtension.cs
@@ -1,3 +1,11 @@
+﻿/******************************************************************************
+ * Copyright (C) Ultraleap, Inc. 2011-2022.                                   *
+ *                                                                            *
+ * Use subject to the terms of the Apache License 2.0 available at            *
+ * http://www.apache.org/licenses/LICENSE-2.0, or another agreement           *
+ * between Ultraleap and you, your company or other organization.             *
+ ******************************************************************************/
+
 #if ENABLE_INPUT_SYSTEM
 
 using UnityEngine.InputSystem;
