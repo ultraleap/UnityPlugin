9f7afe680b436ee39f8da26520fc27f5d242c27a
Elliot Padfield <75795822+ElliotPadfieldUL@users.noreply.github.com>
2022-01-26 10:32:25 +0000

    Adding namespace and header

--- Packages/Tracking/Examples~/Example Assets/Scripts/EditorURL.cs
+++ Packages/Tracking/Examples~/Example Assets/Scripts/EditorURL.cs
@@ -1,106 +1,117 @@
-﻿using System.Linq;
+﻿/******************************************************************************
+ * Copyright (C) Ultraleap, Inc. 2011-2022.                                   *
+ *                                                                            *
+ * Use subject to the terms of the Apache License 2.0 available at            *
+ * http://www.apache.org/licenses/LICENSE-2.0, or another agreement           *
+ * between Ultraleap and you, your company or other organization.             *
+ ******************************************************************************/
+
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-/// <summary>
-/// A script to display GUI displaying a URL in the Game window during editor
-/// </summary>
-[ExecuteAlways]
-public class EditorURL : MonoBehaviour
+namespace Leap.Unity.Examples
 {
-    public string url = "https://docs.ultraleap.com/ultralab/";
-    public GUISkin skin;
-    public Texture logo;
-
-    int fontSize = 25;
-    float logoSize = 15;
-    Color32 highlightColor = new Color32(0, 235, 133, 255);
-
     /// <summary>
-    /// Draw GUI to the screen
+    /// A script to display GUI displaying a URL in the Game window during editor
     /// </summary>
-    private void OnGUI()
+    [ExecuteAlways]
+    public class EditorURL : MonoBehaviour
     {
-        if (skin == null)
+        public string url = "https://docs.ultraleap.com/ultralab/";
+        public GUISkin skin;
+        public Texture logo;
+
+        int fontSize = 25;
+        float logoSize = 15;
+        Color32 highlightColor = new Color32(0, 235, 133, 255);
+
+        /// <summary>
+        /// Draw GUI to the screen
+        /// </summary>
+        private void OnGUI()
         {
-            return;
+            if (skin == null)
+            {
+                return;
+            }
+
+            AdjustSkin(skin);
+            DrawURLGUI(url);
+            DrawLogo();
         }
 
-        AdjustSkin(skin);
-        DrawURLGUI(url);
-        DrawLogo();
-    }
-
-    /// <summary>
-    /// Adjust the GUI skin to display a transparent background
-    /// </summary>
-    /// <param name="skin"></param>
-    void AdjustSkin(GUISkin skin)
-    {
-        Texture2D background = new Texture2D(0, 0);
-        skin.button.normal.background = background;
-        skin.button.hover.background = background;
-        skin.button.hover.textColor = highlightColor;
-    }
-
-    /// <summary>
-    /// Draw a URL to the screen using GUI
-    /// </summary>
-    /// <param name="url"></param>
-    void DrawURLGUI(string url)
-    {
-        GUILayout.BeginArea(CalculateURLRect());
-        if (GUILayout.Button(url, skin.button))
+        /// <summary>
+        /// Adjust the GUI skin to display a transparent background
+        /// </summary>
+        /// <param name="skin"></param>
+        void AdjustSkin(GUISkin skin)
         {
-            Application.OpenURL(url);
+            Texture2D background = new Texture2D(0, 0);
+            skin.button.normal.background = background;
+            skin.button.hover.background = background;
+            skin.button.hover.textColor = highlightColor;
         }
-        GUILayout.EndArea();
-    }
 
-    /// <summary>
-    /// Draw the logo to the screen using GUI
-    /// </summary>
-    void DrawLogo()
-    {
-        GUILayout.BeginArea(CalculateLogoRect());
-        GUILayout.Label(logo);
-        GUILayout.EndArea();
-    }
-
-    /// <summary>
-    /// Calculate a Rect to display a URL
-    /// </summary>
-    /// <returns></returns>
-    Rect CalculateURLRect()
-    {
-        var dif = Screen.width - Screen.height;
-        if(dif >= 0)
+        /// <summary>
+        /// Draw a URL to the screen using GUI
+        /// </summary>
+        /// <param name="url"></param>
+        void DrawURLGUI(string url)
         {
-            skin.button.fontSize = Screen.height / fontSize;
-            return new Rect(0, 0, Screen.width, Screen.height / (fontSize / 2));
+            GUILayout.BeginArea(CalculateURLRect());
+            if (GUILayout.Button(url, skin.button))
+            {
+                Application.OpenURL(url);
+            }
+            GUILayout.EndArea();
         }
-        else
-        {
-            skin.button.fontSize = Screen.width / fontSize;
-            return new Rect(0, 0, Screen.width - Screen.height / fontSize, Screen.height / (fontSize / 2));
-        }
-    }
 
-    /// <summary>
-    /// Calculate a Rect to display a logo
-    /// </summary>
-    /// <returns></returns>
-    Rect CalculateLogoRect()
-    {
-        var dif = Screen.width - Screen.height;
-        if (dif >= 0)
+        /// <summary>
+        /// Draw the logo to the screen using GUI
+        /// </summary>
+        void DrawLogo()
         {
-            return new Rect((Screen.width - Screen.width / logoSize), 0, Screen.width / logoSize, Screen.width / logoSize);
-
+            GUILayout.BeginArea(CalculateLogoRect());
+            GUILayout.Label(logo);
+            GUILayout.EndArea();
         }
-        else
+
+        /// <summary>
+        /// Calculate a Rect to display a URL
+        /// </summary>
+        /// <returns></returns>
+        Rect CalculateURLRect()
         {
-            return new Rect((Screen.width - Screen.height / logoSize), 0, Screen.height / logoSize, Screen.height / logoSize);
+            var dif = Screen.width - Screen.height;
+            if (dif >= 0)
+            {
+                skin.button.fontSize = Screen.height / fontSize;
+                return new Rect(0, 0, Screen.width, Screen.height / (fontSize / 2));
+            }
+            else
+            {
+                skin.button.fontSize = Screen.width / fontSize;
+                return new Rect(0, 0, Screen.width - Screen.height / fontSize, Screen.height / (fontSize / 2));
+            }
+        }
+
+        /// <summary>
+        /// Calculate a Rect to display a logo
+        /// </summary>
+        /// <returns></returns>
+        Rect CalculateLogoRect()
+        {
+            var dif = Screen.width - Screen.height;
+            if (dif >= 0)
+            {
+                return new Rect((Screen.width - Screen.width / logoSize), 0, Screen.width / logoSize, Screen.width / logoSize);
+
+            }
+            else
+            {
+                return new Rect((Screen.width - Screen.height / logoSize), 0, Screen.height / logoSize, Screen.height / logoSize);
+            }
         }
     }
 }
