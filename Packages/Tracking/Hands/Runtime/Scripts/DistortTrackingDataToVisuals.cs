f66d122dbbfc94fc51ee1df7a0a6fefead0f0823
Elliot Padfield <75795822+ElliotPadfieldUL@users.noreply.github.com>
2022-03-18 11:38:23 +0000

    Namespace and Copyright

--- Packages/Tracking/Hands/Runtime/Scripts/DistortTrackingDataToVisuals.cs
+++ Packages/Tracking/Hands/Runtime/Scripts/DistortTrackingDataToVisuals.cs
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 772bcc78 (Namespace and Copyright)
 ﻿/******************************************************************************
  * Copyright (C) Ultraleap, Inc. 2011-2021.                                   *
  *                                                                            *
@@ -67,48 +70,6 @@ namespace Leap.Unity.HandsModule
                 inputFrame.Hands = hands;
             }
         }
-    }
-}
-
-/// <summary>
-/// A post process provider that allows hand visuals to generate leap hands that can be used to distort tracking data to visuals
-/// This should be used in cases where you have visual hands that do not conform to leap data size, e.g. a monster hand that has extra long fingers
-/// </summary>
-public class DistortTrackingDataToVisuals : PostProcessProvider
-{
-    public HandBinder LeftHand;
-    public HandBinder RightHand;
-
-    public float fingerTipScale = 0.8f;
-
-    public override void ProcessFrame(ref Frame inputFrame)
-    {
-        var hands = new List<Hand>();
-
-        if (LeftHand != null)
-        {
-            var lHand = LeftHand.BoundHand.GenerateLeapHand(LeftHand.LeapHand, fingerTipScale);
-
-            if (lHand != null)
-            {
-                lHand.IsLeft = true;
-                hands.Add(lHand);
-            }
-        }
-
-        if (RightHand != null)
-        {
-            var rHand = RightHand.BoundHand.GenerateLeapHand(RightHand.LeapHand, fingerTipScale);
-
-            if (rHand != null)
-            {
-                rHand.IsLeft = false;
-                hands.Add(rHand);
-            }
-        }
-
-        inputFrame.Hands.Clear();
-        inputFrame.Hands = hands;
     }
 }
 
