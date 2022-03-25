d04acf55f1107ed57a622811674ea8d93cd7ce70
JuliaRonneberger <julia.ronneberger@ultraleap.com>
2022-03-25 12:04:09 +0000

    include and update licence headers

--- Packages/Tracking Preview/AggregationProviders/LeapAggregatedProviderBase.cs
+++ Packages/Tracking Preview/AggregationProviders/LeapAggregatedProviderBase.cs
@@ -1,10 +1,9 @@
 /******************************************************************************
- * Copyright (C) Leap Motion, Inc. 2011-2018.                                 *
- * Leap Motion proprietary and confidential.                                  *
+ * Copyright (C) Ultraleap, Inc. 2011-2021.                                   *
  *                                                                            *
- * Use subject to the terms of the Leap Motion SDK Agreement available at     *
- * https://developer.leapmotion.com/sdk_agreement, or another agreement       *
- * between Leap Motion and you, your company or other organization.           *
+ * Use subject to the terms of the Apache License 2.0 available at            *
+ * http://www.apache.org/licenses/LICENSE-2.0, or another agreement           *
+ * between Ultraleap and you, your company or other organization.             *
  ******************************************************************************/
 
 using Leap.Unity.Encoding;
