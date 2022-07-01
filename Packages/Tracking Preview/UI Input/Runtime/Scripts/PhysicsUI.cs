4733f8ab68c6f6adc8a6e94843efb978c797ff22
MattGray <matt.gray@ultraleap.com>
2022-07-01 16:39:55 +0100

    Update copyright headers to 2022

--- Packages/Tracking Preview/UI Input/Runtime/Scripts/PhysicsUI.cs
+++ Packages/Tracking Preview/UI Input/Runtime/Scripts/PhysicsUI.cs
@@ -1,11 +1,5 @@
 /******************************************************************************
- * Copyright (C) Ultraleap, Inc. 2011-2020.                                   *
- *                                                                            *
- * Use subject to the terms of the Apache License 2.0 available at            *
- * http://www.apache.org/licenses/LICENSE-2.0, or another agreement           *
- * between Ultraleap and you, your company or other organization.             *
- ******************************************************************************//******************************************************************************
- * Copyright (C) Ultraleap, Inc. 2011-2021.                                   *
+ * Copyright (C) Ultraleap, Inc. 2011-2022.                                   *
  *                                                                            *
  * Use subject to the terms of the Apache License 2.0 available at            *
  * http://www.apache.org/licenses/LICENSE-2.0, or another agreement           *
