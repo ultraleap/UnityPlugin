e47b10f7d0f854acec17a0d84f7b151f465e39b2
Rory Clark <rory.clark@ultrahaptics.com>
2022-07-26 16:39:03 +0100

    Making the collision disabling optional.

--- Packages/Tracking Preview/PhysicsHands/Runtime/PhysicsIgnoreHelpers.cs
+++ Packages/Tracking Preview/PhysicsHands/Runtime/PhysicsIgnoreHelpers.cs
@@ -4,11 +4,19 @@ namespace Leap.Unity.Interaction.PhysicsHands
 {
     // This script will prevent the physics hands helpers from being applied to your object.
     // This allows you to easily prevent important objects from being affected by the player.
-    // Note that this will not prevent your object from being collided with.
+
     public class PhysicsIgnoreHelpers : MonoBehaviour
     {
+        [Tooltip("This prevents the object from being collided with Physics Hands.")]
+        public bool DisableHandCollisions = true;
+
         private void OnCollisionEnter(Collision collision)
         {
+            if(!DisableHandCollisions)
+            {
+                return;
+            }
+
             if(collision.gameObject != null && collision.gameObject.TryGetComponent<PhysicsBone>(out var temp))
             {
                 foreach (var contact in collision.contacts)
