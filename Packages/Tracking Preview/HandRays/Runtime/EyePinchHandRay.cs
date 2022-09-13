ab8db2f30a6a2ddb8d663be3cae9bb03a22ee8f3
Pip Turner <pip.turner@ultraleap.com>
2022-09-13 15:23:29 +0100

    Add EyePinch Hand Ray

---
+++ Packages/Tracking Preview/HandRays/Runtime/EyePinchHandRay.cs
@@ -0,0 +1,119 @@
+using Leap;
+using Leap.Unity;
+using Leap.Unity.Preview.HandRays;
+using UnityEngine;
+
+public class EyePinchHandRay : HandRay
+{
+    [SerializeField] private InferredBodyPositions inferredBodyPositions;
+
+    [Header("Debug Gizmos")]
+    [SerializeField] private bool drawDebugGizmos;
+
+    [SerializeField] private bool drawRay = true;
+    [SerializeField] private Color rayColor = Color.green;
+
+    [SerializeField] private bool drawRayAimAndOrigin = true;
+    [SerializeField] private Color rayAimAndOriginColor = Color.red;
+
+    private float gizmoRadius = 0.01f;
+
+    /// <summary>
+    /// This local-space offset from the wrist is used to better align the ray to the pinch position
+    /// </summary>
+    private Vector3 wristOffset = new Vector3(0.0425f, 0.0652f, 0.0f);
+    private Transform transformHelper;
+
+    private OneEuroFilter<Vector3> aimPositionFilter;
+    private OneEuroFilter<Vector3> rayOriginFilter;
+
+    /// <summary>
+    /// Beta param for OneEuroFilter (see https://cristal.univ-lille.fr/~casiez/1euro/)
+    /// If you're experiencing high speed lag, increase beta
+    /// </summary>
+    private float oneEuroBeta = 100;
+    /// <summary>
+    /// MinCutoff for OneEuroFilter (see https://cristal.univ-lille.fr/~casiez/1euro/)
+    /// If you're experiencing slow speed jitter, decrease MinCutoff
+    /// </summary>
+    private float oneEuroMinCutoff = 5;
+
+    private readonly float oneEurofreq = 30;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        transformHelper = new GameObject("WristShoulderFarFieldRay_TransformHelper").transform;
+        transformHelper.SetParent(transform);
+
+        if (inferredBodyPositions == null)
+        {
+            inferredBodyPositions = gameObject.AddComponent<InferredBodyPositions>();
+        }
+
+        aimPositionFilter = new OneEuroFilter<Vector3>(oneEurofreq, oneEuroMinCutoff, oneEuroBeta);
+        rayOriginFilter = new OneEuroFilter<Vector3>(oneEurofreq, oneEuroMinCutoff, oneEuroBeta);
+    }
+
+    /// <summary>
+    /// Calculates whether the hand ray should be enabled
+    /// </summary>
+    protected override bool ShouldEnableRay()
+    {
+        if (!base.ShouldEnableRay())
+        {
+            return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// Calculates the Ray Direction using the wrist and shoulder position aimed through a stable pinch position
+    /// </summary>
+    protected override void CalculateRayDirection()
+    {
+        Hand hand = leapProvider.CurrentFrame.GetHand(chirality);
+        if (hand == null)
+        {
+            return;
+        }
+
+        handRayDirection.Hand = hand;
+        handRayDirection.VisualAimPosition = hand.GetPredictedPinchPosition();
+        Vector3 unfilteredRayOrigin = GetRayOrigin();
+
+        //Filtering using the One Euro filter reduces jitter from both positions
+        handRayDirection.AimPosition = aimPositionFilter.Filter(hand.GetPredictedPinchPosition(), Time.time);
+        handRayDirection.RayOrigin = rayOriginFilter.Filter(unfilteredRayOrigin, Time.time);
+
+        handRayDirection.Direction = (handRayDirection.AimPosition - handRayDirection.RayOrigin).normalized;
+        InvokeOnHandRayFrame(handRayDirection);
+    }
+
+    private Vector3 GetRayOrigin()
+    {
+        return chirality == Chirality.Left ? inferredBodyPositions.EyePositions[0] : inferredBodyPositions.EyePositions[1];
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!drawDebugGizmos || !Application.isPlaying || !HandRayEnabled)
+        {
+            return;
+        }
+
+        if (drawRay)
+        {
+            Gizmos.color = rayColor;
+            Gizmos.DrawRay(handRayDirection.RayOrigin, handRayDirection.Direction * 10);
+        }
+
+        if (drawRayAimAndOrigin)
+        {
+            Gizmos.color = rayAimAndOriginColor;
+            Gizmos.DrawCube(handRayDirection.RayOrigin, Vector3.one * gizmoRadius);
+            Gizmos.DrawSphere(handRayDirection.AimPosition, gizmoRadius);
+        }
+    }
+}
