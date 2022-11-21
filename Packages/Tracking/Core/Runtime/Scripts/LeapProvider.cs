aeaf795b6e3abab74ebfb5707a3d34c7b81e4de6
MattGray <matt.gray@ultraleap.com>
2022-11-21 15:44:23 +0000

    added summary for new TrackingSource enum

--- Packages/Tracking/Core/Runtime/Scripts/LeapProvider.cs
+++ Packages/Tracking/Core/Runtime/Scripts/LeapProvider.cs
@@ -82,6 +82,13 @@ namespace Leap.Unity
 
     }
 
+    /// <summary>
+    /// Used to determine the source of the tracking data.
+    /// NONE - Either not available or has not been detemined yet
+    /// LEAPC - A direct connection to the Leap Service via LeapC
+    /// OPENXR - An OpenXR connection, most likely not Ultraleap Tracking
+    /// OPENXR_LEAP - An OpenXR connection, Ultraleap OpenXR layer is also active
+    /// </summary>
     public enum TrackingSource
     {
         NONE,
