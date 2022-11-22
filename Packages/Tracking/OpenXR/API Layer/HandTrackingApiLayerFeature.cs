51d32eb04fdd9d1f8a424761aaf568b417b71677
Robert Blenkinsopp <robert.blenkinsopp@ultraleap.com>
2022-11-22 17:37:21 +0000

    Add build hook to remove libraries that conflict

--- Packages/Tracking/OpenXR/API Layer/HandTrackingApiLayerFeature.cs
+++ Packages/Tracking/OpenXR/API Layer/HandTrackingApiLayerFeature.cs
@@ -1,12 +1,13 @@
 using JetBrains.Annotations;
-using UnityEditor.Build.Reporting;
-using UnityEngine.XR.OpenXR;
+using System.IO;
+using System.Linq;
 using UnityEngine.XR.OpenXR.Features;
 
 #if UNITY_EDITOR
 using System;
 using UnityEditor;
 using UnityEditor.XR.OpenXR.Features;
+using UnityEditor.Build.Reporting;
 #endif
 
 namespace Ultraleap.Tracking.OpenXR.ApiLayer
@@ -23,14 +24,14 @@ namespace Ultraleap.Tracking.OpenXR.ApiLayer
         Category = FeatureCategory.Feature,
         Required = false,
         OpenxrExtensionStrings = "",
-        BuildTargetGroups = new[] {BuildTargetGroup.Android}
+        BuildTargetGroups = new[] { BuildTargetGroup.Android }
     )]
 #endif
     public class HandTrackingApiLayerFeature : OpenXRFeature
     {
         [PublicAPI] public const string FeatureId = "com.ultraleap.tracking.openxr.feature.handtracking.api_layer";
     }
-    
+
 #if UNITY_EDITOR
     public class HandTrackingApiLayerFeatureBuildHooks : OpenXRFeatureBuildHooks
     {
@@ -39,7 +40,6 @@ namespace Ultraleap.Tracking.OpenXR.ApiLayer
 
         protected override void OnPreprocessBuildExt(BuildReport report)
         {
-            
         }
 
         protected override void OnPostGenerateGradleAndroidProjectExt(string path)
@@ -47,15 +47,51 @@ namespace Ultraleap.Tracking.OpenXR.ApiLayer
             // Remove libLeapC.so and AndroidServiceBinder.aar before Gradle is run.
             // This is to work-around the fact that they are also included in the embedded API layer and will cause
             // conflicts.
+            RemoveAAR(path, "UltraleapTrackingServiceBinder");
+            RemoveLibrary(path, "arm64-v8a", "LeapC");
         }
 
         protected override void OnPostprocessBuildExt(BuildReport report)
         {
-            
         }
-        
+
+        /// <summary>
+        /// Removes an AAR from the project and the associated Gradle build script
+        /// </summary>
+        /// <param name="path">The project path</param>
+        /// <param name="aarName">The name of the AAR library with out the `.aar` extension</param>
+        private void RemoveAAR(string path, string aarName)
+        {
+            // Delete the AAR from the project before Gradle is run.
+            var aarPath = Path.Combine(path, "libs", $"{aarName}.aar");
+            if (File.Exists(aarPath))
+            {
+                File.Delete(aarPath);
+            }
+
+            // Remove the reference to the AAR from the Gradle build script.
+            var buildScriptPath = Path.Combine(path, "build.gradle");
+            var buildScriptContent = File.ReadAllLines(buildScriptPath);
+            var modifiedBuildScriptContent = buildScriptContent.Where(
+                line => !(line.Contains("implementation(") && line.Contains(aarName) && line.Contains("ext:'aar'"))
+            );
+            File.WriteAllLines(buildScriptPath, modifiedBuildScriptContent);
+        }
+
+        /// <summary>
+        /// Removes a .so library from the build
+        /// </summary>
+        /// <param name="path">The project path</param>
+        /// <param name="architecture">The CPU architecture of the library</param>
+        /// <param name="libName">the library name without the .so or lib prefix</param>
+        private void RemoveLibrary(string path, string architecture, string libName)
+        {
+            var libPath = Path.Combine(path, "src", "main", "jniLibs", architecture, $"lib{libName}.so");
+            if (File.Exists(libPath))
+            {
+                File.Delete(libPath);
+            }
+        }
     }
 #endif
-    
-    
 }
