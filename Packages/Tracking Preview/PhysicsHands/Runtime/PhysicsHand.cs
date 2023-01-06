120e9e4d318b1830c6fff37b7e52aa385d715114
Pip Turner <pip.turner@ultraleap.com>
2023-01-06 10:58:30 +0000

    Fix physics hands palm velocity reporting as 0 by caching old values at the end of the frame

--- Packages/Tracking Preview/PhysicsHands/Runtime/PhysicsHand.cs
+++ Packages/Tracking Preview/PhysicsHands/Runtime/PhysicsHand.cs
@@ -447,7 +447,6 @@ namespace Leap.Unity.Interaction.PhysicsHands
             HandleIgnoredObjects();
 
             _wasGrasping = IsGrasping;
-            _originalOldPosition = _originalLeapHand.PalmPosition;
 
             if (IsGrasping)
             {
@@ -459,6 +458,10 @@ namespace Leap.Unity.Interaction.PhysicsHands
             PhysicsHandsUtils.ConvertPhysicsToLeapHand(_physicsHand, ref _leapHand, _originalLeapHand, Time.fixedDeltaTime);
 
             OnUpdatePhysics?.Invoke();
+
+            // Cache old positions for next frame
+            _physicsHand.oldPosition = _physicsHand.transform.position;
+            _originalOldPosition = _originalLeapHand.PalmPosition;
         }
 
         private void UpdateSettings()
