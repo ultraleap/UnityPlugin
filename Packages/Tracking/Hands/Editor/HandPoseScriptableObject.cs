f0b07f78913cc74b6818533290830644f9a8970f
BryceNewell <bryce.newell@ultraleap.com>
2023-01-26 16:27:56 +0000

    Added scriptable object which will store Hand data and threshold values for rotation and position of each joint

---
+++ Packages/Tracking/Hands/Editor/HandPoseScriptableObject.cs
@@ -0,0 +1,84 @@
+using Leap;
+using NUnit.Framework.Constraints;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+
+public class NamedListAttribute : PropertyAttribute
+{
+    public readonly string[] names;
+    public NamedListAttribute(string[] names) { this.names = names; }
+}
+
+[CustomPropertyDrawer(typeof(NamedListAttribute))]
+public class NamedArrayDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+    {
+        try
+        {
+            int pos = int.Parse(property.propertyPath.Split('[', ']')[1]);
+            EditorGUI.PropertyField(rect, property, new GUIContent(((NamedListAttribute)attribute).names[pos]), true);
+        }
+        catch
+        {
+            EditorGUI.PropertyField(rect, property, label, true);
+        }
+    }
+}
+
+[CreateAssetMenu(fileName = "HandPose", menuName = "ScriptableObjects/HandPose")]
+public class HandPoseScriptableObject : ScriptableObject
+{
+    private Hand serialisedHand;
+
+    private static Vector3 JointPos = new Vector3(0,0,0);
+    private static Quaternion JointRot = new Quaternion(0, 0, 0, 0);
+
+    [Header("Finger Positional Thresholds")]
+
+    [SerializeField]
+    [NamedListAttribute(new string[] { "Metacarpal", "Proximal", "Intermediate", "Distal" })]
+    private List<Vector3> ThumbJointPosition = new List<Vector3>() { JointPos, JointPos, JointPos, JointPos };
+    [SerializeField]
+    [NamedListAttribute(new string[] { "Metacarpal", "Proximal", "Intermediate", "Distal" })]
+    private List<Vector3> IndexJointPosition = new List<Vector3>() { JointPos, JointPos, JointPos, JointPos };
+    [SerializeField]
+    [NamedListAttribute(new string[] { "Metacarpal", "Proximal", "Intermediate", "Distal" })]
+    private List<Vector3> MiddleJointPosition = new List<Vector3>() { JointPos, JointPos, JointPos, JointPos };
+    [SerializeField]
+    [NamedListAttribute(new string[] { "Metacarpal", "Proximal", "Intermediate", "Distal" })]
+    private List<Vector3> RingJointPosition = new List<Vector3>() { JointPos, JointPos, JointPos, JointPos };
+    [SerializeField]
+    [NamedListAttribute(new string[] { "Metacarpal", "Proximal", "Intermediate", "Distal" })]
+    private List<Vector3> PinkieJointPosition = new List<Vector3>() { JointPos, JointPos, JointPos, JointPos };
+
+
+    [Header("Finger Rotational Thresholds")]
+
+    [SerializeField]
+    [NamedListAttribute(new string[] { "Metacarpal", "Proximal", "Intermediate", "Distal" })]
+    private List<Quaternion> ThumbJointRotation = new List<Quaternion>() { JointRot, JointRot, JointRot, JointRot };
+    [SerializeField]
+    [NamedListAttribute(new string[] { "Metacarpal", "Proximal", "Intermediate", "Distal" })]
+    private List<Quaternion> IndexJointRotataion = new List<Quaternion>() { JointRot, JointRot, JointRot, JointRot };
+    [SerializeField]
+    [NamedListAttribute(new string[] { "Metacarpal", "Proximal", "Intermediate", "Distal" })]
+    private List<Quaternion> MiddleJointRotaion = new List<Quaternion>() { JointRot, JointRot, JointRot, JointRot };
+    [SerializeField]
+    [NamedListAttribute(new string[] { "Metacarpal", "Proximal", "Intermediate", "Distal" })]
+    private List<Quaternion> RingJointRotaion = new List<Quaternion>() { JointRot, JointRot, JointRot, JointRot };
+    [SerializeField]
+    [NamedListAttribute(new string[] { "Metacarpal", "Proximal", "Intermediate", "Distal" })]
+    private List<Quaternion> PinkieJointRotaion = new List<Quaternion>() { JointRot, JointRot, JointRot, JointRot };
+
+
+    public void SaveHandPose(Hand handToSerialise)
+    {
+        serialisedHand = handToSerialise;
+    }
+}
