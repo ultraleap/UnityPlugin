7aa5dd0fc3788d0511f7c68e976d34db2c05c3d5
BryceNewell <bryce.newell@ultraleap.com>
2023-03-21 14:32:59 +0000

    Fixed pose validator capsule hand generator which was overwritten

--- Packages/Tracking/Core/Runtime/Scripts/HandPoses/HandPoseValidator.cs
+++ Packages/Tracking/Core/Runtime/Scripts/HandPoses/HandPoseValidator.cs
@@ -1,6 +1,7 @@
 using Leap.Unity;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 public class HandPoseValidator : MonoBehaviour
@@ -23,7 +24,8 @@ public class HandPoseValidator : MonoBehaviour
     private Color[] _rightCapsuleHandColours = new Color[32];
     private HandPoseDetector _detector;
 
-    public Transform validatorHandPrefab;
+    List<GameObject> lineRenderers = new List<GameObject>();
+
 
     private void Start()
     {
@@ -31,7 +33,7 @@ public class HandPoseValidator : MonoBehaviour
         _detector.EnablePoseCaching();
         if(validationHands.Count == 0)
         {
-            var instCapsuleHands = Instantiate(validatorHandPrefab);
+            var instCapsuleHands = Instantiate(CreateCapsuleHands());
             var capsuleHandScript = instCapsuleHands.GetComponentsInChildren<CapsuleHand>(true);
             foreach (var capHand in capsuleHandScript)
             {
@@ -47,7 +49,43 @@ public class HandPoseValidator : MonoBehaviour
         
     }
 
-    List<GameObject> lineRenderers = new List<GameObject>();
+    public static GameObject CreateCapsuleHands()
+    {
+        string prefabName = "CapsuleHands";
+        var guids = AssetDatabase.FindAssets(prefabName);
+
+        // look for exact matched first
+        foreach (var guid in guids)
+        {
+            string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+
+            string[] assetPathSplit = assetPath.Split('/', '\\', '.');
+
+            if (assetPathSplit[assetPathSplit.Length - 2] == prefabName && assetPathSplit[assetPathSplit.Length - 1] == "prefab")
+            {
+                GameObject newObject = (GameObject)AssetDatabase.LoadAssetAtPath(assetPath, typeof(GameObject));
+
+                if (newObject != null)
+                {
+                    return newObject;
+                }
+            }
+        }
+
+        // fallback to near-matches
+        foreach (var guid in guids)
+        {
+            string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+            GameObject newObject = (GameObject)AssetDatabase.LoadAssetAtPath(assetPath, typeof(GameObject));
+
+            if (newObject != null)
+            {
+                return newObject;
+            }
+        }
+        return null;
+    }
+
 
     private void Update()
     {
