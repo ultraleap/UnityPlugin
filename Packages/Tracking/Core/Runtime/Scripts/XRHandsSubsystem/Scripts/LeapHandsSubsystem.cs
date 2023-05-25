17f06c1fe32c639e8f6cef054ce55ca38072b1a8
BryceNewell <bryce.newell@ultraleap.com>
2023-05-25 16:18:14 +0100

    Hands now appear in mostly the correct place but joint position is not working.

--- Packages/Tracking/Core/Runtime/Scripts/XRHandsSubsystem/Scripts/LeapHandsSubsystem.cs
+++ Packages/Tracking/Core/Runtime/Scripts/XRHandsSubsystem/Scripts/LeapHandsSubsystem.cs
@@ -1,14 +1,18 @@
 using Unity.Collections;
 using UnityEngine;
 using UnityEngine.XR.Hands;
+using UnityEngine.SubsystemsImplementation;
 using UnityEngine.XR.Hands.ProviderImplementation;
 
 using Leap;
 using Leap.Unity;
+using System;
+using static UnityEngine.XR.Hands.XRHandSubsystem;
+using UnityEngine.XR.Hands.OpenXR;
 
 namespace Leap.Unity
 {
-    class LeapHandProvider : XRHandSubsystemProvider
+    class LeapXRHandProvider : XRHandSubsystemProvider
     {
         LeapProvider provider;
 
@@ -30,9 +34,33 @@ namespace Leap.Unity
         {
         }
 
-        public override XRHandSubsystem.UpdateSuccessFlags TryUpdateHands(XRHandSubsystem.UpdateType updateType, ref Pose leftHandRootPose, NativeArray<XRHandJoint> leftHandJoints, ref Pose rightHandRootPose, NativeArray<XRHandJoint> rightHandJoints)
+        static internal string id { get; private set; }
+
+        static LeapXRHandProvider() => id = "UL XR Hands";
+
+        //public Action<XRHandSubsystemProvider, UpdateSuccessFlags, UpdateType> updatedHands;
+        //public UpdateSuccessFlags updateSuccessFlags { get; protected set; }
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        static void Register()
         {
-            Debug.Log("updateHand");
+
+            ///This needs to be implemented here when we have the subsystem in place
+            //var handsSubsystemCinfo = new XRHandSubsystemDescriptor.Cinfo
+            //{
+            //    id = id,
+            //    providerType = typeof(OpenXRHandProvider)
+            //};
+            //XRHandSubsystemDescriptor.Register(handsSubsystemCinfo);
+            
+        }
+
+        public override XRHandSubsystem.UpdateSuccessFlags TryUpdateHands(
+            XRHandSubsystem.UpdateType updateType, 
+            ref Pose leftHandRootPose, 
+            NativeArray<XRHandJoint> leftHandJoints, 
+            ref Pose rightHandRootPose, 
+            NativeArray<XRHandJoint> rightHandJoints)
+        {
             Frame currentFrame = provider.CurrentFrame;
 
             XRHandSubsystem.UpdateSuccessFlags updateSuccessFlags = XRHandSubsystem.UpdateSuccessFlags.None;
@@ -60,6 +88,7 @@ namespace Leap.Unity
             }
 
             rootPose = new Pose(leapHand.PalmPosition, leapHand.Rotation);
+            Handedness handedness = (Handedness)((int)leapHand.GetChirality() + 1); // +1 as unity has "invalid" handedness while we do not 
 
             int jointIndex = 0;
 
@@ -67,7 +96,7 @@ namespace Leap.Unity
             {
                 foreach (var bone in finger.bones)
                 {
-                    handJoints[jointIndex] = XRHandProviderUtility.CreateJoint(
+                    handJoints[jointIndex] = XRHandProviderUtility.CreateJoint(handedness,
                     XRHandJointTrackingState.Pose,
                     XRHandJointIDUtility.FromIndex(jointIndex),
                     new Pose(bone.PrevJoint, bone.Rotation));
@@ -90,10 +119,85 @@ namespace Leap.Unity
             var handsSubsystemCinfo = new XRHandSubsystemDescriptor.Cinfo
             {
                 id = "UL XR Hands",
-                providerType = typeof(LeapHandProvider),
+                providerType = typeof(LeapXRHandProvider),
                 subsystemTypeOverride = typeof(LeapHandSubsystem)
             };
             XRHandSubsystemDescriptor.Register(handsSubsystemCinfo);
+        }
+
+        LeapProvider provider;
+
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            provider = GameObject.FindObjectOfType<XRLeapProviderManager>();
+            Debug.Log("started");
+
+        }
+
+
+        public override UpdateSuccessFlags TryUpdateHands(XRHandSubsystem.UpdateType updateType)
+        {
+            base.TryUpdateHands(updateType);
+
+            Pose leftHandRootPose = new Pose();
+            NativeArray<XRHandJoint> leftHandJoints = new NativeArray<XRHandJoint>(XRHandJointID.EndMarker.ToIndex(), Allocator.Persistent);
+            Pose rightHandRootPose = new Pose();
+            NativeArray< XRHandJoint > rightHandJoints = new NativeArray<XRHandJoint>(XRHandJointID.EndMarker.ToIndex(), Allocator.Persistent);
+
+
+            Debug.Log("Subsystem updateHand");
+            Frame currentFrame = provider.CurrentFrame;
+
+            UpdateSuccessFlags updateSuccessFlags = UpdateSuccessFlags.None;
+
+            if (PopulateXRHandFromLeap(currentFrame.GetHand(Chirality.Left), ref leftHandRootPose, ref leftHandJoints))
+            {
+                updateSuccessFlags |= UpdateSuccessFlags.LeftHandRootPose;
+                updateSuccessFlags |= UpdateSuccessFlags.LeftHandJoints;
+            }
+
+            if (PopulateXRHandFromLeap(currentFrame.GetHand(Chirality.Right), ref rightHandRootPose, ref rightHandJoints))
+            {
+                updateSuccessFlags |= UpdateSuccessFlags.RightHandRootPose;
+                updateSuccessFlags |= UpdateSuccessFlags.RightHandJoints;
+            }
+
+            if (updatedHands != null)
+                updatedHands.Invoke(this, updateSuccessFlags, updateType);
+
+
+
+            return updateSuccessFlags;
+        }
+
+        bool PopulateXRHandFromLeap(Hand leapHand, ref Pose rootPose, ref NativeArray<XRHandJoint> handJoints)
+        {
+            if (leapHand == null)
+            {
+                return false;
+            }
+
+            rootPose = new Pose(leapHand.PalmPosition, leapHand.Rotation);
+            Handedness handedness = (Handedness)((int)leapHand.GetChirality() + 1); // +1 as unity has "invalid" handedness while we do not 
+
+            int jointIndex = 0;
+
+            foreach (var finger in leapHand.Fingers)
+            {
+                foreach (var bone in finger.bones)
+                {
+                    handJoints[jointIndex] = XRHandProviderUtility.CreateJoint(handedness,
+                    XRHandJointTrackingState.Pose,
+                    XRHandJointIDUtility.FromIndex(jointIndex),
+                    new Pose(bone.PrevJoint, bone.Rotation));
+
+                    jointIndex++;
+                }
+            }
+
+            return true;
         }
     }
 }
--- Packages/Tracking/Core/Runtime/Scripts/XRHandsSubsystem/Scripts/SubsystemStarter.cs
+++ Packages/Tracking/Core/Runtime/Scripts/XRHandsSubsystem/Scripts/SubsystemStarter.cs
@@ -1,13 +1,24 @@
 using System;
 using System.Collections.Generic;
+using Unity.Collections;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SubsystemsImplementation;
+using UnityEngine.SubsystemsImplementation.Extensions;
 using UnityEngine.XR.Hands;
+using UnityEngine.XR.Hands.OpenXR;
+using UnityEngine.XR.Hands.ProviderImplementation;
+using UnityEngine.XR.Management;
 
 namespace Leap.Unity
 {
     public class SubsystemStarter : MonoBehaviour
     {
         XRHandSubsystem m_Subsystem = null;
+        XRHandSubsystem m_BaseSubsystem = null;
+        XRHandSubsystemProvider m_subsystemProvider = null;
+
+
 
         private void Awake()
         {
@@ -18,13 +29,27 @@ namespace Leap.Unity
                 if (descriptor.id == "UL XR Hands")
                 {
                     m_Subsystem = descriptor.Create();
+                    var handsSubsystemCinfo = new XRHandSubsystemDescriptor.Cinfo
+                    {
+                        id = "UL XR Hands",
+                        providerType = typeof(LeapXRHandProvider)
+                    };
+                    XRHandSubsystemDescriptor.Register(handsSubsystemCinfo);
+
                 }
+
 
                 if (m_Subsystem != null)
                 {
                     m_Subsystem.Start();
                 }
             }
+
+
+            m_BaseSubsystem = XRGeneralSettings.Instance?
+                .Manager?
+                .activeLoader?
+                .GetLoadedSubsystem<XRHandSubsystem>();
         }
 
         private void OnDestroy()
@@ -32,6 +57,15 @@ namespace Leap.Unity
             if (m_Subsystem != null)
             {
                 m_Subsystem.Stop();
+            }
+        }
+
+        private void Update()
+        {
+            if (m_Subsystem != null)
+            {
+                m_Subsystem.TryUpdateHands(XRHandSubsystem.UpdateType.BeforeRender);
+                
             }
         }
     }
