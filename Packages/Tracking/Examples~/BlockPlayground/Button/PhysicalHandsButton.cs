db25d32a0f7716edb3346eac2b9b315708701edb
BryceNewellUL <bryce.newell@ultraleap.com>
2023-12-06 09:49:20 +0000

    Adding Button Images to Hard and Soft contact buttons

--- Packages/Tracking/Examples~/BlockPlayground/Button/PhysicalHandsButton.cs
+++ Packages/Tracking/Examples~/BlockPlayground/Button/PhysicalHandsButton.cs
@@ -1,41 +1,31 @@
-using Leap.Unity.PhysicalHands;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
-using Leap.Unity;
 
 public class PhysicalHandsButton : MonoBehaviour
 {
-    private const float BUTTON_PRESS_THRESHOLD = 0.01F;
-    private const float BUTTON_PRESS_EXIT_THRESHOLD = 0.09F;
-
     public GameObject buttonObject;
     [Tooltip("The local position which the button will be limited to and will try to return to.")]
     public float buttonHeightLimit = 0.02f;
 
+    private const float BUTTON_PRESS_THRESHOLD = 0.01F;
+    private const float BUTTON_PRESS_EXIT_THRESHOLD = 0.09F;
+
     [SerializeField]
-    ChiralitySelection _whichHandCanPressButton = ChiralitySelection.BOTH;
+    private bool _isButtonPressed = false;
+
+    public UnityEvent OnButtonPressed;
+    public UnityEvent OnButtonUnPressed;
+
+    private List<Collider> _colliders;
 
     [SerializeField]
     private bool _buttonShouldDelayRebound = false;
     [SerializeField]
-    private float _buttonStaydownTimer = 2;
-    [SerializeField]
-    private bool _shouldOnlyBePressedByHand = false;
-    [SerializeField]
-    private bool _isButtonPressed = false;
+    private float buttonStaydownTimer = 2;
 
-    private bool _contactHandPressing = false;
-    private List<Collider> _colliders;
-
-    [Space(10)]
-    public UnityEvent OnButtonPressed;
-    public UnityEvent OnButtonUnPressed;
-
-    private bool _leftHandContacting = false;
-    private bool _rightHandContacting = false;
 
     private void Start()
     {
@@ -45,15 +35,16 @@ public class PhysicalHandsButton : MonoBehaviour
 
     void FixedUpdate()
     {
-        if ((!_isButtonPressed && buttonObject.transform.localPosition.y <= buttonHeightLimit * BUTTON_PRESS_THRESHOLD)
-            && (_contactHandPressing || !_shouldOnlyBePressedByHand))
+        if (!_isButtonPressed && buttonObject.transform.localPosition.y <= buttonHeightLimit * BUTTON_PRESS_THRESHOLD)
         {
+            Debug.Log("Button Pressed");
             _isButtonPressed = true;
             ButtonPressed();
         }
 
         if (_isButtonPressed && buttonObject.transform.localPosition.y >= buttonHeightLimit * BUTTON_PRESS_EXIT_THRESHOLD)
         {
+            Debug.Log("Button UnPressed");
             _isButtonPressed = false;
             ButtonUnpressed();
 
@@ -85,7 +76,7 @@ public class PhysicalHandsButton : MonoBehaviour
             collider.enabled = false;
         }
 
-        yield return new WaitForSecondsRealtime(_buttonStaydownTimer);
+        yield return new WaitForSecondsRealtime(buttonStaydownTimer);
 
         buttonObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
 
@@ -93,65 +84,7 @@ public class PhysicalHandsButton : MonoBehaviour
         {
             collider.enabled = true;
         }
-    }
 
-    public void ContactHandNearbyEnter(ContactHand contactHand)
-    {
-        //if((int)contactHand.Handedness != (int)_whichHandCanPressButton)
-        //{
-        //    return;
-        //}
-
-        if(contactHand.Handedness == Chirality.Left)
-        {
-            _leftHandContacting = true;
-        }
-        else if (contactHand.Handedness == Chirality.Right)
-        {
-            _rightHandContacting = true;
-        }
-        _contactHandPressing = GetChosenHandInContact();
-    }
-
-    public void ContactHandNearbyExit(ContactHand contactHand)
-    {
-        if (contactHand.Handedness == Chirality.Left)
-        {
-            _leftHandContacting = false;
-        }
-        else if (contactHand.Handedness == Chirality.Right)
-        {
-            _rightHandContacting = false;
-        }
-
-        _contactHandPressing = GetChosenHandInContact();
-    }
-
-    private bool GetChosenHandInContact()
-    {
-        switch (_whichHandCanPressButton)
-        {
-            case ChiralitySelection.LEFT:
-                if (_leftHandContacting)
-                {
-                    return true;
-                }
-                break;
-            case ChiralitySelection.RIGHT:
-                if (_rightHandContacting)
-                {
-                    return true;
-                }
-                break;
-            case ChiralitySelection.BOTH:
-                if(_rightHandContacting || _leftHandContacting)
-                {
-                    return true;
-                }
-                break;
-        }
-
-        return false;
     }
 
 }
