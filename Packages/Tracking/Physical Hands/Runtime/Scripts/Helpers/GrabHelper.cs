e12dad1f472f06a61bfaa6ec40bef673d93e554c
MattGray <matt.gray@ultraleap.com>
2024-03-14 16:01:55 +0000

    move bone grabbing reset outside of requiring a _grabHelperObject to exist

--- Packages/Tracking/Physical Hands/Runtime/Scripts/Helpers/GrabHelper.cs
+++ Packages/Tracking/Physical Hands/Runtime/Scripts/Helpers/GrabHelper.cs
@@ -282,19 +282,20 @@ namespace Leap.Unity.PhysicalHands
                     {
                         // Ensure we release the object first
                         _grabHelperObjects[rigid].ReleaseObject();
-
-                        foreach (var bone in _leftContactHand.bones)
-                        {
-                            bone.RemoveGrabbing(rigid);
-                        }
-                        foreach (var bone in _rightContactHand.bones)
-                        {
-                            bone.RemoveGrabbing(rigid);
-                        }
                     }
                     _grabHelperObjects[rigid].ReleaseHelper();
                     _grabHelperObjects.Remove(rigid);
                 }
+
+                foreach (var bone in _leftContactHand.bones)
+                {
+                    bone.RemoveGrabbing(rigid);
+                }
+                foreach (var bone in _rightContactHand.bones)
+                {
+                    bone.RemoveGrabbing(rigid);
+                }
+
                 return true;
             }
             return false;
