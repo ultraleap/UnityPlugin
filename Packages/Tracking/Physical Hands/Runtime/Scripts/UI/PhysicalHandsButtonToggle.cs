cf1b94e27d8105d4dea85c363f6551b5a11d35f3
BryceNewellUL <bryce.newell@ultraleap.com>
2024-04-15 14:34:29 +0100

    can now toggle button from editor, and changed how the button is frozen on press

--- Packages/Tracking/Physical Hands/Runtime/Scripts/UI/PhysicalHandsButtonToggle.cs
+++ Packages/Tracking/Physical Hands/Runtime/Scripts/UI/PhysicalHandsButtonToggle.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.Assertions.Must;
 using UnityEngine.Events;
 
 namespace Leap.Unity.PhysicalHands
@@ -21,13 +22,80 @@ namespace Leap.Unity.PhysicalHands
     public class PhysicalHandsButtonToggle : PhysicalHandsButtonBase
     {
         private bool _canUnpress = false;
+        private RigidbodyConstraints _constaintsBeforeFreeze = RigidbodyConstraints.None;
 
+        public bool IsToggled
+        {
+            get
+            {
+                return _isButtonPressed;
+            }
+            set
+            {
+                _isButtonPressed = value;
+            }
+        }
 
         protected override void Start()
         {
             base.Start();
-            
         }
+
+        public void ToggleButton(bool shouldFirePressEvents = false)
+        {
+            if (_isButtonPressed == false)
+            {
+                MakePressed(shouldFirePressEvents);
+            }
+            else if(_isButtonPressed == true)
+            {
+                MakeUnpressed(shouldFirePressEvents);
+            }
+        }
+
+        public void SetButtonPressed(bool isPressed, bool shouldFirePressEvents = false)
+        {
+            if (isPressed == true)
+            {
+                MakePressed(shouldFirePressEvents);
+            }
+            else if (isPressed == false)
+            {
+                MakeUnpressed(shouldFirePressEvents);
+            }
+        }
+
+        private void MakePressed(bool shouldFirePressEvents)
+        {
+            _isButtonPressed = true;
+            _canUnpress = true;
+            _pressableObject.transform.localPosition = new Vector3(_pressableObject.transform.localPosition.x,
+                0 + _buttonTravelOffset,
+                _pressableObject.transform.localPosition.z);
+            FreezeButtonPosition();
+
+            if (shouldFirePressEvents == true)
+            {
+                OnButtonPressed.Invoke();
+            }
+        }
+
+        private void MakeUnpressed(bool shouldFirePressEvents)
+        {
+            _isButtonPressed = false;
+            _canUnpress = false;
+            _pressableObject.transform.localPosition = new Vector3(_pressableObject.transform.localPosition.x,
+                _buttonTravelOffset + _buttonTravelDistanceLocal,
+                _pressableObject.transform.localPosition.z);
+            UnFreezeButtonPosition();
+
+            if (shouldFirePressEvents == true)
+            {
+                OnButtonUnPressed.Invoke();
+            }
+        }
+
+
 
         /// <summary>
         /// Action to perform when the button is pressed.
@@ -37,7 +105,7 @@ namespace Leap.Unity.PhysicalHands
             base.ButtonPressed();
 
             // Freeze object's position and rotation when pressed
-            _configurableJoint.yMotion = ConfigurableJointMotion.Locked;
+            FreezeButtonPosition();
             _canUnpress = false;
 
             StartCoroutine(DelayUnpress());
@@ -58,11 +126,10 @@ namespace Leap.Unity.PhysicalHands
                 // If not exclusively pressed by hand and object has exited
                 if (_canBePressedByObjects)
                 {
-                    _configurableJoint.yMotion = ConfigurableJointMotion.Limited;
+                    UnFreezeButtonPosition();
                 }
             }
         }
-
 
         protected override void OnHandContactPO(ContactHand contactHand)
         {
@@ -72,10 +139,20 @@ namespace Leap.Unity.PhysicalHands
                 // If the chosen hand is in contact and object has exited
                 if (GetChosenHandInContact())
                 {
-                    _configurableJoint.yMotion = ConfigurableJointMotion.Limited;
-
+                    UnFreezeButtonPosition();
                 }
             }
+        }
+
+        private void FreezeButtonPosition()
+        {
+            _constaintsBeforeFreeze = _pressableObjectRB.constraints;
+            _pressableObjectRB.constraints = RigidbodyConstraints.FreezeAll;
+        }
+
+        private void UnFreezeButtonPosition()
+        {
+            _pressableObjectRB.constraints = _constaintsBeforeFreeze;
         }
 
         private IEnumerator DelayUnpress()
