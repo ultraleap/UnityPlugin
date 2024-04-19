843acfb52aac60d83b037d09ecd03be3e30409ca
MattGray <matt.gray@ultraleap.com>
2024-04-19 13:57:20 +0100

    add comments

--- Packages/Tracking/Physical Hands/Runtime/Scripts/Misc/PhysicalHandsAnchorable.cs
+++ Packages/Tracking/Physical Hands/Runtime/Scripts/Misc/PhysicalHandsAnchorable.cs
@@ -21,12 +21,14 @@ namespace Leap.Unity
             OnDetachedFromAnchor -= OnDetached;
             OnDetachedFromAnchor += OnDetached;
 
+            // Cache the original ignoring hands if they exist so we can revert to them when un-anchored
             if (TryGetComponent<IgnorePhysicalHands>(out _ignorePhysicalHands))
             {
                 originalIgnoreHands = _ignorePhysicalHands.HandToIgnore;
             }
         }
 
+        // If the anchor is an attachment hand, we should ignore grabbing with the specific achored hand
         void OnAttached()
         {
             if (Anchor.AttahedHandChirality != ChiralitySelection.NONE)
@@ -40,6 +42,7 @@ namespace Leap.Unity
             }
         }
 
+        // Revert to original ignorehand state if there was one
         void OnDetached()
         {
             if(_ignorePhysicalHands != null)
@@ -48,6 +51,7 @@ namespace Leap.Unity
             }
         }
 
+        // Keep track of grabbing hands to determine if we should detach or look for possible attachments
         void IPhysicalHandGrab.OnHandGrab(ContactHand hand)
         {
             if (hand.Handedness == Chirality.Left)
@@ -84,6 +88,8 @@ namespace Leap.Unity
             }
         }
 
+
+        // Keep track of hovering hands to feed into the AnchorableBehaviour logic
         void IPhysicalHandHover.OnHandHover(ContactHand hand)
         {
             _hoveringHands.Add(hand.DataHand);
