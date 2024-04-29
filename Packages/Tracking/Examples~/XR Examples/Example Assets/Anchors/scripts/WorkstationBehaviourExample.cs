97ae7b3e77699936025d01994f7ea8900b0c104b
BryceNewellUL <bryce.newell@ultraleap.com>
2024-04-29 11:47:21 +0100

    updating more example scene stuff

--- Packages/Tracking/Examples~/XR Examples/Example Assets/Anchors/scripts/WorkstationBehaviourExample.cs
+++ Packages/Tracking/Examples~/XR Examples/Example Assets/Anchors/scripts/WorkstationBehaviourExample.cs
@@ -19,7 +19,7 @@ namespace Leap.InteractionEngine.Examples
     /// The panel is closed when the object is moved at or over a given speed.
     /// The anchorable object is set to kinematic when in workstation mode.
     /// </summary>
-    [RequireComponent(typeof(AnchorableBehaviour))]
+    //[RequireComponent(typeof(AnchorableBehaviour))]
     public class WorkstationBehaviourExample : MonoBehaviour, IPhysicalHandGrab
     {
         /// <summary>
