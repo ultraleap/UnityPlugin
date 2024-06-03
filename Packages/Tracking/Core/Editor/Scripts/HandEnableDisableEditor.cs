f18d4e11bf68d07c008ddb202add201f28171aa7
MattGray <matt.gray@ultraleap.com>
2024-06-03 16:51:39 +0100

    Add fading hands to HandEnableDisable

---
+++ Packages/Tracking/Core/Editor/Scripts/HandEnableDisableEditor.cs
@@ -0,0 +1,42 @@
+/******************************************************************************
+ * Copyright (C) Ultraleap, Inc. 2011-2024.                                   *
+ *                                                                            *
+ * Use subject to the terms of the Apache License 2.0 available at            *
+ * http://www.apache.org/licenses/LICENSE-2.0, or another agreement           *
+ * between Ultraleap and you, your company or other organization.             *
+ ******************************************************************************/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Leap.Unity
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(HandEnableDisable))]
+    public class HandEnableDisableEditor : CustomEditorBase<HandEnableDisable>
+    {
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            specifyConditionalDrawing("fadeOnHandFound",
+                          "fadeInTime");
+
+            specifyConditionalDrawing("fadeOnHandLost",
+              "fadeOutTime");
+
+            specifyConditionalDrawing(() =>
+            {
+                return target.fadeOnHandFound || target.fadeOnHandLost;
+            }, "customFadeRenderers");
+
+            specifyConditionalDrawing(() =>
+            {
+                return (target.fadeOnHandFound || target.fadeOnHandLost) && target.customFadeRenderers;
+            }, "renderersToFade");
+        }
+    }
+}
--- Packages/Tracking/Core/Runtime/Scripts/HandObservers/HandEnableDisable.cs
+++ Packages/Tracking/Core/Runtime/Scripts/HandObservers/HandEnableDisable.cs
@@ -6,6 +6,8 @@
  * between Ultraleap and you, your company or other organization.             *
  ******************************************************************************/
 
+using Leap.Unity.Attributes;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Leap.Unity
@@ -18,23 +20,35 @@ namespace Leap.Unity
     /// </summary>
     public class HandEnableDisable : HandTransitionBehavior
     {
-
         [Tooltip("When enabled, freezes the hand in its current active state")]
         public bool FreezeHandState = false;
 
-        protected override void Awake()
+        [Header("Fading")]
+        public bool fadeOnHandFound = false;
+        [Indent]
+        public float fadeInTime = 0.1f;
+
+        public bool fadeOnHandLost = true;
+        [Indent]
+        public float fadeOutTime = 0.1f;
+
+        [Space, Tooltip("Show options to reference specific Renderers, Materials and Color parameters to fade")]
+        public bool customFadeRenderers;
+        public RendererMaterialColorReference[] renderersToFade;
+
+        bool fadingIn = false;
+        bool fadingOut = false;
+
+        float fadeEndTime;
+
+        private void Start()
         {
-            // Suppress Warnings Related to Kinematic Rigidbodies not supporting Continuous Collision Detection
-            Rigidbody[] bodies = GetComponentsInChildren<Rigidbody>();
-            foreach (Rigidbody body in bodies)
+            if(!customFadeRenderers)
             {
-                if (body.isKinematic && body.collisionDetectionMode == CollisionDetectionMode.Continuous)
-                {
-                    body.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
-                }
+                AutoPopulateFadeRenderers();
             }
 
-            base.Awake();
+            PopulateFadeDefaultAlphas();
         }
 
         protected override void HandReset()
@@ -44,7 +58,32 @@ namespace Leap.Unity
                 return;
             }
 
+            fadingOut = false;
             gameObject.SetActive(true);
+
+            if (fadeOnHandFound)
+            {
+                fadingIn = true;
+                CacheFadeStartAlphas();
+                fadeEndTime = Time.time + fadeInTime;
+            }
+            else
+            {
+                if (fadeEndTime != 0)
+                {
+                    // We have faded at some point. Reset the fade
+                    foreach (var _renderRef in renderersToFade)
+                    {
+                        for (int i = 0; i < _renderRef.colorParamNames.Length; i++)
+                        {
+                            Color _col = _renderRef.renderer.materials[_renderRef.materialID].GetColor(_renderRef.colorParamNames[i]);
+                            _col.a = _renderRef.defaultAlphas[i];
+
+                            _renderRef.renderer.materials[_renderRef.materialID].SetColor(_renderRef.colorParamNames[i], _col);
+                        }
+                    }
+                }
+            }
         }
 
         protected override void HandFinish()
@@ -53,7 +92,148 @@ namespace Leap.Unity
             {
                 return;
             }
-            gameObject.SetActive(false);
+
+            fadingIn = false;
+
+            if (fadeOnHandLost)
+            {
+                fadingOut = true;
+                CacheFadeStartAlphas();
+                fadeEndTime = Time.time + fadeOutTime;
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
         }
+
+        private void Update()
+        {
+            if(fadingOut)
+            {
+                HandleFade(_fadeIn: false); // Handle the fade out
+
+                if (Time.time > fadeEndTime)
+                {
+                    fadingOut = false;
+                    gameObject.SetActive(false);
+                }
+            }
+
+            if (fadingIn)
+            {
+                HandleFade(_fadeIn: true); // Handle the fade in
+
+                if (Time.time > fadeEndTime)
+                {
+                    fadingIn = false;
+                }
+            }
+        }
+
+        private void HandleFade(bool _fadeIn = false)
+        {
+            foreach (var _renderRef in renderersToFade)
+            {
+                for (int i = 0; i < _renderRef.colorParamNames.Length; i++)
+                {
+                    float _t = 0;
+
+                    if(_fadeIn)
+                    {
+                        _t = Utils.Map(fadeInTime - (fadeEndTime - Time.time), 0f, fadeInTime, 0f, 1f);
+                    }
+                    else
+                    {
+                        _t = Utils.Map(fadeOutTime - (fadeEndTime - Time.time), 0f, fadeOutTime, 0f, 1f);
+                    }
+
+                    Color _col = _renderRef.renderer.materials[_renderRef.materialID].GetColor(_renderRef.colorParamNames[i]);
+
+                    if (_fadeIn)
+                    {
+                        _col.a = Mathf.Lerp(_renderRef.fadeStartAlphas[i], _renderRef.defaultAlphas[i], _t); // Fade to default alpha
+                    }
+                    else
+                    {
+                        _col.a = Mathf.Lerp(_renderRef.fadeStartAlphas[i], 0, _t); // Fade to 0
+                    }
+
+                    _renderRef.renderer.materials[_renderRef.materialID].SetColor(_renderRef.colorParamNames[i], _col);
+                }
+            }
+        }
+
+        private void CacheFadeStartAlphas()
+        {
+            foreach (var _renderRef in renderersToFade)
+            {
+                _renderRef.fadeStartAlphas = new float[_renderRef.colorParamNames.Length];
+
+                for (int i = 0; i < _renderRef.colorParamNames.Length; i++)
+                {
+                    _renderRef.fadeStartAlphas[i] = _renderRef.renderer.materials[_renderRef.materialID].GetColor(_renderRef.colorParamNames[i]).a;
+                }
+            }
+        }
+
+        private void AutoPopulateFadeRenderers()
+        {
+            Renderer[] _renderers = gameObject.GetComponentsInChildren<Renderer>(true);
+
+            renderersToFade = new RendererMaterialColorReference[_renderers.Length];
+
+            for (int i = 0; i < _renderers.Length; i++)
+            {
+                renderersToFade[i] = new RendererMaterialColorReference();
+                renderersToFade[i].renderer = _renderers[i];
+                renderersToFade[i].materialID = 0;
+
+                List<string> _suitableParams = new List<string>
+                    {
+                        "_Color","_MainColor","_OutlineColor","_FresnelColor"
+                    };
+
+                for (int _paramID = 0; _paramID < _suitableParams.Count; _paramID++)
+                {
+                    if (!_renderers[i].materials[0].HasColor(_suitableParams[_paramID]))
+                    {
+                        _suitableParams.RemoveAt(_paramID);
+                        _paramID--;
+                    }
+                }
+
+                renderersToFade[i].colorParamNames = _suitableParams.ToArray();
+            }
+        }
+
+        private void PopulateFadeDefaultAlphas()
+        {
+            foreach (var _renderRef in renderersToFade)
+            {
+                _renderRef.defaultAlphas = new float[_renderRef.colorParamNames.Length];
+
+                for (int i = 0; i < _renderRef.colorParamNames.Length; i++)
+                {
+                    _renderRef.defaultAlphas[i] = _renderRef.renderer.materials[_renderRef.materialID].GetColor(_renderRef.colorParamNames[i]).a;
+                }
+            }
+
+            CacheFadeStartAlphas();
+        }
+    }
+
+    [System.Serializable]
+    public class RendererMaterialColorReference
+    {
+        public Renderer renderer;
+        public int materialID;
+        public string[] colorParamNames;
+
+        [HideInInspector]
+        public float[] defaultAlphas;
+
+        [HideInInspector]
+        public float[] fadeStartAlphas; // The alpha values when the current fade started
     }
 }
