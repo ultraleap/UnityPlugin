2934534b78a8ef046b47226e4b7b95ddc20c0893
Robert Blenkinsopp <robert@blenkinsopp.net>
2024-10-26 13:23:04 +0100

    Fix finger-tip drawing in capsule hands
    As LeapC returns the tip as the skin surface, to ensure that pinches are accurate, move the tip sphere back by it's radius along the bone.

--- Packages/Tracking/Core/Runtime/Scripts/Hands/CapsuleHand.cs
+++ Packages/Tracking/Core/Runtime/Scripts/Hands/CapsuleHand.cs
@@ -380,6 +380,11 @@ namespace Leap
                     else
                     {
                         position = finger.GetBone((Bone.BoneType)j).NextJoint;
+                        if (j == 3)
+                        {
+                            // Fingertips need to be adjusted back by a radii to account for sphere drawing.
+                            position -= finger.GetBone((Bone.BoneType)j).Direction * _jointRadius;
+                        }
                     }
 
                     _spherePositions[key] = position;
