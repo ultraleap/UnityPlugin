5b4ebefaa83b0012f79e4978d6788dffb310deeb
Robert Blenkinsopp <robert@blenkinsopp.net>
2025-02-05 13:13:56 +0000

    Remove OpenXR warning for input system not set to both

--- Packages/Tracking/OpenXR/Runtime/Scripts/HandTrackingFeature.cs
+++ Packages/Tracking/OpenXR/Runtime/Scripts/HandTrackingFeature.cs
@@ -226,22 +226,6 @@ namespace Leap.Tracking.OpenXR
 #if UNITY_EDITOR
         protected override void GetValidationChecks(List<ValidationRule> rules, BuildTargetGroup targetGroup)
         {
-            // Check the active input handling supports New (for OpenXR) and Legacy (for Ultraleap Plugin support).
-            rules.Add(new ValidationRule(this)
-            {
-                message = "Active Input Handling is not set to Both. While New is required for OpenXR, Both is " +
-                          "recommended as the Ultraleap Unity Plugin does not fully support the New Input System.",
-                error = false,
-#if !ENABLE_LEGACY_INPUT_MANAGER || !ENABLE_INPUT_SYSTEM
-                checkPredicate = () => false,
-#else
-                checkPredicate = () => true,
-#endif
-                fixItAutomatic = false,
-                fixItMessage = "Enable the Legacy Input Manager and replacement Input System together (Both)",
-                fixIt = () => SettingsService.OpenProjectSettings("Project/Player"),
-            });
-
             // Check that the Main camera has a suitable near clipping plane for hand-tracking.
             rules.Add(new ValidationRule(this)
             {
