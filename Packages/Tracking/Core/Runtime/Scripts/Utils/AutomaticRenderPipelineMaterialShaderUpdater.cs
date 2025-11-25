37779eba23b0406eaff6fa3683bf67064f6f71b9
Max Palmer <max.palmer@ultraleap.com>
2025-11-25 16:12:06 +0000

    Fixing whitespace issues from code review

--- Packages/Tracking/Core/Runtime/Scripts/Utils/AutomaticRenderPipelineMaterialShaderUpdater.cs
+++ Packages/Tracking/Core/Runtime/Scripts/Utils/AutomaticRenderPipelineMaterialShaderUpdater.cs
@@ -211,7 +211,7 @@ public class AutomaticRenderPipelineMaterialShaderUpdater : ScriptableObject
                             goAhead = false;
                             break;
                     }
-                    
+
                     // Let's give the user the chance to stop being nagged ...
                     if (goAhead == false && NumberOfTimesUserRejectedPrompt >= 3 && NumberOfTimesUserRejectedPrompt <=5)
                     {
